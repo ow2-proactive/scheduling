305d506c625275530ae0717daf616e990562f45e
lmestre <lmestre@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-03-29 11:23:49 +0000

    si xs
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/env.sh
+++ scripts/unix/env.sh
@@ -9,17 +9,6 @@ PROACTIVE=$workingDir/../../.
 
 # ----------------------------------------------------------------------------
 
-
-
-# ----------------------------------------------------------------------------
-#
-# This variable should be set to the directory where is installed ProActive
-#
-
-PROACTIVE_TEMP=$HOME/proactive-tmp
-
-# ----------------------------------------------------------------------------
-
 JAVA_HOME=${JAVA_HOME-NULL};
 if [ "$JAVA_HOME" = "NULL" ]
 then
@@ -32,45 +21,24 @@ exit 127
 fi
 
 # ----
-# Try to set proactive-tmp to the right place
-# 
-# -- try to set proactive-tmp at the same level as ProActive directory
-if [ ! -d $PROACTIVE_TEMP ]
-then
-    PROACTIVE_TEMP=$workingDir/../../../proactive-tmp
-fi
-# -- try to set proactive-tmp one level above ProActive directory
-if [ ! -d $PROACTIVE_TEMP ]
-then
-    PROACTIVE_TEMP=$workingDir/../../../../proactive-tmp
-fi
-# -- proactive-tmp not created yet : back up to user directory
-if [ ! -d $PROACTIVE_TEMP ]
-then
-    PROACTIVE_TEMP=$HOME/proactive-tmp
-fi
-
-echo "PROACTIVE_TEMP="$PROACTIVE_TEMP
-
-# ----
 # Set up the classpath using classes dir or jar files
 # 
-CLASSPATH=.:$PROACTIVE_TEMP
+CLASSPATH=.
 if [ -d $PROACTIVE/classes ]
 then
     CLASSPATH=$CLASSPATH:$PROACTIVE/classes
 fi
-if [ -f $PROACTIVE/lib/ProActive.jar ]
+if [ -f $PROACTIVE/ProActive.jar ]
 then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ProActive.jar
+    CLASSPATH=$CLASSPATH:$PROACTIVE/ProActive.jar
 fi
-if [ -f $PROACTIVE/lib/ProActive_examples.jar ]
+if [ -f $PROACTIVE/ProActive_examples.jar ]
 then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ProActive_examples.jar
+    CLASSPATH=$CLASSPATH:$PROACTIVE/ProActive_examples.jar
 fi
-if [ -f $PROACTIVE/lib/ic2d.jar ]
+if [ -f $PROACTIVE/ic2d.jar ]
 then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ic2d.jar
+    CLASSPATH=$CLASSPATH:$PROACTIVE/ic2d.jar
 fi
 if [ -f $PROACTIVE/lib/jini-core.jar ]
 then
