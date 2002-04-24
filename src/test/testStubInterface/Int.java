2b611f38ea013b97f84b028fe1944092b4504801
jvayssie <jvayssie@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-04-24 12:03:29 +0000

    Adding test classes for the generation of stubs for interfaces
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@34 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/test/testStubInterface/Int.java
@@ -0,0 +1,6 @@
+package test.testStubInterface;
+
+public interface Int
+{
+    public abstract void foo (int i);
+}
---
+++ src/test/testStubInterface/IntImpl.java
@@ -0,0 +1,10 @@
+package test.testStubInterface;
+
+public class IntImpl implements Int
+{
+    public void foo (int i)
+    {
+	System.out.println ("Value is "+i);
+	return;
+    }
+}
