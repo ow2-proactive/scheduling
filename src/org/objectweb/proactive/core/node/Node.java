597ee342f7b6d9ddb39a57e1127500e7f0b9f805
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-10-28 15:20:21 +0000

    Interface providing methods for the new version of NodeImpl
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@145 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/node/Node.java
+++ src/org/objectweb/proactive/core/node/Node.java
@@ -30,80 +30,57 @@
 */ 
 package org.objectweb.proactive.core.node;
 
-import org.objectweb.proactive.Body;
-import org.objectweb.proactive.core.UniqueID;
-import org.objectweb.proactive.core.body.UniversalBody;
-import org.objectweb.proactive.core.mop.ConstructorCall;
-import org.objectweb.proactive.core.mop.ConstructorCallExecutionFailedException;
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.core.runtime.ProActiveRuntime;
 
 /**
  * <p>
  * A <code>Node</code> offers a set of services needed by ProActive to work with
  * remote JVM. Each JVM that is aimed to hold active objects should contains at least
- * one instance of the node class. That instance, when created, will register itself
+ * one instance of the node class. That instance, when created, will be registered
  * to some registry where it is possible to perform a lookup (such as the RMI registry).
  * </p><p>
  * When ProActive needs to interact with a remote JVM, it will lookup for one node associated
- * with that JVM (using typically the RMI Registry) and use the remote interface of the node
- * to perform the interaction.
- * </p><p>
- * Aside the getter giving information about the node, the 2 services offered are :
- * <ul>
- * <li>the creation of active object through the method <code>createBody</code></li>
- * <li>the migration of active object through the method <code>receiveBody</code></li>
- * </ul>
+ * with that JVM (using typically the RMI Registry) and use this node to perform the interaction.
  * </p><p>
  * We expect several concrete implementations of the Node to be wrtten such as a RMI node, a JINI node ...
  * </p>
  *
  * @author  ProActive Team
- * @version 1.0,  2001/10/23
+ * @version 1.1,  2002/08/28
  * @since   ProActive 0.9
  *
  */
-public interface Node {
-
-  /**
-   * <p>
-   * This method is the basis for creating remote active objects.
-   * It receives a <code>ConstructorCall</code> that is the constructor call of the body
-   * of the active object to create. Inside the parameters of this constructor call is
-   * the constructor call of the reified object. Upon execution of the constructor call of the
-   * body, the body holding a reference on the reified object will get created and returned.
-   * </p><p>
-   * The method returns a reference on the RMI stub of the newly created body.
-   * </p>
-   * @param bodyConstructorCall the Constructor call allowing to create the body
-   * @return a stub on the newly created body.
-   * @exception NodeException if a problem occurs due the remote nature of the node
-   * @exception ConstructorCallExecutionFailedException if the constructor call cannot be executed
-   * @exception java.lang.reflect.InvocationTargetException if the java constructor execution failed
-   */
-  public UniversalBody createBody(ConstructorCall bodyConstructorCall) throws NodeException,
-              ConstructorCallExecutionFailedException, java.lang.reflect.InvocationTargetException;
-
-
-  /**
-   * <p>
-   * This method is the basis for migrating active objects.
-   * It receives a <code>Body</code> that embbeds the reified object and its graph of
-   * passive objects. Once transfered remotely using serialization, the body should restart
-   * itself and perform all updates needed to be functionning.
-   * </p><p>
-   * The method returns a reference on the RMI stub of the migrated body.
-   * </p>
-   * @param <code>body</code> the body of the active object migrating to this node.
-   * @return a RMI stub on the migrated body.
-   * @exception NodeException if a problem occurs due the remote nature of the node
-   */
-  public UniversalBody receiveBody(Body body) throws NodeException;
-
-
+public interface Node
+{
+	
   /**
    * Returns the node information as one object. This method allows to
    * retrieve all node information in one call to optimize performance.
    * @return the node information as one object
    */
-  public NodeInformation getNodeInformation();
+	public NodeInformation getNodeInformation();
+	
+		
+	/**
+	 * Returns a reference to the <code>ProActiveRuntime</code> where the node has been created
+	 * @return ProActiveRuntime. The reference to the <code>ProActiveRuntime</code> where the node has been created
+	 */
+	public ProActiveRuntime getProActiveRuntime();
+	
+	
+  /**
+   * Returns all activeObjects deployed on this Node
+   * @return Object[] contains all activeObjects deployed on this Node
+   */
+	public Object[] getActiveObjects() throws NodeException,ActiveObjectCreationException;
+	
+	/**
+	 * Returns all activeObjects with the given name deployed on this Node 
+	 * or null if such objects do not exist
+	 * @param objectName the class of the Active Objects
+	 * @return Object[].The set of activeObjects deployed on this node with the given name
+	 */
+	public Object[] getActiveObject(String objectName) throws NodeException,ActiveObjectCreationException;
 
 }
