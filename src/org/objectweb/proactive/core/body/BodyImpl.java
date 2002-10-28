7b71ea7e57aa5bbd917e4997bc884d53d4989338
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-10-28 14:32:35 +0000

    Not modified since previous commit
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@125 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/body/BodyImpl.java
+++ src/org/objectweb/proactive/core/body/BodyImpl.java
@@ -69,7 +69,7 @@ import org.objectweb.proactive.core.mop.MethodCall;
  * @author  ProActive Team
  * @version 1.0,  2001/10/23
  * @since   ProActive 0.9
- * @see Body
+ * @see org.objectweb.proactive.Body
  * @see UniqueID
  *
  */
