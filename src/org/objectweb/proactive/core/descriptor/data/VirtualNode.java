7189bad82ed19e67e6c4dba63c9d73aa0853a3f6
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-10-28 14:57:40 +0000

    Interface defining method for VirtualNodeImpl
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@134 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/descriptor/data/VirtualNode.java
+++ src/org/objectweb/proactive/core/descriptor/data/VirtualNode.java
@@ -30,89 +30,130 @@
 */ 
 package org.objectweb.proactive.core.descriptor.data;
 
+import org.objectweb.proactive.core.event.RuntimeRegistrationEventListener;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.node.NodeException;
+
 /**
- * This class represents a VirtualNode
+ * A <code>VirtualNode</code> represents a conceptual entity. After activation
+ * a <code>VirtualNode</code> represents one or several nodes.
  *
- * @author       Lionel Mestre
- * @version      1.0
+ * @author  ProActive Team
+ * @version 1.0,  2002/06/20
+ * @since   ProActive 0.9.3
  */
-public class VirtualNode {
+public interface VirtualNode extends RuntimeRegistrationEventListener
+{
 
-  //
-  //  ----- PRIVATE MEMBERS -----------------------------------------------------------------------------------
-  //
+ 
 
-  /** the name of this VirtualNode */
-  private String name;
-
-  /** true if this VirtualNode is cyclic */
-  private boolean cyclic;
-
-  /** true if this VirtualNode should be created localy in case the creation of the target jvm fails */
-  private boolean localBackup;
-
-  /** the list of virtual machines associated with this VirtualNode */
-  private java.util.ArrayList virtualMachines;
-  
-  /** index of the last associated jvm used */
-  private int lastVirtualMachineIndex;
-
-
-  //
-  //  ----- CONSTRUCTORS -----------------------------------------------------------------------------------
-  //
-
- /**
-  * Contructs a new intance of VirtualNode
-  */
-  VirtualNode() {
-    virtualMachines = new java.util.ArrayList(5);
-  }
-
-
-  //
-  //  ----- PUBLIC METHODS -----------------------------------------------------------------------------------
-  //
-  
-  public void setCyclic(boolean b) {
-    cyclic = b;
-  }
-  
-  public boolean getCyclic() {
-    return cyclic;
-  }
-
-
-  public void setLocalBackup(boolean b) {
-    localBackup = b;
-  }
-  
-  public boolean getLocalBackup() {
-    return localBackup;
-  }
-
-
-  public void setName(String s) {
-    name = s;
-  }
-  
-  public String getName() {
-    return name;
-  }
+ 
+  public void setCyclic(boolean b);
   
   
-  public void addVirtualMachine(VirtualMachine virtualMachine) {
-    virtualMachines.add(virtualMachine);
-    System.out.println("mapped VirtualNode="+name+" with VirtualMachine="+virtualMachine.getName());
-  }
+  public boolean getCyclic();
 
-  public VirtualMachine getVirtualMachine() {
-    if (virtualMachines.isEmpty()) return null;
-    VirtualMachine vm = (VirtualMachine) virtualMachines.get(lastVirtualMachineIndex);
-    if (cyclic) {
-      lastVirtualMachineIndex = (lastVirtualMachineIndex + 1) % virtualMachines.size();
-    }
-    return vm;
-  }
+
+  public void setLocalBackup(boolean b);
   
+  public boolean getLocalBackup();
+
+
+  public void setName(String s);
+  
+  
+  public String getName();
+  
+  
+  public void addVirtualMachine(VirtualMachine virtualMachine);
+  
+  
+  public VirtualMachine getVirtualMachine() ;
+  
+  
+  /**
+   * Activates all the Nodes mapped to this VirtualNode in the XML Descriptor
+   */
+  public void activate();
+  
+  
+  /**
+   * Desactivate all the Nodes mapped to This VirtualNode in the XML Descriptor
+   */
+  public void desactivate();
+  
+  
+  /**
+   * Returns the number of Nodes mapped to this VirtualNode in the XML Descriptor
+   * @return int
+   */
+  public int getNodeCount();
+  
+  
+  /**
+   * Returns the number of Nodes already created among the Nodes mapped to this VirtualNode in the XML Descriptor
+   * @return int
+   */
+  public int CreatedNodeCount();
+  
+  
+//  /**
+//   * Waits until at least one Node mapped to this VirtualNode in the XML Descriptor is created
+//   */
+//  public void waitForNodeCreation() throws NodeException;
+  
+  
+  /**
+   * Returns the first Node available among Nodes mapped to this VirtualNode in the XML Descriptor 
+   * @return Node
+   */
+  public Node getNode() throws NodeException;
+
+  
+  
+  /**
+   * Returns the Node mapped to this VirtualNode with the specified index(in the XML Descriptor
+   * @param index
+   * @return Node
+   */
+  public Node getNode(int index);
+  
+  
+	/**
+	 * Returns all nodes name mapped to this VirualNode
+	 * @return String[]. An array of string containing the name of all nodes mapped to
+	 * this VirtualNode in the XML descriptor.
+	 */
+  public String[] getNodesURL() throws NodeException;
+  
+  
+	/**
+	 * Returns all nodes mapped to this VirtualNode
+	 * @return Node[] An array of Node conataining all the nodes mapped to this
+	 * VirtualNode in the XML descriptor
+	 */
+  public Node[] getNodes() throws NodeException;
+  
+  
+//  /**
+//   * Returns the Node mapped to this VirtualNode in the XML Descriptor with the specified name
+//   * @param name
+//   * @return Node
+//   */
+//  public Node getNode(String name) throws NodeException{
+//    Node node = null;
+//    try{
+//    node = NodeFactory.getNode(name);
+//    }catch (Exception ne){
+//    ne.printStackTrace();
+//    throw new NodeException("Cannot get the node of the name"+name);
+//    } 
+//    createdNodes.add(node);
+//    return node;
+//		return null;
+ // }
+  
+  
+  
+   
 }
