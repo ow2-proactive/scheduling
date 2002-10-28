a00c7e39a3f101f5c000474a691d943a36b01a62
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2002-10-28 15:09:16 +0000

    This class represent listeners for RuntimeRegistrationEvent.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@142 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/event/RuntimeRegistrationEventListener.java
@@ -0,0 +1,25 @@
+package org.objectweb.proactive.core.event;
+
+
+
+/**
+ * <p>
+ * A class implementating this interface is listener of <code>RuntimeRegistrationEvent</code>.
+ * </p>
+ * 
+ * @see RuntimeRegistrationEvent
+ * @author  ProActive Team
+ * @version 1.0,  2002/08/06
+ * @since   ProActive 0.9
+ *
+ */
+
+public interface RuntimeRegistrationEventListener extends ProActiveListener
+{
+	/**
+	 * Signals that a registration occured on the runtime encapsulated in the event
+	 * @param event the creation event that details the registration on the runtime
+	 */
+	public void runtimeRegistered(RuntimeRegistrationEvent event) ;
+
+}
