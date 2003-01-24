598e4d060f57e60977529e37a9f7ba3f0617fa07
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-01-24 10:27:52 +0000

    Correction in comments
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@325 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/agentClient.sh
+++ scripts/unix/agentClient.sh
@@ -5,7 +5,7 @@ echo --- upperClient -------------------------------------------
 if [ $# -lt 1 ]; then
     echo "
        Start AgentClient
-         AgentClient.sh <the url of the node where Active Object Upper is going to be migrated to>
+         AgentClient.sh <the url of the node where Active Object Agent is going to be migrated to>
   
          ex : agentClient.sh  //$HOSTNAME/node1
          The node has to be previously launched with the command ./startNode.sh //$HOSTNAME/node1
