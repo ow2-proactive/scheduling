81ec6351e6f5069ea14c59610f092e5f7f25646b
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-03-07 14:10:29 +0000

    Test de la continuation automatique
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@364 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/test/acontinuation/Agent.java
+++ src/test/acontinuation/Agent.java
@@ -2,40 +2,39 @@ package test.acontinuation;
 
 import org.objectweb.proactive.ProActive;
 
+
 public class Agent implements java.io.Serializable {
+    private Agent deleguate;
 
-	private Agent deleguate;
+    public Agent() {
+    }
 
-	public Agent() {
-	}
+    public void init() {
+        try {
+            this.deleguate = (Agent) ProActive.newActive(Agent.class.getName(),
+                    new Object[] {  });
+        } catch (Exception e) {
+        }
+    }
 
-	public void init() {
-		try {
-			this.deleguate = (Agent) ProActive.newActive(Agent.class.getName(), new Object[] {
-			});
-		} catch (Exception e) {
-		}
-	}
+    public Dummy getDummyWait(Dummy d) {
+        return deleguate.getInternDummyWait(d);
+    }
 
-	public void print() {
-		System.out.println("Si cette phrase apparait avant les (ou entre 2) \"****\", la continuation automatique marche !");
-	}
+    public Dummy getInternDummyWait(Dummy d) {
+        try {
+            Thread.sleep(3000);
 
-	public Dummy getDummy() {
-		return deleguate.getInternDummy();
-	}
+            return new Dummy();
+        } catch (Exception e) {
+            e.printStackTrace();
 
-	public Dummy getInternDummy() {
-		try {
-			for (int i = 0; i < 5; i++) {
-				Thread.sleep(1000);
-				System.out.println("****");
-			}
-			return new Dummy();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+            return null;
+        }
+    }
+
+    public Dummy getDummy(Dummy d) {
+        return new Dummy();
+    }
 
 }
--- src/test/acontinuation/Test.java
+++ src/test/acontinuation/Test.java
@@ -1,5 +1,6 @@
 package test.acontinuation;
 
+import java.util.Vector;
 import org.objectweb.proactive.ProActive;
 
 public class Test {
@@ -9,14 +10,32 @@ public class Test {
         	
         	System.setProperty("proactive.future.ac","enable");
         	
-            Dummy d;
+            Dummy d1,d2;
+            Dummy d = new Dummy();
             Agent a = (Agent)ProActive.newActive(Agent.class.getName(),new Object[]{});
             a.init();
             
-            System.out.println("La propriete proactive.future.ac=enable est deja mise par le programme !");
-            d=a.getDummy();
-            a.print();
-            //System.out.println("Après...");
+            
+            //System.out.println("La propriete proactive.future.ac=enable est deja mise par le programme !");
+            d1=a.getDummyWait(d);
+            d2=a.getDummy(d1);
+            
+            
+            //d2 doit arriver avant d1
+            Vector v = new Vector(2);
+            v.add(d1);
+            v.add(d2);
+            
+            if (ProActive.waitForAny(v)==0)
+            	System.out.println("\n#### Automatic Continuation Mechanism is NOT working ####\n");
+            else
+            	System.out.println("\nAutomatic Continuation Mechanism is working\n");	
+            //System.out.println(""+ProActive.waitForAny(v));
+            
+            ProActive.waitForAll(v);
+            
+         
+            
         } catch (Exception e) {e.printStackTrace();}
         
     }
