cd578ae88e5ad7535fa06ac93c2efd61fda1b816
fhuet <fhuet@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-04-29 12:17:52 +0000

    Modification pour bugfix migration TEST
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@383 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/body/migration/MigratableBody.java
+++ src/org/objectweb/proactive/core/body/migration/MigratableBody.java
@@ -37,141 +37,159 @@ import org.objectweb.proactive.core.body.UniversalBody;
 import org.objectweb.proactive.core.event.MigrationEventListener;
 import org.objectweb.proactive.core.node.Node;
 
-public class MigratableBody extends BodyImpl implements Migratable, java.io.Serializable {
 
-  //
-  // -- PROTECTED MEMBERS -----------------------------------------------
-  //
+public class MigratableBody extends BodyImpl implements Migratable,
+    java.io.Serializable {
+    //
+    // -- PROTECTED MEMBERS -----------------------------------------------
+    //
 
-  /** The object responsible for the migration */
-  protected MigrationManager migrationManager;
+    /** The object responsible for the migration */
+    protected MigrationManager migrationManager;
 
-  /** signal that the body has just migrated */
-  protected transient boolean hasJustMigrated;
+    /** signal that the body has just migrated */
+    protected transient boolean hasJustMigrated;
 
-  //
-  // -- CONSTRUCTORS -----------------------------------------------
-  //
-
-  public MigratableBody() {}
-
-  public MigratableBody(Object reifiedObject, String nodeURL, MetaObjectFactory factory) {
-    super(reifiedObject, nodeURL, factory);
-    this.migrationManager = factory.newMigrationManagerFactory().newMigrationManager();
-  }
-
-  //
-  // -- PUBLIC METHODS -----------------------------------------------
-  //
-
-  //
-  // -- implements Migratable -----------------------------------------------
-  //
-
-  public UniversalBody migrateTo(Node node) throws MigrationException {
-    return internalMigrateTo(node, false);
-  }
-
-  public UniversalBody cloneTo(Node node) throws MigrationException {
-    return internalMigrateTo(node, true);
-  }
-
-  public void addMigrationEventListener(MigrationEventListener listener) {
-    if (migrationManager != null)
-      migrationManager.addMigrationEventListener(listener);
-  }
-
-  public void removeMigrationEventListener(MigrationEventListener listener) {
-    if (migrationManager != null)
-      migrationManager.removeMigrationEventListener(listener);
-  }
-
-  //
-  // -- PROTECTED METHODS -----------------------------------------------
-  //
-
-
-  /**
-   * Signals that the activity of this body, managed by the active thread has just started.
-   */
-  protected void activityStarted() {
-    super.activityStarted();
-    //System.out.println("Body run on node "+nodeURL+" migration="+hasJustMigrated);
-    if (hasJustMigrated) {
-      if (migrationManager != null) {
-        migrationManager.startingAfterMigration(this);
-      }
-      hasJustMigrated = false;
+    //
+    // -- CONSTRUCTORS -----------------------------------------------
+    //
+    public MigratableBody() {
     }
-  }
 
-  //
-  // -- PRIVATE METHODS -----------------------------------------------
-  //
-
-  protected UniversalBody internalMigrateTo(Node node, boolean byCopy) throws MigrationException {
-    if (!isAlive()) throw new MigrationException("Attempt to migrate a dead body that has been terminated");
-    if (!isActive()) throw new MigrationException("Attempt to migrate a non active body");
-    // check node with Manager
-    node = migrationManager.checkNode(node);
-
-    // get the name of the node
-    String saveNodeURL = nodeURL;
-    nodeURL = node.getNodeInformation().getURL();
-
-    // stop accepting communication
-    blockCommunication();
-
-    // save the id
-    UniqueID savedID = bodyID;
-    if (byCopy) {
-      // if moving by copy we have to create a new unique ID
-      // the bodyID will be automatically recreate when deserialized
-      bodyID = null;
+    public MigratableBody(Object reifiedObject, String nodeURL,
+        MetaObjectFactory factory) {
+        super(reifiedObject, nodeURL, factory);
+        this.migrationManager = factory.newMigrationManagerFactory()
+                                       .newMigrationManager();
     }
-    // try to migrate
-    UniversalBody migratedBody = null;
-    try { 
-      migratedBody = migrationManager.migrateTo(node, this);
-    } catch (MigrationException e) {
-      nodeURL = saveNodeURL;
-      bodyID = savedID;
-      localBodyStrategy.getFuturePool().unsetMigrationTag();
-      acceptCommunication();
-      throw e;
+
+    //
+    // -- PUBLIC METHODS -----------------------------------------------
+    //
+    //
+    // -- implements Migratable -----------------------------------------------
+    //
+    public UniversalBody migrateTo(Node node) throws MigrationException {
+        return internalMigrateTo(node, false);
     }
-    if (!byCopy) {
-      changeBodyAfterMigration(migratedBody);
-    } else {
-      bodyID = savedID;
-      nodeURL = saveNodeURL;
+
+    public UniversalBody cloneTo(Node node) throws MigrationException {
+        return internalMigrateTo(node, true);
     }
-    acceptCommunication();
-    return migratedBody;
-  }
-  
 
-  protected void changeBodyAfterMigration(UniversalBody migratedBody) {
-    // cleanup after migration
-    requestReceiver = migrationManager.createRequestReceiver(migratedBody, requestReceiver);
-    replyReceiver = migrationManager.createReplyReceiver(migratedBody, replyReceiver);
-    activityStopped();
-    migrationManager = null;
-    // signal that this body (remaining after migration) has just migrated
-    hasJustMigrated = true;
-  }
-  
-  
-  //
-  // -- SERIALIZATION METHODS -----------------------------------------------
-  //
+    public void addMigrationEventListener(MigrationEventListener listener) {
+        if (migrationManager != null) {
+            migrationManager.addMigrationEventListener(listener);
+        }
+    }
 
-  private void writeObject(java.io.ObjectOutputStream out) throws java.io.IOException {
-    out.defaultWriteObject();
-  }
+    public void removeMigrationEventListener(MigrationEventListener listener) {
+        if (migrationManager != null) {
+            migrationManager.removeMigrationEventListener(listener);
+        }
+    }
 
-  private void readObject(java.io.ObjectInputStream in) throws java.io.IOException, ClassNotFoundException {
-    in.defaultReadObject();
-    hasJustMigrated = true;
-  }
+    //
+    // -- PROTECTED METHODS -----------------------------------------------
+    //
+
+    /**
+     * Signals that the activity of this body, managed by the active thread has just started.
+     */
+    protected void activityStarted() {
+        super.activityStarted();
+
+        //System.out.println("Body run on node "+nodeURL+" migration="+hasJustMigrated);
+        if (hasJustMigrated) {
+            if (migrationManager != null) {
+                migrationManager.startingAfterMigration(this);
+            }
+            hasJustMigrated = false;
+        }
+    }
+
+    //
+    // -- PRIVATE METHODS -----------------------------------------------
+    //
+    protected UniversalBody internalMigrateTo(Node node, boolean byCopy)
+        throws MigrationException {
+        if (!isAlive()) {
+            throw new MigrationException(
+                "Attempt to migrate a dead body that has been terminated");
+        }
+
+        if (!isActive()) {
+            throw new MigrationException("Attempt to migrate a non active body");
+        }
+
+        // check node with Manager
+        node = migrationManager.checkNode(node);
+
+        // get the name of the node
+        String saveNodeURL = nodeURL;
+        nodeURL = node.getNodeInformation().getURL();
+
+        // stop accepting communication
+        blockCommunication();
+
+        // save the id
+        UniqueID savedID = bodyID;
+
+        if (byCopy) {
+            // if moving by copy we have to create a new unique ID
+            // the bodyID will be automatically recreate when deserialized
+            bodyID = null;
+        }
+
+        // try to migrate
+        UniversalBody migratedBody = null;
+
+        try {
+            migratedBody = migrationManager.migrateTo(node, this);
+        } catch (MigrationException e) {
+            nodeURL = saveNodeURL;
+            bodyID = savedID;
+            localBodyStrategy.getFuturePool().unsetMigrationTag();
+            acceptCommunication();
+            throw e;
+        }
+		activityStopped();
+
+        if (!byCopy) {
+            changeBodyAfterMigration(migratedBody);
+        } else {
+            bodyID = savedID;
+            nodeURL = saveNodeURL;
+        }
+        acceptCommunication();
+
+        return migratedBody;
+    }
+
+    protected void changeBodyAfterMigration(UniversalBody migratedBody) {
+        // cleanup after migration
+        requestReceiver = migrationManager.createRequestReceiver(migratedBody,
+                requestReceiver);
+        replyReceiver = migrationManager.createReplyReceiver(migratedBody,
+                replyReceiver);
+        activityStopped();
+        migrationManager = null;
+
+        // signal that this body (remaining after migration) has just migrated
+        hasJustMigrated = true;
+    }
+
+    //
+    // -- SERIALIZATION METHODS -----------------------------------------------
+    //
+    private void writeObject(java.io.ObjectOutputStream out)
+        throws java.io.IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(java.io.ObjectInputStream in)
+        throws java.io.IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        hasJustMigrated = true;
+    }
 }
