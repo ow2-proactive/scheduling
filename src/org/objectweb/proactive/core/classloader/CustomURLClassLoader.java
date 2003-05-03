3bb682b48bacfd19582f335834875378828eb1dc
fhuet <fhuet@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-05-03 15:53:15 +0000

    Ajout du ProActiveClassLoader pour regler les problemes de generation de stub en cas de telechargement des classes
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@384 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/classloader/CustomURLClassLoader.java
@@ -0,0 +1,100 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package org.objectweb.proactive.core.classloader;
+
+import sun.misc.Resource;
+import sun.misc.URLClassPath;
+
+import java.lang.reflect.Method;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+/**
+ * @author fhuet
+ *
+ * To change this generated comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class CustomURLClassLoader extends URLClassLoader {
+
+    protected URLClassPath ucp;
+
+    public CustomURLClassLoader(URL[] urls) {
+        super(urls);
+        ucp = new URLClassPath(urls);
+    }
+
+    public CustomURLClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    protected Class findClass(String name) throws ClassNotFoundException {
+
+        String path = name.replace('.', '/').concat(".class");
+        Resource res = ucp.getResource(path, false);
+        if (res != null) {
+            try {
+
+                byte[] b = res.getBytes();
+                Class clc = Class.forName("java.lang.ClassLoader");
+                Class[] argumentTypes = new Class[5];
+                argumentTypes[0] = name.getClass();
+                argumentTypes[1] = b.getClass();
+                argumentTypes[2] = Integer.TYPE;
+                argumentTypes[3] = Integer.TYPE;
+                argumentTypes[4] = Class.forName(
+                        "java.security.ProtectionDomain");
+
+                Method m = clc.getDeclaredMethod("defineClass", argumentTypes);
+                m.setAccessible(true);
+
+                Object[] effectiveArguments = new Object[5];
+                effectiveArguments[0] = name;
+                effectiveArguments[1] = b;
+                effectiveArguments[2] = new Integer(0);
+                effectiveArguments[3] = new Integer(b.length);
+                effectiveArguments[4] = this.getClass().getProtectionDomain();
+                System.out.println("Context class loader " +
+                    Thread.currentThread().getContextClassLoader());
+                return (Class) m.invoke(Thread.currentThread()
+                                              .getContextClassLoader(),
+                    effectiveArguments);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                throw new ClassNotFoundException(ex.getMessage());
+            }
+        } else {
+            throw new ClassNotFoundException(name);
+        }
+    }
+}
