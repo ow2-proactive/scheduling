b6f5fc5811693added849697944dcd1eddabdfce
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-05-13 09:13:01 +0000

    User started with xml descriptors
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@419 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/c3d_add_user.bat
+++ scripts/windows/c3d_add_user.bat
@@ -12,8 +12,8 @@ goto end
 :doit
 SETLOCAL
 call init.bat
-
-%JAVA_CMD% org.objectweb.proactive.examples.c3d.C3DUser
+set XMLDESCRIPTOR=..\..\descriptors\C3D_User.xml
+%JAVA_CMD% org.objectweb.proactive.examples.c3d.C3DUser %XMLDESCRIPTOR%
 ENDLOCAL
 
 :end
