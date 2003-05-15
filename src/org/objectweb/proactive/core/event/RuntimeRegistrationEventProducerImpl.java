2fe233ee96f584ca89d87213dfc1df4e0489b5f9
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-05-15 10:54:25 +0000

    javadoc
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@424 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/event/RuntimeRegistrationEventProducerImpl.java
+++ src/org/objectweb/proactive/core/event/RuntimeRegistrationEventProducerImpl.java
@@ -1,3 +1,33 @@
+/* 
+* ################################################################
+* 
+* ProActive: The Java(TM) library for Parallel, Distributed, 
+*            Concurrent computing with Security and Mobility
+* 
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*  
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*  
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s): 
+* 
+* ################################################################
+*/ 
 package org.objectweb.proactive.core.event;
 
 import org.objectweb.proactive.core.runtime.ProActiveRuntime;
@@ -21,7 +51,7 @@ public class RuntimeRegistrationEventProducerImpl
 	//-------------------implements RuntimeRegistrationEventProducer------------------
 	//
 	/**
-	 * @see org.objectweb.proactive.core.runtime.event.RuntimeRegistrationEventProducer#addRuntimeRegistrationEventListener(RuntimeRegistrationEventListener)
+	 * @see org.objectweb.proactive.core.event.RuntimeRegistrationEventProducer#addRuntimeRegistrationEventListener(RuntimeRegistrationEventListener)
 	 */
 	public void addRuntimeRegistrationEventListener(RuntimeRegistrationEventListener listener){
 		addListener(listener);
@@ -29,7 +59,7 @@ public class RuntimeRegistrationEventProducerImpl
 	
 	
 	/**
-	 * @see org.objectweb.proactive.core.runtime.event.RuntimeRegistrationEventProducer#removeRuntimeRegistrationEventListener(RuntimeRegistrationEventListener)
+	 * @see org.objectweb.proactive.core.event.RuntimeRegistrationEventProducer#removeRuntimeRegistrationEventListener(RuntimeRegistrationEventListener)
 	 */
 	public void removeRuntimeRegistrationEventListener(RuntimeRegistrationEventListener listener){
 		removeListener(listener);
