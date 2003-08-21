04aa1b84b8be5af0c8cf826c29a694120375c843
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-08-21 11:57:13 +0000

    Error Handler for Sax Event
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@524 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/xml/io/SAXParserErrorHandler.java
@@ -0,0 +1,29 @@
+package org.objectweb.proactive.core.xml.io;
+
+
+
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParserErrorHandler extends DefaultHandler {
+	
+	
+	static Logger logger = Logger.getLogger(SAXParserErrorHandler.class.getName());
+
+	public SAXParserErrorHandler() {}
+
+    public void warning(SAXParseException ex) throws SAXException {
+		logger.warn("WARNING: " + ex.getMessage());
+	}
+
+    public void error(SAXParseException ex) throws SAXException {
+		logger.error("ERROR: "+ex.getSystemId()+" Line:"+ex.getLineNumber()+" Message:"+ex.getMessage());
+    } 
+
+    public void fatalError(SAXParseException ex) throws SAXException {
+		logger.fatal("FATAL ERROR: " + ex.getMessage());
+	} 
+} 
+
