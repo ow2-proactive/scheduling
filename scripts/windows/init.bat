ded1c6fdb39978810a4573f55aa5f10860d0f039
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-08-26 14:00:49 +0000

    script cleaned
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@558 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/init.bat
+++ scripts/windows/init.bat
@@ -31,10 +31,8 @@ IF EXIST %PROACTIVE%\lib\cryptix.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\c
 IF EXIST %PROACTIVE%\lib\iaik_jce_full.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\iaik_jce_full.jar
 IF EXIST %PROACTIVE%\lib\iaik_ssl.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\iaik_ssl.jar
 IF EXIST %PROACTIVE%\lib\log4j-core.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\log4j-core.jar
-IF EXIST %PROACTIVE%\lib\ibis.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ibis.jar
 IF EXIST %PROACTIVE%\lib\xercesImpl.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\xercesImpl.jar
 IF EXIST %PROACTIVE%\lib\xmlParserAPIs.jar set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\xmlParserAPIs.jar
-
 echo CLASSPATH=%CLASSPATH%
 
 set JAVA_CMD=%JAVA_HOME%\bin\java.exe -Djava.security.manager -Djava.security.policy=proactive.java.policy -Dlog4j.configuration=proactive-log4j
