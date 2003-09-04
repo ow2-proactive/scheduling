d53896fc10d77de0f01b7cc450ee1896315f23a5
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-04 10:46:35 +0000

    add manager in Group contruction
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@577 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/nonregressiontest/MainManager.java
+++ src/nonregressiontest/MainManager.java
@@ -1,16 +1,18 @@
 package nonregressiontest;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.SimpleLayout;
+
+import testsuite.group.Group;
+
+import testsuite.manager.ProActiveFuncTestManager;
+
 import java.io.File;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.SimpleLayout;
-
-import testsuite.group.Group;
-import testsuite.manager.ProActiveFuncTestManager;
 
 /**
  * @author rquilici
@@ -20,58 +22,48 @@ import testsuite.manager.ProActiveFuncTestManager;
  * To enable and disable the creation of type comments go to
  * Window>Preferences>Java>Code Generation.
  */
-public class MainManager extends ProActiveFuncTestManager
-{
+public class MainManager extends ProActiveFuncTestManager {
 
-	/**
-	 * Constructor for MainManager.
-	 */
-	public MainManager()
-	{
-		this("Main unit test manager","Manage all unit non-regression tests");
-		logger.addAppender(new ConsoleAppender(new SimpleLayout()));
-	}
+    /**
+     * Constructor for MainManager.
+     */
+    public MainManager() {
+        this("Main unit test manager", "Manage all unit non-regression tests");
+        logger.addAppender(new ConsoleAppender(new SimpleLayout()));
+    }
 
-	/**
-	 * Constructor for MainManager.
-	 * @param name
-	 * @param description
-	 */
-	public MainManager(String name, String description)
-	{
-		super(name, description);
-	}
+    /**
+     * Constructor for MainManager.
+     * @param name
+     * @param description
+     */
+    public MainManager(String name, String description) {
+        super(name, description);
+    }
 
-	/**
-	 * @see testsuite.manager.AbstractManager#initManager()
-	 */
-	public void initManager() throws Exception
-	{
-		Group testGroup = new Group("Unit test group","group of unit tests",new File("/net/home/adicosta/workspace/ProActive/classes/"),"nonregressiontest",null,false);
-		add(testGroup);
-	}
-	
-	public static void main(String[] args){
-		MainManager manager = new MainManager();
-		manager.execute(false);
- 		manager.setVerbatim(true);
- 		try
-		{
-			manager.toHTML(new File("/net/home/adicosta/test.html"));
-		}
-		catch (ParserConfigurationException e)
-		{
-			e.printStackTrace();
-		}
-		catch (TransformerException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
- 		System.exit(0);
-	}
+    /**
+     * @see testsuite.manager.AbstractManager#initManager()
+     */
+    public void initManager() throws Exception {
+        Group testGroup = new Group("Unit test group", "group of unit tests",
+                new File("/net/home/adicosta/workspace/ProActive/classes/"),
+                "nonregressiontest", null, false, this);
+        add(testGroup);
+    }
 
+    public static void main(String[] args) {
+        MainManager manager = new MainManager();
+        manager.execute(false);
+        manager.setVerbatim(true);
+        try {
+            manager.toHTML(new File("/net/home/adicosta/test.html"));
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
 }
