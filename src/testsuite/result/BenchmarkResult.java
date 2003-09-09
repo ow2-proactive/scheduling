acd9f493a6b7f2b72e500dd29a2277d730679c85
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-09 14:12:00 +0000

    Add Max, Min, Moy, STDEV in Bench Result
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@595 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/testsuite/result/BenchmarkResult.java
+++ src/testsuite/result/BenchmarkResult.java
@@ -7,8 +7,10 @@ package testsuite.result;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 
 import testsuite.exception.BadTypeException;
+
 import testsuite.test.Benchmark;
 
 
@@ -17,18 +19,42 @@ import testsuite.test.Benchmark;
  *
  */
 public class BenchmarkResult extends TestResult {
-    private double timeResult = 0.0;
+    private long min = -1;
+    private double moy = -1;
+    private double stdev = 0;
+    private long max = -1;
+    private long[] set = null;
 
-    public BenchmarkResult(Benchmark test, int type, String message)
+    public BenchmarkResult(Benchmark test, int type, String message, long[] set)
         throws BadTypeException {
         super(test, type, message);
-        timeResult = test.getResultTime();
+        this.set = set;
+        long sumTest = 0;
+        for (int i = 0; i < set.length; i++) {
+            if ((min < 0) || (set[i] < min)) {
+                min = set[i];
+            }
+            if (set[i] > max) {
+                max = set[i];
+            }
+            sumTest += set[i];
+        }
+        moy = ((double) sumTest / set.length);
+        for (int i = 0; i < set.length; i++)
+            stdev += Math.pow(set[i] - moy, 2);
+        stdev /= set.length;
+        stdev = Math.sqrt(stdev);
     }
 
     public BenchmarkResult(Benchmark test, int type, String message, Throwable e)
         throws BadTypeException {
         super(test, type, message, e);
-        timeResult = test.getResultTime();
+    }
+
+    public BenchmarkResult(Benchmark test, int type, String message)
+        throws BadTypeException {
+        super(test, type, message);
+        min = test.getResultTime();
     }
 
     /**
@@ -40,8 +66,36 @@ public class BenchmarkResult extends TestResult {
         if ((getType() == AbstractResult.RESULT) ||
                 (getType() == AbstractResult.GLOBAL_RESULT)) {
             Element result = document.createElement("Benchmark");
-            Node resultText = document.createTextNode(timeResult + "");
-            result.appendChild(resultText);
+
+            Element bestTime = document.createElement("BestTime");
+            Text bestTimeValue = document.createTextNode(min + "");
+            bestTime.appendChild(bestTimeValue);
+            result.appendChild(bestTime);
+
+            Element maxTime = document.createElement("MaxTime");
+            Text maxTimeValue = document.createTextNode(max + "");
+            maxTime.appendChild(maxTimeValue);
+            result.appendChild(maxTime);
+
+            Element moyTime = document.createElement("MoyTime");
+            Text moyTimeValue = document.createTextNode(moy + "");
+            moyTime.appendChild(moyTimeValue);
+            result.appendChild(moyTime);
+
+            Element stdevTime = document.createElement("STDEV");
+            Text stdevTimeValue = document.createTextNode(stdev + "");
+            stdevTime.appendChild(stdevTimeValue);
+            result.appendChild(stdevTime);
+
+            Element details = document.createElement("Details");
+            for (int i = 0; i < set.length; i++) {
+                Element bench = document.createElement("Bench");
+                Text benchValue = document.createTextNode(set[i] + "");
+                bench.appendChild(benchValue);
+                details.appendChild(bench);
+            }
+            result.appendChild(details);
+
             root.appendChild(result);
         }
 
@@ -54,13 +108,41 @@ public class BenchmarkResult extends TestResult {
     public String toString() {
         return super.toString() +
         (((getType() == RESULT) || (getType() == GLOBAL_RESULT))
-        ? ("Bench run in " + timeResult + "ms\n") : "");
+        ? ("Bench run in " + min + "ms\n") : "");
     }
 
     /**
      * @return
      */
     public double getTimeResult() {
-        return timeResult;
+        return min;
+    }
+
+    /**
+     * @return
+     */
+    public long getMax() {
+        return max;
+    }
+
+    /**
+     * @return
+     */
+    public long[] getSet() {
+        return set;
+    }
+
+    /**
+     * @return
+     */
+    public double getMoy() {
+        return moy;
+    }
+
+    /**
+     * @return
+     */
+    public double getStdev() {
+        return stdev;
     }
 }
