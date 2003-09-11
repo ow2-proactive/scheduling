b14176f3d87c824d28fd1d7ba8a9555b236a4878
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:48:35 +0000

    Test for automatic continuation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@607 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/activeobject/acontinuation/A.java
@@ -0,0 +1,116 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.activeobject.acontinuation;
+
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.core.node.NodeException;
+
+
+public class A implements java.io.Serializable {
+    boolean isFuture;
+    private A deleguate;
+    Id id;
+    Id idSent;
+
+    public A() {
+    }
+
+    public A(String name) {
+        this.id = new Id(name);
+    }
+
+    public void initFirstDeleguate() throws Exception {
+        this.deleguate = (A) ProActive.newActive(A.class.getName(),
+                new Object[] { "deleguate1" });
+        deleguate.initSecondDeleguate();
+    }
+
+    public void initSecondDeleguate() throws Exception {
+        this.deleguate = (A) ProActive.newActive(A.class.getName(),
+                new Object[] { "deleguate2" });
+    }
+
+    public Id getId(String name) {
+        if (id.getName().equals(name)) {
+            return id;
+        } else {
+            return deleguate.getInternalId(name);
+        }
+    }
+
+    public Id getInternalId(String name) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return deleguate.getId(name);
+    }
+
+    public Id getIdforFuture() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+
+    public void forwardID(Id id) {
+        if (deleguate != null) {
+            deleguate.forwardID(id);
+        }
+        isFuture = ProActive.isAwaited(id);
+        idSent = id;
+    }
+
+    public boolean isSuccessful() {
+        if (!isFuture) {
+            return false;
+        }
+
+        if (deleguate != null) {
+            return deleguate.isSuccessful();
+        }
+
+        return isFuture;
+    }
+
+    public String getFinalResult() {
+        if (deleguate != null) {
+            return deleguate.getFinalResult();
+        } else {
+            return idSent.getName();
+        }
+    }
+}
