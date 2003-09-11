984bfbac649b373d8a7433a2ceee511eacc617f3
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:56:03 +0000

    Test lopp migration
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@617 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/activeobject/migration/multiplemigration/Test.java
@@ -0,0 +1,79 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.activeobject.migration.multiplemigration;
+
+import org.objectweb.proactive.ProActive;
+
+import testsuite.manager.ProActiveFuncTestManager;
+
+import testsuite.test.ProActiveFunctionalTest;
+
+
+/**
+ * @author rquilici
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class Test extends ProActiveFunctionalTest {
+    A a;
+
+    public Test() {
+        super("multiple migration", "Test multiple migration with method call");
+    }
+
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    public void action() throws Exception {
+        a = (A) ProActive.newActive(A.class.getName(), new Object[] { "toto" });
+        a.moveTo(((ProActiveFuncTestManager) manager).getRemoteVMNode());
+        a.getNodeUrl();
+        a.moveTo(((ProActiveFuncTestManager) manager).getSameVMNode());
+        a.getNodeUrl();
+        a.moveTo(((ProActiveFuncTestManager) manager).getLocalVMNode());
+        a.getNodeUrl();
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#initTest()
+     */
+    public void initTest() throws Exception {
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#endTest()
+     */
+    public void endTest() throws Exception {
+    }
+}
