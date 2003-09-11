93a304e01b8fde7bf7d0ce564242f305b477f028
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 09:00:52 +0000

    Test stub generation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@624 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/stub/stubgeneration/Test.java
@@ -0,0 +1,84 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.stub.stubgeneration;
+
+import org.objectweb.proactive.core.Constants;
+import org.objectweb.proactive.core.mop.ASMBytecodeStubBuilder;
+import org.objectweb.proactive.core.mop.MOP;
+
+import testsuite.test.FunctionalTest;
+
+
+/**
+ * @author rquilici
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class Test extends FunctionalTest {
+    String stubClassName;
+    byte[] data;
+
+    public Test() {
+        super("Stub Generation",
+            "Testing on-the-fly generation of stub classes in bytecode form");
+    }
+
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    public void action() throws Exception {
+        ASMBytecodeStubBuilder bsb = new ASMBytecodeStubBuilder(
+                "nonregressiontest.stub.stubgeneration.A");
+        data = bsb.create();
+        stubClassName = bsb.getStubClassFullName();
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#initTest()
+     */
+    public void initTest() throws Exception {
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#endTest()
+     */
+    public void endTest() throws Exception {
+    }
+
+    public boolean postConditions() throws Exception {
+        return ((data != null) &&
+        (stubClassName.equals(
+            "pa.stub.nonregressiontest.stub.stubgeneration.Stub_A")));
+    }
+}
