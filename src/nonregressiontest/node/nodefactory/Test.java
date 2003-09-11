e1cc745345e75d8c2395e70582da36fbaac704f2
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:46:27 +0000

    formatted with Jalopy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@606 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/nonregressiontest/node/nodefactory/Test.java
+++ src/nonregressiontest/node/nodefactory/Test.java
@@ -1,9 +1,40 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
 package nonregressiontest.node.nodefactory;
 
 import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.core.node.NodeFactory;
 
 import testsuite.test.FunctionalTest;
+
 
 /**
  * @author rquilici
@@ -13,50 +44,46 @@ import testsuite.test.FunctionalTest;
  * To enable and disable the creation of type comments go to
  * Window>Preferences>Java>Code Generation.
  */
-public class Test extends FunctionalTest
-{
-	Node rmiNode;
-	Node jiniNode;
-	//Node ibisNode;
-	/**
-	 * Constructor for Test.
-	 */
-	public Test()
-	{
-		super("NodeFactory","Test the creation of rmi, jini, ibis node whith the factory");
-	}
+public class Test extends FunctionalTest {
+    Node rmiNode;
+    Node jiniNode;
 
+    //Node ibisNode;
 
-	/**
-	 * @see testsuite.test.FunctionalTest#action()
-	 */
-	public void action() throws Exception
-	{
-		NodeFactory.createNode("//localhost/RMINode");
-		NodeFactory.createNode("jini://localhost/JININode");
-		//NodeFactory.createNode("ibis://localhost/IBISNode");
-	}
+    /**
+     * Constructor for Test.
+     */
+    public Test() {
+        super("NodeFactory",
+            "Test the creation of rmi, jini, ibis node whith the factory");
+    }
 
-	/**
-	 * @see testsuite.test.AbstractTest#initTest()
-	 */
-	public void initTest() throws Exception
-	{
-	}
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    public void action() throws Exception {
+        NodeFactory.createNode("//localhost/RMINode");
+        NodeFactory.createNode("jini://localhost/JININode");
+        //NodeFactory.createNode("ibis://localhost/IBISNode");
+    }
 
-	/**
-	 * @see testsuite.test.AbstractTest#endTest()
-	 */
-	public void endTest() throws Exception
-	{
-		
-	}
-	
-	public boolean postConditions() throws Exception{
-		rmiNode = NodeFactory.getNode("//localhost/RMINode");
-		jiniNode = NodeFactory.getNode("jini://localhost/JININode");
-		//ibisNode = NodeFactory.getNode("ibis://localhost/IBISNode");
-		return ((rmiNode != null) && (jiniNode != null));
-	}
+    /**
+     * @see testsuite.test.AbstractTest#initTest()
+     */
+    public void initTest() throws Exception {
+    }
 
+    /**
+     * @see testsuite.test.AbstractTest#endTest()
+     */
+    public void endTest() throws Exception {
+    }
+
+    public boolean postConditions() throws Exception {
+        rmiNode = NodeFactory.getNode("//localhost/RMINode");
+        jiniNode = NodeFactory.getNode("jini://localhost/JININode");
+        //ibisNode = NodeFactory.getNode("ibis://localhost/IBISNode");
+        return ((rmiNode != null) && (jiniNode != null) &&
+        NodeFactory.isNodeLocal(rmiNode));
+    }
 }
--- src/nonregressiontest/runtime/defaultruntime/Test.java
+++ src/nonregressiontest/runtime/defaultruntime/Test.java
@@ -1,9 +1,40 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
 package nonregressiontest.runtime.defaultruntime;
 
 import org.objectweb.proactive.core.runtime.ProActiveRuntime;
 import org.objectweb.proactive.core.runtime.RuntimeFactory;
 
 import testsuite.test.FunctionalTest;
+
 
 /**
  * @author rquilici
@@ -13,39 +44,33 @@ import testsuite.test.FunctionalTest;
  * To enable and disable the creation of type comments go to
  * Window>Preferences>Java>Code Generation.
  */
-public class Test extends FunctionalTest
-{
-	
-	ProActiveRuntime part;
-	
-	public Test(){
-		super("defaultruntimecreation","Test default runtime creation");
-	}
+public class Test extends FunctionalTest {
+    ProActiveRuntime part;
 
-	/**
-	 * @see testsuite.test.FunctionalTest#action()
-	 */
-	public void action() throws Exception
-	{
-		part = RuntimeFactory.getDefaultRuntime();
-	}
+    public Test() {
+        super("defaultruntimecreation", "Test default runtime creation");
+    }
 
-	/**
-	 * @see testsuite.test.AbstractTest#initTest()
-	 */
-	public void initTest() throws Exception
-	{
-	}
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    public void action() throws Exception {
+        part = RuntimeFactory.getDefaultRuntime();
+    }
 
-	/**
-	 * @see testsuite.test.AbstractTest#endTest()
-	 */
-	public void endTest() throws Exception
-	{
-	}
-	
-	public boolean postConditions() throws Exception{
-		return (part != null);
-	}
+    /**
+     * @see testsuite.test.AbstractTest#initTest()
+     */
+    public void initTest() throws Exception {
+    }
 
+    /**
+     * @see testsuite.test.AbstractTest#endTest()
+     */
+    public void endTest() throws Exception {
+    }
+
+    public boolean postConditions() throws Exception {
+        return (part != null);
+    }
 }
