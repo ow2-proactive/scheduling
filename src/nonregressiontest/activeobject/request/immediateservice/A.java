8d6af12953ec437645378562333179732c77c508
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:59:46 +0000

    Test immediate services
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@623 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/activeobject/request/immediateservice/A.java
@@ -0,0 +1,67 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.activeobject.request.immediateservice;
+
+import org.objectweb.proactive.Body;
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.RunActive;
+
+import java.io.Serializable;
+
+
+/**
+ * @author rquilici
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class A implements Serializable, RunActive {
+    DummyObject dum;
+
+    public A() {
+    }
+
+    public A(String name) {
+        this.dum = new DummyObject(name);
+    }
+
+    public void runActivity(Body body) {
+        while (body.isActive()) {
+            //do nothing
+        }
+    }
+
+    public DummyObject getObject() {
+        return dum;
+    }
+}
