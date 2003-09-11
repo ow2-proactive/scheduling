b7b43a97ab90d6ad51c36b020b8697f68f113be6
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:58:59 +0000

    Test request
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@621 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/activeobject/request/A.java
@@ -0,0 +1,83 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.activeobject.request;
+
+import org.objectweb.proactive.Body;
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.RunActive;
+
+import java.io.Serializable;
+
+
+/**
+ * @author rquilici
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class A implements Serializable, RunActive {
+    int counter = 0;
+
+    public A() {
+    }
+
+    public void runActivity(Body body) {
+        body.blockCommunication();
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        body.acceptCommunication();
+        org.objectweb.proactive.Service service = new org.objectweb.proactive.Service(body);
+        service.fifoServing();
+    }
+
+    public void method1() {
+        counter++;
+    }
+
+    public A method2() {
+        counter++;
+        return new A();
+    }
+
+    public int method3() {
+        counter++;
+        return counter;
+    }
+
+    public void exit() {
+        ProActive.getBodyOnThis().terminate();
+    }
+}
