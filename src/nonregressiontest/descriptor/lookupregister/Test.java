67e6fea95b9ec88b1bc19466fb1a53b8b50708a2
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 09:03:26 +0000

    Test lookup register
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@627 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/descriptor/lookupregister/Test.java
@@ -0,0 +1,92 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
+package nonregressiontest.descriptor.lookupregister;
+
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
+import org.objectweb.proactive.core.descriptor.data.VirtualNode;
+
+import testsuite.test.ProActiveFunctionalTest;
+
+
+/**
+ * @author rquilici
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class Test extends ProActiveFunctionalTest {
+    private static String FS = System.getProperty("file.separator");
+    private static String AGENT_XML_LOCATION_UNIX = System.getProperty(
+            "user.home") + FS + "ProActive" + FS + "descriptors" + FS +
+        "examples" + FS + "Agent.xml";
+    private static String AGENT_XML_LOCATION_WIN = "///Z:/ProActive/descriptors/examples/Agent.xml";
+    A a;
+
+    public Test() {
+        super("lookup register in deployment descriptor",
+            "Test lookup and register in deployment descriptors");
+    }
+
+    /**
+ * @see testsuite.test.FunctionalTest#action()
+ */
+    public void action() throws Exception {
+        ProActiveDescriptor proActiveDescriptorAgent = ProActive.getProactiveDescriptor(
+                "file:" + AGENT_XML_LOCATION_UNIX);
+        proActiveDescriptorAgent.activateMappings();
+        VirtualNode vnAgent = proActiveDescriptorAgent.getVirtualNode("Agent");
+        A b = (A) ProActive.newActive(A.class.getName(),
+                new Object[] { "local" }, vnAgent.getNode());
+
+        VirtualNode vnLookup = ProActive.lookupVirtualNode("//localhost/Agent",
+                "rmi:");
+        a = (A) vnLookup.getUniqueAO();
+    }
+
+    /**
+ * @see testsuite.test.AbstractTest#initTest()
+ */
+    public void initTest() throws Exception {
+    }
+
+    /**
+ * @see testsuite.test.AbstractTest#endTest()
+ */
+    public void endTest() throws Exception {
+    }
+
+    public boolean postConditions() throws Exception {
+        return (a.getName().equals("local"));
+    }
+}
