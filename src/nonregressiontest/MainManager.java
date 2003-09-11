a8103760691398bda5f5edc75602dfab8a4bcfde
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-09-11 08:36:37 +0000

    formatted with Jalopy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@605 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/nonregressiontest/MainManager.java
+++ src/nonregressiontest/MainManager.java
@@ -1,3 +1,33 @@
+/*
+* ################################################################
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
+*            Concurrent computing with Security and Mobility
+*
+* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+* Contact: proactive-support@inria.fr
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+* USA
+*
+*  Initial developer(s):               The ProActive Team
+*                        http://www.inria.fr/oasis/ProActive/contacts.html
+*  Contributor(s):
+*
+* ################################################################
+*/
 package nonregressiontest;
 
 import org.apache.log4j.ConsoleAppender;
@@ -23,30 +53,33 @@ import javax.xml.transform.TransformerException;
  * Window>Preferences>Java>Code Generation.
  */
 public class MainManager extends ProActiveFuncTestManager {
+    private static String FS = System.getProperty("file.separator");
+    private static String RESULT_LOCATION = System.getProperty("user.home") +
+        FS + "test.html";
 
     /**
-     * Constructor for MainManager.
-     */
+ * Constructor for MainManager.
+ */
     public MainManager() {
         this("Main unit test manager", "Manage all unit non-regression tests");
         logger.addAppender(new ConsoleAppender(new SimpleLayout()));
     }
 
     /**
-     * Constructor for MainManager.
-     * @param name
-     * @param description
-     */
+ * Constructor for MainManager.
+ * @param name
+ * @param description
+ */
     public MainManager(String name, String description) {
         super(name, description);
     }
 
     /**
-     * @see testsuite.manager.AbstractManager#initManager()
-     */
+ * @see testsuite.manager.AbstractManager#initManager()
+ */
     public void initManager() throws Exception {
         Group testGroup = new Group("Unit test group", "group of unit tests",
-                new File("/net/home/adicosta/workspace/ProActive/classes/"),
+                new File("/net/home/rquilici/ProActive/classes/"),
                 "nonregressiontest", null, false, this);
         add(testGroup);
     }
@@ -56,7 +89,7 @@ public class MainManager extends ProActiveFuncTestManager {
         manager.execute(false);
         manager.setVerbatim(true);
         try {
-            manager.toHTML(new File("/net/home/adicosta/test.html"));
+            manager.toHTML(new File(RESULT_LOCATION));
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (TransformerException e) {
