c93ebb97c792c89d5c9aeae02cccb230b700a448
fhuet <fhuet@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-11-04 15:21:36 +0000

    fix of a logging line
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@718 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/body/rmi/RemoteBodyAdapter.java
+++ src/org/objectweb/proactive/core/body/rmi/RemoteBodyAdapter.java
@@ -1,33 +1,33 @@
 /*
-* ################################################################
-*
-* ProActive: The Java(TM) library for Parallel, Distributed,
-*            Concurrent computing with Security and Mobility
-*
-* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
-* Contact: proactive-support@inria.fr
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-* USA
-*
-*  Initial developer(s):               The ProActive Team
-*                        http://www.inria.fr/oasis/ProActive/contacts.html
-*  Contributor(s):
-*
-* ################################################################
-*/
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.core.body.rmi;
 
 
@@ -37,6 +37,7 @@ package org.objectweb.proactive.core.body.rmi;
  *   to another remote objects library.
  */
 import org.apache.log4j.Logger;
+
 import org.objectweb.proactive.core.ProActiveException;
 import org.objectweb.proactive.core.UniqueID;
 import org.objectweb.proactive.core.body.UniversalBody;
@@ -64,11 +65,11 @@ public class RemoteBodyAdapter implements UniversalBody, java.io.Serializable {
     }
 
     public RemoteBodyAdapter(RemoteBody remoteBody) throws ProActiveException {
-    //	Thread.dumpStack();
-    //	ProActiveConfiguration.getConfiguration().dumpLoadedProperties();
+        //	Thread.dumpStack();
+        //	ProActiveConfiguration.getConfiguration().dumpLoadedProperties();
         this.proxiedRemoteBody = remoteBody;
         if (logger.isDebugEnabled()) {
-             logger.debug(proxiedRemoteBody.getClass());
+            logger.debug(proxiedRemoteBody.getClass());
         }
         try {
             this.bodyID = remoteBody.getID();
@@ -78,13 +79,13 @@ public class RemoteBodyAdapter implements UniversalBody, java.io.Serializable {
     }
 
     public RemoteBodyAdapter(UniversalBody body) throws ProActiveException {
-        if (logger.isDebugEnabled()) {
-            logger.debug(proxiedRemoteBody.getClass());
-        }
         try {
             this.proxiedRemoteBody = new RemoteBodyImpl(body);
         } catch (java.rmi.RemoteException e) {
             throw new ProActiveException(e);
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug(proxiedRemoteBody.getClass());
         }
         this.bodyID = body.getID();
     }
