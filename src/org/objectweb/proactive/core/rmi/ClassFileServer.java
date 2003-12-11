51acc5fbc426384edf5436a522dfe12bca95ca5f
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-12-11 14:44:12 +0000

    added generation of component functional interfaces if required
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@767 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/rmi/ClassFileServer.java
+++ src/org/objectweb/proactive/core/rmi/ClassFileServer.java
@@ -1,34 +1,37 @@
-/* 
+/*
 * ################################################################
-* 
-* ProActive: The Java(TM) library for Parallel, Distributed, 
+*
+* ProActive: The Java(TM) library for Parallel, Distributed,
 *            Concurrent computing with Security and Mobility
-* 
+*
 * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
 * Contact: proactive-support@inria.fr
-* 
+*
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
 * License as published by the Free Software Foundation; either
 * version 2.1 of the License, or any later version.
-*  
+*
 * This library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 * Lesser General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU Lesser General Public
 * License along with this library; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 * USA
-*  
+*
 *  Initial developer(s):               The ProActive Team
 *                        http://www.inria.fr/oasis/ProActive/contacts.html
-*  Contributor(s): 
-* 
+*  Contributor(s):
+*
 * ################################################################
 */
 package org.objectweb.proactive.core.rmi;
+
+import org.objectweb.proactive.core.component.asmgen.MetaObjectInterfaceClassGenerator;
+import org.objectweb.proactive.core.component.asmgen.RepresentativeInterfaceClassGenerator;
 
 
 /**
@@ -45,34 +48,34 @@ public class ClassFileServer extends ClassServer {
     //
 
     /**
- * Constructs a ClassFileServer.
- * @param classpath the classpath where the server locates classes
- */
+     * Constructs a ClassFileServer.
+     * @param classpath the classpath where the server locates classes
+     */
     public ClassFileServer() throws java.io.IOException {
         this(0, null);
     }
 
     /**
- * Constructs a ClassFileServer.
- * @param classpath the classpath where the server locates classes
- */
+     * Constructs a ClassFileServer.
+     * @param classpath the classpath where the server locates classes
+     */
     public ClassFileServer(int port) throws java.io.IOException {
         this(port, null);
     }
 
     /**
- * Constructs a ClassFileServer.
- * @param classpath the classpath where the server locates classes
- */
+     * Constructs a ClassFileServer.
+     * @param classpath the classpath where the server locates classes
+     */
     public ClassFileServer(String paths) throws java.io.IOException {
         this(0, paths);
     }
 
     /**
- * Constructs a ClassFileServer.
- * @param port the port to bound the server to
- * @param classpath the classpath where the server locates classes
- */
+     * Constructs a ClassFileServer.
+     * @param port the port to bound the server to
+     * @param classpath the classpath where the server locates classes
+     */
     public ClassFileServer(int port, String paths) throws java.io.IOException {
         super(port);
         if (paths != null) {
@@ -104,29 +107,29 @@ public class ClassFileServer extends ClassServer {
     }
 
     /**
- * Main method to create the class server that reads
- * class files. This takes two optional command line arguments, the
- * port on which the server accepts requests and the
- * root of the classpath. To start up the server: <br><br>
- *
- * <code>   java ClassFileServer [&lt;classpath>] [&lt;port>]
- * </code><br><br>
- *
- * The codebase of an RMI server using this webserver would
- * simply contain a URL with the host and port of the web
- * server (if the webserver's classpath is the same as
- * the RMI server's classpath): <br><br>
- *
- * <code>   java -Djava.rmi.server.codebase=http://zaphod:2001/ RMIServer
- * </code> <br><br>
- *
- * You can create your own class server inside your RMI server
- * application instead of running one separately. In your server
- * main simply create a ClassFileServer: <br><br>
- *
- * <code>   new ClassFileServer(port, classpath);
- * </code>
- */
+     * Main method to create the class server that reads
+     * class files. This takes two optional command line arguments, the
+     * port on which the server accepts requests and the
+     * root of the classpath. To start up the server: <br><br>
+     *
+     * <code>   java ClassFileServer [&lt;classpath>] [&lt;port>]
+     * </code><br><br>
+     *
+     * The codebase of an RMI server using this webserver would
+     * simply contain a URL with the host and port of the web
+     * server (if the webserver's classpath is the same as
+     * the RMI server's classpath): <br><br>
+     *
+     * <code>   java -Djava.rmi.server.codebase=http://zaphod:2001/ RMIServer
+     * </code> <br><br>
+     *
+     * You can create your own class server inside your RMI server
+     * application instead of running one separately. In your server
+     * main simply create a ClassFileServer: <br><br>
+     *
+     * <code>   new ClassFileServer(port, classpath);
+     * </code>
+     */
     public static void main(String[] args) {
         int port = 0;
         String classpath = null;
@@ -149,18 +152,18 @@ public class ClassFileServer extends ClassServer {
     //
 
     /**
- * Returns an array of bytes containing the bytecodes for
- * the class represented by the argument <b>path</b>.
- * The <b>path</b> is a dot separated class name with
- * the ".class" extension removed.
- *
- * @return the bytecodes for the class
- * @exception ClassNotFoundException if the class corresponding
- * to <b>path</b> could not be loaded.
- */
+     * Returns an array of bytes containing the bytecodes for
+     * the class represented by the argument <b>path</b>.
+     * The <b>path</b> is a dot separated class name with
+     * the ".class" extension removed.
+     *
+     * @return the bytecodes for the class
+     * @exception ClassNotFoundException if the class corresponding
+     * to <b>path</b> could not be loaded.
+     */
     protected byte[] getBytes(String path)
         throws java.io.IOException, ClassNotFoundException {
-		//	System.out.println("ClassFileServer: looking for class " + path);
+        //	System.out.println("ClassFileServer: looking for class " + path);
         byte[] b = null;
         if (codebases == null) {
             // reading from resources in the classpath
@@ -188,6 +191,22 @@ public class ClassFileServer extends ClassServer {
         if (b != null) {
             return b;
         }
+
+        // COMPONENTS
+        // try to get the class as a generated component interface reference
+        b = RepresentativeInterfaceClassGenerator.getClassData(path);
+
+        if (b != null) {
+            return b;
+        }
+
+        // COMPONENTS
+        // try to get the class as a generated component interface reference
+        b = MetaObjectInterfaceClassGenerator.getClassData(path);
+        if (b != null) {
+            return b;
+        }
+
         throw new ClassNotFoundException("Cannot find class " + path);
     }
 
@@ -196,16 +215,16 @@ public class ClassFileServer extends ClassServer {
     //
 
     /**
- * Returns an array of bytes containing the bytecodes for
- * the class represented by the argument <b>path</b>.
- * The <b>path</b> is a dot separated class name with
- * the ".class" extension removed.
- * @param path the fqn of the class
- * @return the bytecodes for the class
- * @exception java.io.IOException if the class cannot be read
- */
+     * Returns an array of bytes containing the bytecodes for
+     * the class represented by the argument <b>path</b>.
+     * The <b>path</b> is a dot separated class name with
+     * the ".class" extension removed.
+     * @param path the fqn of the class
+     * @return the bytecodes for the class
+     * @exception java.io.IOException if the class cannot be read
+     */
     private byte[] getBytesFromResource(String path) throws java.io.IOException {
-    //	System.out.println("ClassFileServer: looking for class " + path);
+        //	System.out.println("ClassFileServer: looking for class " + path);
         String filename = path.replace('.', '/') + ".class";
         java.io.InputStream in = this.getClass().getClassLoader()
                                      .getResourceAsStream(filename);
@@ -226,15 +245,15 @@ public class ClassFileServer extends ClassServer {
     }
 
     /**
- * Returns an array of bytes containing the bytecodes for
- * the class represented by the argument <b>path</b>.
- * The <b>path</b> is a dot separated class name with
- * the ".class" extension removed.
- * @param path the fqn of the class
- * @param codeBase the File that must be a jar or zip archive that may contain the class
- * @return the bytecodes for the class
- * @exception java.io.IOException if the class cannot be read
- */
+     * Returns an array of bytes containing the bytecodes for
+     * the class represented by the argument <b>path</b>.
+     * The <b>path</b> is a dot separated class name with
+     * the ".class" extension removed.
+     * @param path the fqn of the class
+     * @param codeBase the File that must be a jar or zip archive that may contain the class
+     * @return the bytecodes for the class
+     * @exception java.io.IOException if the class cannot be read
+     */
     private byte[] getBytesFromArchive(String path, java.io.File archive)
         throws java.io.IOException {
         String filename = path.replace('.', '/') + ".class";
@@ -258,15 +277,15 @@ public class ClassFileServer extends ClassServer {
     }
 
     /**
- * Returns an array of bytes containing the bytecodes for
- * the class represented by the argument <b>path</b>.
- * The <b>path</b> is a dot separated class name with
- * the ".class" extension removed.
- * @param path the fqn of the class
- * @param codeBase the File that must be a directory that may contain the class
- * @return the bytecodes for the class
- * @exception java.io.IOException if the class cannot be read
- */
+     * Returns an array of bytes containing the bytecodes for
+     * the class represented by the argument <b>path</b>.
+     * The <b>path</b> is a dot separated class name with
+     * the ".class" extension removed.
+     * @param path the fqn of the class
+     * @param codeBase the File that must be a directory that may contain the class
+     * @return the bytecodes for the class
+     * @exception java.io.IOException if the class cannot be read
+     */
     private byte[] getBytesFromDirectory(String path, java.io.File directory)
         throws java.io.IOException {
         java.io.File f = new java.io.File(directory,
@@ -288,12 +307,12 @@ public class ClassFileServer extends ClassServer {
     }
 
     /**
- * Returns an array of bytes containing the bytecodes for
- * the class represented by the InputStream
- * @param in the inputstream of the class file
- * @return the bytecodes for the class
- * @exception java.io.IOException if the class cannot be read
- */
+     * Returns an array of bytes containing the bytecodes for
+     * the class represented by the InputStream
+     * @param in the inputstream of the class file
+     * @return the bytecodes for the class
+     * @exception java.io.IOException if the class cannot be read
+     */
     private byte[] getBytesFromInputStream(java.io.InputStream in, int length)
         throws java.io.IOException {
         java.io.DataInputStream din = new java.io.DataInputStream(in);
