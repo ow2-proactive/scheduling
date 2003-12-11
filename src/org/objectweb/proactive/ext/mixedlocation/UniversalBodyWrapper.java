fb80604e67e7fd35cf21bfceffa16688b4a64529
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2003-12-11 15:17:06 +0000

    added accessor to component metaobject
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@772 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ext/mixedlocation/UniversalBodyWrapper.java
+++ src/org/objectweb/proactive/ext/mixedlocation/UniversalBodyWrapper.java
@@ -8,23 +8,21 @@
  */
 package org.objectweb.proactive.ext.mixedlocation;
 
-import java.io.IOException;
-
 import org.objectweb.proactive.core.UniqueID;
 import org.objectweb.proactive.core.body.UniversalBody;
 import org.objectweb.proactive.core.body.reply.Reply;
 import org.objectweb.proactive.core.body.request.Request;
+import org.objectweb.proactive.core.component.identity.ProActiveComponent;
+
+import java.io.IOException;
 
 
-public class UniversalBodyWrapper implements UniversalBody,
-                                             Runnable {
-
+public class UniversalBodyWrapper implements UniversalBody, Runnable {
     protected UniversalBody wrappedBody;
     protected long time;
     protected UniqueID id;
-
-	protected boolean stop;
-	protected long creationTime;
+    protected boolean stop;
+    protected long creationTime;
 
     /**
      * Create a time-limited wrapper around a UniversalBody
@@ -34,40 +32,38 @@ public class UniversalBodyWrapper implements UniversalBody,
     public UniversalBodyWrapper(UniversalBody body, long time) {
         this.wrappedBody = body;
         this.time = time;
-this.creationTime = System.currentTimeMillis();
+        this.creationTime = System.currentTimeMillis();
         Thread t = new Thread(this);
         this.id = this.wrappedBody.getID();
-        
-     //   t.start();
+
+        //   t.start();
     }
 
-    public void receiveRequest(Request request)
-                        throws IOException {
+    public void receiveRequest(Request request) throws IOException {
         //       System.out.println("UniversalBodyWrapper.receiveRequest");
         if (this.wrappedBody == null) {
             throw new IOException();
         }
+
         //the forwarder should be dead
         if (System.currentTimeMillis() > (this.creationTime + this.time)) {
-        	   this.updateServer();
-        this.wrappedBody = null;
-        System.gc();
-        	  throw new IOException();
-        	  
+            this.updateServer();
+            this.wrappedBody = null;
+            System.gc();
+            throw new IOException();
         } else {
-        
-        try {
-            this.wrappedBody.receiveRequest(request);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e;
+            try {
+                this.wrappedBody.receiveRequest(request);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw e;
+            }
         }
-        }
-  //      this.stop();
+
+        //      this.stop();
     }
 
-    public void receiveReply(Reply r)
-                      throws IOException {
+    public void receiveReply(Reply r) throws IOException {
         this.wrappedBody.receiveReply(r);
     }
 
@@ -80,24 +76,25 @@ this.creationTime = System.currentTimeMillis();
     }
 
     public void updateLocation(UniqueID id, UniversalBody body)
-                        throws IOException {
+        throws IOException {
         this.wrappedBody.updateLocation(id, body);
     }
 
     public UniversalBody getRemoteAdapter() {
         return this.wrappedBody.getRemoteAdapter();
     }
-    
+
     public void enableAC() throws java.io.IOException {
-    	this.wrappedBody.enableAC();
+        this.wrappedBody.enableAC();
     }
-    
+
     public void disableAC() throws java.io.IOException {
-    	this.wrappedBody.disableAC();
+        this.wrappedBody.disableAC();
     }
-    
-    public void setImmediateService(String methodName) throws java.io.IOException {
-    	this.wrappedBody.setImmediateService(methodName);
+
+    public void setImmediateService(String methodName)
+        throws java.io.IOException {
+        this.wrappedBody.setImmediateService(methodName);
     }
 
     protected void updateServer() {
@@ -111,31 +108,38 @@ this.creationTime = System.currentTimeMillis();
         //        }
     }
 
-//protected synchronized void stop() {
-// this.stop=true;	
-// this.notifyAll();
-//}
-//
-//protected synchronized void waitForStop(long time) {
-//	if (!this.stop) {
-//	 try {
-//		 wait(time);	
-//	} catch (InterruptedException e) {
-//		e.printStackTrace();
-//	}
-//	}
-//	
-//}
+    /**
+     * @see org.objectweb.proactive.core.body.UniversalBody#getProActiveComponentIdentity()
+     */
+    public ProActiveComponent getProActiveComponent() throws IOException {
+        // COMPONENTS
+        return this.wrappedBody.getProActiveComponent();
+    }
 
-
+    //protected synchronized void stop() {
+    // this.stop=true;	
+    // this.notifyAll();
+    //}
+    //
+    //protected synchronized void waitForStop(long time) {
+    //	if (!this.stop) {
+    //	 try {
+    //		 wait(time);	
+    //	} catch (InterruptedException e) {
+    //		e.printStackTrace();
+    //	}
+    //	}
+    //	
+    //}
     public void run() {
         //        System.out.println("UniversalBodyWrapper.run life expectancy " + time);
         try {
-           // Thread.currentThread().sleep(time);
-         //  this.waitForStop(time);
+            // Thread.currentThread().sleep(time);
+            //  this.waitForStop(time);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         //        System.out.println("UniversalBodyWrapper.run end of life...");
         this.updateServer();
         this.wrappedBody = null;
