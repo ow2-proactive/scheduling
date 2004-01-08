b1387a05ebc1715ea4b6fb65608db3562a4784fb
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-01-08 13:20:26 +0000

    jalopy pretty-printing
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@820 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/xml/TypesHandler.java
+++ src/org/objectweb/proactive/core/component/xml/TypesHandler.java
@@ -4,53 +4,53 @@
   */
 package org.objectweb.proactive.core.component.xml;
 
-import java.util.HashMap;
-
 import org.objectweb.proactive.core.xml.handler.CollectionUnmarshaller;
 import org.objectweb.proactive.core.xml.handler.UnmarshallerHandler;
 import org.objectweb.proactive.core.xml.io.Attributes;
+
 import org.xml.sax.SAXException;
+
+import java.util.HashMap;
+
 
 /**
  * @author Matthieu Morel
  */
 public class TypesHandler extends CollectionUnmarshaller {
-	
-	HashMap componentTypes;
+    HashMap componentTypes;
 
-	
+    public TypesHandler(HashMap componentTypes) {
+        this.componentTypes = componentTypes;
+        addHandler(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG,
+            new ComponentTypeHandler());
+    }
 
-		public TypesHandler(HashMap componentTypes) {
-			this.componentTypes =  componentTypes;
-			addHandler(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG, new ComponentTypeHandler());
-		}
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.AbstractUnmarshallerDecorator#notifyEndActiveHandler(java.lang.String, org.objectweb.proactive.core.xml.handler.UnmarshallerHandler)
+     */
+    protected void notifyEndActiveHandler(String name,
+        UnmarshallerHandler activeHandler) throws SAXException {
+        if (name.equals(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG)) {
+            // the result object is a table with 2 elements : {name of the type, ComponentType instance} 
+            Object[] component_type_info = (Object[]) getHandler(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG)
+                                                          .getResultObject();
+            componentTypes.put(component_type_info[0], component_type_info[1]);
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.AbstractUnmarshallerDecorator#notifyEndActiveHandler(java.lang.String, org.objectweb.proactive.core.xml.handler.UnmarshallerHandler)
-	 */
-	protected void notifyEndActiveHandler(String name, UnmarshallerHandler activeHandler) throws SAXException {
-		if (name.equals(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG)) {
-			// the result object is a table with 2 elements : {name of the type, ComponentType instance} 
-			Object[] component_type_info = (Object[])getHandler(ComponentsDescriptorConstants.COMPONENT_TYPE_TAG).getResultObject();
-			componentTypes.put(component_type_info[0], component_type_info[1]);
-		}
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#getResultObject()
+     */
+    public Object getResultObject() throws SAXException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
-
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#getResultObject()
-	 */
-	public Object getResultObject() throws SAXException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#startContextElement(java.lang.String, org.objectweb.proactive.core.xml.io.Attributes)
-	 */
-	public void startContextElement(String name, Attributes attributes) throws SAXException {
-		// TODO Auto-generated method stub
-
-	}
-	
-	}
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#startContextElement(java.lang.String, org.objectweb.proactive.core.xml.io.Attributes)
+     */
+    public void startContextElement(String name, Attributes attributes)
+        throws SAXException {
+        // TODO Auto-generated method stub
+    }
+}
