e136406550c22028caa58fa6166cfca6b441b26e
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-01-08 13:05:11 +0000

    bugfix : now properly handles sub-components
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@814 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/xml/CompositeComponentHandler.java
+++ src/org/objectweb/proactive/core/component/xml/CompositeComponentHandler.java
@@ -34,7 +34,7 @@ import java.util.List;
 /**
  * @author Matthieu Morel
  */
-public class CompositeComponentHandler extends ComponentHandler {
+public class CompositeComponentHandler extends AbstractContainerComponentHandler {
     public static Logger logger = Logger.getLogger(CompositeComponentHandler.class.getName());
     private List subComponents;
 
@@ -43,12 +43,11 @@ public class CompositeComponentHandler extends ComponentHandler {
      * @param componentsCache
      */
     public CompositeComponentHandler(ProActiveDescriptor deploymentDescriptor,
-        ComponentsCache componentsCache, HashMap componentTypes) {
-        super(deploymentDescriptor, componentsCache, componentTypes);
+        ComponentsCache componentsCache, HashMap componentTypes,
+        ComponentsHandler fatherHandler) {
+        super(deploymentDescriptor, componentsCache, componentTypes,
+            fatherHandler);
         componentParameters.setHierarchicalType(ComponentParameters.COMPOSITE);
-        addHandler(ComponentsDescriptorConstants.COMPONENTS_TAG,
-            new ComponentsHandler(deploymentDescriptor, componentsCache,
-                componentTypes));
         addHandler(ComponentsDescriptorConstants.BINDINGS_TAG,
             new BindingsHandler(componentsCache));
     }
@@ -58,57 +57,76 @@ public class CompositeComponentHandler extends ComponentHandler {
      */
     protected void notifyEndActiveHandler(String name,
         UnmarshallerHandler activeHandler) throws SAXException {
-        if (name.equals(ComponentsDescriptorConstants.COMPONENTS_TAG)) {
-            // then instantiate the component and add a stub on it to the cache
-            // TODO : cyclic case : instantiate as many components as there are effective nodes
-            // But may be a non sense to consider cyclic composites
-            VirtualNode vn = deploymentDescriptor.getVirtualNode(virtualNode);
-            try {
-                if (vn.getNodeCount() != 1) {
-                    throw new NodeException(
-                        "can only create a composite on a single node (currently)");
-                }
+        if (getContainerElementHierarchy().containsChild(activeHandler)) {
+            enable();
+        }
+        if (isEnabled()) {
+            Component composite;
+            if (name.equals(ComponentsDescriptorConstants.COMPONENTS_TAG)) {
+                // then instantiate the component and add a stub on it to the cache
+                try {
+                    if (virtualNode.equals(ComponentsDescriptorConstants.NULL)) {
+                        //					componentsCache.addComponent(componentParameters.getName(),
+                        //					//PrimitiveComponentB.class.getName(),
+                        composite = ProActive.newActiveComponent(Composite.class.getName(),
+                                new Object[] {  }, null, null, null,
+                                componentParameters);
+                        componentsCache.addComponent(componentParameters.getName(),
+                            composite);
+                        //PrimitiveComponentB.class.getName(),
+                    } else {
+                        VirtualNode vn = deploymentDescriptor.getVirtualNode(virtualNode);
+                        if (vn.getNodeCount() != 1) {
+                            throw new NodeException(
+                                "can only create a composite on a single node (currently)");
+                        }
 
-                // get corresponding node
-                Node targeted_node = vn.getNode();
-                Component composite = ProActive.newActiveComponent(Composite.class.getName(),
-                        new Object[] {  }, //targeted_node,
-                    null, null, null, componentParameters);
-                componentsCache.addComponent(componentParameters.getName(),
-                    composite);
+                        // get corresponding node
+                        Node targeted_node = vn.getNode();
+                        composite = ProActive.newActiveComponent(Composite.class.getName(),
+                                new Object[] {  }, targeted_node, null, null,
+                                componentParameters);
+                        componentsCache.addComponent(componentParameters.getName(),
+                            composite);
+                    }
 
-                // add sub components
-                List sub_components = (List) getHandler(name).getResultObject();
-                Iterator iterator = sub_components.iterator();
-                while (iterator.hasNext()) {
-                    String sub_component_name = (String) iterator.next();
-                    logger.debug("adding sub component : " +
-                        sub_component_name);
-                    ((ContentController) composite.getFcInterface(Constants.CONTENT_CONTROLLER)).addFcSubComponent(componentsCache.getComponent(
-                            sub_component_name));
+                    // add sub components
+                    List sub_components = (List) getHandler(name)
+                                                     .getResultObject();
+                    Iterator iterator = sub_components.iterator();
+                    while (iterator.hasNext()) {
+                        String sub_component_name = (String) iterator.next();
+                        if (logger.isDebugEnabled()) {
+                            logger.debug("adding sub component : " +
+                                sub_component_name);
+                        }
+                        ((ContentController) composite.getFcInterface(Constants.CONTENT_CONTROLLER)).addFcSubComponent(componentsCache.getComponent(
+                                sub_component_name));
+                    }
+                } catch (NodeException ne) {
+                    logger.error(
+                        "cannot create active component: node exception");
+                    ne.printStackTrace();
+                } catch (ActiveObjectCreationException aoce) {
+                    logger.error(
+                        "cannot create active component : active object creation exception");
+                    aoce.printStackTrace();
+                } catch (NoSuchInterfaceException nsie) {
+                    logger.error(
+                        "cannot create active component : interface not found");
+                    nsie.printStackTrace();
+                } catch (IllegalLifeCycleException ilce) {
+                    logger.error(
+                        "cannot create active component : illegal life cycle operation");
+                    ilce.printStackTrace();
+                } catch (IllegalContentException ice) {
+                    logger.error(
+                        "cannot create active component : illegal content operation");
+                    ice.printStackTrace();
                 }
-            } catch (NodeException ne) {
-                logger.error("cannot create active component: node exception");
-                ne.printStackTrace();
-            } catch (ActiveObjectCreationException aoce) {
-                logger.error(
-                    "cannot create active component : active object creation exception");
-                aoce.printStackTrace();
-            } catch (NoSuchInterfaceException nsie) {
-                logger.error(
-                    "cannot create active component : interface not found");
-                nsie.printStackTrace();
-            } catch (IllegalLifeCycleException ilce) {
-                logger.error(
-                    "cannot create active component : illegal life cycle operation");
-                ilce.printStackTrace();
-            } catch (IllegalContentException ice) {
-                logger.error(
-                    "cannot create active component : illegal content operation");
-                ice.printStackTrace();
+                logger.debug("created composite component : " +
+                    componentParameters.getName());
             }
-            logger.debug("created composite component : " +
-                componentParameters.getName());
         }
     }
 
@@ -116,7 +134,7 @@ public class CompositeComponentHandler extends ComponentHandler {
      * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#getResultObject()
      */
     public Object getResultObject() throws SAXException {
-        return componentParameters.getName();
+        return new ComponentResultObject(componentParameters.getName());
     }
 
     /* (non-Javadoc)
@@ -124,6 +142,8 @@ public class CompositeComponentHandler extends ComponentHandler {
      */
     public void startContextElement(String name, Attributes attributes)
         throws SAXException {
-        super.startContextElement(name, attributes);
+        if (isEnabled()) {
+            super.startContextElement(name, attributes);
+        }
     }
 }
