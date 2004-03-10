78994b5dc9abd7635aefcdef2cf180d736252e93
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-10 09:44:46 +0000

    Interface Job
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@945 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/Job.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 4 mars 2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package org.objectweb.proactive;
+
+/**
+ * @author ProActive Team
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public interface Job {
+	
+	public String getJobID ();
+
+}
