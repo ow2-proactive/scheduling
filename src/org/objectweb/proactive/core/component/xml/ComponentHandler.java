ac5939ac61158a189ac89b1f855fa4afba22138f
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-15 18:58:46 +0000

    now directly uses Fractal API
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@976 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/xml/ComponentHandler.java
+++ src/org/objectweb/proactive/core/component/xml/ComponentHandler.java
@@ -4,79 +4,102 @@
   */
 package org.objectweb.proactive.core.component.xml;
 
-import java.util.HashMap;
-
 import org.apache.log4j.Logger;
+
+import org.objectweb.fractal.api.Component;
+import org.objectweb.fractal.api.NoSuchInterfaceException;
+import org.objectweb.fractal.api.factory.GenericFactory;
+import org.objectweb.fractal.api.factory.InstantiationException;
 import org.objectweb.fractal.api.type.ComponentType;
 import org.objectweb.fractal.api.type.TypeFactory;
-import org.objectweb.proactive.core.component.ComponentParameters;
-import org.objectweb.proactive.core.component.type.ProActiveTypeFactory;
+import org.objectweb.fractal.util.Fractal;
+
+import org.objectweb.proactive.core.ProActiveRuntimeException;
+import org.objectweb.proactive.core.component.ControllerDescription;
 import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
 import org.objectweb.proactive.core.xml.handler.AbstractUnmarshallerDecorator;
 import org.objectweb.proactive.core.xml.handler.UnmarshallerHandler;
 import org.objectweb.proactive.core.xml.io.Attributes;
+
 import org.xml.sax.SAXException;
+
+import java.util.HashMap;
+
 
 /**
  * @author Matthieu Morel
  */
 public abstract class ComponentHandler extends AbstractUnmarshallerDecorator {
-	public static Logger logger = Logger.getLogger(ComponentHandler.class.getName());
+    public static Logger logger = Logger.getLogger(ComponentHandler.class.getName());
 
-	protected ComponentParameters componentParameters;
-	protected String virtualNode;
-	protected ProActiveDescriptor deploymentDescriptor;
-	protected ComponentsCache componentsCache;
-	protected HashMap componentTypes;
-	TypeFactory typeFactory = ProActiveTypeFactory.instance();
+    //protected ComponentParameters componentParameters;
+    protected ControllerDescription controllerDescription;
+    protected String virtualNode;
+    protected ProActiveDescriptor deploymentDescriptor;
+    protected ComponentsCache componentsCache;
+    protected HashMap componentTypes;
+    protected TypeFactory typeFactory;
+    protected GenericFactory cf;
+    protected ComponentType componentType = null;
 
-	/**
-	 * 
-	 */
-	public ComponentHandler(
-		ProActiveDescriptor deploymentDescriptor,
-		ComponentsCache componentsCache,
-		HashMap componentTypes) {
-		componentParameters = new ComponentParameters();
-		this.deploymentDescriptor = deploymentDescriptor;
-		this.componentsCache = componentsCache;
-		this.componentTypes = componentTypes;
-	}
+    /**
+     *
+     */
+    public ComponentHandler(ProActiveDescriptor deploymentDescriptor,
+        ComponentsCache componentsCache, HashMap componentTypes) {
+        controllerDescription = new ControllerDescription();
+        this.deploymentDescriptor = deploymentDescriptor;
+        this.componentsCache = componentsCache;
+        this.componentTypes = componentTypes;
+        try {
+            Component boot = Fractal.getBootstrapComponent();
+            typeFactory = Fractal.getTypeFactory(boot);
+            cf = Fractal.getGenericFactory(boot);
+            if (cf == null) {
+            	System.out.println("PB!!!!!!!!!!!!!!!11");
+            }
+        } catch (InstantiationException e1) {
+            throw new ProActiveRuntimeException("Cannot find Fractal boot component",
+                e1);
+        } catch (NoSuchInterfaceException e1) {
+            throw new ProActiveRuntimeException("Cannot find Fractal interface",
+                e1);
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.AbstractUnmarshallerDecorator#notifyEndActiveHandler(java.lang.String, org.objectweb.proactive.core.xml.handler.UnmarshallerHandler)
-	 */
-	protected void notifyEndActiveHandler(String name, UnmarshallerHandler activeHandler) throws SAXException {
-	}
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.AbstractUnmarshallerDecorator#notifyEndActiveHandler(java.lang.String, org.objectweb.proactive.core.xml.handler.UnmarshallerHandler)
+     */
+    protected void notifyEndActiveHandler(String name,
+        UnmarshallerHandler activeHandler) throws SAXException {
+    }
 
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#getResultObject()
-	 */
-	public Object getResultObject() throws SAXException {
-		return null;
-	}
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#getResultObject()
+     */
+    public Object getResultObject() throws SAXException {
+        return null;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#startContextElement(java.lang.String, org.objectweb.proactive.core.xml.io.Attributes)
-	 */
-	public void startContextElement(String name, Attributes attributes) throws SAXException {
-		logger.debug("start context element");
-
-		String component_name = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_NAME_TAG);
-		if (!checkNonEmpty(component_name)) {
-			throw new SAXException("component's name unspecified");
-		}
-		String virtual_node = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_VIRTUAL_NODE_TAG);
-		if (!checkNonEmpty(virtual_node)) {
-			throw new SAXException("component's virtual node unspecified");
-		}
-		String type_name = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_TYPE_ATTRIBUTE_TAG);
-		if (!checkNonEmpty(type_name)) {
-			throw new SAXException("name of component type unspecified");
-		}
-		componentParameters.setName(component_name);
-		virtualNode = virtual_node;
-		componentParameters.setComponentType((ComponentType)componentTypes.get(type_name));
-	}
-
+    /* (non-Javadoc)
+     * @see org.objectweb.proactive.core.xml.handler.UnmarshallerHandler#startContextElement(java.lang.String, org.objectweb.proactive.core.xml.io.Attributes)
+     */
+    public void startContextElement(String name, Attributes attributes)
+        throws SAXException {
+        String component_name = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_NAME_TAG);
+        if (!checkNonEmpty(component_name)) {
+            throw new SAXException("component's name unspecified");
+        }
+        String virtual_node = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_VIRTUAL_NODE_TAG);
+        if (!checkNonEmpty(virtual_node)) {
+            virtual_node = ComponentsDescriptorConstants.NULL;
+        }
+        String type_name = attributes.getValue(ComponentsDescriptorConstants.COMPONENT_TYPE_ATTRIBUTE_TAG);
+        if (!checkNonEmpty(type_name)) {
+            throw new SAXException("name of component type unspecified");
+        }
+        controllerDescription.setName(component_name);
+        virtualNode = virtual_node;
+        componentType = ((ComponentType) componentTypes.get(type_name));
+    }
 }
