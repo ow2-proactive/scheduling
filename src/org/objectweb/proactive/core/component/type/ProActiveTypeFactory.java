a2d512de062a2286d630f57d21276c82c268175c
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-15 18:56:47 +0000

    manages exceptions thrown upon creation of a type
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@974 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/type/ProActiveTypeFactory.java
+++ src/org/objectweb/proactive/core/component/type/ProActiveTypeFactory.java
@@ -3,6 +3,7 @@ package org.objectweb.proactive.core.component.type;
 
 import org.apache.log4j.Logger;
 
+import org.objectweb.fractal.api.factory.InstantiationException;
 import org.objectweb.fractal.api.type.ComponentType;
 import org.objectweb.fractal.api.type.InterfaceType;
 import org.objectweb.fractal.api.type.TypeFactory;
@@ -40,14 +41,14 @@ public class ProActiveTypeFactory implements TypeFactory {
      * @see org.objectweb.fractal.api.type.TypeFactory#createFcItfType(String, String, boolean, boolean, boolean)
      */
     public InterfaceType createFcItfType(String name, String signature, boolean isClient, boolean isOptional, 
-                                         boolean isCollection) {
+                                         boolean isCollection) throws InstantiationException {
         return new ProActiveInterfaceType(name, signature, isClient, isOptional, isCollection);
     }
 
     /**
      * @see org.objectweb.fractal.api.type.TypeFactory#createFcType(InterfaceType[])
      */
-    public ComponentType createFcType(InterfaceType[] interfaceTypes) {
+    public ComponentType createFcType(InterfaceType[] interfaceTypes) throws InstantiationException {
         return new ProActiveComponentType(interfaceTypes);
     }
 }
