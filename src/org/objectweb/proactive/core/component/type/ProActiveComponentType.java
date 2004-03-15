2ea91ac7f48b1264da7ebb8b43f26a6cc25c18b7
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-15 18:55:08 +0000

    added checks upon creation of a component type
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@973 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/type/ProActiveComponentType.java
+++ src/org/objectweb/proactive/core/component/type/ProActiveComponentType.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 
 import org.objectweb.fractal.api.NoSuchInterfaceException;
 import org.objectweb.fractal.api.Type;
+import org.objectweb.fractal.api.factory.InstantiationException;
 import org.objectweb.fractal.api.type.ComponentType;
 import org.objectweb.fractal.api.type.InterfaceType;
 
@@ -26,8 +27,30 @@ public class ProActiveComponentType implements ComponentType, Serializable {
 	/**
 	 * Constructor for ProActiveComponentType.
 	 */
-	public ProActiveComponentType(final InterfaceType[] interfaceTypes) {
-		this.interfaceTypes = interfaceTypes; //rem : julia uses a clone method		
+	public ProActiveComponentType(final InterfaceType[] interfaceTypes) throws InstantiationException{
+		this.interfaceTypes = clone(interfaceTypes); 
+		// verifications
+		for (int i = 0; i < interfaceTypes.length; ++i) {
+		  String p = interfaceTypes[i].getFcItfName();
+		  boolean collection = interfaceTypes[i].isFcCollectionItf();
+		  for (int j = i + 1; j < interfaceTypes.length; ++j) {
+			String q = interfaceTypes[j].getFcItfName();
+			if (p.equals(q)) {
+			  throw new InstantiationException(
+				 "Two interfaces have the same name '" + q + "'");
+			}
+			if (collection && q.startsWith(p)) {
+			  throw new InstantiationException(
+				"The name of the interface '" + q + "' starts with '" +
+				p + "', which is the name of a collection interface");
+			}
+			if (interfaceTypes[j].isFcCollectionItf() && p.startsWith(q)) {
+			  throw new InstantiationException(
+				"The name of the interface '" + p + "' starts with '" +
+				q + "', which is the name of a collection interface");
+			}
+		  }
+		}		
 	}
 	
 	/**
@@ -67,5 +90,25 @@ public class ProActiveComponentType implements ComponentType, Serializable {
 	 */
 	public boolean isFcSubTypeOf(Type type) {
 		throw new RuntimeException("not yet implemented");
+	}
+	
+	/**
+	 * Returns a copy of the given interface type array. This method is used to
+	 * return a copy of the field of this class, instead of the field itself, so
+	 * that its content can not be modified from outside.
+	 *
+	 * @param types the array to be cloned.
+	 * @return a clone of the given array, or an empty array if <tt>type</tt> is
+	 *      <tt>null</tt>.
+	 */
+
+	private static InterfaceType[] clone (final InterfaceType[] types) {
+	  if (types == null) {
+		return new InterfaceType[0];
+	  } else {
+		InterfaceType[] clone = new InterfaceType[types.length];
+		System.arraycopy(types, 0, clone, 0, types.length);
+		return clone;
+	  }
 	}
 }
