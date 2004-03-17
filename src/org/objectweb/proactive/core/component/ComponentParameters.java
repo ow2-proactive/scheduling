3adebafa276ce0915c65f29d27b098b224ad501b
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-17 16:16:23 +0000

    javadoc update + clone() method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1004 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/ComponentParameters.java
+++ src/org/objectweb/proactive/core/component/ComponentParameters.java
@@ -1,180 +1,166 @@
 package org.objectweb.proactive.core.component;
 
-import java.io.Serializable;
-import java.util.Vector;
-
 import org.apache.log4j.Logger;
+
 import org.objectweb.fractal.api.type.ComponentType;
 import org.objectweb.fractal.api.type.InterfaceType;
-import org.objectweb.proactive.core.ProActiveRuntimeException;
+
 import org.objectweb.proactive.core.component.type.ProActiveComponentType;
+
+import java.io.Serializable;
+
+import java.util.Vector;
+
 
 /** Contains the configuration of a component :
  * - type
- * - interfaces (server and client)
- * - name
- * - hierarchical type (primitive or composite)
+ * - interfaces (server and client) --> in contained ControllerDescription object
+ * - name --> in contained ControllerDescription object
+ * - hierarchical type (primitive or composite) --> in contained ControllerDescription object
+ * - a ref on the stub on the base object
  */
 public class ComponentParameters implements Serializable {
-	protected static Logger logger = Logger.getLogger(ComponentParameters.class.getName());
-	//private String name;
-	private Object stubOnReifiedObject;
-	private ComponentType componentType;
-	private ControllerDescription controllerDesc;
-	
-	/** Constructor for ComponentParameters.
-	 * @param name the name of the component
-	 * @param hierarchicalType the hierarchical type, either PRIMITIVE or COMPOSITE or PARALLEL
-	 * @param componentType
-	 */
-//	public ComponentParameters(String name, String hierarchicalType, ComponentType componentType) {
-//		this.name = name;
-//		this.hierarchicalType = hierarchicalType;
-//		this.componentType = componentType;
-//		//interfaceTypes = componentType.getFcInterfaceTypes();
-//	}
+    protected static Logger logger = Logger.getLogger(ComponentParameters.class.getName());
 
-	public ComponentParameters (String name, String hierarchicalType, ComponentType componentType) {
-		this(componentType, new ControllerDescription(name, hierarchicalType));
-	}
-	
-	public ComponentParameters(ComponentType componentType, ControllerDescription controllerDesc) {
-		this.componentType = componentType;
-		this.controllerDesc = controllerDesc;
-	}
+    private Object stubOnReifiedObject;
+    private ComponentType componentType;
+    private ControllerDescription controllerDesc;
 
-	public ComponentParameters() {
-		controllerDesc = new ControllerDescription();
-	}
-	
-	/**
-	 * copy constructor
-	 * @param componentParameters
-	 */
-	public ComponentParameters(final ComponentParameters componentParameters) {
-		this.componentType = new ProActiveComponentType(componentParameters.getComponentType());
-		this.controllerDesc = new ControllerDescription(componentParameters.getControllerDescription());
-	}
-	
+    /** Constructor for ComponentParameters.
+     * @param name the name of the component
+     * @param hierarchicalType the hierarchical type, either PRIMITIVE or COMPOSITE or PARALLEL
+     * @param componentType
+     */
+    public ComponentParameters(String name, String hierarchicalType,
+        ComponentType componentType) {
+        this(componentType, new ControllerDescription(name, hierarchicalType));
+    }
 
-	/**
-	 * setter for the name
-	 * @param name name of the component
-	 */
-	public void setName(String name) {
-		controllerDesc.setName(name);
-	}
+    /**
+     * Constructor
+     * @param componentType the type of the component
+     * @param controllerDesc a ControllerDescription object
+     */
+    public ComponentParameters(ComponentType componentType,
+        ControllerDescription controllerDesc) {
+        this.componentType = componentType;
+        this.controllerDesc = controllerDesc;
+    }
 
-	/**
-	 * Returns the componentType.
-	 * @return ComponentType
-	 */
-	public ComponentType getComponentType() {
-		return componentType;
-	}
-	
-	public ControllerDescription getControllerDescription() {
-		return controllerDesc;
-	}
-	
-	public void setComponentType(ComponentType componentType) {
-		this.componentType = componentType;
-	}
+    /**
+     * overrides the clone method of Object
+     * @return a clone of this current object
+     */
+    public Object clone() {
+        return new ComponentParameters(new ProActiveComponentType(componentType),
+            new ControllerDescription(controllerDesc));
+    }
 
-	/**
-	 * @param string
-	 */
-	public void setHierarchicalType(String string) {
-		controllerDesc.setHierarchicalType(string);
-	}
-	
-	public String getName() {
-		return controllerDesc.getName();
-	}
+    /**
+     * setter for the name
+     * @param name name of the component
+     */
+    public void setName(String name) {
+        controllerDesc.setName(name);
+    }
 
+    /**
+     * Returns the componentType.
+     * @return ComponentType
+     */
+    public ComponentType getComponentType() {
+        return componentType;
+    }
 
-/**
- * Returns the hierarchicalType.
- * @return String
- */
-public String getHierarchicalType() {
-	return controllerDesc.getHierarchicalType();
-}
+    /**
+     * getter
+     * @return a ControllerDescription object
+     */
+    public ControllerDescription getControllerDescription() {
+        return controllerDesc;
+    }
 
-	/**
-	 * @return the types of server interfaces
-	 */
-	public InterfaceType[] getServerInterfaceTypes() {
-		Vector server_interfaces = new Vector();
-		InterfaceType[] interfaceTypes = componentType.getFcInterfaceTypes();
-		for (int i = 0; i < interfaceTypes.length; i++) {
-			if (!interfaceTypes[i].isFcClientItf()) {
-				server_interfaces.add(interfaceTypes[i]);
-			}
-		}
-		return (InterfaceType[]) server_interfaces.toArray(new InterfaceType[server_interfaces.size()]);
-	}
+    /**
+     * setter
+     * @param componentType the type of the component
+     */
+    public void setComponentType(ComponentType componentType) {
+        this.componentType = componentType;
+    }
 
-	/**
-	 * @return the types of client interfacess
-	 */
-	public InterfaceType[] getClientInterfaceTypes() {
-		Vector client_interfaces = new Vector();
-		InterfaceType[] interfaceTypes = componentType.getFcInterfaceTypes();
-		for (int i = 0; i < interfaceTypes.length; i++) {
-			if (interfaceTypes[i].isFcClientItf()) {
-				client_interfaces.add(interfaceTypes[i]);
-			}
-		}
-		return (InterfaceType[]) client_interfaces.toArray(new InterfaceType[client_interfaces.size()]);
-	}
+    /**
+     * setter
+     * @param string the hierarchical type (primitive, composite or parallel)
+     */
+    public void setHierarchicalType(String string) {
+        controllerDesc.setHierarchicalType(string);
+    }
 
+    /**
+     * getter
+     * @return the name
+     */
+    public String getName() {
+        return controllerDesc.getName();
+    }
 
-	/**
-	 * accessor on the standard ProActive stub
-	 * @return standard ProActive stub on the reified object
-	 */
-	public Object getStubOnReifiedObject() {
-		return stubOnReifiedObject;
-	}
+    /**
+     * Returns the hierarchicalType.
+     * @return String
+     */
+    public String getHierarchicalType() {
+        return controllerDesc.getHierarchicalType();
+    }
 
-	/**
-	 * keeps a reference on the standard ProActive stub
-	 * @param ref on an instance of a standard ProActive stub on the reified object
-	 */
-	public void setStubOnReifiedObject(Object object) {
-		stubOnReifiedObject = object;
-	}
+    /**
+     * @return the types of server interfaces
+     */
+    public InterfaceType[] getServerInterfaceTypes() {
+        Vector server_interfaces = new Vector();
+        InterfaceType[] interfaceTypes = componentType.getFcInterfaceTypes();
+        for (int i = 0; i < interfaceTypes.length; i++) {
+            if (!interfaceTypes[i].isFcClientItf()) {
+                server_interfaces.add(interfaceTypes[i]);
+            }
+        }
+        return (InterfaceType[]) server_interfaces.toArray(new InterfaceType[server_interfaces.size()]);
+    }
 
-	/**
-	 * @return
-	 */
-	public InterfaceType[] getInterfaceTypes() {
-		return componentType.getFcInterfaceTypes();
-	}
+    /**
+     * @return the types of client interfacess
+     */
+    public InterfaceType[] getClientInterfaceTypes() {
+        Vector client_interfaces = new Vector();
+        InterfaceType[] interfaceTypes = componentType.getFcInterfaceTypes();
+        for (int i = 0; i < interfaceTypes.length; i++) {
+            if (interfaceTypes[i].isFcClientItf()) {
+                client_interfaces.add(interfaceTypes[i]);
+            }
+        }
+        return (InterfaceType[]) client_interfaces.toArray(new InterfaceType[client_interfaces.size()]);
+    }
 
-	/**
-	 * @param types
-	 */
-	public void setInterfaceTypes(InterfaceType[] types) {
-		throw new ProActiveRuntimeException("setInterfaceTypes should not be called anymore. Use ComponentType instead"); 
-		//interfaceTypes = types;
-	}
+    /**
+     * accessor on the standard ProActive stub
+     * @return standard ProActive stub on the reified object
+     */
+    public Object getStubOnReifiedObject() {
+        return stubOnReifiedObject;
+    }
 
-	public void addInterfaceType(InterfaceType type) {
-		InterfaceType[] initial_array = getInterfaceTypes();
-		InterfaceType[] interfaceTypes = componentType.getFcInterfaceTypes();
-		if (initial_array == null) {
-			interfaceTypes = new InterfaceType[] { type };
-		} else {
-			InterfaceType[] new_array = new InterfaceType[initial_array.length + 1];
-			System.arraycopy(initial_array, 0, new_array, 0, initial_array.length);
-			new_array[new_array.length-1] = type;
-			interfaceTypes = new_array;
-		}
-	}
-	
-	
+    /**
+     * keeps a reference on the standard ProActive stub
+     * @param ref on an instance of a standard ProActive stub on the reified object
+     */
+    public void setStubOnReifiedObject(Object object) {
+        stubOnReifiedObject = object;
+    }
 
-
+    /**
+     * getter
+     * @return a table of interface types
+     */
+    public InterfaceType[] getInterfaceTypes() {
+        return componentType.getFcInterfaceTypes();
+    }
 }
