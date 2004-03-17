30c5d41b24c7eccd713617c6b32a80387b936edb
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-17 12:35:02 +0000

    script improved
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@999 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/cluster/startRuntime.sh
+++ scripts/unix/cluster/startRuntime.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-HOSTS=`echo $LSB_HOSTS | /usr/bin/tr [:blank:] "\n" | /bin/sort -u -m`
-echo "Hosts = $HOSTS"
-echo $HOSTS > hostsList
+echo $LSB_HOSTS
+#HOSTS=`echo $LSB_HOSTS | /usr/bin/tr [:blank:] "\n" | /bin/sort -u -m`
+#echo "Hosts = $HOSTS"
+#echo $HOSTS > hostsList
 
 ARGS=""
 for i in $@
@@ -16,20 +16,20 @@ echo "command = $CMD"
 nodesList=""
 
 # node1
-for i in $HOSTS
+for i in $LSB_HOSTS
 do
   /usr/local/lsf/bin/lsgrun -p -m "$i" $CMD &
-  nodesList="${nodesList} //$i/${i}Node1"
+#  nodesList="${nodesList} //$i/${i}Node1"
 done
-echo $nodesList > nodesList
-echo local > localFileTag
-sleep 5
+#echo $nodesList > nodesList
+#echo local > localFileTag
+#sleep 5
 
 # node2
-for i in $HOSTS
-do
-  /usr/local/lsf/bin/lsgrun -p -m "$i" $CMD &
-done
+#for i in $HOSTS
+#do
+#  /usr/local/lsf/bin/lsgrun -p -m "$i" $CMD &
+#done
 
 # echo Copying files 
 #copyFiles.sh 
