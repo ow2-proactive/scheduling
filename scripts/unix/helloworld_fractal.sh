0b377b89f1a6b5dd9a978c610309000ea834a144
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-23 18:10:50 +0000

    script for the helloworld example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1034 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/unix/helloworld_fractal.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo
+echo --- Fractal Helloworld example ---------------------------------------------
+
+workingDir=`dirname $0`
+. $workingDir/env.sh
+
+if [ $# -lt 1 ]; then
+    echo "
+       usage : 
+         helloworld-fractal.sh <parameters>
+		
+		parameters are :
+			- parser
+			- wrapper
+			- distributed (needs parser)  
+ 
+    "
+    exit 1
+fi
+
+
+$JAVACMD org.objectweb.proactive.examples.components.helloworld.HelloWorld $1 $2 $3
+
+
+
+echo
+echo ---------------------------------------------------------
