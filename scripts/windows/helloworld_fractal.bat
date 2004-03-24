c21da6cd459cb6d7e997c0ad347759b1ba153ff4
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-03-24 09:13:26 +0000

    script for fractal helloworld
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1035 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/windows/helloworld_fractal.bat
@@ -0,0 +1,27 @@
+@echo off
+echo. 
+echo --- Fractal HelloWorld example ----------------------------------------
+if "%1" == "" goto usage
+
+goto doit
+
+:usage
+echo. 
+echo helloworld-fractal.sh <parameters>
+echo		
+echo		parameters are :
+echo			- parser
+echo			- wrapper
+echo			- distributed (needs parser)  echo. 
+goto doit
+
+
+:doit
+SETLOCAL
+call init.bat
+%JAVA_CMD%  org.objectweb.proactive.examples.components.helloworld.HelloWorld %1 %2 %3 
+ENDLOCAL
+
+:end
+echo. 
+echo ---------------------------------------------------------
