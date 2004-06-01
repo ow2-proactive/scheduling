b4fc4bff0dd8799c2c58f4739b8e2fae264145cf
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-06-01 14:44:10 +0000

    Random name of node
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1223 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/nonregressiontest/node/nodefactory/Test.java
+++ src/nonregressiontest/node/nodefactory/Test.java
@@ -1,33 +1,33 @@
 /*
-* ################################################################
-*
-* ProActive: The Java(TM) library for Parallel, Distributed,
-*            Concurrent computing with Security and Mobility
-*
-* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
-* Contact: proactive-support@inria.fr
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-* USA
-*
-*  Initial developer(s):               The ProActive Team
-*                        http://www.inria.fr/oasis/ProActive/contacts.html
-*  Contributor(s):
-*
-* ################################################################
-*/
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package nonregressiontest.node.nodefactory;
 
 import org.objectweb.proactive.core.node.Node;
@@ -36,20 +36,23 @@ import org.objectweb.proactive.core.util.UrlBuilder;
 
 import testsuite.test.FunctionalTest;
 
-
 /**
  * @author rquilici
- *
+ * 
  * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * Window>Preferences>Java>Templates. To enable and disable the creation of type
+ * comments go to Window>Preferences>Java>Code Generation.
  */
 public class Test extends FunctionalTest {
+
     Node rmiNode;
+
     Node jiniNode;
+
     private String rmiURL;
-    private String jiniURL = "jini://localhost/JININode" + System.currentTimeMillis();
+
+    private String jiniURL = "jini://localhost/JININode"
+            + System.currentTimeMillis();
 
     //Node ibisNode;
 
@@ -58,7 +61,7 @@ public class Test extends FunctionalTest {
      */
     public Test() {
         super("NodeFactory",
-            "Test the creation of rmi, jini, ibis node whith the factory");
+                "Test the creation of rmi, jini, ibis node whith the factory");
     }
 
     /**
@@ -67,33 +70,38 @@ public class Test extends FunctionalTest {
     public void action() throws Exception {
         NodeFactory.createNode(rmiURL);
         NodeFactory.createNode(jiniURL);
-//        NodeFactory.createNode("ibis://localhost/IBISNode");
+        //        NodeFactory.createNode("ibis://localhost/IBISNode");
     }
 
     /**
      * @see testsuite.test.AbstractTest#initTest()
      */
     public void initTest() throws Exception {
-    	String port = System.getProperty("proactive.rmi.port");
-    	if (port != null) rmiURL = UrlBuilder.buildUrl("localhost","RMINode", "rmi:", new Integer(port).intValue());
-    	else rmiURL = UrlBuilder.buildUrl("localhost","RMINode", "rmi:");
+        String port = System.getProperty("proactive.rmi.port");
+        if (port != null)
+            rmiURL = UrlBuilder.buildUrl("localhost", "RMINode"
+                    + System.currentTimeMillis(), "rmi:", new Integer(port)
+                    .intValue());
+        else
+            rmiURL = UrlBuilder.buildUrl("localhost", "RMINode"
+                    + System.currentTimeMillis(), "rmi:");
     }
 
     /**
      * @see testsuite.test.AbstractTest#endTest()
      */
     public void endTest() throws Exception {
-    	NodeFactory.killNode(rmiURL);
-		NodeFactory.killNode(jiniURL);
+        NodeFactory.killNode(rmiURL);
+        NodeFactory.killNode(jiniURL);
     }
 
     public boolean postConditions() throws Exception {
         rmiNode = NodeFactory.getNode(rmiURL);
         jiniNode = NodeFactory.getNode(jiniURL);
         //ibisNode = NodeFactory.getNode("ibis://localhost/IBISNode");
-        return ((rmiNode != null) && (jiniNode != null) &&
-        NodeFactory.isNodeLocal(rmiNode));
-//        return ((rmiNode != null) &&
-//        		NodeFactory.isNodeLocal(rmiNode));
+        return ((rmiNode != null) && (jiniNode != null) && NodeFactory
+                .isNodeLocal(rmiNode));
+        //        return ((rmiNode != null) &&
+        //        		NodeFactory.isNodeLocal(rmiNode));
     }
 }
