e1473a8e09f31aea257a57e4650e2ec80ad7b5b5
gchazara <gchazara@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-06-18 14:54:50 +0000

    Cleanup now that we have Icons.java
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1257 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ic2d/gui/jobmonitor/JobMonitorTreeCellRenderer.java
+++ src/org/objectweb/proactive/ic2d/gui/jobmonitor/JobMonitorTreeCellRenderer.java
@@ -2,56 +2,14 @@ package org.objectweb.proactive.ic2d.gui.jobmonitor;
 
 import java.awt.Component;
 
-import javax.swing.*;
+import javax.swing.Icon;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 import org.objectweb.proactive.ic2d.gui.jobmonitor.data.DataTreeNode;
 
 public class JobMonitorTreeCellRenderer extends DefaultTreeCellRenderer implements JobMonitorConstants
 {
-	private static final String IMAGES_DIRECTORY	= "images/";
-	
-	private static final String NODE_ICON_GIF 		= "node_icon.gif";
-	private static final String VN_ICON_GIF 			= "vn_icon.gif";
-	private static final String JVM_ICON_GIF 			= "jvm_icon.gif";
-	private static final String AO_ICON_GIF 			= "ao_icon.gif";
-	private static final String JOB_ICON_GIF 			= "job_icon.gif";
-	private static final String HOST_ICON_GIF 		= "host_icon.gif";
-	
-	private static final String NODE_ICON 				= IMAGES_DIRECTORY + NODE_ICON_GIF;
-	private static final String VN_ICON 					= IMAGES_DIRECTORY + VN_ICON_GIF;
-	private static final String JVM_ICON 					= IMAGES_DIRECTORY + JVM_ICON_GIF;
-	private static final String AO_ICON 					= IMAGES_DIRECTORY + AO_ICON_GIF;
-	private static final String JOB_ICON 					= IMAGES_DIRECTORY + JOB_ICON_GIF;
-	private static final String HOST_ICON 				= IMAGES_DIRECTORY + HOST_ICON_GIF;
-	
-	private static Icon job, host, jvm, vn, node, ao;
-	
-	public JobMonitorTreeCellRenderer ()
-	{
-		if (job == null)
-		{
-			host = createImageIcon (HOST_ICON);
-			job = createImageIcon (JOB_ICON);
-			ao = createImageIcon (AO_ICON);
-			jvm = createImageIcon (JVM_ICON);
-			vn = createImageIcon (VN_ICON);
-			node = createImageIcon (NODE_ICON);
-		}
-	}
-	
-	 public ImageIcon createImageIcon (String path)
-	 {
-       java.net.URL imgURL = JobMonitorPanel.class.getResource (path);
-       if (imgURL != null)
-           return new ImageIcon (imgURL);
-       else
-       {
-//           System.err.println ("Couldn't find file: " + path);
-           return null;
-       }
-   }
-	 
 	public Component getTreeCellRendererComponent (JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row,boolean hasFocus)
 	{
 		super.getTreeCellRendererComponent (tree, value, sel, expanded, leaf, row, hasFocus);
