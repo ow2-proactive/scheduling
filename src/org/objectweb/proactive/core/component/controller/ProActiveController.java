cbde7b75acfc244590d30d60e68fa3b37f2a75b0
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-07-15 15:29:38 +0000

    1. changed the instantiation mechanism 2. added utility methods for hierarchical type
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1384 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/controller/ProActiveController.java
+++ src/org/objectweb/proactive/core/component/controller/ProActiveController.java
@@ -1,33 +1,33 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive-support@inria.fr
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.core.component.controller;
 
 import org.objectweb.fractal.api.Component;
@@ -36,13 +36,10 @@ import org.objectweb.fractal.api.NoSuchInterfaceException;
 import org.objectweb.fractal.api.Type;
 import org.objectweb.fractal.api.control.IllegalLifeCycleException;
 import org.objectweb.fractal.api.control.LifeCycleController;
-import org.objectweb.fractal.api.factory.InstantiationException;
 import org.objectweb.fractal.api.type.InterfaceType;
-import org.objectweb.fractal.api.type.TypeFactory;
 
 import org.objectweb.proactive.core.ProActiveRuntimeException;
 import org.objectweb.proactive.core.component.Constants;
-import org.objectweb.proactive.core.component.type.ProActiveTypeFactory;
 
 import java.io.Serializable;
 
@@ -57,7 +54,6 @@ public abstract class ProActiveController implements Interface, Serializable {
     private Component owner;
     private boolean isInternal = true;
     private InterfaceType interfaceType;
-    private String controllerName;
 
     /**
      * Constructor for ProActiveController.
@@ -65,39 +61,8 @@ public abstract class ProActiveController implements Interface, Serializable {
      * @param controllerName the name of the controller (the list of controller names
      * is in the {@link Constants} class.
      */
-    public ProActiveController(Component owner, String controllerName) {
+    public ProActiveController(Component owner) {
         this.owner = owner;
-        //this.interfaceType = interfaceType;
-        this.controllerName = controllerName;
-        try {
-            if (controllerName.equals(Constants.CONTENT_CONTROLLER)) {
-                interfaceType = ProActiveTypeFactory.instance().createFcItfType(Constants.CONTENT_CONTROLLER,
-                        ProActiveContentController.class.getName(),
-                        TypeFactory.SERVER, TypeFactory.MANDATORY,
-                        TypeFactory.SINGLE);
-            } else if (controllerName.equals(Constants.BINDING_CONTROLLER)) {
-                interfaceType = ProActiveTypeFactory.instance().createFcItfType(Constants.BINDING_CONTROLLER,
-                        ProActiveBindingController.class.getName(),
-                        TypeFactory.SERVER, TypeFactory.MANDATORY,
-                        TypeFactory.SINGLE);
-            } else if (controllerName.equals(Constants.LIFECYCLE_CONTROLLER)) {
-                interfaceType = ProActiveTypeFactory.instance().createFcItfType(Constants.LIFECYCLE_CONTROLLER,
-                        ProActiveLifeCycleController.class.getName(),
-                        TypeFactory.SERVER, TypeFactory.MANDATORY,
-                        TypeFactory.SINGLE);
-            } else if (controllerName.equals(
-                        Constants.COMPONENT_PARAMETERS_CONTROLLER)) {
-                interfaceType = ProActiveTypeFactory.instance().createFcItfType(Constants.COMPONENT_PARAMETERS_CONTROLLER,
-                        ProActiveComponentParametersController.class.getName(),
-                        TypeFactory.SERVER, TypeFactory.MANDATORY,
-                        TypeFactory.SINGLE);
-            } else {
-                throw new ProActiveRuntimeException("unknown controller");
-            }
-        } catch (InstantiationException e) {
-            throw new ProActiveRuntimeException("cannot create component or interface type",
-                e);
-        }
     }
 
     /**
@@ -118,7 +83,7 @@ public abstract class ProActiveController implements Interface, Serializable {
      * see {@link org.objectweb.fractal.api.Interface#getFcItfName()}
      */
     public String getFcItfName() {
-        return controllerName;
+        return interfaceType.getFcItfName();
     }
 
     /**
@@ -142,6 +107,35 @@ public abstract class ProActiveController implements Interface, Serializable {
             throw new ProActiveRuntimeException(
                 "life cycle controller interface not found");
         }
+    }
+
+    // component parameters controller has to to be instantiated prior to this call
+    protected String getHierarchicalType() {
+        try {
+            ComponentParametersController controller = (ComponentParametersController) getFcItfOwner()
+                                                                                           .getFcInterface(Constants.COMPONENT_PARAMETERS_CONTROLLER);
+            return controller.getComponentParameters().getHierarchicalType();
+            //return (((ComponentParametersController))getFcItfOwner().getFcInterface(Constants.COMPONENT_PARAMETERS_CONTROLLER)).getComponentParameters().getHierarchicalType();
+        } catch (NoSuchInterfaceException e) {
+            throw new ProActiveRuntimeException(
+                "There is no component parameters controller for this component");
+        }
+    }
+
+    protected boolean isPrimitive() {
+        return Constants.PRIMITIVE.equals(getHierarchicalType());
+    }
+
+    protected boolean isComposite() {
+        return Constants.COMPOSITE.equals(getHierarchicalType());
+    }
+
+    protected boolean isParallel() {
+        return Constants.PARALLEL.equals(getHierarchicalType());
+    }
+
+    protected void setItfType(InterfaceType itfType) {
+        this.interfaceType = itfType;
     }
 
     //	// -------------------------------------------------------------------------
