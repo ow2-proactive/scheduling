4c915c6de38fefcfdf467bd59a5d8cca336ca891
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-07-19 13:05:02 +0000

    provides assertion methods
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1417 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/testsuite/test/Assertions.java
@@ -0,0 +1,109 @@
+/*
+ * Created on Jul 19, 2004
+ * author : Matthieu Morel
+  */
+package testsuite.test;
+
+import testsuite.exception.AssertionFailedException;
+
+/**
+ * This class provides assertion methods which upon failure throw an {@link testsuite.exception.AssertionFailedException} 
+ * leading to the exit of the current test.
+ * @author Matthieu Morel
+ */
+public class Assertions {
+
+	/**
+	 * Asserts a boolean condition.
+	 * If it is false, a runtime exception is thrown (AssertionFailedException) and
+	 * the current test is terminated
+	 * @param condition the boolean condition
+	 */
+	static public void assertTrue(boolean condition) {
+		assertTrue(null, condition);
+	}
+
+	/**
+	 * Asserts a boolean condition.
+	 * If it is false, a runtime exception is thrown with the given message (AssertionFailedException) and
+	 * the current test is terminated
+	 * @param failureMessage failure explanation
+	 * @param condition the boolean condition
+	 */
+	static public void assertTrue(String failureMessage, boolean condition) {
+		if (!condition) {
+			failureNotification(failureMessage);
+		}
+	}
+
+	/**
+	 * Asserts a boolean condition
+	 * If it is true, a runtime exception is thrown (AssertionFailedException) and
+	 * the current test is terminated
+	 * @param failureMessage failure explanation
+	 * @param condition the boolean condition
+	 */
+	static public void assertFalse(String failureMessage, boolean condition) {
+		if (condition) {
+			failureNotification(failureMessage);
+		}
+	}
+
+	/**
+	 * Asserts a boolean condition
+	 * If it is true, a runtime exception is thrown (AssertionFailedException) and
+	 * the current test is terminated
+	 * @param condition the boolean condition
+	 */
+	static public void assertFalse(boolean condition) {
+		assertFalse(null, condition);
+	}
+
+	/**
+	 * Asserts the equality of 2 elements. <br>
+	 * The elements  can be null
+	 * If the elements are different , a runtime exception is thrown (AssertionFailedException) and
+	 * the current test is terminated
+	* 
+	 * @param o1 first object
+	 *  @param o2 second object
+	 */
+	static public void assertEquals(Object o1, Object o2) {
+		assertEquals(null, o1, o2);
+	}
+	
+	/**
+	 * Asserts the equality of 2 elements. <br>
+	 * The elements  can be null
+	 * If the elements are different , a runtime exception is thrown (AssertionFailedException) and
+	 * the current test is terminated
+	*  @param failureMessage failure explanation
+	 * @param o1 first object
+	 *  @param o2 second object
+	 */
+	static public void assertEquals(String failureMessage, Object o1, Object o2) {
+		if ((o1 == null) || (o2 == null)) {
+			if (o1 != o2) {
+				throw new AssertionFailedException(failureMessage);
+			} else {
+				return;
+			}
+		}
+		if (!o1.equals(o2)) {
+			throw new AssertionFailedException(failureMessage);
+		}
+	}
+
+	static private void failureNotification() {
+		failureNotification(null);
+	}
+
+	static private void failureNotification(String message) {
+		if (message == null) {
+			throw new AssertionFailedException();
+		} else {
+			throw new AssertionFailedException(message);
+		}
+	}
+
+}
