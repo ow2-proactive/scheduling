7b8a606594373c94444b34512d85339ef8a69471
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-07-19 13:06:08 +0000

    corresponds to a failed assertion
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1419 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/testsuite/exception/AssertionFailedException.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Jul 16, 2004
+ * author : Matthieu Morel
+  */
+package testsuite.exception;
+
+/**
+ * @author Matthieu Morel
+ */
+public class AssertionFailedException extends RuntimeException {
+	
+	public AssertionFailedException(){
+		super();
+	}
+	
+	public AssertionFailedException(String message) {
+		super(message);
+		
+	}
+
+}
