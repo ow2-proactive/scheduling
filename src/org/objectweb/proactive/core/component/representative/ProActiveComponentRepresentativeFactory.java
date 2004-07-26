0d4b8dddd6c2c10b818ea616fb17bcaf20be83d4
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-07-26 15:35:56 +0000

    method signature update
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1445 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/representative/ProActiveComponentRepresentativeFactory.java
+++ src/org/objectweb/proactive/core/component/representative/ProActiveComponentRepresentativeFactory.java
@@ -1,58 +1,61 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive-support@inria.fr
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.core.component.representative;
 
 import org.apache.log4j.Logger;
 
-import org.objectweb.proactive.core.component.ComponentParameters;
+import org.objectweb.fractal.api.type.ComponentType;
+
 import org.objectweb.proactive.core.mop.Proxy;
 
+
 public class ProActiveComponentRepresentativeFactory {
-	protected static Logger logger = Logger.getLogger(ProActiveComponentRepresentativeFactory.class.getName());
-	private static ProActiveComponentRepresentativeFactory INSTANCE = null;
+    protected static Logger logger = Logger.getLogger(ProActiveComponentRepresentativeFactory.class.getName());
+    private static ProActiveComponentRepresentativeFactory INSTANCE = null;
 
-	private ProActiveComponentRepresentativeFactory() {
-	}
+    private ProActiveComponentRepresentativeFactory() {
+    }
 
-	public static ProActiveComponentRepresentativeFactory instance() {
-		if (INSTANCE == null) {
-			return (INSTANCE = new ProActiveComponentRepresentativeFactory());
-		} else {
-			return INSTANCE;
-		}
-	}
+    public static ProActiveComponentRepresentativeFactory instance() {
+        if (INSTANCE == null) {
+            return (INSTANCE = new ProActiveComponentRepresentativeFactory());
+        } else {
+            return INSTANCE;
+        }
+    }
 
-	public ProActiveComponentRepresentative createComponentRepresentative(
-		ComponentParameters componentParameters,
-		Proxy proxy) {
-		return new ProActiveComponentRepresentativeImpl(componentParameters, proxy);
-	}
+    public ProActiveComponentRepresentative createComponentRepresentative(
+        ComponentType componentType, Proxy proxy) {
+        ProActiveComponentRepresentative representative = new ProActiveComponentRepresentativeImpl(componentType);
+        representative.setProxy(proxy);
+        return representative;
+    }
 }
