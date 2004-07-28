575ea99eef6a6314017e46d1fecdb9c3384e674a
mlacage <mlacage@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-07-28 12:57:47 +0000

    improve comment wording to explain why we do this rather than what we do.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1457 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/mop/MOPClassLoader.java
+++ src/org/objectweb/proactive/core/mop/MOPClassLoader.java
@@ -219,15 +219,9 @@ public class MOPClassLoader extends URLClassLoader {
                         "Any other setting will result in the use of ASM, the default bytecode manipulator framework");
                 }
 
-                // System.out.println ("Classfile created with length "+data.length);
-                // Now, try to define the class
-                // We use the method defineClass, as redefined in class SecureClassLoader,
-                // so that we can specify a SourceCode object
-                //                    Class c = this.defineClass(name, data, 0, data.length, this.getClass().getProtectionDomain().getCodeSource());
-                //   this.getParent().findClass("toto");
-                //		    Class c = this.getParent().defineClass(name, data, 0, data.length, this.getClass().getProtectionDomain());
-                // The following code invokes defineClass on the parent classloader by Reflection
-                //	System.out.println("XXXXXXXXXXXXXXX");
+                // We use introspection to invoke the defineClass method to avoid the normal 
+                // class Access checking. This method is supposed to be protected which means 
+                // we should not be accessing it but the access policy file allows us to access it freely.
                 try {
                     Class clc = Class.forName("java.lang.ClassLoader");
                     Class[] argumentTypes = new Class[5];
