13e97b8a84fb7ddfedd1c1b395f325bf3693969b
gchazara <gchazara@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-08-05 13:01:15 +0000

    Update to new command line and fix typos
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1508 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- dev/scripts/p2pServiceOASIS.sh
+++ dev/scripts/p2pServiceOASIS.sh
@@ -3,11 +3,11 @@
 for i in $*
 do
     echo "Start Daemon on $i"
-    rsh $i /net/home/adicosta/ProActiveSecure/ProActive/scripts/unix/startP2PService.sh rmi 2410 /net/home/adicosta/ProActiveSecure/ProActive/ServerList3 &
+    rsh $i /net/home/adicosta/ProActiveSecure/ProActive/scripts/unix/startP2PService.sh rmi 2410 -f /net/home/adicosta/ProActiveSecure/ProActive/ServerList3 &
     if [ $? = 0 ]
     then
-	echo "rsh $i succefully"
+	echo "rsh $i successfully"
     else
-	echo "Could't do rsh in $i"
+	echo "Couldn't do rsh in $i"
     fi
 done
