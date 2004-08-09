593ac74f27bae689b1527a3484b782a579875fb1
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-08-09 11:12:20 +0000

    default rmi port property added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1547 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/config/ProActiveConfiguration.java
+++ src/org/objectweb/proactive/core/config/ProActiveConfiguration.java
@@ -127,33 +127,32 @@ public class ProActiveConfiguration {
         loadDefaultProperties();
     }
 
-//    /**
-//     * Dump loaded properties, i.e properties found in the configuration file
-//     */
-//    public void dumpLoadedProperties() {
-//        Iterator it = loadedProperties.keySet().iterator();
-//        while (it.hasNext()) {
-//            String name = (String) it.next();
-//
-//            //            System.out.println("Name = " + name);
-//            //            System.out.println("Value = " + this.loadedProperties.get(name));
-//        }
-//    }
-//
-//    /**
-//     * Dump properties added to the system, i.e loaded properties
-//     * which were not already in the system
-//     */
-//    public void dumpAddedProperties() {
-//        Iterator it = addedProperties.keySet().iterator();
-//        while (it.hasNext()) {
-//            String name = (String) it.next();
-//
-//            //            System.out.println("Name = " + name);
-//            //            System.out.println("Value = " + this.addedProperties.get(name));
-//        }
-//    }
-
+    //    /**
+    //     * Dump loaded properties, i.e properties found in the configuration file
+    //     */
+    //    public void dumpLoadedProperties() {
+    //        Iterator it = loadedProperties.keySet().iterator();
+    //        while (it.hasNext()) {
+    //            String name = (String) it.next();
+    //
+    //            //            System.out.println("Name = " + name);
+    //            //            System.out.println("Value = " + this.loadedProperties.get(name));
+    //        }
+    //    }
+    //
+    //    /**
+    //     * Dump properties added to the system, i.e loaded properties
+    //     * which were not already in the system
+    //     */
+    //    public void dumpAddedProperties() {
+    //        Iterator it = addedProperties.keySet().iterator();
+    //        while (it.hasNext()) {
+    //            String name = (String) it.next();
+    //
+    //            //            System.out.println("Name = " + name);
+    //            //            System.out.println("Value = " + this.addedProperties.get(name));
+    //        }
+    //    }
     public static String getLocationServerClass() {
         return System.getProperty("proactive.locationserver");
     }
@@ -199,6 +198,9 @@ public class ProActiveConfiguration {
         setProperty("schema.validation", "disable");
         if (System.getProperty("log4j.configuration") == null) {
             loadLogger();
+        }
+        if (System.getProperty("proactive.communication.protocol").equals("rmi")) {
+            setProperty("proactive.rmi.port", "1099");
         }
     }
 
