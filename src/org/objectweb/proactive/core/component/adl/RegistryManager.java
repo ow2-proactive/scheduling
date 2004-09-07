522ac26678e60f68347ff0d24ad9c5ccc5d39cc3
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-09-07 14:57:13 +0000

    added copyright + clear method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1681 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/adl/RegistryManager.java
+++ src/org/objectweb/proactive/core/component/adl/RegistryManager.java
@@ -1,18 +1,66 @@
 /*
- * Created on Apr 21, 2004
- * author : Matthieu Morel
-  */
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.core.component.adl;
 
 import org.objectweb.fractal.adl.ADLException;
 import org.objectweb.fractal.api.Component;
 
+
 /**
+ * This interface defines facilities for using a shared static registry storing component
+ * instances according to their name.
+ *
  * @author Matthieu Morel
  */
 public interface RegistryManager {
 
-	public void addComponent(Component component) throws ADLException;
-	
-	public Component getComponent(String name);
+    /**
+     * Adds a component instance.
+     * (The name is retreived automatically from the NameController)
+     *
+     * @param component the instance of the component
+     * @throws ADLException
+     */
+    public void addComponent(Component component) throws ADLException;
+
+    /**
+     * Retreives an instance of a  component according to the name (from its NameController controller)
+     * @param name the name of the instance of the component
+     * @return the selected component
+     */
+    public Component getComponent(String name);
+
+    /**
+     * Empties the registry
+     *
+     */
+    public void clear();
 }
