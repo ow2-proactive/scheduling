2e65d900b812d75b85d9c9475bbfca227e11db03
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-09-07 15:12:33 +0000

    added copyright + some work moved here from the builder
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1686 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/adl/implementations/ProActiveImplementationCompiler.java
+++ src/org/objectweb/proactive/core/component/adl/implementations/ProActiveImplementationCompiler.java
@@ -1,15 +1,38 @@
 /*
- * Created on Apr 20, 2004
- * author : Matthieu Morel
-  */
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.core.component.adl.implementations;
-
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 import org.objectweb.deployment.scheduling.component.api.FactoryProviderTask;
 import org.objectweb.deployment.scheduling.component.lib.AbstractInstanceProviderTask;
+
 import org.objectweb.fractal.adl.ADLException;
 import org.objectweb.fractal.adl.Definition;
 import org.objectweb.fractal.adl.Node;
@@ -18,121 +41,176 @@ import org.objectweb.fractal.adl.components.Component;
 import org.objectweb.fractal.adl.components.ComponentContainer;
 import org.objectweb.fractal.adl.implementations.ControllerContainer;
 import org.objectweb.fractal.adl.implementations.Implementation;
+import org.objectweb.fractal.adl.implementations.ImplementationCompiler;
 import org.objectweb.fractal.adl.implementations.ImplementationContainer;
-import org.objectweb.fractal.adl.implementations.TemplateControllerContainer;
+import org.objectweb.fractal.adl.nodes.VirtualNodeContainer;
+
+import org.objectweb.proactive.core.ProActiveRuntimeException;
 import org.objectweb.proactive.core.component.Constants;
 import org.objectweb.proactive.core.component.ContentDescription;
 import org.objectweb.proactive.core.component.ControllerDescription;
+import org.objectweb.proactive.core.component.adl.nodes.VirtualNode;
+import org.objectweb.proactive.core.component.type.Composite;
+import org.objectweb.proactive.core.component.type.ParallelComposite;
+
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * @author Matthieu Morel
  */
 public class ProActiveImplementationCompiler extends ImplementationCompiler {
+    private static int counter = 0;
 
-	public void compile(final List path, final ComponentContainer container, final TaskMap tasks, final Map context) throws ADLException {
-		if (!System.getProperty("fractal.provider").equals("org.objectweb.proactive.core.component.Fractive")) {
-			throw new ADLException(
-				"ProActiveImplementationCompiler is only usable with ProActive implementation of the fractal API",
-				(Node) container);
-		}
-		boolean template = context != null && "true".equals(context.get("template"));
+    public void compile(final List path, final ComponentContainer container,
+        final TaskMap tasks, final Map context) throws ADLException {
+        counter++;
+        String implementation = null;
+        if (container instanceof ImplementationContainer) {
+            ImplementationContainer ic = (ImplementationContainer) container;
+            Implementation i = ic.getImplementation();
+            if (i != null) {
+                implementation = i.getClassName();
+            }
+        }
 
-		String implementation = null;
-		if (container instanceof ImplementationContainer) {
-			ImplementationContainer ic = (ImplementationContainer) container;
-			Implementation i = ic.getImplementation();
-			if (i != null) {
-				implementation = i.getClassName();
-			}
-		}
+        String controller = null;
+        if (container instanceof ControllerContainer) {
+            ControllerContainer cc = (ControllerContainer) container;
+            if (cc.getController() != null) {
+                controller = cc.getController().getDescriptor();
+            }
+        }
+        if (controller == null) {
+            controller = "composite"; // that's the default
+        }
 
-		String controller = null;
-		if (container instanceof ControllerContainer) {
-			ControllerContainer cc = (ControllerContainer) container;
-			if (cc.getController() != null) {
-				controller = cc.getController().getDescriptor();
-			}
-		}
+        String name = null;
+        if (container instanceof Definition) {
+            name = ((Definition) container).getName();
+        } else if (container instanceof Component) {
+            name = ((Component) container).getName();
+        }
 
-		String templateController = null;
-		if (container instanceof TemplateControllerContainer) {
-			TemplateControllerContainer tcc = (TemplateControllerContainer) container;
-			if (tcc.getTemplateController() != null) {
-				templateController = tcc.getTemplateController().getDescriptor();
-				template = true;
-			}
-		}
+        String definition = null;
+        if (container instanceof Definition) {
+            definition = name;
+        } else {
+            definition = (String) ((Node) container).astGetDecoration(
+                    "definition");
+        }
 
-		String name = null;
-		if (container instanceof Definition) {
-			name = ((Definition) container).getName();
-		} else if (container instanceof Component) {
-			name = ((Component) container).getName();
-		}
+        Component[] comps = ((ComponentContainer) container).getComponents();
 
-		String definition = null;
-		if (container instanceof Definition) {
-			definition = name;
-		} else {
-			definition = (String) ((Node) container).astGetDecoration("definition");
-		}
+        VirtualNode n = null;
+        if (container instanceof VirtualNodeContainer) {
+            try {
+                n = (VirtualNode) ((VirtualNodeContainer) container).getVirtualNode();
+            } catch (ClassCastException e) {
+                throw new ProActiveRuntimeException(
+                    "DOCTYPE definition should be the following when using ProActive : \n" +
+                    "<!DOCTYPE definition PUBLIC \"-//objectweb.org//DTD Fractal ADL 2.0//EN\" \"classpath://org/objectweb/proactive/core/component/adl/xml/proactive.dtd\">");
+            }
+            if (n == null) {
+                // see Leclerq modification request : try to find a vn specified
+                // in a parent component
+                for (int i = path.size() - 1; i >= 0; --i) {
+                    if (path.get(i) instanceof VirtualNodeContainer) {
+                        try {
+                            n = (VirtualNode) ((VirtualNodeContainer) path.get(i)).getVirtualNode();
+                            if (n != null) {
+                                break;
+                            }
+                        } catch (ClassCastException e) {
+                            throw new ProActiveRuntimeException(
+                                "DOCTYPE definition should be the following when using ProActive : \n" +
+                                "<!DOCTYPE definition PUBLIC \"-//objectweb.org//DTD Fractal ADL 2.0//EN\" \"classpath://org/objectweb/proactive/core/component/adl/xml/proactive.dtd\">");
+                        }
+                    }
+                }
+            }
+        }
 
-		Component[] comps = ((ComponentContainer) container).getComponents();
+        AbstractInstanceProviderTask createTask;
 
-		try {
-			// the task may already exist, in case of a shared component
-			tasks.getTask("create", container);
-		} catch (NoSuchElementException e) {
-			AbstractInstanceProviderTask createTask;
-			if (comps.length > 0 || implementation == null) {
-				if (implementation != null) {
-					throw new ADLException("Implementation must be empty", (Node) container);
-				}
-				if (controller == null) {
-					controller = "composite";
-				}
-				if (template) {
-					if (templateController == null) {
-						templateController = "compositeTemplate";
-					}
-					createTask = newCreateTask(path, container, name, definition, templateController, new Object[] { controller, null }, context);
-				} else {
-					ControllerDescription controllerDesc = null;
-					if (controller == "composite") {
-						controllerDesc = new ControllerDescription(name, Constants.COMPOSITE);
-					}
-					if (controller == "parallel") {
-						controllerDesc = new ControllerDescription(name, Constants.PARALLEL);
-					}
-					//	PROACTIVE. This is a composite of a parallel component
-					createTask = newCreateTask(path, container, name, definition, controllerDesc, null, context);
-				}
-			} else {
-				if (controller == null) {
-					controller = "primitive";
-				}
-				if (template) {
-					if (templateController == null) {
-						templateController = "primitiveTemplate";
-					}
-					createTask = newCreateTask(path, container, name, definition, templateController, new Object[] { controller, implementation }, context);
-				} else {
-					// This is a primitive component
-					createTask =
-						newCreateTask(path, 
-							container,
-							name,
-							definition,
-							new ControllerDescription(name, Constants.PRIMITIVE),
-							new ContentDescription(implementation),
-							context);
-				}
-			}
+        ContentDescription contentDesc = null;
+        ControllerDescription controllerDesc = null;
 
-			FactoryProviderTask typeTask = (FactoryProviderTask) tasks.getTask("type", container);
-			createTask.setFactoryProviderTask(typeTask);
+        if (implementation == null) {
+            // a composite (or parallel) component 
+            if (controller.equals("composite")) {
+                controllerDesc = new ControllerDescription(name,
+                        Constants.COMPOSITE);
+                contentDesc = new ContentDescription(Composite.class.getName());
+            } else if (controller.equals("parallel")) {
+                controllerDesc = new ControllerDescription(name,
+                        Constants.PARALLEL);
+                contentDesc = new ContentDescription(ParallelComposite.class.getName());
+            } else {
+                controllerDesc = null;
+                throw new ADLException(" if no implementation is specified, a component should have the controller value set either to composite or to parallel",
+                    (Node) container);
+            }
+        } else {
+            // a primitive component
+            if (implementation instanceof String) {
+                // that seems to be the case with the fractaladl
+                contentDesc = new ContentDescription((String) implementation);
+                controllerDesc = new ControllerDescription(name,
+                        Constants.PRIMITIVE);
+            }
+        }
 
-			tasks.addTask("create", container, createTask);
-		}
-	}
+        createTask = new CreateTask((ProActiveImplementationBuilder) builder,
+                container, name, definition, controllerDesc, contentDesc, n,
+                context);
+        FactoryProviderTask typeTask = (FactoryProviderTask) tasks.getTask("type",
+                container);
+        createTask.setFactoryProviderTask(typeTask);
+
+        tasks.addTask("create", container, createTask);
+    }
+
+    static class CreateTask extends AbstractInstanceProviderTask {
+        ProActiveImplementationBuilder builder;
+        String name;
+        String definition;
+        ControllerDescription controllerDesc;
+        ContentDescription contentDesc;
+        VirtualNode vn;
+        Map context;
+        ComponentContainer container;
+
+        public CreateTask(final ProActiveImplementationBuilder builder,
+            final ComponentContainer container, final String name,
+            final String definition,
+            final ControllerDescription controllerDesc,
+            final ContentDescription contentDesc, final VirtualNode vn,
+            final Map context) {
+            this.builder = builder;
+            this.container = container;
+            this.name = name;
+            this.definition = definition;
+            this.controllerDesc = controllerDesc;
+            this.contentDesc = contentDesc;
+            this.vn = vn;
+            this.context = context;
+        }
+
+        public void execute(final Object context) throws Exception {
+            if (getInstance() != null) {
+                return;
+            }
+            Object type = getFactoryProviderTask().getFactory();
+            Object result = builder.createComponent(type, name, definition,
+                    controllerDesc, contentDesc, vn, (Map) context);
+            setInstance(result);
+        }
+
+        public String toString() {
+            return "T" + System.identityHashCode(this) + "[CreateTask(" + name +
+            "," + controllerDesc + "," + contentDesc + ")]";
+        }
+    }
 }
