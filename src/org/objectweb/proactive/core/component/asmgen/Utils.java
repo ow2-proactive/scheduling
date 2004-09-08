c5ba67861ef359fe8046b99c34ca9334e6bccc2e
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-09-08 08:05:14 +0000

    jalopy pretty printing
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1699 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/asmgen/Utils.java
+++ src/org/objectweb/proactive/core/component/asmgen/Utils.java
@@ -1,53 +1,50 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive-support@inria.fr
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.core.component.asmgen;
-
-
-import org.apache.log4j.Logger;
 
 
 /**
  * Utility class for bytecode generation operations.
- * 
+ *
  * @author Matthieu Morel
  *
  */
 public class Utils {
-    protected static Logger logger = Logger.getLogger(Utils.class.getName());
     public static final String GENERATED_DEFAULT_PREFIX = "Generated_";
     public static final String REPRESENTATIVE_DEFAULT_SUFFIX = "_representative";
     public static final String COMPOSITE_REPRESENTATIVE_SUFFIX = "_composite";
     public static final String STUB_DEFAULT_PACKAGE = null;
 
-    public static String convertClassNameToRepresentativeClassName(String classname) {
+    public static String convertClassNameToRepresentativeClassName(
+        String classname) {
         if (classname.length() == 0) {
             return classname;
         }
@@ -57,22 +54,22 @@ public class Utils {
             // no package
             return STUB_DEFAULT_PACKAGE + GENERATED_DEFAULT_PREFIX + classname;
         } else {
-            return STUB_DEFAULT_PACKAGE + classname.substring(0, n + 1) + GENERATED_DEFAULT_PREFIX + 
-                   classname.substring(n + 1);
+            return STUB_DEFAULT_PACKAGE + classname.substring(0, n + 1) +
+            GENERATED_DEFAULT_PREFIX + classname.substring(n + 1);
         }
     }
 
-    public static String getMetaObjectClassName(String functionalInterfaceName, String javaInterfaceName) {
+    public static String getMetaObjectClassName(
+        String functionalInterfaceName, String javaInterfaceName) {
         // just a way to have an identifier (possibly not unique ? ... but readable)
-        return (GENERATED_DEFAULT_PREFIX + javaInterfaceName.replace('.', '_') + "_" + 
-               functionalInterfaceName.replace('.', '/').replace('-', '_'));
+        return (GENERATED_DEFAULT_PREFIX + javaInterfaceName.replace('.', '_') +
+        "_" + functionalInterfaceName.replace('.', '/').replace('-', '_'));
     }
 
-    public static String getMetaObjectComponentRepresentativeClassName(String functionalInterfaceName, 
-                                                                                String javaInterfaceName) {
+    public static String getMetaObjectComponentRepresentativeClassName(
+        String functionalInterfaceName, String javaInterfaceName) {
         // just a way to have an identifier (possibly not unique ... but readable)
-        return (getMetaObjectClassName(functionalInterfaceName, javaInterfaceName) + REPRESENTATIVE_DEFAULT_SUFFIX);
+        return (getMetaObjectClassName(functionalInterfaceName,
+            javaInterfaceName) + REPRESENTATIVE_DEFAULT_SUFFIX);
     }
-
-
 }
