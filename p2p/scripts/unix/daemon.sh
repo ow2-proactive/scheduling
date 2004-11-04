b2842b5d2864486fbac3feff7a762b302ab59124
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-11-04 16:52:05 +0000

    scripts for the daemon
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1907 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ p2p/scripts/unix/daemon.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+workingDir=`dirname $0`
+
+cd $PWD/../../../scripts/unix
+. env.sh
+cd $workingDir
+
+# TODO: the same on win32
+# We do this to avoid the storm when everyody access the NFS.
+# Of course the NFS may be down just after the test ...
+if echo $@ | grep -q -- -delay ; then
+    TOTAL_DELAY=0
+    NB_DELAY=0
+    while ! head -c1 lib/ProActive.jar >/dev/null || ! [ -d ../logs ]; do
+	DELAY=$(($RANDOM % 600))
+	sleep $DELAY
+	TOTAL_DELAY=$(($TOTAL_DELAY + $DELAY))
+	NB_DELAY=$(($NB_DELAY + 1))
+    done
+
+    if [ $NB_DELAY -gt 0 ]; then
+	echo "Slept $NB_DELAY times => $TOTAL_DELAY seconds waiting for ProActive.jar availability" >&2
+    fi
+fi
+
+exec nice -19 java -cp $CLASSPATH                               \
+                   -Djava.security.manager                      \
+                   -Djava.security.policy=proactive.java.policy \
+                   -Dlog4j.configuration=proactive-log4j        \
+                   org.objectweb.proactive.p2p.core.daemon.Daemon "$@"
