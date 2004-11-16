7d54efbfa9e47e54aae68394f9b92fe10ce4e8e7
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-11-16 16:40:48 +0000

    reformatted
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1929 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/component/adl/FactoryFactory.java
+++ src/org/objectweb/proactive/core/component/adl/FactoryFactory.java
@@ -1,63 +1,61 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2004 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive-support@inria.fr
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.core.component.adl;
-
-import java.util.HashMap;
 
 import org.objectweb.fractal.adl.ADLException;
 import org.objectweb.fractal.adl.Factory;
 
+import java.util.HashMap;
+
+
 /**
- * ProActive factory customizing the Fractal ADL.
- *   
+ * A ProActive factory customizing the Fractal ADL.
+ *
  * @author Matthieu Morel
  */
 public class FactoryFactory {
-	
+    public final static String PROACTIVE_BACKEND = "org.objectweb.proactive.core.component.adl.ProActiveBackend";
+    public final static String PROACTIVE_FACTORY = "org.objectweb.proactive.core.component.adl.ProActiveFactory";
 
-		public final static String PROACTIVE_BACKEND =   "org.objectweb.proactive.core.component.adl.ProActiveBackend";
-		public final static String PROACTIVE_FACTORY = "org.objectweb.proactive.core.component.adl.ProActiveFactory";
+    private FactoryFactory() {
+    }
 
-		  private FactoryFactory()
-		  {
-		  }
-
-		  /**
-		   * Returns a factory for the ProActive ADL.
-		   * 
-		   * @see org.objectweb.fractal.adl.FactoryFactory#getFactory(java.lang.String,
-		   *      java.lang.String, java.util.Map)
-		   */
-		  public static Factory getFactory() throws ADLException
-		  {
-		    return org.objectweb.fractal.adl.FactoryFactory.getFactory(PROACTIVE_FACTORY, PROACTIVE_BACKEND, new HashMap());
-		  }
+    /**
+     * Returns a factory for the ProActive ADL.
+     *
+     * @see org.objectweb.fractal.adl.FactoryFactory#getFactory(java.lang.String,
+     *      java.lang.String, java.util.Map)
+     */
+    public static Factory getFactory() throws ADLException {
+        return org.objectweb.fractal.adl.FactoryFactory.getFactory(PROACTIVE_FACTORY,
+            PROACTIVE_BACKEND, new HashMap());
+    }
 }
