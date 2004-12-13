29bc2ec73142ba646d5b0266dddd9e3d2cfdaccb
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2004-12-13 10:34:36 +0000

    Named loggers centralization
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1948 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/util/Loggers.java
@@ -0,0 +1,15 @@
+package org.objectweb.proactive.core.util;
+
+/**
+ * Centralizes the names of the loggers.
+ * 
+ * @author Matthieu Morel
+ *
+ */
+public interface Loggers {
+
+    static final public String COMPONENTS="proactive.components";
+    static final public String CLASSLOADER="proactive.classloader"; 
+    
+    
+}
