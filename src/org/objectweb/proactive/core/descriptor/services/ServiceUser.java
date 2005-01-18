c7523aadf0c240df1875f58deb7021700ea261fc
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-01-18 14:28:34 +0000

    Interface added to set services
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1980 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/descriptor/services/ServiceUser.java
@@ -0,0 +1,58 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.core.descriptor.services;
+
+import org.objectweb.proactive.core.ProActiveException;
+
+
+/**
+ *
+ * An object implementing this interface, contains an UniversalService
+ * @author  ProActive Team
+ * @version 1.0,  2005/01/20
+ * @since   ProActive 2.0.4
+ * @see UniversalService
+ */
+public interface ServiceUser {
+
+    /**
+     * Sets the given service
+     * @param service the service to set
+     * @throws ProActiveException if the given service cannot be set on the user object
+     */
+    public void setService(UniversalService service) throws ProActiveException;
+    
+    /**
+     * Returns the real class of the service user
+     * @return the real class of the service user
+     */
+    public String getUserClass();
+}
