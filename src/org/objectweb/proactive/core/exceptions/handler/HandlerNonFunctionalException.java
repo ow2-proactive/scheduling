af078a7753ad7aff08e677e392870203fab6dd83
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-01-21 16:59:00 +0000

    This time, I definitly hide "NFE message solution".
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@1987 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/exceptions/handler/HandlerNonFunctionalException.java
+++ src/org/objectweb/proactive/core/exceptions/handler/HandlerNonFunctionalException.java
@@ -1,33 +1,33 @@
 /*
-* ################################################################
-*
-* ProActive: The Java(TM) library for Parallel, Distributed,
-*            Concurrent computing with Security and Mobility
-*
-* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
-* Contact: proactive-support@inria.fr
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-* USA
-*
-*  Initial developer(s):               The ProActive Team
-*                        http://www.inria.fr/oasis/ProActive/contacts.html
-*  Contributor(s):
-*
-* ################################################################
-*/
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.core.exceptions.handler;
 
 import org.apache.log4j.Logger;
@@ -69,8 +69,8 @@ public class HandlerNonFunctionalException implements Handler {
     }
 
     /**
-    * @return Suggestion to handle the problem more properly
-    */
+     * @return Suggestion to handle the problem more properly
+     */
     public String getSuggestion() {
         return suggestion;
     }
@@ -98,23 +98,25 @@ public class HandlerNonFunctionalException implements Handler {
         if (HandlerManager.isGraph()) {
             HandlerManager.handleWindow(nfe, this, info);
         } else {
-            loggerNFE.info("");
-            loggerNFE.info(
-                "******************************************************************************************************************************************");
-            loggerNFE.info("ERROR on NODE " + ((String) info));
-            loggerNFE.info("PROBLEM is " + nfe.getMessage());
-            loggerNFE.info("EXCEPTION [" + nfe.getDescription() +
-                "] HANDLED with [" + this.getClass().getName() + "]");
-            loggerNFE.info(suggestion);
-            loggerNFE.info("Solution 2: SET a new HANDLER for NFE [" +
-                nfe.getClass().getName() + "]");
-            loggerNFE.info(
-                "******************************************************************************************************************************************");
-            loggerNFE.info("");
-            //loggerNFE.error("NFE", nfe);
-            nfe.printStackTrace();
+            //            if (loggerNFE.isInfoEnabled()) {
+            //                loggerNFE.info("");
+            //                loggerNFE.info(
+            //                    "******************************************************************************************************************************************");
+            //                loggerNFE.info("ERROR on NODE " + ((String) info));
+            //                loggerNFE.info("PROBLEM is " + nfe.getMessage());
+            //                loggerNFE.info("EXCEPTION [" + nfe.getDescription() +
+            //                    "] HANDLED with [" + this.getClass().getName() + "]");
+            //                loggerNFE.info(suggestion);
+            //                loggerNFE.info("Solution 2: SET a new HANDLER for NFE [" +
+            //                    nfe.getClass().getName() + "]");
+            //                loggerNFE.info(
+            //                    "******************************************************************************************************************************************");
+            //                loggerNFE.info("");
+            //                //loggerNFE.error("NFE", nfe);
+            //                nfe.printStackTrace();
+            //            }
         }
-    } 
+    }
 
     /**
      * Provide a treatment for the handled exception(s)
@@ -125,19 +127,21 @@ public class HandlerNonFunctionalException implements Handler {
         if (HandlerManager.isGraph() && !HandlerManager.isQuiet()) {
             HandlerManager.handleWindow(nfe, this, info);
         } else {
-            loggerNFE.info("");
-            loggerNFE.info(
-                "******************************************************************************************************************************************");
-            loggerNFE.info("ERROR on NODE " + ((String) info));
-            loggerNFE.info("PROBLEM is " + nfe.getMessage());
-            loggerNFE.info("EXCEPTION [" + nfe.getDescription() +
-                "] HANDLED with [" + this.getClass().getName() + "]");
-            loggerNFE.info(suggestion);
-            loggerNFE.info("Solution 2: SET a new HANDLER for NFE [" +
-                nfe.getClass().getName() + "]");
-            loggerNFE.info(
-                "******************************************************************************************************************************************");
-            loggerNFE.info("");
+            //            if (loggerNFE.isInfoEnabled()) {
+            //                loggerNFE.info("");
+            //                loggerNFE.info(
+            //                    "******************************************************************************************************************************************");
+            //                loggerNFE.info("ERROR on NODE " + ((String) info));
+            //                loggerNFE.info("PROBLEM is " + nfe.getMessage());
+            //                loggerNFE.info("EXCEPTION [" + nfe.getDescription() +
+            //                    "] HANDLED with [" + this.getClass().getName() + "]");
+            //                loggerNFE.info(suggestion);
+            //                loggerNFE.info("Solution 2: SET a new HANDLER for NFE [" +
+            //                    nfe.getClass().getName() + "]");
+            //                loggerNFE.info(
+            //                    "******************************************************************************************************************************************");
+            //                loggerNFE.info("");
+            //            }
         }
         throw e;
     }
