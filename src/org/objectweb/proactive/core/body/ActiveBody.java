1a9a7b24cefad9f939c030f3e091857036125d21
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-02-03 09:39:45 +0000

    Fix: activity stopped after the endActive, not before.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2006 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/body/ActiveBody.java
+++ src/org/objectweb/proactive/core/body/ActiveBody.java
@@ -1,33 +1,33 @@
 /*
-* ################################################################
-*
-* ProActive: The Java(TM) library for Parallel, Distributed,
-*            Concurrent computing with Security and Mobility
-*
-* Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
-* Contact: proactive-support@inria.fr
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-* USA
-*
-*  Initial developer(s):               The ProActive Team
-*                        http://www.inria.fr/oasis/ProActive/contacts.html
-*  Contributor(s):
-*
-* ################################################################
-*/
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
 package org.objectweb.proactive.core.body;
 
 
@@ -45,6 +45,7 @@ package org.objectweb.proactive.core.body;
  *
  */
 import org.apache.log4j.Logger;
+
 import org.objectweb.proactive.Active;
 import org.objectweb.proactive.Body;
 import org.objectweb.proactive.EndActive;
@@ -151,20 +152,19 @@ public class ActiveBody extends MigratableBody implements Runnable,
                 }
             }
         } catch (Exception e) {
-            
             logger.error("Exception occured in runActivity method of body " +
-                    toString() + ". Now terminating the body");
-           
+                toString() + ". Now terminating the body");
+
             e.printStackTrace();
             terminate();
         } finally {
-            if (isActive()) {
-                activityStopped();
-            }
-
             // execute the end of activity if not after migration
             if ((!hasJustMigrated) && (endActive != null)) {
                 endActive.endActivity(this);
+            }
+
+            if (isActive()) {
+                activityStopped();
             }
         }
     }
