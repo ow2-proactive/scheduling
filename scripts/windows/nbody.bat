4a78e3d534901c48aaf1b0a2e319156b67b62651
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-04-06 17:35:10 +0000

    script corrected
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2257 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/nbody.bat
+++ scripts/windows/nbody.bat
@@ -1,17 +1,28 @@
 @echo off
 echo. 
 echo --- N-body with ProActive ---------------------------------
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
 IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\.
+
 SETLOCAL
-
 call %PROACTIVE%\scripts\windows\init.bat
+if "%1" == "-displayft" goto ft
+if "%1" == "" goto noft
 
-IF %1 == "displayft"
+:ft
 set XMLDESCRIPTOR=%PROACTIVE%\descriptors\FaultTolerantWorkers.xml
-else
+
+:noft
 set XMLDESCRIPTOR=%PROACTIVE%\descriptors\Workers.xml
 
-%JAVA_CMD% org.objectweb.proactive.examples.nbody.Start %XMLDESCRIPTOR% %1 %2 %3
+%JAVA_CMD% org.objectweb.proactive.examples.nbody.common.Start %XMLDESCRIPTOR% %1 %2 %3
 
 
 ENDLOCAL
