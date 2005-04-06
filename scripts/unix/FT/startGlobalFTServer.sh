c673bb9faba77e5a70d3a8aa45501fdaa285bb4d
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-04-06 17:56:31 +0000

    scritp changed
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2258 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/FT/startGlobalFTServer.sh
+++ scripts/unix/FT/startGlobalFTServer.sh
@@ -7,6 +7,6 @@ CLASSPATH=.
 fi
 . $PROACTIVE/scripts/unix/env.sh
 
-java -Djava.security.policy=proactive.java.policy -Xms64m -Xmx1024m org.objectweb.proactive.core.body.ft.util.StartFTServer $1 $2 $3 $4 $5 $6
+$JAVACMD -Xms64m -Xmx1024m org.objectweb.proactive.core.body.ft.util.StartFTServer $1 $2 $3 $4 $5 $6
 
 # DEBUG :   -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -Djava.compiler=NONE
--- scripts/windows/FT/startGlobalFTServer.bat
+++ scripts/windows/FT/startGlobalFTServer.bat
@@ -5,6 +5,6 @@ SETLOCAL
 call %PROACTIVE%\scripts\windows\init.bat
 
 
-java -Djava.security.policy=proactive.java.policy -Xms64m -Xmx1024m org.objectweb.proactive.core.body.ft.util.StartFTServer %1 %2 %3 %4 %5 %6
+%JAVA_CMD% -Xms64m -Xmx1024m org.objectweb.proactive.core.body.ft.util.StartFTServer %1 %2 %3 %4 %5 %6
 
-ENDLOCAL
+ENDLOCAL
