546ba756cb3dd968a3a545299c7c98afa39780b9
jbustos <jbustos@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-05-24 11:45:46 +0000

    P2P Load Balancer
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2356 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/p2p/loadbalancer/LoadBalancer.java
@@ -0,0 +1,196 @@
+/*
+ * ################################################################
+ * 
+ * ProActive: The Java(TM) library for Parallel, Distributed, Concurrent
+ * computing with Security and Mobility
+ * 
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis Contact:
+ * proactive-support@inria.fr
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Initial developer(s): The ProActive Team
+ * http://www.inria.fr/oasis/ProActive/contacts.html Contributor(s): Javier
+ * Bustos ################################################################
+ */
+
+package org.objectweb.proactive.p2p.loadbalancer;
+
+import org.apache.log4j.Logger;
+import org.objectweb.proactive.Body;
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.body.BodyMap;
+import org.objectweb.proactive.core.body.LocalBodyStore;
+import org.objectweb.proactive.core.body.migration.Migratable;
+import org.objectweb.proactive.core.body.migration.MigrationException;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.node.NodeFactory;
+import org.objectweb.proactive.core.runtime.RuntimeFactory;
+import org.objectweb.proactive.p2p.service.P2PService;
+import org.objectweb.proactive.p2p.service.node.P2PNode;
+
+public class LoadBalancer {
+
+	static Logger logger = Logger.getLogger(LoadBalancer.class.getName());
+
+	private P2PService p2pSer;
+
+	private int firstToMigrate;
+
+	private boolean underloaded;
+
+	private LoadMonitor lm;
+
+	private double rank;
+
+	public LoadBalancer(P2PService p2pSer) {
+//		System.err.println("[LoadBalancer] Monitor and Balancer started.");
+		this.p2pSer = p2pSer;
+		underloaded = false;
+		lm = new LoadMonitor(this);
+		lm.start();
+		rank = lm.getRanking();
+	}
+
+	public void register(long load) {
+		   	System.err.println("*** ["+p2pSer.getAddress()+"] reporting load = "+load+" ranking = "+rank);
+		if ((double) load > 90.0) {
+			if (underloaded) {
+				underloaded = false;
+			}
+			loadBalance();
+		} else if ((double) load >= 20.0 * rank) {
+			if (underloaded) {
+				underloaded = false;
+			}
+		} else if (!underloaded) {
+			underloaded = true;
+		}
+	}
+
+	/***************************************************************************
+	 * THIS IS THE METHOD CALLED BY THE MACHINE
+	 **************************************************************************/
+
+	public void loadBalance() {
+		System.err.println("*** [" + p2pSer.getAddress()
+				+ "] asking for Load Balance");
+		//    	P2PService neighbors[] = p2pSer.getAquaintances();
+		//    	if (neighbors == null) return;
+		//    	
+		//    	for (int i = 0; i < neighbors.length; i++) {
+		//    		if (neighbors[i].AreYouUnderloaded()) {
+		//    		// P2PNode p2pDestNode = neighbors[i].giveMeANode();
+		//		while (true) {
+		Object futureNode = p2pSer.getANode();
+		Node destNode = null;
+		ProActive.waitFor(futureNode);
+		if (futureNode != null) {
+			if (!ProActive.isException(futureNode)) {
+				P2PNode toto = (P2PNode) ProActive.getFutureValue(futureNode);
+				if (toto != null) {
+					destNode = toto.getNode();
+				} else {
+					System.err.println("alex null");
+					return;
+				}
+				if (destNode == null)
+					return;
+				destNode.getNodeInformation().setJobID("1");
+
+				try {
+					RuntimeFactory.getDefaultRuntime().addAcquaintance(
+							destNode.getProActiveRuntime().getURL());
+				} catch (ProActiveException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				P2PloadBalance(destNode);
+			} else {
+				((Exception) futureNode).printStackTrace();
+			}
+			//				break;
+			//			} else {
+			//				try {
+			//					Thread.sleep(1000);
+			//				} catch (InterruptedException e) {
+			//					// TODO Auto-generated catch block
+			//					e.printStackTrace();
+			//				}
+		}
+
+	}
+
+	//    		}
+	//    	
+	//    	}
+
+	public void P2PloadBalance(Node destNode) {
+		try {
+
+			logger.info("****************** entering P2PloadBalance to "
+					+ destNode.getNodeInformation().getURL());
+			BodyMap knownBodies = LocalBodyStore.getInstance().getLocalBodies();
+
+			if (knownBodies.size() < 1)
+				return;
+
+			int candidate = (int) (Math.random() * knownBodies.size());
+
+			java.util.Iterator bodiesIterator = knownBodies.bodiesIterator();
+
+			/** ******** Choosing the shortest service queue ******** */
+			int minLength = Integer.MAX_VALUE;
+			Body minBody = null;
+
+			/** ******** Choosing the shortest service queue ******** */
+			while (bodiesIterator.hasNext()) {
+				Body activeObjectBody = (Body) bodiesIterator.next();
+				Object testObject = activeObjectBody.getReifiedObject();
+				boolean testSerialization = testObject instanceof Migratable;
+
+				if (activeObjectBody.isAlive())
+					if (activeObjectBody.isActive() && testSerialization) {
+						int aoQueueLenght = activeObjectBody.getRequestQueue()
+								.size();
+						if (aoQueueLenght < minLength) {
+							minLength = aoQueueLenght;
+							minBody = activeObjectBody;
+						}
+					}
+			}
+
+			if (NodeFactory.isNodeLocal(destNode))
+				return;
+
+			if (minBody != null && minBody.isActive()) {
+				System.out.println("[Loadbalancer] Migrating from "
+						+ minBody.getNodeURL() + " to "
+						+ destNode.getNodeInformation().getURL());
+				ProActive.migrateTo(minBody, destNode, true);
+			}
+		} catch (IllegalArgumentException e) {
+			System.err.println("[ERR] Argument not valid");
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (MigrationException e) {
+			/** ****** if you cannot migrate, is not my business ********** */
+		}
+	}
+
+	public boolean AreYouUnderloaded() {
+		return underloaded;
+	}
+}
---
+++ src/org/objectweb/proactive/p2p/loadbalancer/LoadMonitor.java
@@ -0,0 +1,162 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *									Javier Bustos
+ * ################################################################
+ */
+
+package org.objectweb.proactive.p2p.loadbalancer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.Random;
+
+public class LoadMonitor extends Thread
+{
+
+    private double load,oldTotal,oldUsed;;
+    private LoadBalancer lb;
+    private Random r;
+    private int secLoad[];
+   private String name;
+    private double ranking;
+    private RandomAccessFile statfile;
+    
+    public synchronized double getRanking()
+    {
+    return ranking;	
+    }
+
+    public LoadMonitor(LoadBalancer lb)
+    {
+        this.lb = lb;
+        load = 0;
+        r = new Random();
+
+		BufferedReader br;
+		ranking = 1.0;
+		String line=null;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream("/proc/cpuinfo")));
+			statfile = new RandomAccessFile("/proc/stat","r");
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("bogomips")) {
+						String splited[] = line.split(":");
+						double bogomips = Double.parseDouble(splited[1]);   // obtaining the bogomips
+						double bmbase10 = Math.log(bogomips)/Math.log(10)-3;  // 3 is our zero reference
+						ranking = bmbase10+0.7;   // 1+atan(bmbase10)/1.3 => [0; oo)
+						}
+				}
+		statfile.seek(5);
+		line = statfile.readLine();
+
+		long user, nice, system, idle;
+        java.util.StringTokenizer st = new java.util.StringTokenizer(line," ");
+        user   = Long.parseLong(st.nextToken());
+        nice   = Long.parseLong(st.nextToken());
+        system = Long.parseLong(st.nextToken());
+        idle   = Long.parseLong(st.nextToken());
+		
+        oldTotal  = user + nice + system + idle;
+        oldUsed   = user + nice + system;
+        
+        load = 0;
+        calculateLoad();
+		} catch (FileNotFoundException e) {
+			System.err.println("[LoadMonitor] File not found Exception");
+		} catch (NumberFormatException e) {
+			System.err.println("[LoadMonitor] Number format Exception");
+		} catch (IOException e) {
+			System.err.println("[LoadMonitor] Input/Output Exception");
+		}
+		
+    }
+    public synchronized double  getLoad() {
+            long user, nice, system, idle;
+            double totalTime, realUsedTime;
+            String cpuLine=null;
+            try {
+				// skip "cpu"
+				statfile.seek(5);
+				cpuLine = statfile.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+            // read "cpu x x x x"           
+			
+            java.util.StringTokenizer st = new java.util.StringTokenizer(cpuLine," ");
+            user   = Long.parseLong(st.nextToken());
+            nice   = Long.parseLong(st.nextToken());
+            system = Long.parseLong(st.nextToken());
+            idle   = Long.parseLong(st.nextToken());
+
+            // compute load
+            totalTime  = user + nice + system + idle - oldTotal;
+            realUsedTime   = user + nice + system - oldUsed;
+            if (totalTime < 1 ) totalTime = realUsedTime;
+            double thisLoad = realUsedTime/(totalTime+1);
+            oldTotal = user + nice + system + idle;
+            oldUsed = user + nice + system;
+            return thisLoad;
+            
+    }
+    
+	private synchronized void calculateLoad() {
+		double newload = getLoad();
+		
+		newload = (0.7*newload + 0.3*load);
+		load = newload;
+	}
+
+    public void run()
+    {
+        int i = 0;
+        do
+        {
+        	calculateLoad();
+            lb.register(Math.round(load*100));
+            try
+            {
+            	double sl, slreal;
+            	slreal=0;
+            	if (load == 0) sl = 10000;
+            	else {
+            		sl  = 30000 + Math.abs(r.nextGaussian())*15000;
+            		slreal = sl/1000;
+            		sl = sl / Math.log(1+load*100);
+            	}
+                Thread.sleep(Math.round(sl));
+            }
+            catch(InterruptedException interruptedexception) { }
+        } while(true);
+    }
+
+}
