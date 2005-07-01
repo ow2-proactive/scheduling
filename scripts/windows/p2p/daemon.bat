f1b5c4d80d12cf910dafc51a5975592f86c0ce18
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-07-01 08:42:55 +0000

    path changed
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2401 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/p2p/daemon.bat
+++ scripts/windows/p2p/daemon.bat
@@ -1,6 +1,19 @@
-setlocal enabledelayedexpansion
-call ..\init.bat
+@echo off
+echo.
+echo --- StartDeamon -------------------------------------
 
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
+setlocal enabledelayedexpansion
+IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\..\.
+
+call %PROACTIVE%\scripts\windows\init.bat
 %JAVA_CMD% org.objectweb.proactive.p2p.daemon.Daemon %*
 exit %errorlevel%
 
--- scripts/windows/p2p/startP2PService.bat
+++ scripts/windows/p2p/startP2PService.bat
@@ -1,8 +1,20 @@
-SETLOCAL enabledelayedexpansion
-call ..\init.bat
-
+@echo off
 echo.
 echo --- StartP2PService -------------------------------------
+
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
+SETLOCAL enabledelayedexpansion
+IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\..\.
+
+call %PROACTIVE%\scripts\windows\init.bat
+
 
 %JAVA_CMD% org.objectweb.proactive.p2p.service.StartP2PService %*
 
