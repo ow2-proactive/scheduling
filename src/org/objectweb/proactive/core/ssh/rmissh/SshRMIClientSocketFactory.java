ed3d06c91d617ecf4d9c93eab20d8e7d9a54cf08
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-07-07 09:07:53 +0000

    hashCode and equals methods added
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2408 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/ssh/rmissh/SshRMIClientSocketFactory.java
+++ src/org/objectweb/proactive/core/ssh/rmissh/SshRMIClientSocketFactory.java
@@ -1,18 +1,33 @@
 package org.objectweb.proactive.core.ssh.rmissh;
 
-import java.net.Socket;
 import java.io.IOException;
+
+import java.net.Socket;
+
 import java.rmi.server.RMIClientSocketFactory;
+
 
 /**
  * @author mlacage
  */
-public class SshRMIClientSocketFactory implements RMIClientSocketFactory, java.io.Serializable {
-	
-	public SshRMIClientSocketFactory () {}
-	
-	public Socket createSocket(String host, int port) throws IOException {
-		Socket socket = new SshSocket (host, port);
-		return socket;
-	}
+public class SshRMIClientSocketFactory implements RMIClientSocketFactory,
+    java.io.Serializable {
+    public SshRMIClientSocketFactory() {
+    }
+
+    public Socket createSocket(String host, int port) throws IOException {
+        Socket socket = new SshSocket(host, port);
+        return socket;
+    }
+
+    public boolean equals(Object obj) {
+        // the equals method is class based, since all instances are functionally equivalent.
+        // We could if needed compare on an instance basic for instance with the host and port
+        // Same for hashCode
+        return this.getClass().equals(obj.getClass());
+    }
+
+    public int hashCode() {
+        return this.getClass().hashCode();
+    }
 }
--- src/org/objectweb/proactive/core/ssh/rmissh/SshRMIServerSocketFactory.java
+++ src/org/objectweb/proactive/core/ssh/rmissh/SshRMIServerSocketFactory.java
@@ -1,15 +1,29 @@
 package org.objectweb.proactive.core.ssh.rmissh;
 
 import java.io.IOException;
+
 import java.net.ServerSocket;
+
 import java.rmi.server.RMIServerSocketFactory;
 
-public class SshRMIServerSocketFactory implements RMIServerSocketFactory, java.io.Serializable {
-	
-	public SshRMIServerSocketFactory () {}
 
-	public ServerSocket createServerSocket(int port) throws IOException {
-		return new ServerSocket (port);
-	}
+public class SshRMIServerSocketFactory implements RMIServerSocketFactory,
+    java.io.Serializable {
+    public SshRMIServerSocketFactory() {
+    }
 
+    public ServerSocket createServerSocket(int port) throws IOException {
+        return new ServerSocket(port);
+    }
+
+    public boolean equals(Object obj) {
+        // the equals method is class based, since all instances are functionally equivalent.
+        // We could if needed compare on an instance basic for instance with the host and port
+        // Same for hashCode
+        return this.getClass().equals(obj.getClass());
+    }
+
+    public int hashCode() {
+        return this.getClass().hashCode();
+    }
 }
