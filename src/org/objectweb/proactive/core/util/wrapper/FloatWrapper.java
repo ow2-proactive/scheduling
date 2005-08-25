4cb3ca46e9da2aaeabd2fdfc324f6f2cc460ddf0
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-08-25 11:49:46 +0000

    Float wrapper added.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2529 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/util/wrapper/FloatWrapper.java
@@ -0,0 +1,80 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.core.util.wrapper;
+
+import java.io.Serializable;
+
+
+/**
+ * <p>An reifiable object for wrapping the primitive Java type <code>float</code>.</p>
+ * <p>Use this class as result for ProActive asynchronous method calls.</p>
+ *
+ * @author Alexandre di Costanzo
+ *
+ * Created on Jul 28, 2005
+ */
+public class FloatWrapper implements Serializable {
+
+    /**
+     * The primitive value.
+     */
+    private float value;
+
+    /**
+     * The no arguments constructor for ProActive.
+     */
+    public FloatWrapper() {
+        // nothing to do
+    }
+
+    /**
+     * Construct an reifiable object for a <code>float</code>.
+     * @param value the primitive <code>float</code> value.
+     */
+    public FloatWrapper(float value) {
+        this.value = value;
+    }
+
+    /**
+     * Return the value of the <code>float</code>.
+     * @return the primitive value.
+     */
+    public float floatValue() {
+        return this.value;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return this.value + "";
+    }
+}
