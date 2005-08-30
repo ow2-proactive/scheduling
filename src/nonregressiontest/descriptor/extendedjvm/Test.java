0c920a5f6898a9a2cc0d1936d6aa80852be0d344
rquilici <rquilici@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-08-30 12:15:33 +0000

    non regression test for extended jvm
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2580 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/nonregressiontest/descriptor/extendedjvm/Test.java
@@ -0,0 +1,147 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package nonregressiontest.descriptor.extendedjvm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
+import org.objectweb.proactive.core.descriptor.data.VirtualNode;
+
+import testsuite.test.FunctionalTest;
+
+
+
+public class Test extends FunctionalTest {
+    ProActiveDescriptor descriptor;
+    A a1,a2,a3;
+
+    public Test() {
+        super("Jvm extension in deployment descriptor",
+            "Jvm extension in deployment descriptor");
+    }
+
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    public void action() throws Exception {
+    	VirtualNode vn1 = descriptor.getVirtualNode("vn1");
+    	VirtualNode vn2 = descriptor.getVirtualNode("vn2");
+    	VirtualNode vn3 = descriptor.getVirtualNode("vn3");
+    	a1 = (A)ProActive.newActive(A.class.getName(), new Object[]{}, vn1.getNode());
+    	a2 = (A)ProActive.newActive(A.class.getName(), new Object[]{}, vn2.getNode());
+    	a3 = (A)ProActive.newActive(A.class.getName(), new Object[]{}, vn3.getNode());
+    	  
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#initTest()
+     */
+    public void initTest() throws Exception {
+        String oldFilePath = getClass()
+                                 .getResource("/nonregressiontest/descriptor/extendedjvm/JVMExtension.xml")
+                                 .getPath();
+        String newFilePath = oldFilePath.replaceFirst("JVMExtension.xml",
+                "JVMExtension-tmp.xml");
+
+        // if tests are run from the /compile directory : getParent for root directory 
+        File userDir = new File(System.getProperty("user.dir"));
+        String proactiveDir;
+        if (userDir.getName().equals("compile")) {
+            proactiveDir = userDir.getParent();
+        } else {
+            proactiveDir = userDir.getPath();
+        }
+        searchAndReplace(oldFilePath, newFilePath, "proactive.home",
+            proactiveDir);
+        descriptor = ProActive.getProactiveDescriptor(getClass()
+                                                          .getResource("/nonregressiontest/descriptor/extendedjvm/JVMExtension-tmp.xml")
+                                                          .getPath());
+        descriptor.activateMappings();
+    }
+
+    /**
+     * @see testsuite.test.AbstractTest#endTest()
+     */
+    public void endTest() throws Exception {
+        descriptor.killall(false);
+    }
+
+    public boolean postConditions() throws Exception {
+        if(  a2.getTiti()!= null || a2.getTata()== null || a3.getTiti()== null || a3.getToto() == null){
+        	return false;
+        }
+        if(a2.getClassPath().indexOf("ProActive.jar") < 0 || a2.getPolicy().indexOf("test")<0){
+        	return false;
+        }
+        return true;
+    }
+
+    private void searchAndReplace(String oldFilePath, String newFilePath,
+        String oldString, String newString) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(
+                        oldFilePath));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(
+                        newFilePath));
+            while (true) {
+                String oldLine = reader.readLine();
+                if (oldLine == null) {
+                    break;
+                }
+                String newLine = oldLine.replaceAll(oldString, newString);
+                writer.write(newLine);
+                writer.newLine();
+            }
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+		Test test = new Test();
+		try {
+			test.initTest();
+			test.action();
+			System.out.println(test.postConditions());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
