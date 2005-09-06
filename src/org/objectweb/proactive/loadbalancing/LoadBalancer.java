c1ea0530b75d45ff1e5e2425ba5d94bcfd7d35e4
jbustos <jbustos@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-09-06 21:01:04 +0000

    Adding work stealing capabilities to load balancer infrastructure.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2617 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/loadbalancing/LoadBalancer.java
+++ src/org/objectweb/proactive/loadbalancing/LoadBalancer.java
@@ -70,32 +70,42 @@ public class LoadBalancer {
      * @param <code>load</code> is the load value, using the load index from the load monitor.
      * @return none
      */
-    public void register(double load) {
-        myLoad = load;
-        if (load > LoadBalancingConstants.OVERLOADED_THREASHOLD) {
-            if (underloaded) {
-                underloaded = false;
-            }
-            startBalancing();
-        } else if (load >= (LoadBalancingConstants.UNDERLOADED_THREASHOLD * normalization)) {
-            if (underloaded) {
-                underloaded = false;
-            }
-        } else if (!underloaded) {
-            underloaded = true;
-        }
-    }
-
+	public void register(double load) {
+		myLoad = load;
+		if (load > LoadBalancingConstants.OVERLOADED_THREASHOLD) {
+			if (underloaded) {
+				underloaded = false;
+			}
+			startBalancing();
+		} else if (load >= LoadBalancingConstants.UNDERLOADED_THREASHOLD * normalization) {
+			if (underloaded) {
+				underloaded = false;
+			}
+		} else {
+			if (!underloaded) {
+				underloaded = true;
+				}
+			stealWork();
+		}
+	}
+	
     /**
      * This method has to be implemented for load balancing algorithms,
      * it starts the load balance process
-     * @param none
+     * @param none 
      * @return none
      */
-    public void startBalancing() {
-    }
-    ;
+	public void startBalancing() {};
 
+	/**
+	 * This method has to be implemented for load balancing algorithms,
+	 * it starts the work stealing process (underloaded processores which "steal"
+	 * work from others
+	 * @param none
+	 * @return none
+	 */
+	public void stealWork() {};
+	
     /**
      * This method sends an active object to a destiny, choosing the active objects
      * which implements <code>Balanceable</code> having the shortest queue.
@@ -103,68 +113,69 @@ public class LoadBalancer {
      * If this node is local, this method does nothing.
      * @return none
      */
-    public void sendActiveObjectsTo(Node destNode) {
-        if (NodeFactory.isNodeLocal(destNode)) {
-            return;
-        }
+	public void sendActiveObjectsTo(Node destNode) {
 
-        try {
-            BodyMap knownBodies = LocalBodyStore.getInstance().getLocalBodies();
+		if (NodeFactory.isNodeLocal(destNode))
+			return;
 
-            if (knownBodies.size() < 1) {
-                return;
-            }
+		try {
 
-            int candidate = (int) (Math.random() * knownBodies.size());
+			BodyMap knownBodies = LocalBodyStore.getInstance().getLocalBodies();
 
-            java.util.Iterator bodiesIterator = knownBodies.bodiesIterator();
+			if (knownBodies.size() < 1)
+				return;
 
-            /** ******** Choosing the shortest service queue ******** */
-            int minLength = Integer.MAX_VALUE;
-            Body minBody = null;
+			java.util.Iterator bodiesIterator = knownBodies.bodiesIterator();
 
-            while (bodiesIterator.hasNext()) {
-                Body activeObjectBody = (Body) bodiesIterator.next();
-                Object testObject = activeObjectBody.getReifiedObject();
+			/** ******** Choosing the shortest service queue ******** */
+			int minLength = Integer.MAX_VALUE;
+			Body minBody = null;
 
-                /********** Only some Active Objects can migrate *************/
-                boolean testSerialization = testObject instanceof Balanceable;
+			while (bodiesIterator.hasNext()) {
+				Body activeObjectBody = (Body) bodiesIterator.next();
+				Object testObject = activeObjectBody.getReifiedObject();
 
-                if (activeObjectBody.isAlive()) {
-                    if (activeObjectBody.isActive() && testSerialization) {
-                        int aoQueueLenght = activeObjectBody.getRequestQueue()
-                                                            .size();
-                        if (aoQueueLenght < minLength) {
-                            minLength = aoQueueLenght;
-                            minBody = activeObjectBody;
-                        }
-                    }
-                }
-            }
+			/********** Only some Active Objects can migrate *************/
+				
+				boolean testSerialization = testObject instanceof Balanceable;
 
-            /***********  we have the Active Object with shortest queue, so we send the migration call ********/
-            if ((minBody != null) && minBody.isActive()) {
-                logger.info("[Loadbalancer] Migrating from " +
-                    minBody.getNodeURL() + " to " +
-                    destNode.getNodeInformation().getURL());
-                ProActive.migrateTo(minBody, destNode, true);
-            }
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (MigrationException e) {
+				if (activeObjectBody.isAlive()) {
+					
+					if (activeObjectBody.isActive() && testSerialization) {
+						int aoQueueLenght = activeObjectBody.getRequestQueue()
+								.size();
+						if (aoQueueLenght < minLength) {
+							minLength = aoQueueLenght;
+							minBody = activeObjectBody;
+						}
+					}
+				}
+			}
 
-            /** ****** if you cannot migrate, is not my business ********** */
-        }
-    }
+			/***********  we have the Active Object with shortest queue, so we send the migration call ********/
+			
+			if (minBody != null && minBody.isActive()) {
+				logger.debug("[Loadbalancer] Migrating from "
+						+ minBody.getNodeURL() + " to "
+						+ destNode.getNodeInformation().getURL());
+				ProActive.migrateTo(minBody, destNode, true);
+			}
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (MigrationException e) {
+			/** ****** if you cannot migrate, is not my business ********** */
+		}
+	}
 
     /**
      * This method returns if this machine is in an underloaded state
-     * @param none
+     * @param none 
      * @return none
      */
-    public boolean AreYouUnderloaded() {
-        return underloaded;
-    }
+	public boolean AreYouUnderloaded() {
+		return underloaded;
+	}
+
 }
