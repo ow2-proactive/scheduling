4cac5ff26bc6e3dcef6fa008f5fa747d9d961a01
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-10-28 08:49:41 +0000

    Improvement of the file path separtor fix.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2867 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/ext/util/StubGenerator.java
+++ src/org/objectweb/proactive/ext/util/StubGenerator.java
@@ -25,12 +25,8 @@ public class StubGenerator {
         } else {
             tmp = name.substring(0, i);
         }
-        String tmp2 = null;
-        if (tmp.indexOf('/') != -1){
-        tmp2 = tmp.replace('/', '.');
-        } else {
-        	tmp2 = tmp.replace('\\', '.');
-        }
+        
+        String tmp2 = tmp.replace(File.separatorChar, '.');
 
         if (tmp2.indexOf('.') == 0) {
             return tmp2.substring(1);
