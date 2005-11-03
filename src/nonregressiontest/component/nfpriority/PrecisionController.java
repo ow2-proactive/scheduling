12174f446e91b46ed38a35855662acd49ea9a779
mmorel <mmorel@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-11-03 14:12:07 +0000

    removed tests for features under development
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@2918 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/nonregressiontest/component/nfpriority/PrecisionController.java
+++
@@ -1,9 +0,0 @@
-package nonregressiontest.component.nfpriority;
-
-public interface PrecisionController {
-    public static final String PRECISION_CONTROLLER_NAME = "precision-controller";
-
-    public void increasePrecision();
-
-    public void decreasePrecision();
-}
--- src/nonregressiontest/component/nfpriority/Sensor.java
+++
@@ -1,8 +0,0 @@
-package nonregressiontest.component.nfpriority;
-
-import java.math.BigDecimal;
-
-
-public interface Sensor {
-    public BigDecimal getValue();
-}
