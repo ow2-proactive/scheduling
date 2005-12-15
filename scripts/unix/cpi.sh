b2e93d7acaf7dbda42166670bdd85d4049f6f66d
smariani <smariani@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-12-15 11:41:26 +0000

    Script to run the mpi example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3069 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/unix/cpi.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo
+echo --- MPI deployment example ---------------------------------------------
+
+workingDir=`dirname $0`
+. $workingDir/env.sh
+PROACTIVE=$workingDir/../..
+XMLDESCRIPTOR=$PROACTIVE/descriptors/MPI-descriptor.xml
+$JAVACMD org.objectweb.proactive.examples.mpi.Cpi  $XMLDESCRIPTOR
+
+echo
+echo ------------------------------------------------------------
