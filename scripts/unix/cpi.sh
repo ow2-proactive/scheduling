5f37bc408daea6f405b3596e7eedd22c7773dc31
smariani <smariani@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2005-12-15 11:53:23 +0000

    add number of iteration to run
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3071 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/cpi.sh
+++ scripts/unix/cpi.sh
@@ -7,7 +7,7 @@ workingDir=`dirname $0`
 . $workingDir/env.sh
 PROACTIVE=$workingDir/../..
 XMLDESCRIPTOR=$PROACTIVE/descriptors/MPI-descriptor.xml
-$JAVACMD org.objectweb.proactive.examples.mpi.Cpi  $XMLDESCRIPTOR
+$JAVACMD org.objectweb.proactive.examples.mpi.Cpi 5  $XMLDESCRIPTOR
 
 echo
 echo ------------------------------------------------------------
