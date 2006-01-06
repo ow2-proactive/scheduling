b3ea34cd0df975f2794ccb3d72dfadaee41fd2f1
irosenbe <irosenbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-01-06 10:35:59 +0000

    Fixed broken image copy process for doc files.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3118 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- compile/generate_doc.sh
+++ compile/generate_doc.sh
@@ -9,17 +9,18 @@ html_dir=$docs_dir/html
 pdf_dir=$docs_dir/pdf
 xml_src_dir=$base/src/org/objectweb/proactive/doc-files
 
-rm -rf $html_dir $pdf_dir  
-mkdir -p $html_dir 
-mkdir -p $pdf_dir 
+rm -rf $html_dir/* $pdf_dir/*
+mkdir -p $html_dir
+mkdir -p $pdf_dir
 
-
-echo "    COPYING IMAGE FILES <comment : number of files copied should be more restrained, no?>" 
-cp -r $xml_src_dir $html_dir/
-cp $xml_src_dir/ProActive.css $html_dir/
+# This copy part is done very easily with ant, even with file selection
+echo "    COPYING IMAGE FILES "
+for f in components/ development/ eclipse_files/ guided_tour/ images/ osgi_files/ p2p_files/ PA_config.xsl ProActive.css ProActiveRefBook.pdf security_images/ test_documentation/ webservices/ ; do
+    cp -r $xml_src_dir/$f $html_dir/
+done
 
 echo "    GENERATING html pages in $html_dir"
-xmlto xhtml -o $html_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml 
+xmlto xhtml -o $html_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml
 
-echo "    GENERATING pdf in $pdf_dir"
-xmlto pdf -o $pdf_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml   --searchpath $(pwd)/$base/src/org/objectweb/proactive/
+#echo "    GENERATING pdf in $pdf_dir"
+#xmlto pdf -o $pdf_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml   --searchpath $(pwd)/$base/src/org/objectweb/proactive/
