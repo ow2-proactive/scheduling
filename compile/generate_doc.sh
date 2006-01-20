40637f843c1a58908039814abf222690355dfd8e
irosenbe <irosenbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-01-20 15:52:09 +0000

    This is the start of the howto, to help people get involved in writing doc following the oficial guidelines. DocBookExample.xml should become a repository of "best practices"
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3148 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- compile/generate_doc.sh
+++ compile/generate_doc.sh
@@ -7,20 +7,29 @@ base=..
 docs_dir=$base/docs
 html_dir=$docs_dir/html
 pdf_dir=$docs_dir/pdf
+howto_dir=$docs_dir/howto
 xml_src_dir=$base/src/org/objectweb/proactive/doc-files
 
-rm -rf $html_dir/* $pdf_dir/*
-mkdir -p $html_dir
-mkdir -p $pdf_dir
+rm -rf $html_dir/* $pdf_dir/* $howto_dir/*
+mkdir -p $html_dir $pdf_dir $howto_dir
 
 # This copy part is done very easily with ant, even with file selection
+# This copy part is needed for the html referencing, which is not done automatically
 echo "    COPYING IMAGE FILES "
-for f in components/ development/ eclipse_files/ guided_tour/ images/ osgi_files/ p2p_files/ mpi_files/ PA_config.xsl ProActive.css ProActiveRefBook.pdf security_images/ test_documentation/ webservices/ ; do
-    cp -r $xml_src_dir/$f $html_dir/
+for f in components/ development/ eclipse_files/ guided_tour/ images/ osgi_files/ p2p_files/ PA_config.xsl ProActive.css ProActiveRefBook.pdf security_images/ test_documentation/ webservices/ ; do
+   cp -r $xml_src_dir/$f $html_dir/
 done
 
 echo "    GENERATING html pages in $html_dir"
 xmlto xhtml -o $html_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml
 
 #echo "    GENERATING pdf in $pdf_dir"
+# searchpath indicates where to find the images
 #xmlto pdf -o $pdf_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/PA_index.xml   --searchpath $(pwd)/$base/src/org/objectweb/proactive/
+echo "    IF YOU WANT TO GENERATE THE pdf (real slow)), UNCOMMENT ABOVE LINE"
+
+echo "    GENERATING howto in $howto_dir"
+# just a hack to copy the image needed in the figures section
+mkdir $howto_dir/images  && cp $xml_src_dir/images/e1.png $howto_dir/images/
+xmlto xhtml -o $howto_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/doc_howto.xml
+xmlto pdf -o $howto_dir -m $xml_src_dir/PA_config.xsl $xml_src_dir/doc_howto.xml   --searchpath $(pwd)/$base/src/org/objectweb/proactive/
