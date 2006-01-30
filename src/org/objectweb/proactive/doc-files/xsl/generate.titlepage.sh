ae30cfceb2ff4d827bc8a6b39542586c95c58ef5
irosenbe <irosenbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-01-30 12:16:53 +0000

    oups, had hard linked a file - this is a template to generate titlepages
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3193 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/doc-files/xsl/generate.titlepage.sh
+++ src/org/objectweb/proactive/doc-files/xsl/generate.titlepage.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-ORIGINAL=/usr/local/xxe-std-30p1/addon/config/docbook/xsl
 xsltproc      \
    -output  html.titlepage.xsl  \
-    $ORIGINAL/template/titlepage.xsl \
+    titlepage.templates.xsl \
     html.titlepage.templates.xml
 xsltproc      \
    -output  pdf.titlepage.xsl  \
-    $ORIGINAL/template/titlepage.xsl \
+    titlepage.templates.xsl \
     pdf.titlepage.templates.xml
