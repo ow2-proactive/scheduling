16b42bde74ec4d9deaba24ecf7ea988e9f10bf7f
smariani <smariani@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-02-01 17:16:14 +0000

    Script for example
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3208 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/cpi.sh
+++ scripts/unix/cpi.sh
@@ -2,12 +2,16 @@
 
 echo
 echo --- MPI deployment example ---------------------------------------------
-lamboot
+
 workingDir=`dirname $0`
 . $workingDir/env.sh
 PROACTIVE=$workingDir/../..
-XMLDESCRIPTOR=$PROACTIVE/descriptors/MPI-descriptor.xml
-$JAVACMD org.objectweb.proactive.examples.mpi.Cpi 5  $XMLDESCRIPTOR
+XMLDESCRIPTOR=$PROACTIVE/descriptors/MPIRemote-descriptor.xml
+#$JAVACMD -classpath $CLASSPATH    -Dlog4j.configuration=file:/user/smariani/home/ProActive/compile/proactive-log4j -Dproactive.rmi.port=6099 -Dproactive.configuration=/user/smariani/home/ProActiveConfiguration.xml org.objectweb.proactive.examples.mpi.Cpi  $XMLDESCRIPTOR
+
+
+$JAVACMD -classpath $CLASSPATH -Dproactive.rmi.port=6099 -Dproactive.configuration=/user/smariani/home/ProActiveConfiguration.xml org.objectweb.proactive.examples.mpi.Cpi  $XMLDESCRIPTOR 
+
 
 echo
 echo ------------------------------------------------------------
