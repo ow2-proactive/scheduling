ed82b6e6c515662567c1798569f0157952c6be76
adicosta <adicosta@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-02-21 15:56:29 +0000

    The Technical Service interface.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3257 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/core/descriptor/services/TechnicalService.java
@@ -0,0 +1,60 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2005 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.core.descriptor.services;
+
+import java.util.HashMap;
+
+import org.objectweb.proactive.core.node.Node;
+
+/**
+ * <p>Interface to implement for defining  a Technical Service.</p>
+ * <b>Definition of Technical Service:</b>
+ * <p>A Technical Service is a non-functional requirement that may be dynamically
+ * fulfilled at runtime by updating the configuration of selected resources (here a
+ * ProActive Node).</p>
+ * @author Alexandre di Costanzo
+ *
+ */
+public interface TechnicalService {
+
+    /**
+     * Initialize the Technical Service with its argument values.
+     * @param argValues values of the Technical Service arguments.
+     */
+    public abstract void init(HashMap argValues);
+    
+    /**
+     * Initialize the given node with the Technical Service.
+     * @param node the node where to apply the Technical Service.
+     */
+    public abstract void apply(Node node);
+    
+}
