61a9367dfbee37600b22c121b4a9631f2e1ad244
irosenbe <irosenbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-05-04 20:18:02 +0000

    Overall manual improvements, including   removed unused images   moved java code files to better place (doc-files dir will be moved to PA root)   added screen shots   added xrefs where needed
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3371 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/objectweb/proactive/doc-files/guided_tour/code/Hello.java
@@ -0,0 +1,38 @@
+package code;
+import org.objectweb.proactive.ProActive;
+
+
+public class Hello {
+    private String name;
+    private String hi = "Hello world";
+    private java.text.DateFormat dateFormat = new java.text.SimpleDateFormat(
+            "dd/MM/yyyy HH:mm:ss");
+
+    public Hello() {
+    }
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String sayHello() {
+        return hi + " at " + dateFormat.format(new java.util.Date()) +
+        " from node : " + ProActive.getBodyOnThis().getNodeURL();
+    }
+
+    public static void main(String[] args) {
+        // Registers it with an URL
+        try {
+            // Creates an active instance of class HelloServer on the local node
+            Hello hello = (Hello) org.objectweb.proactive.ProActive.newActive(
+                    Hello.class.getName(),
+                    new Object[] { "remote" });
+            java.net.InetAddress localhost = java.net.InetAddress.getLocalHost();
+            org.objectweb.proactive.ProActive.register(hello,
+                "//" + localhost.getHostName() + "/Hello");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
