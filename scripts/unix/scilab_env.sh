b8640ab167f545a4afa3746a1aebaba02808f513
amangin <amangin@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-06-29 18:16:43 +0000

    add scilab_env.h to config local environment variables
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3624 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/unix/scilab_env.sh
@@ -0,0 +1,12 @@
+#Local deployment of Grid Scilab ToolBox
+#To define enviroment Variables
+
+#export SCIDIR=<your current scilab path>
+#export SCI=<your current scilab path>
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCIDIR/bin:.
+#export CLASSPATH=$CLASSPATH:$SCIDIR/bin:.
+
+export SCIDIR=/user/amangin/home/scilab/build4/scilab
+export SCI=$SCIDIR
+export LD_LIBRARY_PATH=$SCIDIR/bin
+export CLASSPATH=$SCIDIR/javasci.jar:.
