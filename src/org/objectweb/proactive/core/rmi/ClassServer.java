e7f4d5296626f8f5b94bd5d7f196be41317dea71
vlegrand <vlegrand@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-07-26 13:03:16 +0000

    fix the problem between ProActive and Jini/JavaSpaces (see mail 04/04/06)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@3767 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/core/rmi/ClassServer.java
+++ src/org/objectweb/proactive/core/rmi/ClassServer.java
@@ -1,43 +1,44 @@
-/* 
+/*
  * ################################################################
- * 
- * ProActive: The Java(TM) library for Parallel, Distributed, 
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
- * 
+ *
  * Copyright (C) 1997-2006 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive@objectweb.org
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- *  
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
- *  
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s): 
- * 
+ *  Contributor(s):
+ *
  * ################################################################
- */ 
+ */
 package org.objectweb.proactive.core.rmi;
 
-import java.net.UnknownHostException;
-
 import org.apache.log4j.Logger;
+
 import org.objectweb.proactive.core.rmi.ClassServerServlet;
 import org.objectweb.proactive.core.util.UrlBuilder;
 import org.objectweb.proactive.core.util.log.Loggers;
 import org.objectweb.proactive.core.util.log.ProActiveLogger;
 import org.objectweb.proactive.osgi.OsgiParameters;
+
+import java.net.UnknownHostException;
 
 
 public class ClassServer implements Runnable {
@@ -144,7 +145,8 @@ public class ClassServer implements Runnable {
 
         if (this.paths == null) {
             logger.info(
-                " --> This ClassFileServer is reading resources from classpath");
+                " --> This ClassFileServer is reading resources from classpath " +
+                port);
         } else {
             logger.info(
                 " --> This ClassFileServer is reading resources from the following paths");
@@ -157,6 +159,9 @@ public class ClassServer implements Runnable {
     }
 
     public static int getServerSocketPort() {
+        if (OsgiParameters.servletEnabled()) {
+            return ClassServerServlet.getPort();
+        }
         return port;
     }
 
@@ -202,7 +207,7 @@ public class ClassServer implements Runnable {
                 return;
             }
         }
-    }
+    } 
 
     private void newListener() {
         (new Thread(this, "ClassServer-" + hostname + ":" + port)).start();
