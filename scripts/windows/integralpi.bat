81dcecc6f47af463c06c9cfce935b7580f331504
bodnartv <bodnartv@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-10-27 15:07:23 +0000

    Addition of the windows launch script for IntegralPi example.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4100 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/windows/integralpi.bat
@@ -0,0 +1,19 @@
+@echo off
+echo. 
+echo --- IntegralPi --------------------------------------------------
+
+SETLOCAL
+set np=%1
+
+IF "%1"=="" set np=4
+
+echo The number of workers is %np%
+echo Feel free to edit this script if you want to specify another deployement descriptor.
+
+call init.bat
+IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\.
+%JAVA_CMD% org.objectweb.proactive.examples.integralpi.Launcher %PROACTIVE%\descriptors\Matrix.xml %np%
+ENDLOCAL
+
+echo. 
+echo ------------------------------------------------------------------
