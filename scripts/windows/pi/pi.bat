9e9d5e552bfc8dd57204c19ace4e695ca0697d2f
pnaoumen <pnaoumen@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2006-11-15 16:29:36 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4154 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/pi/pi.bat
+++ scripts/windows/pi/pi.bat
@@ -4,5 +4,5 @@ echo.
 SETLOCAL
 call init.bat
 set PROACTIVE=..\..\..\.
-%JAVA_CMD%  -Xmx256000000 -classpath %JAVA_HOME%\lib\tools.jar:%PROACTIVE%\compile\ant.jar:%PROACTIVE%\compile\ant-launcher.jar:%PROACTIVE%\compile\xml-apis.jar:%PROACTIVE%\compile\xercesImpl.jar org.apache.tools.ant.Main -buildfile %PROACTIVE%\src\org\objectweb\proactive\examples\pi\scripts\build.xml %*
+%JAVA_CMD%  -Xmx256000000 -classpath %JAVA_HOME%\lib\tools.jar;%PROACTIVE%\compile\ant.jar;%PROACTIVE%\compile\ant-launcher.jar;%PROACTIVE%\compile\xml-apis.jar;%PROACTIVE%\compile\xercesImpl.jar org.apache.tools.ant.Main -buildfile %PROACTIVE%\src\org\objectweb\proactive\examples\pi\scripts\build.xml %*
 ENDLOCAL
