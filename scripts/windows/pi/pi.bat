b6ecb0bf59d34396881baf8b1dc23a807efbb018
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-01-11 18:38:29 +0000

    fixed some path issues on windows
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4341 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/pi/pi.bat
+++ scripts/windows/pi/pi.bat
@@ -2,7 +2,9 @@
 echo. 
 
 SETLOCAL
-call init.bat
-set PROACTIVE=..\..\..\.
-%JAVA_CMD%  -Xmx256000000 -classpath %JAVA_HOME%\lib\tools.jar;%PROACTIVE%\compile\ant.jar;%PROACTIVE%\compile\ant-launcher.jar;%PROACTIVE%\compile\xml-apis.jar;%PROACTIVE%\compile\xercesImpl.jar org.apache.tools.ant.Main -buildfile %PROACTIVE%\src\org\objectweb\proactive\examples\pi\scripts\build.xml %*
+IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\..\.
+
+call %PROACTIVE%\scripts\windows\init.bat
+
+%JAVA_CMD%  -Xmx256000000 -classpath %CLASSPATH%;"%JAVA_HOME%\lib\tools.jar";"%PROACTIVE%\compile\ant.jar";"%PROACTIVE%\compile\ant-launcher.jar";"%PROACTIVE%\compile\xml-apis.jar";"%PROACTIVE%\compile\xercesImpl.jar" org.apache.tools.ant.Main -buildfile %PROACTIVE%\src\org\objectweb\proactive\examples\pi\scripts\build.xml %*
 ENDLOCAL
