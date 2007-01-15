51df15ef0be5a9b38943581cdab2d75f1d2f95c1
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-01-15 14:51:53 +0000

    added a printstack trace when connecting to the scheduler
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4354 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/objectweb/proactive/scheduler/Scheduler.java
+++ src/org/objectweb/proactive/scheduler/Scheduler.java
@@ -242,7 +242,7 @@ public class Scheduler implements java.io.Serializable, SchedulerConstants {
                 return null;
             }
         } catch (Exception e) {
-            //    		 TODO Auto-generated catch block
+        	e.printStackTrace();
             logger.error("error connecting the scheduler service");
             return null;
         }
