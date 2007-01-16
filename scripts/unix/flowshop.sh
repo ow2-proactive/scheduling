d7f632312e24031aef2a5ba874c41dd231d012f3
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-01-16 12:19:15 +0000

    updated unix flowshop script with default descriptor and taillard files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4362 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/flowshop.sh
+++ scripts/unix/flowshop.sh
@@ -3,17 +3,23 @@
 echo
 echo --- Flowshop ---------------------------------------------
 
-echo Usage: $0 bench_file descriptors.xml
-echo
-_bench=$1
-_desc=$2
-shift
-shift
+if [ $# -eq 2 ] 
+then
+	_bench=$1
+	_desc=$2
+elif [ $# -eq 0 ] 
+then
+	_bench=../../src/org/objectweb/proactive/examples/flowshop/taillard/test_10_10.txt
+	_desc=../../descriptors/Workers.xml
+else 
+	echo Usage: $0 "[<bench_file> <descriptors_location>]"
+	echo
+fi
 
 workingDir=`dirname $0`
 . $workingDir/env.sh
 
-$JAVACMD -Dflowshopparser.taillard=false org.objectweb.proactive.examples.flowshop.Main -bench $_bench -desc $_desc $*
+$JAVACMD -Dflowshopparser.taillard=false org.objectweb.proactive.examples.flowshop.Main -bench $_bench -desc $_desc
 
 echo
 echo ------------------------------------------------------------
