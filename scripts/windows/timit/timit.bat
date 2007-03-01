e280999399a48a247dd0cfc43055c1216c61b04c
bodnartv <bodnartv@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-03-01 17:50:03 +0000

    Same modification for the windows version as it was done for the unix version.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4497 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/timit/timit.bat
+++ scripts/windows/timit/timit.bat
@@ -3,8 +3,8 @@ echo.
 echo --- TimIt --------------------------------------------------
 
 SETLOCAL
-call init.bat
-set TIMIT_DEFAULT_CONFIG_FILE="%PROACTIVE%\src\org\objectweb\proactive\examples\timit"
+call ..\init.bat
+set TIMIT_DEFAULT_CONFIG_FILE="%PROACTIVE%\scripts\windows\timit"
 %JAVA_CMD% org.objectweb.proactive.benchmarks.timit.TimIt -c %TIMIT_DEFAULT_CONFIG_FILE%\config.xml
 ENDLOCAL
 
