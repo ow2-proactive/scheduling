f289846226513994bc1631c2c17fb32ed1069531
bodnartv <bodnartv@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-03-02 15:48:26 +0000

    Correction of the PROACTIVE var which was badly setted.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4503 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/timit/timit.bat
+++ scripts/windows/timit/timit.bat
@@ -3,9 +3,12 @@ echo.
 echo --- TimIt --------------------------------------------------
 
 SETLOCAL
-call ..\init.bat
-set TIMIT_DEFAULT_CONFIG_FILE="%PROACTIVE%\scripts\windows\timit"
-%JAVA_CMD% org.objectweb.proactive.benchmarks.timit.TimIt -c %TIMIT_DEFAULT_CONFIG_FILE%\config.xml
+IF NOT DEFINED PROACTIVE set PROACTIVE=..\..\..
+
+call "%PROACTIVE%\scripts\windows\init.bat"
+
+set TIMIT_DEFAULT_CONFIG_FILE_PATH="%PROACTIVE%\scripts\windows\timit"
+%JAVA_CMD% org.objectweb.proactive.benchmarks.timit.TimIt -c %TIMIT_DEFAULT_CONFIG_FILE_PATH%\config.xml
 ENDLOCAL
 
 echo. 
