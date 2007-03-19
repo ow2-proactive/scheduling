3b44e38a3fe23e97becf0e3601cf12b08e1f7237
pnaoumen <pnaoumen@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-03-19 13:11:37 +0000

    Interface and implementation for non functional component representtaive
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4618 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Core/org/objectweb/proactive/core/component/representative/ProActiveNFComponentRepresentative.java
@@ -0,0 +1,9 @@
+package org.objectweb.proactive.core.component.representative;
+
+/**
+ * This is the marker interface implemented by non functional components
+ * @author Paul Naoumenko
+ *
+ */
+public interface ProActiveNFComponentRepresentative {
+}
