1a211482576ec4e7955c6ac7aba522117c856f2e
pnaoumen <pnaoumen@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-03-19 13:07:22 +0000

    Added methods for non functional components
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4616 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/group/ProActiveComponentGroup.java
+++ src/Core/org/objectweb/proactive/core/group/ProActiveComponentGroup.java
@@ -4,7 +4,7 @@
  * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
  *
- * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
+ * Copyright (C) 1997-2006 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive@objectweb.org
  *
  * This library is free software; you can redistribute it and/or
@@ -38,6 +38,7 @@ import org.objectweb.fractal.api.Interface;
 import org.objectweb.fractal.api.factory.InstantiationException;
 import org.objectweb.fractal.api.type.ComponentType;
 import org.objectweb.proactive.core.component.ControllerDescription;
+import org.objectweb.proactive.core.component.NFControllerDescription;
 import org.objectweb.proactive.core.component.ProActiveInterface;
 import org.objectweb.proactive.core.component.ProActiveInterfaceImpl;
 import org.objectweb.proactive.core.component.exceptions.InterfaceGenerationFailedException;
@@ -46,6 +47,7 @@ import org.objectweb.proactive.core.component.group.ProxyForComponentGroup;
 import org.objectweb.proactive.core.component.group.ProxyForComponentInterfaceGroup;
 import org.objectweb.proactive.core.component.representative.ProActiveComponentRepresentative;
 import org.objectweb.proactive.core.component.representative.ProActiveComponentRepresentativeImpl;
+import org.objectweb.proactive.core.component.representative.ProActiveNFComponentRepresentativeImpl;
 import org.objectweb.proactive.core.component.type.ProActiveInterfaceType;
 import org.objectweb.proactive.core.mop.ClassNotReifiableException;
 import org.objectweb.proactive.core.mop.ConstructionOfProxyObjectFailedException;
@@ -142,6 +144,57 @@ public class ProActiveComponentGroup {
 
             // build the constructor call for the proxy object to create
             ConstructorCall reifiedCall = MOP.buildTargetObjectConstructorCall(ProActiveComponentRepresentativeImpl.class,
+                    new Object[] {
+                        componentType, controllerDesc.getHierarchicalType(),
+                        controllerDesc.getControllersConfigFileLocation()
+                    });
+
+            // Instanciates the proxy object
+            ProxyForComponentGroup proxy = (ProxyForComponentGroup) MOP.createProxyObject(ProxyForComponentGroup.class.getName(),
+                    MOP.EMPTY_OBJECT_ARRAY, reifiedCall);
+
+            // connect the stub to the proxy
+            result.setProxy(proxy);
+
+            proxy.className = Component.class.getName();
+            proxy.setComponentType(componentType);
+            proxy.setControllerDesc(controllerDesc);
+
+            return result;
+        } catch (Exception e) {
+            throw new InstantiationException(
+                "cannot create group of component representatives : " +
+                e.getMessage());
+        }
+    }
+
+    /**
+     * Creates an empty  non-functional component stub+a group proxy.
+     * The stub in front of the group proxy is a non-functional component stub (instance of NFComponentRepresentativeImpl),
+     * that offers references to the functional interfaces defined in the type of the component.
+     * @param componentType parameters of this component
+     * @param controllerDesc TODO
+     * @return a stub/proxy
+     * @throws ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     */
+    public static ProActiveComponentRepresentative newNFComponentRepresentativeGroup(
+        ComponentType componentType, NFControllerDescription controllerDesc)
+        throws ClassNotFoundException, InstantiationException {
+        try {
+            ProActiveComponentRepresentative result = null;
+
+            // create the stub with the appropriate parameters
+            Constructor constructor = ProActiveNFComponentRepresentativeImpl.class.getConstructor(new Class[] {
+                        ComponentType.class, String.class, String.class
+                    });
+            result = (ProActiveComponentRepresentative) constructor.newInstance(new Object[] {
+                        componentType, controllerDesc.getHierarchicalType(),
+                        controllerDesc.getControllersConfigFileLocation()
+                    });
+
+            // build the constructor call for the proxy object to create
+            ConstructorCall reifiedCall = MOP.buildTargetObjectConstructorCall(ProActiveNFComponentRepresentativeImpl.class,
                     new Object[] {
                         componentType, controllerDesc.getHierarchicalType(),
                         controllerDesc.getControllersConfigFileLocation()
