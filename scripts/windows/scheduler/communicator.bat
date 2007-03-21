9828ba19a0c4e8f8042e8ff1ad363d8b68fc2dfd
walzouab <walzouab@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-03-21 19:08:22 +0000

    further modification to merge task scheduler with the trunk
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4658 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/windows/scheduler/communicator.bat
@@ -0,0 +1,42 @@
+@echo off
+echo
+echo --- Communicator ----------------------------------------------
+
+rem --- Verifying current directory
+SET COMMAND=%0
+IF NOT "%COMMAND:~-4%" == ".bat" (
+ SET COMMAND=%0.bat
+)
+ 
+SET OK=0
+FOR /F %%i in ('dir /b') do IF "%%i" == "%COMMAND%" SET OK=1
+
+IF %OK% == 0 (
+echo scripts must be started in the same directory as the script.
+goto end
+)
+
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
+
+IF  "%1" == "" (
+  SET SCHEDULER_URL=//localhost/SCHEDULER_NODE 
+ ) ELSE (
+  SET SCHEDULER_URL=%1
+)
+
+SETLOCAL
+IF NOT DEFINED PROACTIVE set PROACTIVE=%CD%\..\..\..
+call "%PROACTIVE%\scripts\windows\init.bat"
+
+%JAVA_CMD% -Dproactive.rmi.port=1234 org.objectweb.proactive.extra.scheduler.AdminCommunicator %SCHEDULER_URL%
+
+:end
+echo. 
+echo ---------------------------------------------------------
---
+++ scripts/windows/scheduler/scheduler.bat
@@ -0,0 +1,43 @@
+@echo off
+echo. 
+echo --- Scheduler ---------------------------------------------
+
+rem --- Verifying current directory
+SET COMMAND=%0
+IF NOT "%COMMAND:~-4%" == ".bat" (
+ SET COMMAND=%0.bat
+)
+ 
+SET OK=0
+FOR /F %%i in ('dir /b') do IF "%%i" == "%COMMAND%" SET OK=1
+
+IF %OK% == 0 (
+echo scripts must be started in the same directory as the script.
+goto end
+)
+
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
+
+  SET SCHEDULER_URL=%1
+  SET RM=%2
+
+
+SETLOCAL
+IF NOT DEFINED PROACTIVE set PROACTIVE=%CD%\..\..\..
+call "%PROACTIVE%\scripts\windows\init.bat"
+
+rem This program will enable you to launch the scheduler daemon
+
+%JAVA_CMD% org.objectweb.proactive.examples.scheduler.LocalSchedulerExample %SCHEDULER_URL% %RM%
+ENDLOCAL
+
+:end
+echo. 
+echo ---------------------------------------------------------
