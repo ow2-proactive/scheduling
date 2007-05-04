c4afce5bf0894d38aecf70a180cf140257accadc
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-05-04 14:59:32 +0000

    Escape spaces in cmd.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4934 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/Helper.java
+++ src/Tests/functionalTests/Helper.java
@@ -3,6 +3,8 @@ package functionalTests;
 import java.io.File;
 import java.io.IOException;
 
+import org.objectweb.proactive.core.util.OperatingSystem;
+
 
 public class Helper {
 	
@@ -11,7 +13,16 @@ public class Helper {
 		File cmd = new File(dir + "/dev/scripts/killTests");
 		if (cmd.exists()) {
 			try {
-				Runtime.getRuntime().exec(cmd.toString(), null, dir);
+				switch (OperatingSystem.getOperatingSystem()) {
+				case unix:
+					Runtime.getRuntime().exec(cmd.getAbsolutePath().replaceAll(" ", "\\ "), null, dir);
+					break;
+
+				default:
+					System.err.println("TODO: Kill JVMs on Windows also !");
+					break;
+				}
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
