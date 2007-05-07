2b712dc8f198b7972ef66d24a3c617c70ec78ffa
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-05-07 09:00:55 +0000

    organize imports
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@4946 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Core/org/objectweb/proactive/core/component/NFControllerDescription.java
+++ src/Core/org/objectweb/proactive/core/component/NFControllerDescription.java
@@ -30,10 +30,6 @@
  */
 package org.objectweb.proactive.core.component;
 
-import java.io.Serializable;
-import java.util.Map;
-
-import org.objectweb.proactive.core.component.Constants;
 import org.objectweb.proactive.core.component.identity.ProActiveComponentImpl;
 
 
