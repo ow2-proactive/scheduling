1dfc69e143e211c872ee4349c8ee5bbc74ee87d0
bodnartv <bodnartv@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-05-31 13:22:05 +0000

    Addition of a performance test that can be used to compare different version of ProActive (after a commit for example). The performance test involves a Caller and a Callee. The Caller sends some basic requests to the Callee. Then these two active objects are terminated and generate an xml output file. This xml file is used to generate a comparative chart between the current SVN version of ProActive (the version is provided by ProActive.getProActiveVersion()).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5160 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/performanceTests/simple/Caller.java
@@ -0,0 +1,75 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package performanceTests.simple;
+
+import java.io.IOException;
+
+import org.objectweb.proactive.ProActive;
+import org.objectweb.proactive.core.util.wrapper.IntWrapper;
+
+
+/**
+ *
+ * @author vbodnart
+ */
+public class Caller implements java.io.Serializable {
+    public static final int NB_ITERATIONS = 15;
+    private Callee calleeReference;
+
+    public Caller() {
+    }
+
+    public Caller(Callee calleeReference) {
+        this.calleeReference = calleeReference;
+    }
+
+    /**
+     * Synchronous
+     * @return Always true to force syncronous call
+     */
+    public boolean performTest() {
+        for (int i = 0; i < NB_ITERATIONS; i++) {
+            // perform future async call in order to have all basic timers used
+            int[] arr = this.calleeReference.futureAsyncCall2(new int[15000]);
+            arr.hashCode();
+        }
+        return true;
+    }
+
+    /**
+     * Since an instance of this class could be active this method
+     * should be called to terminate its activity.
+     */
+    public boolean kill() {
+        ProActive.terminateActiveObject(true);
+        return true;
+    }
+}
