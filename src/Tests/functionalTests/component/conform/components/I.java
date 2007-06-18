159be30f83cd350f092d5ea3962af362312a5147
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-06-18 13:56:57 +0000

    right copyright
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5350 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/component/conform/components/I.java
+++ src/Tests/functionalTests/component/conform/components/I.java
@@ -1,16 +1,11 @@
-/*
- * ################################################################
- *
- * ProActive: The Java(TM) library for Parallel, Distributed,
- *            Concurrent computing with Security and Mobility
- *
- * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
- * Contact: proactive@objectweb.org
+/***
+ * Julia: France Telecom's implementation of the Fractal API
+ * Copyright (C) 2001-2002 France Telecom R&D
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
+ * version 2 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,14 +14,11 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Initial developer(s):               The ProActive Team
- *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s):
+ * Contact: Eric.Bruneton@rd.francetelecom.com
  *
- * ################################################################
+ * Author: Eric Bruneton
  */
 package functionalTests.component.conform.components;
 
