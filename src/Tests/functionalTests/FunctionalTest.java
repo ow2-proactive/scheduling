da25d5aebdb1fc45f5f13c2d78ab9896068ab405
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-06-19 12:13:19 +0000

    Added a timeout for each test (see -Dproactive.test.timeout). By default each test has 5 minutes to complete
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5367 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/FunctionalTest.java
+++ src/Tests/functionalTests/FunctionalTest.java
@@ -32,6 +32,8 @@ package functionalTests;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
@@ -40,13 +42,67 @@ import org.objectweb.proactive.core.util.OperatingSystem;
 
 
 public class FunctionalTest {
-    protected Logger logger = Logger.getLogger("testsuite");
+    static final protected Logger logger = Logger.getLogger("testsuite");
+
+    /** The amount of time given to a test to success or fail */
+    static final private long TIMEOUT = 300000;
+
+    /** A shutdown hook to kill all forked JVMs when exiting the main JVM */
+    static final private Thread shutdownHook = new Thread() {
+            public void run() {
+                logger.trace("FunctionalTest Shutdown Hook");
+                killProActive();
+            }
+        };
+
+    @BeforeClass
+    public static void beforeClass() {
+        logger.trace("beforeClass");
+
+        logger.trace("Killing remaining ProActive Runtime");
+        killProActive();
+
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+
+        long timeout = TIMEOUT;
+        String ptt = System.getProperty("proactive.test.timeout");
+        if (ptt != null) {
+            timeout = Long.parseLong(ptt);
+        }
+
+        if (timeout != 0) {
+            logger.trace("Timer will be fired in " + timeout + " ms");
+            TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        logger.warn("FunctionalTest timeout reached !");
+                        killProActive();
+                        System.err.println("Timeout reached, aborting !");
+                        System.exit(12);
+                    }
+                };
+
+            Timer timer = new Timer(true);
+            timer.schedule(timerTask, timeout);
+        } else {
+            logger.trace("Timeout disabled");
+        }
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        logger.trace("afterClass");
+
+        logger.trace("Removing the shutdownHook");
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
+
+        logger.trace("Killing remaining ProActive Runtime");
+        killProActive();
+    }
 
     /**
      * Kill all ProActive runtimes
      */
-    @BeforeClass
-    @AfterClass
     public static void killProActive() {
         File dir = new File(System.getProperty("proactive.dir"));
         File cmd = new File(dir + "/dev/scripts/killTests");
