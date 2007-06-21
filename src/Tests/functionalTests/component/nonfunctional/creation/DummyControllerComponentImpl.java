5664e829efb6735170da15262bcadf8ff0e8020e
pnaoumen <pnaoumen@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-06-21 14:15:19 +0000

    Jalopy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5392 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/component/nonfunctional/creation/DummyControllerComponentImpl.java
+++ src/Tests/functionalTests/component/nonfunctional/creation/DummyControllerComponentImpl.java
@@ -1,20 +1,17 @@
 package functionalTests.component.nonfunctional.creation;
+
 /**
- * 
+ *
  * @author Paul Naoumenko
  *
  * Content class of the dummy controller component
  */
 public class DummyControllerComponentImpl implements DummyControllerItf {
+    public String dummyMethodWithResult() {
+        return "Message from dummy controller";
+    }
 
-	public String dummyMethodWithResult() {
-		
-		return "Message from dummy controller";
-	}
-
-	public void dummyVoidMethod(String message) {
-		System.out.println("Received message :"+message);
-
-	}
-
+    public void dummyVoidMethod(String message) {
+        System.out.println("Received message :" + message);
+    }
 }
