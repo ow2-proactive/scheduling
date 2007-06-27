500f170d287a61979b65bfe390e6963c129954d8
gchazara <gchazara@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-06-27 13:31:51 +0000

    F***ing Jalopy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5424 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/activeobject/futuremonitoring/Test.java
+++ src/Tests/functionalTests/activeobject/futuremonitoring/Test.java
@@ -39,7 +39,6 @@ import org.objectweb.proactive.core.node.Node;
 import functionalTests.FunctionalTest;
 import static junit.framework.Assert.assertTrue;
 
-
 /**
  * Test monitoring the futures
  */
