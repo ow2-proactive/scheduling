b54f913f8ed4cb492666c1098fcb7a9de4bb8ee0
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-07-30 13:45:42 +0000

    add a regression test for immediate service with component
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5684 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/component/immediateservice/Test.java
@@ -0,0 +1,85 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package functionalTests.component.immediateservice;
+
+import org.objectweb.fractal.api.Component;
+import org.objectweb.fractal.api.factory.GenericFactory;
+import org.objectweb.fractal.api.type.InterfaceType;
+import org.objectweb.fractal.api.type.TypeFactory;
+import org.objectweb.fractal.util.Fractal;
+import org.objectweb.proactive.core.component.Constants;
+import org.objectweb.proactive.core.component.ContentDescription;
+import org.objectweb.proactive.core.component.ControllerDescription;
+
+import functionalTests.ComponentTest;
+
+
+/**
+ * @author Cedric Dalmasso
+ *
+ * creates a new component
+ */
+public class Test extends ComponentTest {
+
+    /**
+         *
+         */
+    private static final long serialVersionUID = 4191411789650566643L;
+
+    /**
+     * @see testsuite.test.FunctionalTest#action()
+     */
+    @org.junit.Test
+    public void action() throws Exception {
+        Component boot = Fractal.getBootstrapComponent();
+        TypeFactory type_factory = Fractal.getTypeFactory(boot);
+        GenericFactory cf = Fractal.getGenericFactory(boot);
+
+        Component comp = cf.newFcInstance(type_factory.createFcType(
+                    new InterfaceType[] {
+                        type_factory.createFcItfType("itf",
+                            Itf.class.getName(), false, false, false)
+                    }),
+                new ControllerDescription("component", Constants.PRIMITIVE),
+                new ContentDescription(A.class.getName(), new Object[] {  }));
+
+        Fractal.getLifeCycleController(comp).startFc();
+        // first execute an infinite loop in the component activity
+        ((Itf) comp.getFcInterface("itf")).loopQueueMethod();
+        // call an immediate service: it is executed concurrently with the activity 
+        System.err.println("MAIN: result is '" +
+            ((Itf) comp.getFcInterface("itf")).immediateMethod("a ") + "'");
+        //Fractal.getLifeCycleController(comp).startFc();
+        // call an immediate service to set the condition false and thus terminate the loopQueueMethod
+        ((Itf) comp.getFcInterface("itf")).immediateStopLoopMethod();
+        Fractal.getLifeCycleController(comp).stopFc();
+    }
+}
