744d43583b4771b28d0baf61cc723fb601161532
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-14 15:36:21 +0000

    added masterslave non regression tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5863 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/masterslave/A.java
@@ -0,0 +1,67 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package functionalTests.masterslave;
+
+import org.objectweb.proactive.extra.masterslave.interfaces.SlaveMemory;
+import org.objectweb.proactive.extra.masterslave.interfaces.Task;
+
+
+public class A implements Task<Integer> {
+
+    /**
+         *
+         */
+    private static final long serialVersionUID = 7562698745274296370L;
+    private int taskcounter;
+    private long delay;
+    private boolean exception;
+
+    public A(int counter) {
+        this(counter, 0, false);
+    }
+
+    public A(int counter, long delay, boolean exception) {
+        this.taskcounter = counter;
+        this.delay = delay;
+        this.exception = exception;
+    }
+
+    public Integer run(SlaveMemory memory) throws Exception {
+        if (exception) {
+            int a = 1 / 0;
+        }
+
+        if (delay > 0) {
+            Thread.sleep(delay);
+        }
+        return taskcounter;
+    }
+}
