e463a85512c2ecf4a03cfc6f8f4d78178c615dd6
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-22 09:18:38 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5993 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/unitTests/descriptorParser/TestApplicationDescriptorParser.java
@@ -0,0 +1,115 @@
+package unitTests.descriptorParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+
+import org.junit.Test;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.ApplicationParsers.AbstractApplicationParser;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplicationDescriptor;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplicationDescriptorImpl;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplicationParser;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplicationParserImpl;
+import org.objectweb.proactive.extra.gcmdeployment.process.CommandBuilder;
+import org.objectweb.proactive.extra.gcmdeployment.process.commandbuilder.CommandBuilderScript;
+import org.w3c.dom.Node;
+
+
+public class TestApplicationDescriptorParser {
+    final static String TEST_APP_DIR = TestApplicationDescriptorParser.class.getClass()
+                                                                            .getResource("/unitTests/descriptorParser/testfiles/application")
+                                                                            .getFile();
+
+    //    @Test
+    public void test() throws IOException {
+        for (File descriptor : getApplicationDescriptors()) {
+            if (descriptor.toString().contains("script_ext")) {
+                continue;
+            }
+
+            GCMApplicationParserImpl parser = new GCMApplicationParserImpl(descriptor);
+
+            parser.getCommandBuilder();
+            parser.getVirtualNodes();
+            parser.getResourceProviders();
+        }
+    }
+
+    /**
+     * User application node parser used to demonstrate how to install custom app parsers
+     * @author glaurent
+     *
+     */
+    protected static class UserApplicationNodeParser
+        extends AbstractApplicationParser {
+        @Override
+        protected CommandBuilder createCommandBuilder() {
+            return new CommandBuilderScript();
+        }
+
+        public String getNodeName() {
+            return "paext:myapplication";
+        }
+
+        @Override
+        public void parseApplicationNode(Node paNode,
+            GCMApplicationParser applicationParser, XPath xpath) {
+            super.parseApplicationNode(paNode, applicationParser, xpath);
+
+            System.out.println("User Application Parser - someattr value = " +
+                paNode.getAttributes().getNamedItem("someattr").getNodeValue());
+        }
+    }
+
+    @Test
+    public void userSchemaTest() throws IOException {
+        for (File file : getApplicationDescriptors()) {
+            if (!file.toString().contains("script_ext")) {
+                continue;
+            }
+            System.out.println(file);
+
+            String userSchema = getClass()
+                                    .getResource("/unitTests/descriptorParser/testfiles/application/SampleApplicationExtension.xsd")
+                                    .toString();
+
+            ArrayList<String> schemas = new ArrayList<String>();
+            schemas.add(userSchema);
+
+            GCMApplicationParserImpl parser = new GCMApplicationParserImpl(file,
+                    schemas);
+
+            parser.registerApplicationParser(new UserApplicationNodeParser());
+
+            parser.getCommandBuilder();
+            parser.getVirtualNodes();
+            parser.getResourceProviders();
+        }
+    }
+
+    //    @Test
+    public void doit() throws IOException {
+        for (File file : getApplicationDescriptors()) {
+            if (!file.toString().contains("scriptHostname")) {
+                continue;
+            }
+            System.out.println(file);
+
+            GCMApplicationDescriptor gcma = new GCMApplicationDescriptorImpl(file);
+            gcma.awaitTermination();
+        }
+    }
+
+    private List<File> getApplicationDescriptors() {
+        List<File> ret = new ArrayList<File>();
+        File dir = new File(TEST_APP_DIR);
+
+        for (String file : dir.list()) {
+            ret.add(new File(dir, file));
+        }
+        return ret;
+    }
+}
---
+++ src/Tests/unitTests/descriptorParser/TestDeploymentDescriptorParser.java
@@ -0,0 +1,113 @@
+package unitTests.descriptorParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.junit.Test;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.BridgeParsers.AbstractBridgeParser;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GCMDeploymentParserImpl;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.AbstractGroupParser;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.GroupParsers.GroupParser;
+import org.objectweb.proactive.extra.gcmdeployment.process.bridge.AbstractBridge;
+import org.objectweb.proactive.extra.gcmdeployment.process.group.AbstractGroup;
+import org.w3c.dom.Node;
+
+
+public class TestDeploymentDescriptorParser {
+    //    @Test
+    public void test() throws IOException, XPathExpressionException {
+        File descriptor = new File(getClass()
+                                       .getResource("/unitTests/descriptorParser/testfiles/deployment.xml")
+                                       .getFile());
+
+        GCMDeploymentParserImpl parser = new GCMDeploymentParserImpl(descriptor);
+
+        parser.parseEnvironment();
+        parser.parseInfrastructure();
+        parser.parseResources();
+    }
+
+    //
+    // Examples of custom group & bridge parsers
+    //
+    protected static class UserGroup extends AbstractGroup {
+        @Override
+        public List<String> internalBuildCommands() {
+            return new ArrayList<String>();
+        }
+    }
+
+    protected static class UserBridge extends AbstractBridge {
+        @Override
+        public String internalBuildCommand() {
+            return "";
+        }
+    }
+
+    protected static class UserGroupParser extends AbstractGroupParser {
+        @Override
+        public AbstractGroup createGroup() {
+            return new UserGroup();
+        }
+
+        public String getNodeName() {
+            return "paext:myGroup";
+        }
+
+        @Override
+        public void parseGroupNode(Node groupNode, XPath xpath) {
+            super.parseGroupNode(groupNode, xpath);
+
+            System.out.println("User Group Parser - someattr value = " +
+                groupNode.getAttributes().getNamedItem("someattr").getNodeValue());
+        }
+    }
+
+    protected static class UserBridgeParser extends AbstractBridgeParser {
+        @Override
+        public AbstractBridge createBridge() {
+            return new UserBridge();
+        }
+
+        public String getNodeName() {
+            return "paext:myBridge";
+        }
+
+        @Override
+        public void parseBridgeNode(Node bridgeNode, XPath xpath) {
+            super.parseBridgeNode(bridgeNode, xpath);
+            System.out.println("User Bridge Parser - someattr value = " +
+                bridgeNode.getAttributes().getNamedItem("someattr")
+                          .getNodeValue());
+        }
+    }
+
+    @Test
+    public void userSchemaTest() throws IOException, XPathExpressionException {
+        File descriptor = new File(getClass()
+                                       .getResource("/unitTests/descriptorParser/testfiles/deployment/group_bridge_ext.xml")
+                                       .getFile());
+
+        String userSchema = getClass()
+                                .getResource("/unitTests/descriptorParser/testfiles/deployment/SampleDeploymentExtension.xsd")
+                                .toString();
+
+        ArrayList<String> schemas = new ArrayList<String>();
+        schemas.add(userSchema);
+
+        GCMDeploymentParserImpl parser = new GCMDeploymentParserImpl(descriptor,
+                schemas);
+
+        parser.registerGroupParser(new UserGroupParser());
+        parser.registerBridgeParser(new UserBridgeParser());
+
+        parser.parseEnvironment();
+        parser.parseInfrastructure();
+        parser.parseResources();
+    }
+}
