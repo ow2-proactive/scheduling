9c9d3030e71922bad2b47ea5e6d2fede9945f5b9
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-22 09:19:19 +0000

    Added unit test to the trunk
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@5995 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/unitTests/listGenerator/TestListGenerator.java
@@ -0,0 +1,80 @@
+package unitTests.listGenerator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.extra.gcmdeployment.process.ListGenerator;
+
+
+public class TestListGenerator {
+    final static private String validResource = "/unitTests/listGenerator/data.valid.txt";
+    final static private String invalidResource = "/unitTests/listGenerator/data.invalid.txt";
+
+    //    @Test
+    public void singleTest() {
+        //        ListGenerator.generateNames("node[1,2,5]^[2-4]");
+        ListGenerator.generateNames("node[1,2,3]^");
+    }
+
+    //    @Test
+    public void testValid() throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(
+                    getClass().getResource(validResource).getFile()));
+
+        while (true) {
+            String question = br.readLine();
+            String response = br.readLine();
+            br.readLine(); // Empty line
+
+            if (question == null) { // End of File
+                break;
+            }
+
+            if (response == null) {
+                throw new IllegalArgumentException(
+                    "Illegal format for a data file");
+            }
+
+            Assert.assertEquals("question=\"" + question + "\"", response,
+                concat(ListGenerator.generateNames(question)));
+        }
+    }
+
+    @Test
+    public void testInvalid() throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(
+                    getClass().getResource(invalidResource).getFile()));
+
+        while (true) {
+            String question = br.readLine();
+            br.readLine(); // Empty line
+
+            if (question == null) { // End of File
+                break;
+            }
+
+            try {
+                List<String> ret = ListGenerator.generateNames(question);
+                Assert.fail("Question=" + question + "\" response=\"" +
+                    concat(ret) + "\"");
+            } catch (IllegalArgumentException e) {
+                // An IllegalArguementException is expected
+            }
+        }
+    }
+
+    static private String concat(List<String> lstr) {
+        String ret = "";
+        for (String str : lstr)
+            ret += (str + " ");
+
+        if (ret.length() > 1) {
+            ret = ret.substring(0, ret.length() - 1);
+        }
+
+        return ret;
+    }
+}
