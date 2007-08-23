3d3887815975a50620d71b2d507c2714b8ab70c4
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-23 15:11:08 +0000

    renamed shType to more appropriate abstractGroupType
    added common types in separate file
    
    added modularized types for bsub and glite groups
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6030 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/descriptor/basic/TestBasicDescriptorParsing.java
@@ -0,0 +1,17 @@
+package functionalTests.descriptor.basic;
+
+import org.junit.Test;
+import org.objectweb.proactive.ProActive;
+
+public class TestBasicDescriptorParsing {
+
+    @Test
+    public void action() throws Exception {
+        
+        String descriptorLocation = getClass().getResource("SSH_LSF_Example.xml").getPath();
+        
+        Object proActiveDescriptor = ProActive.getProactiveDescriptor("file:" +
+                descriptorLocation);
+    }
+    
+}
