f117149d754d8f6d44eba4a76fbde59f1bfbad32
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-08-23 15:16:46 +0000

    oops
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6032 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/descriptor/basic/TestBasicDescriptorParsing.java
+++ src/Tests/functionalTests/descriptor/basic/TestBasicDescriptorParsing.java
@@ -3,15 +3,15 @@ package functionalTests.descriptor.basic;
 import org.junit.Test;
 import org.objectweb.proactive.ProActive;
 
-public class TestBasicDescriptorParsing {
 
+public class TestBasicDescriptorParsing {
     @Test
     public void action() throws Exception {
-        
-        String descriptorLocation = getClass().getResource("SSH_LSF_Example.xml").getPath();
-        
+        String descriptorLocation = getClass()
+                                        .getResource("SSH_LSF_Example.xml")
+                                        .getPath();
+
         Object proActiveDescriptor = ProActive.getProactiveDescriptor("file:" +
                 descriptorLocation);
     }
-    
 }
