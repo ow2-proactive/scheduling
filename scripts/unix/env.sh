669e9b44264a5e4bc217972e6d33b239521eea0d
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-09-07 16:02:38 +0000

    Moved all JARs inside lib/. Updated the descriptors
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6159 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/env.sh
+++ scripts/unix/env.sh
@@ -35,8 +35,6 @@ then
     CLASSPATH=$CLASSPATH:$PROACTIVE/classes/Extensions
     CLASSPATH=$CLASSPATH:$PROACTIVE/classes/Extra
     CLASSPATH=$CLASSPATH:$PROACTIVE/classes/Examples
-    CLASSPATH=$CLASSPATH:$PROACTIVE/classes/IC2D-old
-    CLASSPATH=$CLASSPATH:$PROACTIVE/classes/GCMTests
 fi
 if [ -f $PROACTIVE/ProActive.jar ]
 then
@@ -46,219 +44,17 @@ if [ -f $PROACTIVE/ProActive_examples.jar ]
 then
     CLASSPATH=$CLASSPATH:$PROACTIVE/ProActive_examples.jar
 fi
-if [ -f $PROACTIVE/lib/log4j.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/log4j.jar
-fi
-if [ -f $PROACTIVE/lib/xercesImpl.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/xercesImpl.jar
-fi
-if [ -f $PROACTIVE/lib/bouncycastle.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/bouncycastle.jar
-fi
-if [ -f $PROACTIVE/lib/components/fractal.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/fractal.jar
-fi
 if [ -f $PROACTIVE/ic2d.jar ]
 then
     CLASSPATH=$CLASSPATH:$PROACTIVE/ic2d.jar
 fi
-if [ -f $PROACTIVE/lib/javassist.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/javassist.jar
-fi
 
-#--------------------------------------------------
-# jar to set when using RMI/SSH
-if [ -f $PROACTIVE/lib/ganymed-ssh2-build210.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ganymed-ssh2-build210.jar
-fi
-
-#--------------------------------------------------
-# jars to set when using Globus
-if [ -f $PROACTIVE/lib/globus/cog-jglobus-1.2.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/cog-jglobus-1.2.jar
-fi
-if [ -f $PROACTIVE/lib/globus/cryptix.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/cryptix.jar
-fi
-if [ -f $PROACTIVE/lib/globus/cryptix32.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/cryptix32.jar
-fi
-if [ -f $PROACTIVE/lib/globus/cryptix-asn1.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/cryptix-asn1.jar
-fi
-if [ -f $PROACTIVE/lib/globus/puretls.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/puretls.jar
-fi
-if [ -f $PROACTIVE/lib/globus/cog-ogce.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/globus/cog-ogce.jar
-fi
-#--------------------------------------------------
-# jar to set when using gLite
-if [ -f $PROACTIVE/lib/glite/classad.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/glite/classad.jar
-fi
-if [ -f $PROACTIVE/lib/glite/glite-wms-jdlj.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/glite/glite-wms-jdlj.jar
-fi
-
-#--------------------------------------------------
-# jar to set when using Ibis
-if [ -d $PROACTIVE/lib/ibis ]
-then
-    for i in $PROACTIVE/lib/ibis/*.jar 
-    do 
-        CLASSPATH=$CLASSPATH:$i
-    done
-fi
-
-#--------------------------------------------------
-# jars to set when using Fractal GUI
-if [ -f $PROACTIVE/lib/components/asm-2.2.1.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/asm-2.2.1.jar
-fi
-if [ -f $PROACTIVE/lib/components/fractal-adl.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/fractal-adl.jar
-fi
-if [ -f $PROACTIVE/lib/components/dtdparser.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/dtdparser.jar
-fi
-if [ -f $PROACTIVE/lib/components/ow_deployment_scheduling.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/ow_deployment_scheduling.jar
-fi
-if [ -f $PROACTIVE/lib/components/fractal-gui.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/fractal-gui.jar
-fi
-if [ -f $PROACTIVE/lib/components/fractal-swing.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/fractal-swing.jar
-fi
-if [ -f $PROACTIVE/lib/components/julia-runtime.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/julia-runtime.jar
-fi
-if [ -f $PROACTIVE/lib/components/julia-asm.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/julia-asm.jar
-fi
-if [ -f $PROACTIVE/lib/components/julia-mixins.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/julia-mixins.jar
-fi
-if [ -f $PROACTIVE/lib/components/SVGGraphics.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/components/SVGGraphics.jar
-fi
-
-#--------------------------------------------------
-# jars to set when using Web Services
-if [ -f $PROACTIVE/lib/ws/soap.jar ] 
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/soap.jar
-fi
-if [ -f $PROACTIVE/lib/ws/wsdl4j.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/wsdl4j.jar
-fi
-
-if [ -f $PROACTIVE/lib/ws/axis.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/axis.jar
-fi
-if [ -f $PROACTIVE/lib/ws/jaxrpc.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/jaxrpc.jar
-fi
-if [ -f $PROACTIVE/lib/ws/activation.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/activation.jar
-fi
-if [ -f $PROACTIVE/lib/ws/saaj-api.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/saaj-api.jar
-fi 
-
-if [ -f $PROACTIVE/lib/ws/commons-logging.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/commons-logging.jar
-fi
-if [ -f $PROACTIVE/lib/ws/commons-discovery.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/commons-discovery.jar
-fi
-if [ -f $PROACTIVE/lib/ws/mail.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/mail.jar
-fi
-if [ -f $PROACTIVE/lib/ws/xml-apis.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/ws/xml-apis.jar
-fi
-
-#--------------------------------------------------------
-# jars to set when using Scilab
-if [ -f $PROACTIVE/lib/scilab/javasci.jar ]
-then
-    CLASSPATH=$CLASSPATH:$PROACTIVE/lib/scilab/javasci.jar
-fi 
-
-#--------------------------------------------------------
-# when using TimIt
-if [ -f $PROACTIVE/lib/timit/batik-awt-util.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/batik-awt-util.jar
-fi
-if [ -f $PROACTIVE/lib/timit/batik-dom.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/batik-dom.jar
-fi
-if [ -f $PROACTIVE/lib/timit/batik-svggen.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/batik-svggen.jar
-fi
-if [ -f $PROACTIVE/lib/timit/batik-util.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/batik-util.jar
-fi
-if [ -f $PROACTIVE/lib/timit/batik-xml.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/batik-xml.jar
-fi
-if [ -f $PROACTIVE/lib/timit/commons-cli-1.0.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/commons-cli-1.0.jar
-fi
-if [ -f $PROACTIVE/lib/timit/jcommon-1.0.6.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/jcommon-1.0.6.jar
-fi
-if [ -f $PROACTIVE/lib/timit/jdom.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/jdom.jar
-fi
-if [ -f $PROACTIVE/lib/timit/jfreechart-1.0.2.jar ]
-then
-	CLASSPATH=$CLASSPATH:$PROACTIVE/lib/timit/jfreechart-1.0.2.jar
-fi
-
+# wildcard in classpath is not available in Java 5
+#CLASSPATH="$PROACTIVE/lib/*"
+for i in $PROACTIVE/lib/*.jar ; 
+do
+    CLASSPATH=$CLASSPATH:$i
+done
 
 
 #echo "CLASSPATH"=$CLASSPATH 
--- scripts/windows/init.bat
+++ scripts/windows/init.bat
@@ -28,7 +28,7 @@ IF EXIST "%PROACTIVE%\ProActive_examples.jar" set CLASSPATH=%CLASSPATH%;%PROACTI
 IF EXIST "%PROACTIVE%\lib\log4j.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\log4j.jar
 IF EXIST "%PROACTIVE%\lib\bouncycastle.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\bouncycastle.jar
 IF EXIST "%PROACTIVE%\lib\xercesImpl.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\xercesImpl.jar
-IF EXIST "%PROACTIVE%\lib\components\fractal.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\fractal.jar
+IF EXIST "%PROACTIVE%\lib\fractal.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\fractal.jar
 IF EXIST "%PROACTIVE%\lib\ganymed-ssh2-build210.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ganymed-ssh2-build210.jar
 IF EXIST "%PROACTIVE%\ic2d.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\ic2d.jar
 IF EXIST "%PROACTIVE%\lib\javassist.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\javassist.jar
@@ -36,72 +36,72 @@ IF EXIST "%PROACTIVE%\lib\javassist.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\l
 rem -------------------------------------------------
 rem jars for Globus
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\globus\cog-jglobus.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\cog-jglobus.jar
-IF EXIST "%PROACTIVE%\lib\globus\cog-ogce.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\cog-ogce.jar
-IF EXIST "%PROACTIVE%\lib\globus\cryptix.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\cryptix.jar
-IF EXIST "%PROACTIVE%\lib\globus\cryptix32.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\cryptix32.jar
-IF EXIST "%PROACTIVE%\lib\globus\cryptix-asn1.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\cryptix-asn1.jar
-IF EXIST "%PROACTIVE%\lib\globus\puretls.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\globus\puretls.jar
+IF EXIST "%PROACTIVE%\lib\cog-jglobus.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\cog-jglobus.jar
+IF EXIST "%PROACTIVE%\lib\cog-ogce.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\cog-ogce.jar
+IF EXIST "%PROACTIVE%\lib\cryptix.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\cryptix.jar
+IF EXIST "%PROACTIVE%\lib\cryptix32.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\cryptix32.jar
+IF EXIST "%PROACTIVE%\lib\cryptix-asn1.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\cryptix-asn1.jar
+IF EXIST "%PROACTIVE%\lib\puretls.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\puretls.jar
 
 rem -------------------------------------------------
 rem jars for Ibis
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\ibis\ibis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ibis\ibis.jar
+IF EXIST "%PROACTIVE%\lib.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib.jar
 
 rem -------------------------------------------------
 rem jars for Fractal GUI
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\components\fractal-adl.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\fractal-adl.jar
-IF EXIST "%PROACTIVE%\lib\components\dtdparser.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\dtdparser.jar
-IF EXIST "%PROACTIVE%\lib\components\asm-2.2.1.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\asm-2.2.1.jar
-IF EXIST "%PROACTIVE%\lib\components\ow_deployment_scheduling.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\ow_deployment_scheduling.jar
-IF EXIST "%PROACTIVE%\lib\components\fractal-gui.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\fractal-gui.jar
-IF EXIST "%PROACTIVE%\lib\components\fractal-swing.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\fractal-swing.jar
-IF EXIST "%PROACTIVE%\lib\components\julia-asm.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\julia-asm.jar
-IF EXIST "%PROACTIVE%\lib\components\julia-mixins.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\julia-mixins.jar
-IF EXIST "%PROACTIVE%\lib\components\julia-runtime.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\julia-runtime.jar
-IF EXIST "%PROACTIVE%\lib\components\SVGGraphics.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\components\SVGGraphics.jar
+IF EXIST "%PROACTIVE%\lib\fractal-adl.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\fractal-adl.jar
+IF EXIST "%PROACTIVE%\lib\dtdparser.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\dtdparser.jar
+IF EXIST "%PROACTIVE%\lib\asm-2.2.1.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\asm-2.2.1.jar
+IF EXIST "%PROACTIVE%\lib\ow_deployment_scheduling.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ow_deployment_scheduling.jar
+IF EXIST "%PROACTIVE%\lib\fractal-gui.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\fractal-gui.jar
+IF EXIST "%PROACTIVE%\lib\fractal-swing.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\fractal-swing.jar
+IF EXIST "%PROACTIVE%\lib\julia-asm.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\julia-asm.jar
+IF EXIST "%PROACTIVE%\lib\julia-mixins.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\julia-mixins.jar
+IF EXIST "%PROACTIVE%\lib\julia-runtime.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\julia-runtime.jar
+IF EXIST "%PROACTIVE%\lib\SVGGraphics.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\SVGGraphics.jar
 
 rem -------------------------------------------------
 rem jars for Web Services
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\ws\soap.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\soap.jar
-IF EXIST "%PROACTIVE%\lib\ws\wsdl4j.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\wsdl4j.jar
-IF EXIST "%PROACTIVE%\lib\ws\axis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\axis.jar
-IF EXIST "%PROACTIVE%\lib\ws\jaxrpc.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\jaxrpc.jar
-IF EXIST "%PROACTIVE%\lib\ws\activation.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\activation.jar
-IF EXIST "%PROACTIVE%\lib\ws\saaj-api.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\saaj-api.jar
-IF EXIST "%PROACTIVE%\lib\ws\commons-logging.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\commons-logging.jar
-IF EXIST "%PROACTIVE%\lib\ws\commons-discovery.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\commons-discovery.jar
-IF EXIST "%PROACTIVE%\lib\ws\mail.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\mail.jar
-IF EXIST "%PROACTIVE%\lib\ws\xml-apis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\xml-apis.jar
+IF EXIST "%PROACTIVE%\lib\soap.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\soap.jar
+IF EXIST "%PROACTIVE%\libdl4j.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\libdl4j.jar
+IF EXIST "%PROACTIVE%\lib\axis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\axis.jar
+IF EXIST "%PROACTIVE%\lib\jaxrpc.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\jaxrpc.jar
+IF EXIST "%PROACTIVE%\lib\activation.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\activation.jar
+IF EXIST "%PROACTIVE%\lib\saaj-api.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\saaj-api.jar
+IF EXIST "%PROACTIVE%\lib\commons-logging.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\commons-logging.jar
+IF EXIST "%PROACTIVE%\lib\commons-discovery.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\commons-discovery.jar
+IF EXIST "%PROACTIVE%\lib\mail.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\mail.jar
+IF EXIST "%PROACTIVE%\lib\xml-apis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\xml-apis.jar
 
 rem -------------------------------------------------
 rem jars for Web Services
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\ws\soap.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\soap.jar
-IF EXIST "%PROACTIVE%\lib\ws\wsdl4j.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\wsdl4j.jar
-IF EXIST "%PROACTIVE%\lib\ws\axis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\axis.jar
-IF EXIST "%PROACTIVE%\lib\ws\jaxrpc.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\jaxrpc.jar
-IF EXIST "%PROACTIVE%\lib\ws\activation.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\activation.jar
-IF EXIST "%PROACTIVE%\lib\ws\saaj-api.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\saaj-api.jar
-IF EXIST "%PROACTIVE%\lib\ws\commons-logging.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\commons-logging.jar
-IF EXIST "%PROACTIVE%\lib\ws\commons-discovery.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\commons-discovery.jar
-IF EXIST "%PROACTIVE%\lib\ws\mail.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\mail.jar
-IF EXIST "%PROACTIVE%\lib\ws\xml-apis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\ws\xml-apis.jar
+IF EXIST "%PROACTIVE%\lib\soap.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\soap.jar
+IF EXIST "%PROACTIVE%\libdl4j.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\libdl4j.jar
+IF EXIST "%PROACTIVE%\lib\axis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\axis.jar
+IF EXIST "%PROACTIVE%\lib\jaxrpc.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\jaxrpc.jar
+IF EXIST "%PROACTIVE%\lib\activation.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\activation.jar
+IF EXIST "%PROACTIVE%\lib\saaj-api.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\saaj-api.jar
+IF EXIST "%PROACTIVE%\lib\commons-logging.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\commons-logging.jar
+IF EXIST "%PROACTIVE%\lib\commons-discovery.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\commons-discovery.jar
+IF EXIST "%PROACTIVE%\lib\mail.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\mail.jar
+IF EXIST "%PROACTIVE%\lib\xml-apis.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\xml-apis.jar
 
 rem -------------------------------------------------
 rem jars for TimIt
 rem -------------------------------------------------
-IF EXIST "%PROACTIVE%\lib\timit\batik-awt-util.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\batik-awt-util.jar
-IF EXIST "%PROACTIVE%\lib\timit\batik-dom.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\batik-dom.jar
-IF EXIST "%PROACTIVE%\lib\timit\batik-svggen.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\batik-svggen.jar
-IF EXIST "%PROACTIVE%\lib\timit\batik-util.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\batik-util.jar
-IF EXIST "%PROACTIVE%\lib\timit\batik-xml.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\batik-xml.jar
-IF EXIST "%PROACTIVE%\lib\timit\commons-cli-1.0.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\commons-cli-1.0.jar
-IF EXIST "%PROACTIVE%\lib\timit\jcommon-1.0.6.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\jcommon-1.0.6.jar
-IF EXIST "%PROACTIVE%\lib\timit\jdom.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\jdom.jar
-IF EXIST "%PROACTIVE%\lib\timit\jfreechart-1.0.2.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\timit\jfreechart-1.0.2.jar
+IF EXIST "%PROACTIVE%\lib\batik-awt-util.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\batik-awt-util.jar
+IF EXIST "%PROACTIVE%\lib\batik-dom.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\batik-dom.jar
+IF EXIST "%PROACTIVE%\lib\batik-svggen.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\batik-svggen.jar
+IF EXIST "%PROACTIVE%\lib\batik-util.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\batik-util.jar
+IF EXIST "%PROACTIVE%\lib\batik-xml.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\batik-xml.jar
+IF EXIST "%PROACTIVE%\lib\commons-cli-1.0.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\commons-cli-1.0.jar
+IF EXIST "%PROACTIVE%\lib\jcommon-1.0.6.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\jcommon-1.0.6.jar
+IF EXIST "%PROACTIVE%\lib\jdom.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\jdom.jar
+IF EXIST "%PROACTIVE%\lib\jfreechart-1.0.2.jar" set CLASSPATH=%CLASSPATH%;%PROACTIVE%\lib\jfreechart-1.0.2.jar
 
 
 
