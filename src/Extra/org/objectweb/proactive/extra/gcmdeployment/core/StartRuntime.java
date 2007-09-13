67c16e110007c4ba6c61aed750b75749a1921608
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-09-13 13:56:49 +0000

    Fixed compile error
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6245 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/gcmdeployment/core/StartRuntime.java
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/core/StartRuntime.java
@@ -207,8 +207,6 @@ public class StartRuntime {
 
                 ProActiveRuntimeImpl.getProActiveRuntime()
                                     .setParent(parentRuntime);
-
-                parentRuntime.registerVirtualNode(localRuntime);
                 waitUntilInterupted();
             } catch (ProActiveException e) {
                 logger.warn("Cannot register to my parent", e);
