8603f03bfaf5e0df3c45658774b41a8fbf8e0ed0
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-09-20 07:50:22 +0000

    removed the old scheduler and related files - the new scheduler is on his way
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6332 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/infrastructuremanager/core/IMConstants.java
+++
@@ -1,6 +0,0 @@
-package org.objectweb.proactive.extra.infrastructuremanager.core;
-
-public interface IMConstants {
-    public static final String NAME_NODE_IM = "IMNODE";
-    public static final String NAME_ACTIVE_OBJECT_IMCORE = "IMCORE";
-}
