850a3d81f0275831e1881029aa9e6ed593017caf
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-10-11 15:31:05 +0000

    Arf ... Add TaskCreationException
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6509 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extra/org/objectweb/proactive/extra/scheduler/common/exception/TaskCreationException.java
@@ -0,0 +1,71 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive-support@inria.fr
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://www.inria.fr/oasis/ProActive/contacts.html
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.extra.scheduler.common.exception;
+
+
+/**
+ * @author cdelbe
+ * @since 2.2
+ */
+public class TaskCreationException extends SchedulerException {
+
+    /**
+     * @param arg0
+     */
+    public TaskCreationException(String arg0) {
+        super(arg0);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     *
+     */
+    public TaskCreationException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public TaskCreationException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param cause
+     */
+    public TaskCreationException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+}
