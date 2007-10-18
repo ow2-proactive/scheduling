379a6f12d551ab9ed9f15df7f878465e00d0145a
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-10-18 13:26:30 +0000

    Added deployment method
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6559 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- doc-src/guided_tour/examples/DeployedHelloWorld/src/active/Main.java
+++ doc-src/guided_tour/examples/DeployedHelloWorld/src/active/Main.java
@@ -24,7 +24,7 @@
  *
  *  Initial developer(s):               The ProActive Team
  *                        http://proactive.inria.fr/team_members.htm
- *  Contributor(s):
+ *  Contributor(s):	Vasile Jureschi
  *
  * ################################################################
  */
@@ -35,35 +35,39 @@ import org.objectweb.proactive.ProActive;
 import org.objectweb.proactive.api.ProDeployment;
 import org.objectweb.proactive.core.ProActiveException;
 import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
-import org.objectweb.proactive.core.descriptor.data.VirtualNode;
 import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.core.node.NodeException;
 import org.objectweb.proactive.api.ProActiveObject;
 
 public class Main{
-	public static void main(String args[])
-	{
-		String path = new String(System.getProperty("user.dir") + 
-								"/src/active/deployment.xml");
+	//deployment method
+	private static Node deploy(String descriptor)	{
 		ProActiveDescriptor pad;
-		VirtualNode remoteNode; 
 		Node node;
 		try {
-			pad = ProDeployment.getProactiveDescriptor(path);
-			remoteNode = pad.getVirtualNode("remoteNode");
-			// Returns the VirtualNode Workers described
-			// in the xml file as a java object
-			remoteNode.activate();
-			// Activates the VirtualNode
-			node = remoteNode.getNode();
-			// Returns the first node available among nodes
-			// mapped to the VirtualNode
-			//shutdown hook
-			
-			InitializedHelloWorld ao = (InitializedHelloWorld)ProActiveObject.newActive(
-					InitializedHelloWorld.class.getName(),
-		            new Object [] {},
-		            node);
+			//create object representation of the deployment file
+			pad = ProDeployment.getProactiveDescriptor(descriptor);
+			//active all Virtual Nodes
+			pad.activateMappings();
+			//get the first Node available in the first Virtual Node 
+			//specified in the descriptor file
+			node = pad.getVirtualNodes()[0].getNode();
+			return node;
+		}
+		catch (NodeException nodeExcep){
+			System.err.println(nodeExcep.getMessage());
+		}
+		catch(ProActiveException proExcep){
+			System.err.println(proExcep.getMessage());
+		}
+		return null;
+	}
+	public static void main(String args[]){
+		try{
+		 	InitializedHelloWorld ao = (InitializedHelloWorld)ProActiveObject.newActive(
+		 								InitializedHelloWorld.class.getName(),
+		 								new Object [] {}, deploy(args[0]));
+			//say hello 
 			System.out.println(ao.sayHello()); //possible wait-by-necessity
 			ao.terminate();
 		}
@@ -75,9 +79,6 @@ public class Main{
 		}
 		catch(IOException ioExcep){
 			System.err.println(ioExcep.getMessage());
-		}
-		catch(ProActiveException proExcep){
-			System.err.println(proExcep.getMessage());
 		}
 		//quitting
 		ProActive.exitSuccess();
