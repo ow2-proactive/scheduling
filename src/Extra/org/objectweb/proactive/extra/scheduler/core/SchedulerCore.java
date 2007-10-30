4f39270ed3abc916d7c1687cc75d541c1b74d572
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-10-30 12:50:28 +0000

    - Unwrap future for task results - Remove unused code
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6706 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/scheduler/core/SchedulerCore.java
+++ src/Extra/org/objectweb/proactive/extra/scheduler/core/SchedulerCore.java
@@ -634,15 +634,13 @@ public class SchedulerCore implements SchedulerCoreInterface, RunActive {
             //if the node has died, a runtimeException is sent instead of the result
             TaskResult res = null;
             res = job.getJobResult().getTaskResults().get(descriptor.getName());
+            // unwrap future
+            res = (TaskResult) ProFuture.getFutureValue(res);
 
-            // TMP : HANDLE DESCIPTORS
+            // HANDLE DESCIPTORS
             if (res != null) {
                 res.setDescriptorClass(descriptor.getResultDescriptor());
             }
-            // ARGGGGL : UGLYYYYY
-
-            // set descriptor if any
-            descriptor.getResultDescriptor();
 
             if (res != null) {
                 if (ProException.isException(res)) {
