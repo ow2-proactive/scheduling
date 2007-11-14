c943389fad02dac071901f8e71d872ff5432b56e
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-11-14 16:50:37 +0000

    Remove duplicated LGPL licence.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@6861 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Examples/org/objectweb/proactive/examples/eratosthenes/PrimeNumberImpl.java
+++ src/Examples/org/objectweb/proactive/examples/eratosthenes/PrimeNumberImpl.java
@@ -35,37 +35,6 @@ import org.objectweb.proactive.core.util.log.Loggers;
 import org.objectweb.proactive.core.util.log.ProActiveLogger;
 
 
-/*
- * ################################################################
- *
- * ProActive: The Java(TM) library for Parallel, Distributed,
- *            Concurrent computing with Security and Mobility
- *
- * Copyright (C) 1997-2002 INRIA/University of Nice-Sophia Antipolis
- * Contact: proactive-support@inria.fr
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- * USA
- *
- *  Initial developer(s):               The ProActive Team
- *                        http://www.inria.fr/oasis/ProActive/contacts.html
- *  Contributor(s):
- *
- * ################################################################
- */
-
 /**
  * @author Jonathan Streit
  *
