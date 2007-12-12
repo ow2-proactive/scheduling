81f8cd2ace0e530923a97ceb99ee746fdefa64cc
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-12-12 16:56:39 +0000

    finding scilab command windows 32 bit
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7382 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/scheduler/ext/scilab/find_scilab_command.bat
+++ src/Extensions/org/objectweb/proactive/extensions/scheduler/ext/scilab/find_scilab_command.bat
@@ -1,4 +1,11 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
-rem NOT IMPLEMENTED YET
+rem ************* We look at the Registry key where the Path to Scilab is stored *************
+FOR /F "usebackq skip=2 tokens=1,3 delims=	" %%i in ( `REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Scilab" /s` ) DO (
+	rem ************** We look at the right key among the results, we exit at the first Matlab instance ***********************
+	IF "%%i" == "    SCIPATH" (
+		echo %%j
+		break
+	)
+)
 ENDLOCAL
