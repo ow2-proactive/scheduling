16952ded767a368f5c762e8a5c1a7c5efcdee9c7
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-12-12 16:14:12 +0000

    retrieving matlab version in windows script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7377 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/scheduler/ext/matlab/find_matlab_command.bat
+++ src/Extensions/org/objectweb/proactive/extensions/scheduler/ext/matlab/find_matlab_command.bat
@@ -1,6 +1,7 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 rem ************* We look at the Registry key where the Path to Matlab is stored *************
+rem *** retrieve the Matlab command
 FOR /F "usebackq skip=2 tokens=1,3 delims=	" %%i in ( `REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB" /s` ) DO (
 	rem ************** We look at the right key among the results, we exit at the first Matlab instance ***********************
 	IF "%%i" == "    MATLABROOT" (
@@ -8,5 +9,13 @@ FOR /F "usebackq skip=2 tokens=1,3 delims=	" %%i in ( `REG QUERY "HKEY_LOCAL_MAC
 		break
 	)
 )
-echo win32
+echo bin\win32
+rem *** retrieve the Matlab version
+FOR /F "usebackq skip=2 tokens=1,2,3,4,5 delims=\" %%i in ( `REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Mathworks\MATLAB" /s` ) DO (
+	rem ************** We look at the right key among the results, we exit at the first Matlab instance ***********************
+	IF "%%m" NEQ "" (
+		echo %%m
+		break
+	)
+)
 ENDLOCAL
