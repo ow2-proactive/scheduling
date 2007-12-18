711325eedb957c3236fe3f5ccdb304488514df82
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2007-12-18 10:41:38 +0000

    - Remove 'Verifying current directory' part in scripts
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7494 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/scheduler/RMlauncher.bat
+++ scripts/windows/scheduler/RMlauncher.bat
@@ -2,27 +2,6 @@
 echo. 
 echo --- Resource Manager -------------------------------------
 
-rem --- Verifying current directory
-SET COMMAND=%0
-IF NOT "%COMMAND:~-4%" == ".bat" (
- SET COMMAND=%0.bat
-)
- 
-SET OK=0
-FOR /F %%i in ('dir /b') do IF "%%i" == "%COMMAND%" SET OK=1
-
-IF %OK% == 0 (
-echo scripts must be started in the same directory as the script.
-goto end
-)
-
-goto doit
-
-:usage
-echo. 
-goto end
-:doit
-
 SETLOCAL ENABLEDELAYEDEXPANSION
 IF NOT DEFINED PROACTIVE set PROACTIVE=%CD%\..\..\..
 call "%PROACTIVE%\scripts\windows\init.bat"
--- scripts/windows/scheduler/communicator.bat
+++ scripts/windows/scheduler/communicator.bat
@@ -2,28 +2,6 @@
 echo
 echo --- Communicator ----------------------------------------------
 
-rem --- Verifying current directory
-SET COMMAND=%0
-IF NOT "%COMMAND:~-4%" == ".bat" (
- SET COMMAND=%0.bat
-)
- 
-SET OK=0
-FOR /F %%i in ('dir /b') do IF "%%i" == "%COMMAND%" SET OK=1
-
-IF %OK% == 0 (
-echo scripts must be started in the same directory as the script.
-goto end
-)
-
-goto doit
-
-:usage
-echo. 
-goto end
-
-
-:doit
   SET SCHEDULER_URL=%1
 
 SETLOCAL ENABLEDELAYEDEXPANSION
