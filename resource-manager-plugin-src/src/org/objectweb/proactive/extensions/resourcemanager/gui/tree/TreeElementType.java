373bb41f38207786513d1786486f0de2429e5a7d
jfradj <jfradj@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-15 13:05:43 +0000

    1/2 Suppress garbage to replace it with another garbage
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7733 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- resource-manager-plugin-src/src/org/objectweb/proactive/extensions/resourcemanager/gui/tree/TreeElementType.java
+++
@@ -1,8 +0,0 @@
-package org.objectweb.proactive.extensions.resourcemanager.gui.tree;
-
-/**
- * @author FRADJ Johann
- */
-public enum TreeElementType {
-    ROOT, RESOURCE_MANAGER, SOURCE, HOST, VIRTUAL_MACHINE, NODE;
-}
