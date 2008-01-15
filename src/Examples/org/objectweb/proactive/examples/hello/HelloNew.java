beacc6718f0dd4894bbf21e3a1095592823a2f13
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-15 12:45:07 +0000

    Format
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7732 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Examples/org/objectweb/proactive/examples/hello/HelloNew.java
+++ src/Examples/org/objectweb/proactive/examples/hello/HelloNew.java
@@ -36,24 +36,24 @@ public class HelloNew implements java.io.Serializable {
         return ProActiveInet.getInstance().getInetAddress().getHostName();
     }
 
-    
-//    public static class Deployer {
-//        public boolean gotNodeAttached = false;
-//
-//        public void nodeAttached(Node node, VirtualNode vNode) {
-//            System.out.println("Recieved Node Attachment notif");
-//            gotNodeAttached = true;
-//        }
-//        
-//    }
-    
+    //    public static class Deployer {
+    //        public boolean gotNodeAttached = false;
+    //
+    //        public void nodeAttached(Node node, VirtualNode vNode) {
+    //            System.out.println("Recieved Node Attachment notif");
+    //            gotNodeAttached = true;
+    //        }
+    //        
+    //    }
+
     /** The call that starts the Acive Objects, and displays results.
      * @param args must contain the name of an xml descriptor */
     public static void main(String[] args) throws Exception {
         // Access the nodes of the descriptor file
-        
-        GCMApplicationDescriptor applicationDescriptor = API.getGCMApplicationDescriptor(new File("/home/glaurent/workspace/proactive_trunk/descriptors/helloApplicationLocal.xml"));
-        
+
+        GCMApplicationDescriptor applicationDescriptor = API.getGCMApplicationDescriptor(new File(
+            "/home/glaurent/workspace/proactive_trunk/descriptors/helloApplicationLocal.xml"));
+
         VirtualNode vnode = applicationDescriptor.getVirtualNode("Hello");
 
         applicationDescriptor.startDeployment();
@@ -61,14 +61,13 @@ public class HelloNew implements java.io.Serializable {
         while (vnode.getNbCurrentNodes() == 0) {
             Thread.sleep(1000);
         }
-                
+
         Node firstNode = vnode.getCurrentNodes().iterator().next();
-        
+
         // Creates an active instance of class Hello2 on the local node
         final HelloNew hello = (HelloNew) PAActiveObject.newActive(Hello.class.getName(), // the class to deploy
                 null, // the arguments to pass to the constructor, here none
                 firstNode); // which jvm should be used to hold the Active Object
-
 
         StringMutableWrapper received = hello.sayHello(); // possibly remote call
         logger.info("On " + getHostName() + ", a message was received: " + received); // potential wait-by-necessity 
