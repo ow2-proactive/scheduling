6ca397896c4e94e8e18e726791a08d50e9310d6e
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-15 13:14:29 +0000

    update documentation & getResult.sh script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7735 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/unix/scheduler/getResult.sh
+++ scripts/unix/scheduler/getResult.sh
@@ -9,7 +9,7 @@ PROACTIVE=$workingDir/../..
 CLASSPATH=.
 . $workingDir/env.sh
 
-$JAVACMD org.objectweb.proactive.extensions.scheduler.examples.GetJobResult $SCHEDULER_URL
+$JAVACMD org.objectweb.proactive.extensions.scheduler.examples.GetJobResult $1 $2 $3
 
 echo
 
