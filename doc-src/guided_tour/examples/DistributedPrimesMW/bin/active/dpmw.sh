087530ae56482fe5e5d10b3b800ca56d71c706eb
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-01-28 17:13:55 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7849 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- doc-src/guided_tour/examples/DistributedPrimesMW/bin/active/dpmw.sh
+++ doc-src/guided_tour/examples/DistributedPrimesMW/bin/active/dpmw.sh
@@ -5,7 +5,7 @@ echo --- Distributed Primes With Master-Worker API -------------------
 
 workingDir=`dirname $0`
 . $workingDir/env.sh
-$JAVACMD active.DistributedPrimesMW 100
+$JAVACMD active.DistributedPrimesMW $1
 
 echo
 echo -----------------------------------------------------------------
