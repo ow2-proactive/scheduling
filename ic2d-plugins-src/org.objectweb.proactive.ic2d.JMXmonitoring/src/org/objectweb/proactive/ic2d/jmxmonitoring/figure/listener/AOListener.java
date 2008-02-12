429a2bdf449bc61f7de8330cd6af85aacb798f8c
glaurent <glaurent@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-02-12 16:43:41 +0000

    format (not mine, just that running build format changed it)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@7947 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring/src/org/objectweb/proactive/ic2d/jmxmonitoring/figure/listener/AOListener.java
+++ ic2d-plugins-src/org.objectweb.proactive.ic2d.JMXmonitoring/src/org/objectweb/proactive/ic2d/jmxmonitoring/figure/listener/AOListener.java
@@ -52,131 +52,129 @@ import org.objectweb.proactive.ic2d.jmxmonitoring.figure.NodeFigure;
 import org.objectweb.proactive.ic2d.jmxmonitoring.figure.RoundedLine;
 import org.objectweb.proactive.ic2d.jmxmonitoring.view.MonitoringView;
 
+
 public class AOListener implements MouseListener, MouseMotionListener {
-	private ActiveObject ao;
-	private AOFigure figure;
-	private ActionRegistry registry;
-	private DragAndDrop dnd;
-	private NodeFigure parentFigure;
+    private ActiveObject ao;
+    private AOFigure figure;
+    private ActionRegistry registry;
+    private DragAndDrop dnd;
+    private NodeFigure parentFigure;
 
-	public AOListener(ActiveObject ao, AOFigure figure,
-			MonitoringView monitoringView, NodeFigure parentFigure) {
-		this.ao = ao;
-		this.figure = figure;
-		this.parentFigure = parentFigure;
-		this.dnd = monitoringView.getDragAndDrop();
-		this.registry = monitoringView.getGraphicalViewer().getActionRegistry();
-	}
+    public AOListener(ActiveObject ao, AOFigure figure, MonitoringView monitoringView, NodeFigure parentFigure) {
+        this.ao = ao;
+        this.figure = figure;
+        this.parentFigure = parentFigure;
+        this.dnd = monitoringView.getDragAndDrop();
+        this.registry = monitoringView.getGraphicalViewer().getActionRegistry();
+    }
 
-	public void mousePressed(MouseEvent me) {
-		// Left button click
-		if (me.button == 1) {
-			dnd.setSource(ao);
-			dnd.setSourceFigure(figure);
-			figure.setHighlight(ColorConstants.green);
-			// All actions that implements IActionExtPoint selects the current
-			// active object
-			final Iterator it = registry.getActions();
-			while (it.hasNext()) {
-				final IAction act = (IAction) it.next();
-				if (act instanceof IActionExtPoint) {
-					((IActionExtPoint) act).setActiveSelect(this.ao);
-				}
-			}
-		} else if (me.button == 3) {
-			final Iterator it = registry.getActions();
-			while (it.hasNext()) {
-				final IAction act = (IAction) it.next();
-				// System.out.println(act);
-				if ((act instanceof ShowConnectionsAction)
-						&& !WorldEditPart.displayTopology()) {
-					ShowConnectionsAction showInConnectionsAction = (ShowConnectionsAction) act;
-					showInConnectionsAction.setObject(this.figure);
-					showInConnectionsAction.setEnabled(true);
-				} else if (act instanceof IActionExtPoint) {
-					IActionExtPoint extensionAction = (IActionExtPoint) act;
-					extensionAction.setAbstractDataObject(this.ao);
-				} else if (act instanceof ZoomOutAction
-						|| act instanceof ZoomInAction) {
-					act.setEnabled(true);
-				} else {
-					act.setEnabled(false);
-				}
-			}
+    public void mousePressed(MouseEvent me) {
+        // Left button click
+        if (me.button == 1) {
+            dnd.setSource(ao);
+            dnd.setSourceFigure(figure);
+            figure.setHighlight(ColorConstants.green);
+            // All actions that implements IActionExtPoint selects the current
+            // active object
+            final Iterator it = registry.getActions();
+            while (it.hasNext()) {
+                final IAction act = (IAction) it.next();
+                if (act instanceof IActionExtPoint) {
+                    ((IActionExtPoint) act).setActiveSelect(this.ao);
+                }
+            }
+        } else if (me.button == 3) {
+            final Iterator it = registry.getActions();
+            while (it.hasNext()) {
+                final IAction act = (IAction) it.next();
+                // System.out.println(act);
+                if ((act instanceof ShowConnectionsAction) && !WorldEditPart.displayTopology()) {
+                    ShowConnectionsAction showInConnectionsAction = (ShowConnectionsAction) act;
+                    showInConnectionsAction.setObject(this.figure);
+                    showInConnectionsAction.setEnabled(true);
+                } else if (act instanceof IActionExtPoint) {
+                    IActionExtPoint extensionAction = (IActionExtPoint) act;
+                    extensionAction.setAbstractDataObject(this.ao);
+                } else if (act instanceof ZoomOutAction || act instanceof ZoomInAction) {
+                    act.setEnabled(true);
+                } else {
+                    act.setEnabled(false);
+                }
+            }
 
-			// // Monitor a new host
-			// registry.getAction(NewHostAction.NEW_HOST).setEnabled(false);
-			//			
-			// // Set depth control
-			// registry.getAction(SetDepthAction.SET_DEPTH).setEnabled(false);
-			//				
-			// // Refresh
-			// registry.getAction(RefreshAction.REFRESH).setEnabled(false);
-			//			
-			// // Set time to refresh
-			// registry.getAction(SetTTRAction.SET_TTR).setEnabled(false);
-			//
-			// // Look for new JVM
-			// registry.getAction(RefreshHostAction.REFRESH_HOST).setEnabled(false);
-			//			
-			// // Look for new Nodes
-			// registry.getAction(RefreshJVMAction.REFRESH_JVM).setEnabled(false);
-			//			
-			// // Look for new Active Objects
-			// registry.getAction(RefreshNodeAction.REFRESH_NODE).setEnabled(false);
-			//			
-			// // Stop monitoring this ...
-			// registry.getAction(StopMonitoringAction.STOP_MONITORING).setEnabled(false);
-			//			
-			// // Kill VM
-			// registry.getAction(KillVMAction.KILLVM).setEnabled(false);
-			//			
-			// // Set update frequence...
-			// registry.getAction(SetUpdateFrequenceAction.SET_UPDATE_FREQUENCE).setEnabled(false);
-			//			
-			// // Vertical Layout
-			// registry.getAction(VerticalLayoutAction.VERTICAL_LAYOUT).setEnabled(false);
-			//			
-			// // Horizontal Layout
-			// registry.getAction(HorizontalLayoutAction.HORIZONTAL_LAYOUT).setEnabled(false);
-			//			
-			// // Manual handling of an action for timer snapshot ... needs
-			// improvement
-			// IAction anAction = registry.getAction("Get timer snapshot");
-			// if ( anAction != null ){
-			// ((IActionExtPoint)anAction).setAbstractDataObject(this.ao);
-			// anAction.setText("Get Timers Snapshot");
-			// anAction.setEnabled(true);
-			// }
-		}
-	}
+            // // Monitor a new host
+            // registry.getAction(NewHostAction.NEW_HOST).setEnabled(false);
+            //			
+            // // Set depth control
+            // registry.getAction(SetDepthAction.SET_DEPTH).setEnabled(false);
+            //				
+            // // Refresh
+            // registry.getAction(RefreshAction.REFRESH).setEnabled(false);
+            //			
+            // // Set time to refresh
+            // registry.getAction(SetTTRAction.SET_TTR).setEnabled(false);
+            //
+            // // Look for new JVM
+            // registry.getAction(RefreshHostAction.REFRESH_HOST).setEnabled(false);
+            //			
+            // // Look for new Nodes
+            // registry.getAction(RefreshJVMAction.REFRESH_JVM).setEnabled(false);
+            //			
+            // // Look for new Active Objects
+            // registry.getAction(RefreshNodeAction.REFRESH_NODE).setEnabled(false);
+            //			
+            // // Stop monitoring this ...
+            // registry.getAction(StopMonitoringAction.STOP_MONITORING).setEnabled(false);
+            //			
+            // // Kill VM
+            // registry.getAction(KillVMAction.KILLVM).setEnabled(false);
+            //			
+            // // Set update frequence...
+            // registry.getAction(SetUpdateFrequenceAction.SET_UPDATE_FREQUENCE).setEnabled(false);
+            //			
+            // // Vertical Layout
+            // registry.getAction(VerticalLayoutAction.VERTICAL_LAYOUT).setEnabled(false);
+            //			
+            // // Horizontal Layout
+            // registry.getAction(HorizontalLayoutAction.HORIZONTAL_LAYOUT).setEnabled(false);
+            //			
+            // // Manual handling of an action for timer snapshot ... needs
+            // improvement
+            // IAction anAction = registry.getAction("Get timer snapshot");
+            // if ( anAction != null ){
+            // ((IActionExtPoint)anAction).setAbstractDataObject(this.ao);
+            // anAction.setText("Get Timers Snapshot");
+            // anAction.setEnabled(true);
+            // }
+        }
+    }
 
-	public void mouseReleased(MouseEvent me) {
-		parentFigure.handleMouseReleased(me);
-	}
+    public void mouseReleased(MouseEvent me) {
+        parentFigure.handleMouseReleased(me);
+    }
 
-	public void mouseEntered(MouseEvent me) {
-		parentFigure.handleMouseEntered(me);
-	}
+    public void mouseEntered(MouseEvent me) {
+        parentFigure.handleMouseEntered(me);
+    }
 
-	public void mouseExited(MouseEvent me) {
-		// // Disable Timer Snapshot
-		// IAction anAction = registry.getAction("Get timers snapshot");
-		// if (anAction != null) {
-		// anAction.setEnabled(false);
-		// }
-		parentFigure.handleMouseMoved(me);
-	}
+    public void mouseExited(MouseEvent me) {
+        // // Disable Timer Snapshot
+        // IAction anAction = registry.getAction("Get timers snapshot");
+        // if (anAction != null) {
+        // anAction.setEnabled(false);
+        // }
+        parentFigure.handleMouseMoved(me);
+    }
 
-	public void mouseDoubleClicked(MouseEvent me) { /* Do nothing */
-	}
+    public void mouseDoubleClicked(MouseEvent me) { /* Do nothing */
+    }
 
-	public void mouseDragged(MouseEvent me) { /* Do nothing */
-	}
+    public void mouseDragged(MouseEvent me) { /* Do nothing */
+    }
 
-	public void mouseHover(MouseEvent me) { /* Do nothing */
-	}
+    public void mouseHover(MouseEvent me) { /* Do nothing */
+    }
 
-	public void mouseMoved(MouseEvent me) { /* Do nothing */
-	}
+    public void mouseMoved(MouseEvent me) { /* Do nothing */
+    }
 }
