ea59057a2cb529a0b7170f721db2c174d07443f3
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-05 09:48:46 +0000

    Fixed LoadBalacing functional test gcm deployment compatible
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8130 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/loadbalancing/Test.java
+++ src/Tests/functionalTests/loadbalancing/Test.java
@@ -30,47 +30,42 @@
  */
 package functionalTests.loadbalancing;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.objectweb.proactive.api.PAActiveObject;
-import org.objectweb.proactive.api.PADeployment;
-import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
-import org.objectweb.proactive.core.descriptor.data.VirtualNode;
 import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.loadbalancing.LoadBalancing;
 import org.objectweb.proactive.loadbalancing.metrics.currenttimemillis.CurrentTimeMillisMetricFactory;
 
-import functionalTests.FunctionalTest;
-import static junit.framework.Assert.assertTrue;
+import functionalTests.FunctionalTestDefaultNodes;
 
 
 /**
  * Test load balancing
  */
-public class Test extends FunctionalTest {
-    private String XML_LOCATION = Test.class.getResource("/functionalTests/loadbalancing/LoadBalancing.xml")
-            .getPath();
-    private ProActiveDescriptor pad;
-    private VirtualNode vn1;
+public class Test extends FunctionalTestDefaultNodes {
     A a;
     Node nodeOne;
     Node nodeTwo;
 
+    public Test() {
+        super(DeploymentType._1x2);
+    }
+
     @org.junit.Test
     public void action() throws Exception {
         a = (A) PAActiveObject.newActive(A.class.getName(), null, nodeOne);
+
+        String oldLocation = a.getNodeUrl();
         Thread.sleep(1000);
-        assertTrue(a.getNodeUrl().equals(nodeTwo.getNodeInformation().getURL()));
+        String newLocation = a.getNodeUrl();
+        Assert.assertNotSame(oldLocation, newLocation);
     }
 
     @Before
     public void initTest() throws Exception {
-        this.pad = PADeployment.getProactiveDescriptor(XML_LOCATION);
-        this.pad.activateMappings();
-        this.vn1 = this.pad.getVirtualNode("VN");
-        assertTrue(this.vn1.getMinNumberOfNodes() <= this.vn1.getNumberOfCreatedNodesAfterDeployment());
-        this.nodeOne = this.vn1.getNode();
-        this.nodeTwo = this.vn1.getNode();
-
+        this.nodeOne = super.getANode();
+        this.nodeTwo = super.getANode();
         LoadBalancing.activateOn(new Node[] { nodeOne, nodeTwo }, new CurrentTimeMillisMetricFactory());
     }
 }
