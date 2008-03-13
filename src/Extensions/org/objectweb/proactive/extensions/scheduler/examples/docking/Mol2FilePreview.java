31879672824b100daeba6a434d1816fccbfcb3b5
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-13 13:17:00 +0000

    Formaaaaaaaaaaaaaaaaaaaaaaaaat
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8177 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/scheduler/examples/docking/Mol2FilePreview.java
+++ src/Extensions/org/objectweb/proactive/extensions/scheduler/examples/docking/Mol2FilePreview.java
@@ -11,46 +11,47 @@ import org.objectweb.proactive.extensions.scheduler.common.task.ResultPreview;
 import org.objectweb.proactive.extensions.scheduler.common.task.TaskResult;
 import org.objectweb.proactive.extensions.scheduler.common.task.util.ResultPreviewTool.SimpleTextPanel;
 
+
 /**
  * @author cdelbe
  *
  */
 public class Mol2FilePreview extends ResultPreview {
 
-	private static final String MATCH_PATTERN = "Produced output file : ";
+    private static final String MATCH_PATTERN = "Produced output file : ";
 
-	@Override
-	public JPanel getGraphicalDescription(TaskResult r) {
-		try {
-			String pathToResult = this.getPathToFile(r.getOuput().getStdoutLogs(false));
-			String molEditor = System.getenv("MOL_EDITOR");
-			if (molEditor==null){
-				// mercury is default (supposed to be in the path)
-				molEditor = "mercury";
-			}
-			String command = molEditor + " " + pathToResult;
-			try {
-				Runtime.getRuntime().exec(command); 
-			} catch (IOException e) {
-				return new SimpleTextPanel("Unable to open external display : " + e.getMessage());
-			}
-			return new SimpleTextPanel("External display : " + command);
-		} catch (RuntimeException e) {
-			return new SimpleTextPanel("Unable to open external display : " + e.getMessage());
-		}
-	}
+    @Override
+    public JPanel getGraphicalDescription(TaskResult r) {
+        try {
+            String pathToResult = this.getPathToFile(r.getOuput().getStdoutLogs(false));
+            String molEditor = System.getenv("MOL_EDITOR");
+            if (molEditor == null) {
+                // mercury is default (supposed to be in the path)
+                molEditor = "mercury";
+            }
+            String command = molEditor + " " + pathToResult;
+            try {
+                Runtime.getRuntime().exec(command);
+            } catch (IOException e) {
+                return new SimpleTextPanel("Unable to open external display : " + e.getMessage());
+            }
+            return new SimpleTextPanel("External display : " + command);
+        } catch (RuntimeException e) {
+            return new SimpleTextPanel("Unable to open external display : " + e.getMessage());
+        }
+    }
 
-	@Override
-	public String getTextualDescription(TaskResult r) {
-		return "Ouput file : " + this.getPathToFile(r.getOuput().getStdoutLogs(false));
-	}
+    @Override
+    public String getTextualDescription(TaskResult r) {
+        return "Ouput file : " + this.getPathToFile(r.getOuput().getStdoutLogs(false));
+    }
 
-	private String getPathToFile(String output) {
-		int pos = output.indexOf(MATCH_PATTERN, 0);
-		pos += MATCH_PATTERN.length();
-		String extracted = output.substring(pos, output.indexOf(".mol2", pos));
-		//extracted = ResultPreviewTool.getSystemCompliantPath(extracted);
-		return extracted + ".mol2";
-	}
+    private String getPathToFile(String output) {
+        int pos = output.indexOf(MATCH_PATTERN, 0);
+        pos += MATCH_PATTERN.length();
+        String extracted = output.substring(pos, output.indexOf(".mol2", pos));
+        //extracted = ResultPreviewTool.getSystemCompliantPath(extracted);
+        return extracted + ".mol2";
+    }
 
 }
