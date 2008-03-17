b41a01297f9078be87c732b6aedf2209d8ac0bce
tdobek <tdobek@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-17 10:34:39 +0000

    p2p gcm deployment parser
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8192 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/GCMDeployment/GCMDeploymentAcquisition.java
@@ -0,0 +1,30 @@
+package org.objectweb.proactive.extra.gcmdeployment.GCMDeployment;
+
+import java.util.List;
+
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.acquisition.LookupEntry;
+import org.objectweb.proactive.extra.gcmdeployment.GCMDeployment.acquisition.P2PEntry;
+
+public class GCMDeploymentAcquisition {
+
+	private List<LookupEntry> lookupEntries;
+
+	private List<P2PEntry> p2pEntries;
+
+	public List<LookupEntry> getLookupEntries() {
+		return lookupEntries;
+	}
+
+	public void setLookupEntries(List<LookupEntry> lookupEntries) {
+		this.lookupEntries = lookupEntries;
+	}
+
+	public List<P2PEntry> getP2pEntries() {
+		return p2pEntries;
+	}
+
+	public void setP2pEntries(List<P2PEntry> entries) {
+		p2pEntries = entries;
+	}
+
+}
