5fbd9b66cc6f85323a026ef16c0409010ce61773
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-18 17:42:58 +0000

    example for initialized cma
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8223 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Examples/org/objectweb/proactive/examples/userguide/cmagent/initialized/CMAgentInitialized.java
@@ -0,0 +1,21 @@
+//@snippet-start cma_init_full
+package org.objectweb.proactive.examples.userguide.cmagent.initialized;
+
+import org.objectweb.proactive.Body;
+import org.objectweb.proactive.EndActive;
+import org.objectweb.proactive.InitActive;
+import org.objectweb.proactive.examples.userguide.cmagent.simple.CMAgent;
+
+public class CMAgentInitialized extends CMAgent implements InitActive,EndActive {
+
+	public void initActivity(Body body) {
+    	System.out.println("--> Started Active object " + body.getMBean().getName() + 
+    			" on " + body.getMBean().getNodeUrl());
+    }
+
+	public void endActivity(Body body) {
+		System.out.println("Active object stopped.");
+	}
+
+}
+//@snippet-end cma_init_full
