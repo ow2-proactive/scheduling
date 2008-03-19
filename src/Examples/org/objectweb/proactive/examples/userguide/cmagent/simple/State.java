8db61394d8b6642d5f271ef69e6113f25bf9d3e0
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-19 10:11:42 +0000

    not commited and it broke the build (not to mentionm kill a lot of kittens in the process) because other cma packages depend on it
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8234 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Examples/org/objectweb/proactive/examples/userguide/cmagent/simple/State.java
@@ -0,0 +1,51 @@
+//@snippet-start state_full
+package org.objectweb.proactive.examples.userguide.cmagent.simple;
+
+import java.io.Serializable;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+
+public class State implements Serializable {
+    private long commitedMemory = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getCommitted();
+    private long initMemory = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getInit();
+    private long maxMemory = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax();
+    private long usedMemory = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed();
+    private double loadAverage = ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage();
+    private String osArch = ManagementFactory.getOperatingSystemMXBean().getArch();
+    private String osName = ManagementFactory.getOperatingSystemMXBean().getName();
+    private String osVersion = ManagementFactory.getOperatingSystemMXBean().getVersion();
+    private int osProcs = ManagementFactory.getOperatingSystemMXBean().getAvailableProcessors();
+    private int liveThreads = ManagementFactory.getThreadMXBean().getThreadCount();
+    private long startedThreads = ManagementFactory.getThreadMXBean().getTotalStartedThreadCount();
+    private int peakThreads = ManagementFactory.getThreadMXBean().getPeakThreadCount();
+    private int deamonThreads = ManagementFactory.getThreadMXBean().getDaemonThreadCount();
+    private Date timePoint = new Date();
+    private String hostname;
+    {
+        try {
+            hostname = InetAddress.getLocalHost().toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public State() {
+    }
+
+    public String toString() {
+
+        return new String("======= [" + "State at " + timePoint + " on " + hostname + "] =======" +
+            "\nCommited memory: " + commitedMemory + " bytes\nInitial memory requested: " + initMemory +
+            " bytes\nMaximum memory available: " + maxMemory + " bytes\nUsed memory: " + usedMemory +
+            " bytes\nLoad average: " + loadAverage + "\nOperating System: " + osName + " " + osVersion + " " +
+            osArch + "\nProcessors: " + osProcs + "\nCurrent live threads: " + liveThreads +
+            "\nTotal started threads: " + startedThreads + "\nPeak number of live threads: " + peakThreads +
+            "\nCurrent daemon threads: " + deamonThreads +
+            "\n===============================================================================\n");
+
+    }
+}
+//@snippet-end state_full
