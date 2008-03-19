5cf5240d9e19610799a39807198b4815769ab59a
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-19 18:08:26 +0000

    Added a menu for selecting which node to monitor
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8241 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Examples/org/objectweb/proactive/examples/userguide/cmagent/migration/Main.java
+++ src/Examples/org/objectweb/proactive/examples/userguide/cmagent/migration/Main.java
@@ -31,14 +31,17 @@
 //@snippet-start cma_main_migrator
 package org.objectweb.proactive.examples.userguide.cmagent.migration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.api.PADeployment;
+import org.objectweb.proactive.api.PALifeCycle;
 import org.objectweb.proactive.core.ProActiveException;
 import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptor;
 import org.objectweb.proactive.core.descriptor.data.VirtualNode;
 import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.core.node.NodeException;
-import org.objectweb.proactive.examples.userguide.cmagent.initialized.CMAgentInitialized;
 import org.objectweb.proactive.ActiveObjectCreationException;
 
 
@@ -63,8 +66,9 @@ public class Main {
         return null;
     }
 
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String args[]) {
         try {
+            BufferedReader inputBuffer = new BufferedReader(new InputStreamReader(System.in));
             VirtualNode vn = deploy(args[0]);
             String currentState = new String();
             //@snippet-start cma_migrator_ao
@@ -73,20 +77,48 @@ public class Main {
                     new Object[] {}, vn.getNode());
             //@snippet-end cma_migrator_ao
 
-            for (Node node : vn.getNodes()) {
-                ao.migrateTo(node);
-                currentState = ao.getCurrentState().toString();
-                System.out.println(currentState);
-                //wait for 3 seconds and move on
-                Thread.sleep(3000);
+            //  int input = 0;
+            int k = 1;
+            int choice;
+            while (k != 0) {
+
+                //display the menu with the available nodes 
+                k = 1;
+                for (Node node : vn.getNodes()) {
+                    System.out.println(k + ".  Statistics for node :" + node.getNodeInformation().getURL());
+                    k++;
+                }
+                System.out.println("0.  Exit");
+
+                //select a node
+                do {
+                    System.out.print("Choose a node :> ");
+                    try {
+                        // Read am option from keyboard
+                        choice = Integer.parseInt(inputBuffer.readLine().trim());
+                    } catch (NumberFormatException noExcep) {
+                        choice = -1;
+                    }
+                } while (!(choice >= 1 && choice < k || choice == 0));
+                if (choice == 0)
+                    break;
+
+                //display information for the selected node 
+                ao.migrateTo(vn.getNodes()[choice - 1]); //migrate
+                currentState = ao.getCurrentState().toString(); //get the state
+                System.out.println("\n" + currentState);
             }
 
+            //stopping all the objects and JVMS
             PAActiveObject.terminateActiveObject(ao, false);
-
+            vn.killAll(true);
+            PALifeCycle.exitSuccess();
         } catch (NodeException nodeExcep) {
             System.err.println(nodeExcep.getMessage());
         } catch (ActiveObjectCreationException aoExcep) {
             System.err.println(aoExcep.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
