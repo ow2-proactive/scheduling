8576c26ce7032ea1f269cae0b37f3900e7624575
Vasile Jureschi <Vasile Jureschi@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-03-20 12:40:54 +0000

    added a report on requests served and life time
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8250 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Examples/org/objectweb/proactive/examples/userguide/cmagent/initialized/CMAgentInitialized.java
+++ src/Examples/org/objectweb/proactive/examples/userguide/cmagent/initialized/CMAgentInitialized.java
@@ -7,16 +7,18 @@ import org.objectweb.proactive.InitActive;
 import org.objectweb.proactive.RunActive;
 import org.objectweb.proactive.Service;
 import org.objectweb.proactive.core.util.wrapper.LongWrapper;
-import org.objectweb.proactive.examples.doctor.Cure;
 import org.objectweb.proactive.examples.userguide.cmagent.simple.CMAgent;
 
 
 public class CMAgentInitialized extends CMAgent implements InitActive, RunActive, EndActive {
     private long lastRequestDuration;
-
+    private long startTime;
+    private long requestsServed = 0;
     public void initActivity(Body body) {
         System.out.println("### Started Active object " + body.getMBean().getName() + " on " +
             body.getMBean().getNodeUrl());
+        //get start time
+        startTime = System.currentTimeMillis();
     }
 
     public void runActivity(Body body) {
@@ -36,11 +38,18 @@ public class CMAgentInitialized extends CMAgent implements InitActive, RunActive
             // value of the getLastRequestServeTime call 
             // AFTER the previous calculated value has been returned
             lastRequestDuration = currentRequestDuration;
+            //increment the number of requests served
+            requestsServed++;
         }
     }
 
     public void endActivity(Body body) {
-        System.out.println("### Active object stopped.");
+    	long runningTime = System.currentTimeMillis() - startTime;
+        System.out.println("### You have killed the active object. The final" +
+        		" resting place is on " + body.getNodeURL() + "\n### It has faithfully served " 
+        		+ requestsServed + " requests(including migrations) " +
+        		"and has been an upstanding active object for " +
+        		runningTime + " ms ");
     }
 
     public LongWrapper getLastRequestServeTime() {
