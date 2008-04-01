5952a7c5063fd8b25d53ba6fc95873b541db07a6
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-01 13:47:35 +0000

    Use waitReady instead waitAllocation (faster & safer)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8356 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/gcmdeployment/virtualnode/TestVirtualNode11.java
+++ src/Tests/functionalTests/gcmdeployment/virtualnode/TestVirtualNode11.java
@@ -39,15 +39,17 @@ import org.objectweb.proactive.extra.gcmdeployment.PAGCMDeployment;
 import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplication;
 import org.objectweb.proactive.extra.gcmdeployment.core.GCMVirtualNode;
 
+import functionalTests.GCMDeploymentReady;
 import functionalTests.gcmdeployment.Abstract;
 
 
+@GCMDeploymentReady
 public class TestVirtualNode11 extends Abstract {
     @Test
     public void test() throws FileNotFoundException, ProActiveException {
         GCMApplication gcma = PAGCMDeployment.loadApplicationDescriptor(getDescriptor(this));
         gcma.startDeployment();
-        waitAllocation();
+        gcma.waitReady();
 
         GCMVirtualNode vn = gcma.getVirtualNode("vn");
         Assert.assertEquals(7, vn.getCurrentNodes().size());
