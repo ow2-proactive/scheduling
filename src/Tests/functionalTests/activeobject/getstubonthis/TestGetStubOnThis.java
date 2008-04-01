0352ec59c9796ca040401de25395a1f396198a8f
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-01 16:22:39 +0000

    Added a test case for PROACTIVE-273
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8362 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/activeobject/getstubonthis/TestGetStubOnThis.java
@@ -0,0 +1,35 @@
+package functionalTests.activeobject.getstubonthis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.mop.StubObject;
+import org.objectweb.proactive.core.node.NodeException;
+
+import functionalTests.FunctionalTest;
+import functionalTests.GCMDeploymentReady;
+
+
+/**
+ * See PROACTIVE-273
+ */
+@GCMDeploymentReady
+public class TestGetStubOnThis extends FunctionalTest {
+    public TestGetStubOnThis() {
+
+    }
+
+    @Test
+    public void notFromServiceThread() {
+        StubObject stub = PAActiveObject.getStubOnThis();
+        Assert.assertNotNull("Stub Object should not be null", stub);
+    }
+
+    @Test
+    public void fromServiceThread() throws ActiveObjectCreationException, NodeException {
+        AO ao = (AO) PAActiveObject.newActive(AO.class.getName(), new Object[] {});
+        Assert.assertTrue(ao.isOk());
+    }
+
+}
