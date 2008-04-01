eed1a1ee294a1b5846a82d6cbb6f646e918771c5
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-01 18:04:39 +0000

    comments
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8369 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Utils/doc/snippets/JavaSnippetExtractor.java
+++ src/Utils/doc/snippets/JavaSnippetExtractor.java
@@ -35,34 +35,66 @@ import java.io.File;
 
 /**
  * Extracts snippets from a java file.
- * @author The ProActive Team
+ * @author vjuresch
  *
  */
 public class JavaSnippetExtractor extends SnippetExtractor {
 
     /**
-     * @param f
-     * @param targetDir
-     * @param startA
-     * @param endA
-     * @param startE
-     * @param endE
+     * Constructs a new JavaSnippetExtractor that
+     * will parse the file and output in the 
+     * target directory the parts of the code between 
+     * the annotations.
+     * 
+     * @param file  	file to extract from 
+     * @param targetDir	directory to save the file to 
+     * @param startA	start annotation
+     * @param endA		end annotation
+     */		
+    public JavaSnippetExtractor(final File file, final File targetDir, final String startA,
+    		final String endA) {
+        super(file, targetDir, startA, endA);
+    }
+
+    /**
+     * Constructs a new JavaSnippetExtractor that
+     * will parse the file and output in the 
+     * target directory the parts of the code between 
+     * the annotations. It will not include the
+     * code between the exclude tags.
+     * 
+     * @param file  	file to extract from 
+     * @param targetDir	directory to save the file to 
+     * @param startA	start annotation
+     * @param endA		end annotation		
+     * @param startE	start exclusion annotation (not implemented) - beginning of exclusion from
+     * 						included part  
+     * @param endE		end exclusion annotation - ending of exclusion from included part
      */
-    public JavaSnippetExtractor(File f, File targetDir, String startA, String endA, String startE, String endE) {
-        super(f, targetDir, startA, endA, startE, endE);
+    public JavaSnippetExtractor(final File file, final File targetDir, final String startA,
+    		final String endA, final String startE, final String endE) {
+        super(file, targetDir, startA, endA, startE, endE);
     }
 
     /* (non-Javadoc)
-     * @see doc.snippets.SnippetExtractor#extractAnnotation()
+     * @see doc.snippets.SnippetExtractor#extractAnnotation(java.lang.String, java.lang.String)
+     *
+     */
+    /**
+     * @param line the line from which to extract the snippet name
+     * @param annotation the annotation on the line
+     * 
+     * @return returns only the name of the snippet 
      */
     @Override
-    public String extractAnnotation(String line, String annotation) {
+    public String extractAnnotation(final String  line, final String annotation) {
         //snippets are in the form
-        String name = new String(line);
+        String name = line;
         name = name.replace("/", " ");
         name = name.replace(annotation, " ");
         name = name.trim();
-        logger.debug("Extracted the " + name + " snippet from " + target);
+        SnippetExtractor.logger.
+        	debug("Extracted the " + name + " snippet from " + target);
         return name;
     }
 }
