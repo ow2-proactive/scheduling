be591005278e1df102009c58722425abcb5be2e2
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-01 17:24:54 +0000

    Added some tests for PAFuture
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8367 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/activeobject/future/TestPAFuture.java
@@ -0,0 +1,68 @@
+package functionalTests.activeobject.future;
+
+import java.util.Vector;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.api.PAFuture;
+import org.objectweb.proactive.core.ProActiveException;
+
+
+public class TestPAFuture {
+
+    @Test
+    public void isAwaitedNoFuture() {
+        Object o = new Object();
+        boolean resp = PAFuture.isAwaited(o);
+        Assert.assertFalse("O is not a future, should not been awaited", resp);
+    }
+
+    @Test(timeout = 500)
+    public void waitForNoFuture() {
+        Object o = new Object();
+        PAFuture.waitFor(o);
+    }
+
+    @Test(timeout = 500)
+    public void waitForWithTimeoutNoFuture() throws ProActiveException {
+        Object o = new Object();
+        PAFuture.waitFor(o, 1000);
+    }
+
+    @Test
+    public void waitForAny() {
+
+    }
+
+    @Test
+    public void waitForAnyNoFture() {
+        Vector<Object> v = new Vector<Object>();
+        v.add(new Object());
+        v.add(new Object());
+
+        int index;
+        index = PAFuture.waitForAny(v);
+        v.remove(index);
+        index = PAFuture.waitForAny(v);
+        v.remove(index);
+        Assert.assertTrue(v.isEmpty());
+    }
+
+    @Test(timeout = 500)
+    public void waitForAllNoFture() {
+        Vector<Object> v = new Vector<Object>();
+        v.add(new Object());
+        v.add(new Object());
+
+        PAFuture.waitForAll(v);
+    }
+
+    @Test(timeout = 500)
+    public void waitForAllWithTimeoutNoFture() throws ProActiveException {
+        Vector<Object> v = new Vector<Object>();
+        v.add(new Object());
+        v.add(new Object());
+
+        PAFuture.waitForAll(v, 1000);
+    }
+}
