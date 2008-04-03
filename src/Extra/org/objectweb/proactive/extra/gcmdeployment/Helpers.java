fbea0a8dbba5f9f92f24a8abd1bd3a80687eab03
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-03 11:14:55 +0000

    Print the absolutePath instead of filename on error
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8388 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extra/org/objectweb/proactive/extra/gcmdeployment/Helpers.java
+++ src/Extra/org/objectweb/proactive/extra/gcmdeployment/Helpers.java
@@ -37,18 +37,21 @@ public class Helpers {
 
     /**
      * Checks that descriptor exist, is a file and is readable
-     * @param descriptor The File to be checked
-     * @throws IllegalArgumentException If the File is does not exist, is not a file or is not readable
+     * 
+     * @param descriptor
+     *            The File to be checked
+     * @throws IllegalArgumentException
+     *             If the File is does not exist, is not a file or is not readable
      */
     public static File checkDescriptorFileExist(File descriptor) throws IllegalArgumentException {
         if (!descriptor.exists()) {
-            throw new IllegalArgumentException(descriptor.getName() + " does not exist");
+            throw new IllegalArgumentException(descriptor.getAbsolutePath() + " does not exist");
         }
         if (!descriptor.isFile()) {
-            throw new IllegalArgumentException(descriptor.getName() + " is not a file");
+            throw new IllegalArgumentException(descriptor.getAbsolutePath() + " is not a file");
         }
         if (!descriptor.canRead()) {
-            throw new IllegalArgumentException(descriptor.getName() + " is not readable");
+            throw new IllegalArgumentException(descriptor.getAbsolutePath() + " is not readable");
         }
 
         return descriptor;
