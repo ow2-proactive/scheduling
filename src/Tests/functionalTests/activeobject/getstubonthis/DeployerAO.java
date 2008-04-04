5d10668c03d639f7ff4090ed40e384fc1dbe2ee5
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-04 14:21:47 +0000

    PROACTIVE-273 Added a test for NodeMapper issue
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8417 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/activeobject/getstubonthis/DeployerAO.java
@@ -0,0 +1,67 @@
+package functionalTests.activeobject.getstubonthis;
+
+import java.io.File;
+import java.io.Serializable;
+
+import org.objectweb.proactive.Body;
+import org.objectweb.proactive.InitActive;
+import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.util.OperatingSystem;
+import org.objectweb.proactive.core.xml.VariableContractImpl;
+import org.objectweb.proactive.core.xml.VariableContractType;
+import org.objectweb.proactive.extra.gcmdeployment.PAGCMDeployment;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplication;
+import org.objectweb.proactive.extra.gcmdeployment.core.GCMVirtualNode;
+
+import functionalTests.GCMFunctionalTestDefaultNodes;
+import functionalTests.GCMFunctionalTestDefaultNodes.DeploymentType;
+
+
+public class DeployerAO implements Serializable, InitActive {
+    GCMApplication gcma;
+    boolean notified = false;
+
+    public DeployerAO() {
+
+    }
+
+    public DeployerAO(GCMApplication gcma) {
+        this.gcma = gcma;
+    }
+
+    public void initActivity(Body body) {
+        try {
+            File appDesc = new File(this.getClass().getResource("/functionalTests/_CONFIG/JunitApp.xml")
+                    .getFile());
+
+            VariableContractImpl vContract = new VariableContractImpl();
+            vContract.setVariableFromProgram(GCMFunctionalTestDefaultNodes.VAR_DEPDESCRIPTOR, "localhost/" +
+                DeploymentType._1x1.filename, VariableContractType.DescriptorDefaultVariable);
+            vContract.setVariableFromProgram(GCMFunctionalTestDefaultNodes.VAR_OS, OperatingSystem
+                    .getOperatingSystem().name(), VariableContractType.DescriptorDefaultVariable);
+
+            GCMApplication gcma = PAGCMDeployment.loadApplicationDescriptor(appDesc, vContract);
+
+            GCMVirtualNode vn = gcma.getVirtualNode("nodes");
+            vn.subscribeNodeAttachment(PAActiveObject.getStubOnThis(), "callback");
+
+            gcma.startDeployment();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void callback(Node node, GCMVirtualNode vn) {
+        System.out.println("Callback called");
+    }
+
+    public boolean waitUntilCallbackOccur() throws InterruptedException {
+        while (!notified) {
+            Thread.sleep(250);
+        }
+
+        return true;
+    }
+
+}
---
+++ src/Tests/functionalTests/activeobject/getstubonthis/TestGCMDeployment.java
@@ -0,0 +1,19 @@
+package functionalTests.activeobject.getstubonthis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.node.NodeException;
+
+import functionalTests.FunctionalTest;
+
+
+public class TestGCMDeployment extends FunctionalTest {
+
+    @Test
+    public void test() throws ActiveObjectCreationException, NodeException, InterruptedException {
+        DeployerAO ao = (DeployerAO) PAActiveObject.newActive(DeployerAO.class.getName(), new Object[] {});
+        Assert.assertTrue(ao.waitUntilCallbackOccur());
+    }
+}
