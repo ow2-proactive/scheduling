7da8fb72a3701ca74e94534da4bdb19999102fed
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-04 13:25:10 +0000

    Refactored GCM Functional Tests. Easier to extend when a custom GCM Application Descriptor is used.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8413 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/functionalTests/GCMFunctionalTest.java
@@ -0,0 +1,48 @@
+package functionalTests;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.objectweb.proactive.core.ProActiveException;
+import org.objectweb.proactive.core.util.OperatingSystem;
+import org.objectweb.proactive.core.xml.VariableContractImpl;
+import org.objectweb.proactive.core.xml.VariableContractType;
+import org.objectweb.proactive.extra.gcmdeployment.PAGCMDeployment;
+import org.objectweb.proactive.extra.gcmdeployment.GCMApplication.GCMApplication;
+
+
+public class GCMFunctionalTest extends FunctionalTest {
+
+    static public final String VAR_OS = "os";
+
+    public File applicationDescriptor;
+    public VariableContractImpl vContract;
+    public GCMApplication gcmad;
+
+    public GCMFunctionalTest() {
+        vContract = new VariableContractImpl();
+        vContract.setVariableFromProgram(VAR_OS, OperatingSystem.getOperatingSystem().name(),
+                VariableContractType.DescriptorDefaultVariable);
+    }
+
+    public GCMFunctionalTest(File applicationDescriptor) {
+        this();
+        this.applicationDescriptor = applicationDescriptor;
+    }
+
+    @Before
+    public void startDeployment() throws ProActiveException {
+        if (gcmad != null) {
+            throw new IllegalStateException("deployment already started");
+        }
+
+        gcmad = PAGCMDeployment.loadApplicationDescriptor(applicationDescriptor, vContract);
+        gcmad.startDeployment();
+    }
+
+    @After
+    public void killDeployment() {
+        gcmad.kill();
+    }
+}
