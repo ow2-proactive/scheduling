35f2a55f9feadd6e0262ac6d70726eaed128c264
cdalmass <cdalmass@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-07 16:44:10 +0000

    test refactoring (inner class are not executed)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8441 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/component/conform/TestLifeCycleController.java
+++ src/Tests/functionalTests/component/conform/TestLifeCycleController.java
@@ -58,10 +58,14 @@ public class TestLifeCycleController extends Conformtest {
         tf = Fractal.getTypeFactory(boot);
         gf = Fractal.getGenericFactory(boot);
         t = tf.createFcType(new InterfaceType[] {
-                tf.createFcItfType("server", I.class.getName(), false, false, false),
-                tf.createFcItfType("servers", I.class.getName(), false, false, true),
-                tf.createFcItfType("client", I.class.getName(), true, false, false),
-                tf.createFcItfType("clients", I.class.getName(), true, false, true) });
+                tf.createFcItfType("server", I.class.getName(), TypeFactory.SERVER, TypeFactory.MANDATORY,
+                        TypeFactory.SINGLE),
+                tf.createFcItfType("servers", I.class.getName(), TypeFactory.SERVER, TypeFactory.MANDATORY,
+                        TypeFactory.COLLECTION),
+                tf.createFcItfType("client", I.class.getName(), TypeFactory.CLIENT, TypeFactory.MANDATORY,
+                        TypeFactory.SINGLE),
+                tf.createFcItfType("clients", I.class.getName(), TypeFactory.CLIENT, TypeFactory.MANDATORY,
+                        TypeFactory.COLLECTION) });
         setUpComponents();
     }
 
@@ -137,93 +141,6 @@ public class TestLifeCycleController extends Conformtest {
             Fractal.getBindingController(c).unbindFc("clients0");
             fail();
         } catch (IllegalLifeCycleException e) {
-        }
-    }
-
-    // ---
-    public static class Composite extends TestLifeCycleController {
-        protected Component r;
-
-        @Override
-        protected void setUpComponents() throws Exception {
-            Component o = gf.newFcInstance(t, "composite", null);
-            r = gf.newFcInstance(t, "composite", null);
-            c = gf.newFcInstance(t, "primitive", C.class.getName());
-            d = gf.newFcInstance(t, "primitive", C.class.getName());
-            Fractal.getContentController(o).addFcSubComponent(r);
-            Fractal.getContentController(r).addFcSubComponent(c);
-            Fractal.getContentController(r).addFcSubComponent(d);
-        }
-
-        @Test
-        public void testRecursiveStartStop() throws Exception {
-            ContentController cc = Fractal.getContentController(r);
-            Fractal.getBindingController(r).bindFc("server", c.getFcInterface("server"));
-            Fractal.getBindingController(c).bindFc("client", d.getFcInterface("server"));
-            Fractal.getBindingController(d).bindFc("client", cc.getFcInternalInterface("client"));
-            Fractal.getBindingController(r).bindFc("client", r.getFcInterface("server"));
-
-            Fractal.getLifeCycleController(r).startFc();
-            assertEquals("STARTED", Fractal.getLifeCycleController(r).getFcState());
-            assertEquals("STARTED", Fractal.getLifeCycleController(c).getFcState());
-            assertEquals("STARTED", Fractal.getLifeCycleController(d).getFcState());
-            final I i = (I) r.getFcInterface("server");
-            Thread t = new Thread(new Runnable() {
-                public void run() {
-                    i.m(true);
-                }
-            });
-            t.start();
-            t.join(50);
-            assertTrue(!t.isAlive());
-
-            Fractal.getLifeCycleController(r).stopFc();
-            assertEquals("STOPPED", Fractal.getLifeCycleController(r).getFcState());
-            assertEquals("STOPPED", Fractal.getLifeCycleController(c).getFcState());
-            assertEquals("STOPPED", Fractal.getLifeCycleController(d).getFcState());
-
-            // TODO test issue: this test assumes a call on a stopped interface hangs
-            //      t = new Thread(new Runnable() {
-            //        public void run () { i.m(true); }
-            //      });
-            //      t.start();
-            //      t.join(50);    
-            //      assertTrue(t.isAlive());
-        }
-
-        @Override
-        @Test
-        @Ignore
-        public void testMandatoryInterfaceNotBound() throws Exception {
-            super.testMandatoryInterfaceNotBound();
-            ContentController cc = Fractal.getContentController(r);
-            cc.removeFcSubComponent(c);
-            cc.removeFcSubComponent(d);
-            Fractal.getBindingController(r).bindFc("client", r.getFcInterface("server"));
-            try {
-                Fractal.getLifeCycleController(c).startFc();
-                fail();
-            } catch (IllegalLifeCycleException e) {
-            }
-        }
-
-        @Test
-        @Ignore
-        public void testRemoveNotStopped() throws Exception {
-            ContentController cc = Fractal.getContentController(r);
-            Fractal.getBindingController(r).bindFc("server", c.getFcInterface("server"));
-            Fractal.getBindingController(c).bindFc("client", cc.getFcInternalInterface("client"));
-            Fractal.getBindingController(r).bindFc("client", r.getFcInterface("server"));
-            cc.removeFcSubComponent(d);
-            Fractal.getLifeCycleController(r).startFc();
-
-            // TODO test issue: adding a sub-component in a started composite automatically starts the added one?
-            cc.addFcSubComponent(d);
-            try {
-                cc.removeFcSubComponent(d);
-                // fail();
-            } catch (IllegalLifeCycleException e) {
-            }
         }
     }
 }
