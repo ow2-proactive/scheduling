3d982f18ae9746ccfda112cfbfc6f727b0ecae23
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-10 15:23:58 +0000

    Added TestIbis (excluded since it does not work)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8491 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Tests/performanceTests/throughput/TestIbis.java
@@ -0,0 +1,97 @@
+package performanceTests.throughput;
+
+import java.io.Serializable;
+
+import org.junit.Test;
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.config.PAProperties;
+import org.objectweb.proactive.core.node.NodeException;
+import org.objectweb.proactive.core.xml.VariableContractType;
+
+import performanceTests.HudsonReport;
+import functionalTests.GCMFunctionalTestDefaultNodes;
+
+
+public class TestIbis extends GCMFunctionalTestDefaultNodes {
+    static {
+        PAProperties.PA_COMMUNICATION_PROTOCOL.setValue("ibis");
+    }
+
+    public TestIbis() {
+        super(DeploymentType._1x1);
+        super.vContract.setVariableFromProgram(super.VAR_JVMARG, PAProperties.PA_COMMUNICATION_PROTOCOL
+                .getCmdLine() +
+            "ibis", VariableContractType.DescriptorDefaultVariable);
+    }
+
+    @Test
+    public void test() throws ActiveObjectCreationException, NodeException {
+        Server server = (Server) PAActiveObject.newActive(Server.class.getName(), new Object[] {}, super
+                .getANode());
+        Client client = (Client) PAActiveObject.newActive(Client.class.getName(), new Object[] { server });
+        client.startTest();
+    }
+
+    static public class Server implements Serializable {
+        boolean firstRequest = true;
+        long count = 0;
+        long startTime;
+
+        public Server() {
+
+        }
+
+        public void serve() {
+            if (firstRequest) {
+                startTime = System.currentTimeMillis();
+                firstRequest = false;
+            }
+
+            count++;
+        }
+
+        public void finish() {
+            long endTime = System.currentTimeMillis();
+            double throughput = (1000.0 * count) / (endTime - startTime);
+
+            System.out.println("Count: " + count);
+            System.out.println("Duration: " + (endTime - startTime));
+            System.out.println("Throughput " + throughput);
+            HudsonReport.reportToHudson(TestIbis.class, throughput);
+        }
+    }
+
+    static public class Client implements Serializable {
+        private Server server;
+
+        public Client() {
+
+        }
+
+        public Client(Server server) {
+            this.server = server;
+        }
+
+        public int startTest() {
+            // Warmup
+            for (int i = 0; i < 1000; i++) {
+                server.serve();
+            }
+
+            long startTime = System.currentTimeMillis();
+            while (true) {
+                if (System.currentTimeMillis() - startTime > Throughput.DURATION)
+                    break;
+
+                for (int i = 0; i < 50; i++) {
+                    server.serve();
+                }
+            }
+            server.finish();
+
+            // startTest must be sync 
+            return 0;
+        }
+    }
+}
