6c526a6cda31c39d78a80f162a599e44b7b3bf92
jfradj <jfradj@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-11 11:55:23 +0000

    Adding 2 new Job : 	- KillJob has a task that will kill the node on which it was lanched by calling a System.exit 	- AbortJob has a task which will throw an exception This two jobs allow to have all different tasks' states and to have ABORTED and FAILED job's state.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8511 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extensions/org/objectweb/proactive/extensions/scheduler/examples/AbortJob.java
@@ -0,0 +1,50 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ */
+package org.objectweb.proactive.extensions.scheduler.examples;
+
+import org.objectweb.proactive.extensions.scheduler.common.task.TaskResult;
+import org.objectweb.proactive.extensions.scheduler.common.task.executable.JavaExecutable;
+
+
+public class AbortJob extends JavaExecutable {
+
+    @Override
+    public Object execute(TaskResult... results) throws Throwable {
+        try {
+            System.out.println("I will throw a runtime excetiopn in 3 sec");
+
+            Thread.sleep(3000);
+
+        } catch (Exception e) {
+        }
+        throw new RuntimeException("Aborted job exception");
+    }
+}
