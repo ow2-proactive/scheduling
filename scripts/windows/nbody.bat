1ba914af64787303d97052b8d491ea4c8dedbd14
jkrzemin <jkrzemin@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-04-21 08:31:22 +0000

    adding option for running on CCS
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8604 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/windows/nbody.bat
+++ scripts/windows/nbody.bat
@@ -15,19 +15,34 @@ goto noft
 
 :ft
 set XMLDESCRIPTOR="%PROACTIVE%\descriptors\FaultTolerantWorkersLocal.xml"
+SHIFT
+:Loop
+IF [%1] NEQ [] (
+    SET params=%params% %1
+	SHIFT
+	GOTO Loop
+)
 goto cmd
 
 :ccs
 set XMLDESCRIPTOR="%PROACTIVE%\descriptors\WorkersApplicationCCS.xml"
+SHIFT
+:Loop
+IF [%1] NEQ [] (
+    SET params=%params% %1
+	SHIFT
+	GOTO Loop
+)
 goto cmd
 
 :noft
 set XMLDESCRIPTOR="%PROACTIVE%\descriptors\WorkersApplication.xml"
+SET params=%*
 goto cmd
 
 :cmd
 
-%JAVA_CMD% org.objectweb.proactive.examples.nbody.common.Start %XMLDESCRIPTOR% %*
+%JAVA_CMD% org.objectweb.proactive.examples.nbody.common.Start %XMLDESCRIPTOR% %params%
 ENDLOCAL
 
 pause
