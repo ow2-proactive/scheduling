ebee88e829b7d32b3ba2e07f35f69dc07e8bdf4f
tdobek <tdobek@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-05-21 12:55:56 +0000

    Windows Daemon Starter Script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@8880 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ scripts/windows/p2p/windaemon.bat
@@ -0,0 +1,25 @@
+rem @echo off
+echo.
+echo --- StartDeamon -------------------------------------
+
+
+goto doit
+
+:usage
+echo. 
+goto end
+
+
+:doit
+SETLOCAL ENABLEDELAYEDEXPANSION enabledelayedexpansion
+IF NOT DEFINED PROACTIVE set PROACTIVE=%CD%\..\..\..
+
+call "%PROACTIVE%\scripts\windows\init.bat"
+
+echo %CLASSPATH%
+
+%JAVA_CMD% org.objectweb.proactive.p2p.daemon.WinDaemon %*
+pause
+rem exit %errorlevel%
+
+rem endlocal
