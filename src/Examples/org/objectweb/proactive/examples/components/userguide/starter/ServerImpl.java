fc5620d26979cb5d061232ec1a0d38e940f92199
bsauvan <bsauvan@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-02 14:25:42 +0000

    Java 5 compliance
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9020 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Examples/org/objectweb/proactive/examples/components/userguide/starter/ServerImpl.java
+++ src/Examples/org/objectweb/proactive/examples/components/userguide/starter/ServerImpl.java
@@ -1,7 +1,6 @@
 package org.objectweb.proactive.examples.components.userguide.starter;
 
 public class ServerImpl implements Service {
-    @Override
     public void print(String msg) {
         System.err.println("=> Server: " + msg);
     }
