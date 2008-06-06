173e5ae6062de3ddf36508d90ff1a96a71aed7c0
cmathieu <cmathieu@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-06 12:31:57 +0000

    Disabled future monitoring for TestExceptionInInitActivity
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9076 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Tests/functionalTests/activeobject/onfailure/TestExceptionInInitActivity.java
+++ src/Tests/functionalTests/activeobject/onfailure/TestExceptionInInitActivity.java
@@ -1,15 +1,23 @@
 package functionalTests.activeobject.onfailure;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.objectweb.proactive.ActiveObjectCreationException;
 import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.core.body.exceptions.BodyTerminatedException;
+import org.objectweb.proactive.core.config.PAProperties;
 import org.objectweb.proactive.core.node.NodeException;
 
 import functionalTests.FunctionalTest;
 
 
 public class TestExceptionInInitActivity extends FunctionalTest {
+    @Before
+    public void before() {
+        // Disable future monitoring
+        PAProperties.PA_FUTUREMONITORING_TTM.setValue(10000);
+    }
+
     @Test(expected = BodyTerminatedException.class)
     public void test() throws ActiveObjectCreationException, NodeException, InterruptedException {
         ExceptionInInitActivityAO ao = (ExceptionInInitActivityAO) PAActiveObject.newActive(
