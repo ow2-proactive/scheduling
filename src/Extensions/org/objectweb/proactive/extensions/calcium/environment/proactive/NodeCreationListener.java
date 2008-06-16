8bdf8b16e36e24ce1e39ac6dddad469a6197ba98
mleyton <mleyton@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-06-16 13:07:20 +0000

    Forgot to add this new file to version control
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/trunk@9197 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/Extensions/org/objectweb/proactive/extensions/calcium/environment/proactive/NodeCreationListener.java
@@ -0,0 +1,38 @@
+package org.objectweb.proactive.extensions.calcium.environment.proactive;
+
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.node.NodeException;
+
+
+public class NodeCreationListener {
+
+    int times;
+
+    AOTaskPool taskpool;
+    FileServerClientImpl fserver;
+    AOInterpreterPool interpool;
+
+    public NodeCreationListener(AOTaskPool taskpool, FileServerClientImpl fserver,
+            AOInterpreterPool interpool, int times) {
+        super();
+
+        this.taskpool = taskpool;
+        this.fserver = fserver;
+        this.interpool = interpool;
+
+        this.times = times;
+    }
+
+    public void listener(Node node, String vnName) throws ActiveObjectCreationException, NodeException {
+
+        AOInterpreter interp = (AOInterpreter) PAActiveObject.newActive(AOInterpreter.class.getName(),
+                new Object[] { taskpool, fserver }, node);
+
+        AOStageIn stageIn = interp.getStageIn(interpool);
+
+        interpool.putInRandomPosition(stageIn, times);
+
+    }
+}
