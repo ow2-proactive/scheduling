0a584cd4ad6f31a7602477bfb12b99173503b92d
esalagea <esalagea@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-07 15:39:08 +0000

    add startNode scripts
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@9599 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ bin/unix/startNode.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo
+echo --- StartNode -------------------------------------------
+
+if [ $# -lt 1 ]; then
+    echo "
+       Start a new Node
+         startNode.sh <the url of the node to create>
+
+         ex : startNode.sh  rmi://$HOSTNAME/node1
+         ex : startNode.sh jini://$HOSTNAME/node2
+
+    "
+    exit 1
+fi
+
+workingDir=`dirname $0`
+. $workingDir/env.sh
+$JAVACMD org.objectweb.proactive.core.node.StartNode "$@"
+
+echo
+echo ---------------------------------------------------------
---
+++ bin/windows/startNode.bat
@@ -0,0 +1,29 @@
+@echo off
+echo.
+echo --- StartNode----------------------------------------
+
+if "%1" == "" goto usage
+
+goto doit
+
+:usage
+echo.
+echo Start a new Node
+echo    - 1 : the url of the node to create
+echo.
+echo ex : startNode  rmi://localhost/node1
+echo.
+echo Node started with a random name
+echo.
+goto doit
+
+
+:doit
+SETLOCAL ENABLEDELAYEDEXPANSION
+call init.bat
+%JAVA_CMD%  org.objectweb.proactive.core.node.StartNode %*
+ENDLOCAL
+
+:end
+echo.
+echo ---------------------------------------------------------
