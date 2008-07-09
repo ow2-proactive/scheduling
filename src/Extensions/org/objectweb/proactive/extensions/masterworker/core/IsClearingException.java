5d00dd33211931e48899a3e8c975b3cd22b67e0b
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-07-09 12:36:08 +0000

    unnecessary package (exists in ProActive)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@9651 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/Extensions/org/objectweb/proactive/extensions/masterworker/core/IsClearingException.java
+++
@@ -1,9 +0,0 @@
-package org.objectweb.proactive.extensions.masterworker.core;
-
-/**
- * IsClearingException
- *
- * @author The ProActive Team
- */
-public class IsClearingException extends RuntimeException {
-}
