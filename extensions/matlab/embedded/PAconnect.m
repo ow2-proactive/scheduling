9cefb896c287de1004cf9926151309a15cd0985a
vjuresch <vjuresch@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-08-20 11:43:48 +0000

    copyright and mail update
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@9972 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- extensions/matlab/embedded/PAconnect.m
+++ extensions/matlab/embedded/PAconnect.m
@@ -21,8 +21,8 @@
 % * ProActive: The Java(TM) library for Parallel, Distributed,
 % *            Concurrent computing with Security and Mobility
 % *
-% * Copyright (C) 1997-2007 INRIA/University of Nice-Sophia Antipolis
-% * Contact: proactive@objectweb.org
+% * Copyright (C) 1997-2008 INRIA/University of Nice-Sophia Antipolis
+% * Contact: proactive@ow2.org
 % *
 % * This library is free software; you can redistribute it and/or
 % * modify it under the terms of the GNU General Public License
