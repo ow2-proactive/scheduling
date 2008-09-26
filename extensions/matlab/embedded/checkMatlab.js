364960469c09ca72f1133088bb4375cb1ed41836
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2008-09-26 09:46:53 +0000

    selection script now works on windows and detects a Matlab installation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@10207 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- extensions/matlab/embedded/checkMatlab.js
+++ extensions/matlab/embedded/checkMatlab.js
@@ -1,14 +1,15 @@
 importPackage(java.lang);
 if (System.getProperty("os.name").startsWith("Windows")) {
-        selected=false;
+    selected = (java.lang.Runtime.getRuntime().exec("REG QUERY \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Mathworks\\MATLAB\" /s").waitFor() == 0);
 }
 else {       
-selected = (java.lang.Runtime.getRuntime().exec("which matlab2007b").waitFor() == 0);
-if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2007a").waitFor() == 0);
-if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2006b").waitFor() == 0);
-if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2006a").waitFor() == 0);
-if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab71").waitFor() == 0);
-if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab7").waitFor() == 0);
+    selected = (java.lang.Runtime.getRuntime().exec("which matlab2007b").waitFor() == 0);
+    if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2007a").waitFor() == 0);
+    if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2006b").waitFor() == 0);
+    if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab2006a").waitFor() == 0);
+    if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab71").waitFor() == 0);
+    if (!selected) selected=(java.lang.Runtime.getRuntime().exec("which matlab").waitFor() == 0);
+}
 if (selected) print("Good : "+ java.net.InetAddress.getLocalHost().getHostName()+"\n");
 else print("Not Good : "+ java.net.InetAddress.getLocalHost().getHostName()+"\n");
-}
+
