07b3fe12f8675f75b977b1b95f11b0ef5741f730
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-02-05 15:44:28 +0000

    Update Scheduler Authentication GUI Helper with an historic and Validation key listener
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@11215 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/common/scheduler/SchedulerAuthenticationGUIHelper.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/common/scheduler/SchedulerAuthenticationGUIHelper.java
@@ -39,9 +39,18 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.security.auth.login.LoginException;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -67,6 +76,7 @@ public class SchedulerAuthenticationGUIHelper {
 
     /**
      * Connect the scheduler and return the authentication interface, username and password of the current user.
+     * Return null if the authentication has been canceled (cancel button) or if username is not mentioned.
      * 
      * @param schedulerURL the URL of the scheduler to connect
      * @return the authentication interface, username and password of the current user.
@@ -84,8 +94,8 @@ public class SchedulerAuthenticationGUIHelper {
                 e.printStackTrace();
             }
         }
-        String username = helper.jTextField_UserName.getText();
-        String password = String.valueOf(helper.getJPasswordField_Password().getPassword());
+        String username = helper.getUserName();
+        String password = String.valueOf(helper.getPassword());
         if (helper.cancel || username == null || username.length() == 0) {
             return null;
         } else {
@@ -183,15 +193,24 @@ public class SchedulerAuthenticationGUIHelper {
 
     static class AuthGraphicHelper extends JFrame implements ActionListener, WindowListener, KeyListener {
 
+        private static final long serialVersionUID = 4089171167027536460L;
+        private static final String TMP_FILE_NAME = "AuthGrapHelpGUI.tmp";
+        private static final File TMP_AUTH_FILE = new File(System.getProperty("java.io.tmpdir") +
+            File.separator + TMP_FILE_NAME);
         private JPanel jContentPane = null;
         private JButton jButton_OK = null;
         private JButton jButton_Cancel = null;
+        private JComboBox jComboBox_url = null;
         private JTextField jTextField_UserName = null;
         private JPasswordField jPasswordField_Password = null;
+        private JLabel jLabel_url = null;
         private JLabel jLabel_UserName = null;
         private JLabel jLabel_Password = null;
         private JLabel jLabel_Info = null;
         private boolean cancel = false;
+        private String defaultURL = "";
+        private String defaultUserName = "";
+        private Set<String> URLs = new HashSet<String>();
 
         public void windowActivated(WindowEvent e) {
         }
@@ -226,13 +245,33 @@ public class SchedulerAuthenticationGUIHelper {
         }
 
         /**
+         * This is an other constructor that is build with default URL
+         */
+        private AuthGraphicHelper(String defaultURL) {
+            super();
+            this.defaultURL = defaultURL;
+            initialize();
+        }
+
+        /**
+         * This is an other constructor that is build with default URL and UserName
+         */
+        private AuthGraphicHelper(String defaultURL, String defaultUserName) {
+            super();
+            this.defaultURL = defaultURL;
+            this.defaultUserName = defaultUserName;
+            initialize();
+        }
+
+        /**
          * This method initializes this
          * 
          * @return void
          */
         private void initialize() {
-            int thisH = 169;
-            int thisW = 327;
+            loadURLs();
+            int thisH = 200;
+            int thisW = 450;
             this.setSize(thisW, thisH);
             this.setContentPane(getJContentPane());
             this.setTitle("ProActive Scheduler Authentication");
@@ -252,10 +291,11 @@ public class SchedulerAuthenticationGUIHelper {
         private JButton getJButton_OK() {
             if (jButton_OK == null) {
                 jButton_OK = new JButton();
-                jButton_OK.setBounds(new Rectangle(35, 106, 100, 22));
+                jButton_OK.setBounds(new Rectangle(85, 136, 100, 22));
                 jButton_OK.setText("OK");
                 jButton_OK.addActionListener(this);
                 jButton_OK.setActionCommand("JButton_OK");
+                jButton_OK.addKeyListener(this);
             }
             return jButton_OK;
         }
@@ -268,12 +308,34 @@ public class SchedulerAuthenticationGUIHelper {
         private JButton getJButton_Cancel() {
             if (jButton_Cancel == null) {
                 jButton_Cancel = new JButton();
-                jButton_Cancel.setBounds(new Rectangle(180, 106, 100, 22));
+                jButton_Cancel.setBounds(new Rectangle(260, 136, 100, 22));
                 jButton_Cancel.setText("Cancel");
                 jButton_Cancel.addActionListener(this);
                 jButton_Cancel.setActionCommand("JButton_Cancel");
+                jButton_Cancel.addKeyListener(this);
             }
             return jButton_Cancel;
+        }
+
+        /**
+         * This method initializes JComboBox_url	
+         * 	
+         * @return javax.swing.getJComboBox	
+         */
+        private JComboBox getJComboBox_url() {
+            if (jComboBox_url == null) {
+                jComboBox_url = new JComboBox();
+                jComboBox_url.setBounds(new Rectangle(130, 40, 300, 22));
+                for (String s : URLs) {
+                    jComboBox_url.addItem(s);
+                }
+                if (!"".equals(defaultURL)) {
+                    jComboBox_url.setSelectedItem(defaultURL);
+                }
+                jComboBox_url.setEditable(true);
+                jComboBox_url.addKeyListener(this);
+            }
+            return jComboBox_url;
         }
 
         /**
@@ -284,10 +346,20 @@ public class SchedulerAuthenticationGUIHelper {
         private JTextField getJTextField_UserName() {
             if (jTextField_UserName == null) {
                 jTextField_UserName = new JTextField();
-                jTextField_UserName.setBounds(new Rectangle(130, 40, 170, 22));
+                jTextField_UserName.setBounds(new Rectangle(130, 70, 300, 22));
+                jTextField_UserName.setText(defaultUserName);
                 jTextField_UserName.addKeyListener(this);
             }
             return jTextField_UserName;
+        }
+
+        /**
+         * Get the userName as it has been set
+         * 
+         * @return the userName as it has been set
+         */
+        public String getUserName() {
+            return jTextField_UserName.getText();
         }
 
         /**
@@ -298,10 +370,19 @@ public class SchedulerAuthenticationGUIHelper {
         private JPasswordField getJPasswordField_Password() {
             if (jPasswordField_Password == null) {
                 jPasswordField_Password = new JPasswordField();
-                jPasswordField_Password.setBounds(new Rectangle(130, 70, 170, 22));
+                jPasswordField_Password.setBounds(new Rectangle(130, 100, 300, 22));
                 jPasswordField_Password.addKeyListener(this);
             }
             return jPasswordField_Password;
+        }
+
+        /**
+         * Get the password as it has been set
+         * 
+         * @return the password as it has been set
+         */
+        public char[] getPassword() {
+            return jPasswordField_Password.getPassword();
         }
 
         /**
@@ -315,23 +396,29 @@ public class SchedulerAuthenticationGUIHelper {
                 jLabel_Info.setBounds(new Rectangle(12, 10, 287, 22));
                 jLabel_Info.setHorizontalAlignment(SwingConstants.RIGHT);
                 jLabel_Info.setText("Identification required to connect the Scheduler :");
+                jLabel_url = new JLabel();
+                jLabel_url.setBounds(new Rectangle(12, 40, 90, 22));
+                jLabel_url.setHorizontalAlignment(SwingConstants.RIGHT);
+                jLabel_url.setText("URL : ");
                 jLabel_UserName = new JLabel();
-                jLabel_UserName.setBounds(new Rectangle(12, 40, 90, 22));
+                jLabel_UserName.setBounds(new Rectangle(12, 70, 90, 22));
                 jLabel_UserName.setHorizontalAlignment(SwingConstants.RIGHT);
                 jLabel_UserName.setText("Username : ");
                 jLabel_Password = new JLabel();
-                jLabel_Password.setBounds(new Rectangle(12, 70, 90, 22));
+                jLabel_Password.setBounds(new Rectangle(12, 100, 90, 22));
                 jLabel_Password.setHorizontalAlignment(SwingConstants.RIGHT);
                 jLabel_Password.setText("Password : ");
                 jContentPane = new JPanel();
                 jContentPane.setLayout(null);
                 jContentPane.add(getJButton_OK(), null);
                 jContentPane.add(getJButton_Cancel(), null);
+                jContentPane.add(getJComboBox_url(), null);
                 jContentPane.add(getJTextField_UserName(), null);
                 jContentPane.add(getJPasswordField_Password(), null);
                 jContentPane.add(jLabel_UserName, null);
                 jContentPane.add(jLabel_Password, null);
                 jContentPane.add(jLabel_Info, null);
+                jContentPane.add(jLabel_url, null);
             }
             return jContentPane;
         }
@@ -340,11 +427,21 @@ public class SchedulerAuthenticationGUIHelper {
          * Terminate this authentication panel by closing it and unlock awaiting threads
          */
         private void terminate() {
+            saveURLs();
             this.setVisible(false);
             synchronized (this) {
                 this.notifyAll();
             }
             this.dispose();
+        }
+
+        /**
+         * Return true if the form has been canceled.
+         * 
+         * @return true if the form has been canceled, false otherwise.
+         */
+        public boolean hasBeenCanceled() {
+            return this.cancel;
         }
 
         /**
@@ -369,8 +466,57 @@ public class SchedulerAuthenticationGUIHelper {
          * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
          */
         public void keyPressed(KeyEvent e) {
+            if (e.getSource() == this.jButton_Cancel && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                this.cancel = true;
+                terminate();
+            }
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                 terminate();
+            }
+        }
+
+        private void saveURLs() {
+            if (!"".equals((String) jComboBox_url.getSelectedItem())) {
+                URLs.add((String) jComboBox_url.getSelectedItem());
+            }
+            PrintWriter pw = null;
+            try {
+                pw = new PrintWriter(new FileOutputStream(TMP_AUTH_FILE));
+                for (String url : URLs) {
+                    pw.println(url);
+                }
+            } catch (Exception e) {
+                //not a big deal
+            } finally {
+                if (pw != null) {
+                    pw.close();
+                }
+            }
+        }
+
+        private void loadURLs() {
+            if (!"".equals(defaultURL)) {
+                URLs.add(defaultURL);
+            }
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(TMP_AUTH_FILE));
+                String url;
+                while ((url = br.readLine()) != null) {
+                    if (!"".equals(url)) {
+                        URLs.add(url);
+                    }
+                }
+            } catch (Exception e) {
+                //If file not found, it will be created after
+                //if other, we cannot read historic -> not a big deal
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                    }
+                }
             }
         }
 
