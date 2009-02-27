2ec8ed4b3a5ab0bcea239680214a5d6d710ca113
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-02-27 10:45:29 +0000

    Add support for rmissh.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@11402 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler_plugins/org.ow2.proactive.scheduler/src/org/ow2/proactive/scheduler/Activator.java
+++ scheduler_plugins/org.ow2.proactive.scheduler/src/org/ow2/proactive/scheduler/Activator.java
@@ -30,9 +30,19 @@
  */
 package org.ow2.proactive.scheduler;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Hashtable;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.objectweb.proactive.core.ssh.httpssh.Handler;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.url.URLConstants;
+import org.osgi.service.url.URLStreamHandlerService;
+import org.osgi.service.url.URLStreamHandlerSetter;
 
 
 /**
@@ -57,6 +67,11 @@ public class Activator extends AbstractUIPlugin {
      */
     @Override
     public void start(BundleContext context) throws Exception {
+        /* This code is used to the httpssh, fixes an Eclipse bug */
+        Hashtable<String, String[]> properties = new Hashtable<String, String[]>(1);
+        properties.put(URLConstants.URL_HANDLER_PROTOCOL, new String[] { "httpssh" });
+        String serviceClass = URLStreamHandlerService.class.getName();
+        context.registerService(serviceClass, new IC2DHandler(), properties);
         super.start(context);
     }
 
@@ -88,4 +103,72 @@ public class Activator extends AbstractUIPlugin {
     public static ImageDescriptor getImageDescriptor(String path) {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
     }
+
+    //
+    //---INNER CLASS---------------------------------------------
+    //
+    public class IC2DHandler extends Handler implements URLStreamHandlerService {
+        protected URLStreamHandlerSetter realHandler;
+
+        public boolean equals(URL u1, URL u2) {
+            return super.equals(u1, u2);
+        }
+
+        public int getDefaultPort() {
+            return super.getDefaultPort();
+        }
+
+        public InetAddress getHostAddress(URL u) {
+            return super.getHostAddress(u);
+        }
+
+        public int hashCode(URL u) {
+            return super.hashCode(u);
+        }
+
+        public boolean hostsEqual(URL u1, URL u2) {
+            return super.hostsEqual(u1, u2);
+        }
+
+        public URLConnection openConnection(URL u) throws IOException {
+            return super.openConnection(u);
+        }
+
+        public void parseURL(URLStreamHandlerSetter realHandler, URL u, String spec, int start, int limit) {
+            this.realHandler = realHandler;
+            super.parseURL(u, spec, start, limit);
+        }
+
+        public boolean sameFile(URL u1, URL u2) {
+            return super.sameFile(u1, u2);
+        }
+
+        public String toExternalForm(URL u) {
+            return super.toExternalForm(u);
+        }
+
+        /**
+         * This method calls
+         * <code>realHandler.setURL(URL,String,String,int,String,String)</code>.
+         *
+         * @see "java.net.URLStreamHandler.setURL(URL,String,String,int,String,String)"
+         * @deprecated This method is only for compatibility with handlers written
+         *             for JDK 1.1.
+         */
+        protected void setURL(URL u, String proto, String host, int port, String file, String ref) {
+            realHandler.setURL(u, proto, host, port, file, ref);
+        }
+
+        /**
+         * This method calls
+         * <code>realHandler.setURL(URL,String,String,int,String,String,String,String)</code>.
+         *
+         * @see "java.net.URLStreamHandler.setURL(URL,String,String,int,String,String,String,String)"
+         */
+        protected void setURL(URL u, String proto, String host, int port, String auth, String user,
+                String path, String query, String ref) {
+            realHandler.setURL(u, proto, host, port, auth, user, path, query, ref);
+        }
+    }
+
 }
