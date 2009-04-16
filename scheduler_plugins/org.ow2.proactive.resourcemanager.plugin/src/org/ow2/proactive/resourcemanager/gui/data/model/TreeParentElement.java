573b0c2cc5953ebe38449116f2bc43ad4f1a400b
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-16 13:34:30 +0000

    Add missing copyright headers.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@11999 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler_plugins/org.ow2.proactive.resourcemanager.plugin/src/org/ow2/proactive/resourcemanager/gui/data/model/TreeParentElement.java
+++ scheduler_plugins/org.ow2.proactive.resourcemanager.plugin/src/org/ow2/proactive/resourcemanager/gui/data/model/TreeParentElement.java
@@ -1,3 +1,34 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s): ActiveEon Team - http://www.activeeon.com
+ *
+ * ################################################################
+ * $$ACTIVEEON_CONTRIBUTOR$$
+ */
 package org.ow2.proactive.resourcemanager.gui.data.model;
 
 import java.util.ArrayList;
