35bd93a24b7c06d157836c8d4c6aa2cafbdebef1
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-22 14:58:31 +0000

    Update copyright (2008->2009)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@12184 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/common/task/executable/Executable.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/common/task/executable/Executable.java
@@ -4,7 +4,7 @@
  * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
  *
- * Copyright (C) 1997-2008 INRIA/University of Nice-Sophia Antipolis
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
  * Contact: proactive@ow2.org
  *
  * This library is free software; you can redistribute it and/or
