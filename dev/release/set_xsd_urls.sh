a02815de928473df88ea8f69e5030c07fe993c4e
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-04-22 16:46:41 +0000

    Add specific scripts for schemas URL update.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_1.0.x@12191 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ dev/release/set_xsd_urls.sh
@@ -0,0 +1,26 @@
+SCHEDULER_DIR=$1
+VERSION=$2
+
+# to avoid replacement in this file
+PREVIOUS_TAG=dev
+
+# mv to scheduler dir
+cd $SCHEDULER_DIR
+
+# Replace all 'dev' version number by this version number in every XML, XSD, RNC files
+echo Replacing \'$PREVIOUS_TAG\' tag with current version for XML files
+
+# Do not replace in .* dirs (like .git...)
+for sd in $(ls); 
+do
+find ./$sd -type f -exec sed -i "s#urn:proactive:jobdescriptor:$PREVIOUS_TAG#urn:proactive:jobdescriptor:$VERSION#g" {} \;
+find ./$sd -type f -exec sed -i "s#http://proactive.inria.fr/schemas/jobdescriptor/$PREVIOUS_TAG/schedulerjob.xsd#http://proactive.inria.fr/schemas/jobdescriptor/$VERSION/schedulerjob.xsd#g" {} \;
+find ./$sd -type f -exec sed -i "s#org/ow2/proactive/scheduler/common/xml/schemas/jobdescriptor/$PREVIOUS_TAG/schedulerjob.xsd#org/ow2/proactive/scheduler/common/xml/schemas/jobdescriptor/$VERSION/schedulerjob.xsd#g" {} \;
+done
+
+# Update the website with new schema version
+echo Update the website with new schema version
+ssh sea.inria.fr mkdir /net/servers/www-sop/teams/oasis/proactive/schemas/jobdescriptor/$VERSION
+scp src/scheduler/src/org/ow2/proactive/scheduler/common/xml/schemas/jobdescriptor/$PREVIOUS_TAG/schedulerjob.xsd $USER@sea.inria.fr:/net/servers/www-sop/teams/oasis/proactive/schemas/jobdescriptor/$VERSION/schedulerjob.xsd
+ssh sea.inria.fr chmod 555 /net/servers/www-sop/teams/oasis/proactive/schemas/jobdescriptor/$VERSION
+ssh sea.inria.fr chmod 444 /net/servers/www-sop/teams/oasis/proactive/schemas/jobdescriptor/$VERSION/schedulerjob.xsd
