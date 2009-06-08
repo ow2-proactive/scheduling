bcd47ddfe6e695b435442791264ae1f6cf52375a
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-08 15:26:47 +0000

    PICKED FROM TRUNK 12976
    windows command, unix to Dos format
    
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_1.0.x@12981 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- bin/windows/startScheduler.bat
+++ bin/windows/startScheduler.bat
@@ -3,7 +3,6 @@ echo.
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 call init.bat scheduler-log4j-server
-
 %JAVA_CMD% -Dderby.stream.error.file="%PA_SCHEDULER%\.logs\derby.log" org.ow2.proactive.scheduler.util.SchedulerStarter %*
 
 ENDLOCAL
