f4604a854abb0d5886fd316acc7ab6207233f90c
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-06-16 15:33:12 +0000

    PICKED FROM TRUNK 13071
    Fix SCHEDULING-341 :
    getActive shell only if shell No 1 does not exist
    
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_1.0.x@13106 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler_plugins/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/views/SeparatedJobView.java
+++ scheduler_plugins/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/views/SeparatedJobView.java
@@ -414,7 +414,11 @@ public class SeparatedJobView extends ViewPart {
         setVisible(false);
 
         if (schedulerShell == null) {
-            schedulerShell = Display.getDefault().getActiveShell();
+		try {
+			schedulerShell = Display.getDefault().getShells()[1];
+		} catch (RuntimeException e){
+			schedulerShell = Display.getDefault().getActiveShell();
+		}
         }
     }
 
