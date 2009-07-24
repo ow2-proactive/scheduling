a9c5474b9a10a21dc5af9d4f8cafd81bbd0ee1db
oleg_smirnov <oleg_smirnov@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-07-24 11:20:32 +0000

    SCHEDULING-371 Reverted extra changes in regular scheduler starting scripts and remove extra config files.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@13608 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- bin/unix/startRM.sh
+++ bin/unix/startRM.sh
@@ -5,6 +5,6 @@ workingDir=`dirname $0`
 . $workingDir/env.sh rm-log4j-server
 
 opt="-Xms128m -Xmx2048m"
-eval $JAVACMD $opt -Dproactive.configuration=$PA_SCHEDULER/config/proactive/ProActiveConfigurationStartRM.xml  org.ow2.proactive.resourcemanager.utils.RMStarter $@
+eval $JAVACMD $opt org.ow2.proactive.resourcemanager.utils.RMStarter $@
 
 echo
