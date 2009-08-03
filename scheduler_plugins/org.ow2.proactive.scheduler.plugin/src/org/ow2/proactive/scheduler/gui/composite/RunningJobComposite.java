62dbf9fba29db031befcfa122edc5297379a845a
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-08-03 15:37:27 +0000

    Fix SCHEDULING-382: Improve progress bar management
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@13736 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler_plugins/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/composite/RunningJobComposite.java
+++ scheduler_plugins/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/composite/RunningJobComposite.java
@@ -32,11 +32,14 @@ package org.ow2.proactive.scheduler.gui.composite;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.TableEditor;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -44,6 +47,7 @@ import org.eclipse.swt.widgets.TableItem;
 import org.ow2.proactive.scheduler.common.job.JobId;
 import org.ow2.proactive.scheduler.common.job.JobInfo;
 import org.ow2.proactive.scheduler.common.job.JobState;
+import org.ow2.proactive.scheduler.common.job.JobStatus;
 import org.ow2.proactive.scheduler.common.task.TaskInfo;
 import org.ow2.proactive.scheduler.common.task.util.ResultPreviewTool.SimpleTextPanel;
 import org.ow2.proactive.scheduler.gui.data.ActionsManager;
@@ -75,6 +79,8 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
     /** This list is used to remember which progress bar weren't disposed */
     private List<ProgressBar> progressBars = null;
 
+    private Timer dotTimer;
+
     // -------------------------------------------------------------------- //
     // --------------------------- constructor ---------------------------- //
     // -------------------------------------------------------------------- //
@@ -92,6 +98,7 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
         jobsController.addEventJobsListener(this);
         tableEditors = new ArrayList<TableEditor>();
         progressBars = new ArrayList<ProgressBar>();
+        dotTimer = new Timer("DotTimer");
     }
 
     // -------------------------------------------------------------------- //
@@ -137,13 +144,13 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
         Table table = super.createTable(parent, tableId);
         TableColumn tc = new TableColumn(table, SWT.RIGHT, 2);
         tc.setText(COLUMN_PROGRESS_TEXT_TITLE);
-        tc.setWidth(70);
+        tc.setWidth(78);
         tc.setMoveable(true);
         tc.setToolTipText("You cannot sort by this column");
 
         tc = new TableColumn(table, SWT.NONE, 2);
         tc.setText(COLUMN_PROGRESS_BAR_TITLE);
-        tc.setWidth(70);
+        tc.setWidth(100);
         tc.setMoveable(true);
         tc.setToolTipText("You cannot sort by this column");
         return table;
@@ -160,22 +167,9 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
         for (int i = 0; i < cols.length; i++) {
             String title = cols[i].getText();
             if (title.equals(COLUMN_PROGRESS_BAR_TITLE)) {
-                //                ProgressBar bar;
-                //                if (job.getNumberOfFinishedTasks() > 0) {
-                //                    bar = new ProgressBar(table, SWT.NONE);
-                //                    bar.setMaximum(job.getTotalNumberOfTasks());
-                //                    bar.setSelection(job.getNumberOfFinishedTasks());
-                //                } else {
-                //                    bar = new ProgressBar(table, SWT.INDETERMINATE);
-                //                }
-                ProgressBar bar;
-                if (job.getTotalNumberOfTasks() == 1) {
-                    bar = new ProgressBar(table, SWT.INDETERMINATE);
-                } else {
-                    bar = new ProgressBar(table, SWT.NONE);
-                    bar.setMaximum(job.getTotalNumberOfTasks());
-                    bar.setSelection(job.getNumberOfFinishedTasks());
-                }
+                ProgressBar bar = new ProgressBar(table, SWT.NONE);
+                bar.setMaximum(job.getTotalNumberOfTasks() * 100);
+                bar.setSelection(job.getNumberOfFinishedTasks() * 100);
                 TableEditor editor = new TableEditor(table);
                 editor.grabHorizontal = true;
                 editor.grabVertical = true;
@@ -188,6 +182,10 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
                 progressBars.add(bar);
             } else if (title.equals(COLUMN_PROGRESS_TEXT_TITLE)) {
                 item.setText(i, job.getNumberOfFinishedTasks() + "/" + job.getTotalNumberOfTasks());
+            } else if (title.equals(COLUMN_STATE_TITLE)) {
+                DotTask dt = new DotTask(getDisplay(), item, i, job);
+                item.setData("dotTask", dt);
+                dotTimer.schedule(dt, 1000, DotTask.REFRESH_BAR_DELAY * 1000);
             }
         }
         return item;
@@ -222,13 +220,21 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
                     int[] j = getTable().getSelectionIndices();
 
                     TableItem item = getTable().getItem(i);
-                    ProgressBar bar = ((ProgressBar) item.getData("bar"));
                     TableEditor editor = ((TableEditor) item.getData("editor"));
+                    ProgressBar bar = ((ProgressBar) item.getData("bar"));
+                    JobState job = ((DotTask) item.getData("dotTask")).getJob();
+                    bar.setSelection(bar.getMaximum());
+                    try {
+                        Thread.sleep(150);
+                    } catch (Exception e) {
+                    }
                     bar.dispose();
                     progressBars.remove(bar);
                     editor.dispose();
                     tableEditors.remove(editor);
                     getTable().remove(i);
+
+                    DotTask.updateRunningFactor(job);
 
                     if (j.length == 1) {
                         if (i == j[0]) {
@@ -270,8 +276,9 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
                     }
 
                     TableColumn[] cols = getTable().getColumns();
-                    for (TableColumn col : cols)
+                    for (TableColumn col : cols) {
                         col.notifyListeners(SWT.Move, null);
+                    }
                     // enabling/disabling button permitted with this job
                     ActionsManager.getInstance().update();
                     decreaseCount();
@@ -288,6 +295,20 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
      */
     public void runningTaskEvent(TaskInfo info) {
         super.stateUpdate(info.getJobId());
+        switch (info.getStatus()) {
+            case WAITING_ON_FAILURE:
+            case WAITING_ON_ERROR:
+            case FAULTY:
+            case ABORTED:
+            case FAILED:
+                TableItem[] items = getTable().getItems();
+                for (TableItem it : items) {
+                    if (((JobId) (it.getData())).equals(info.getJobId())) {
+                        ((DotTask) it.getData("dotTask")).restartTask();
+                        break;
+                    }
+                }
+        }
     }
 
     /**
@@ -310,24 +331,13 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
                         }
                     }
 
+                    ((DotTask) item.getData("dotTask")).finishedTask();
+
                     TableColumn[] cols = table.getColumns();
                     JobState job = JobsController.getLocalView().getJobById(taskInfo.getJobId());
                     for (int i = 0; i < cols.length; i++) {
                         String title = cols[i].getText();
-                        if ((title != null) &&
-                            (title.equals(COLUMN_PROGRESS_BAR_TITLE) && (!item.isDisposed()))) {
-                            //                            if (job.getNumberOfFinishedTasks() == 1 && job.getTotalNumberOfTasks() > 1) {
-                            //                                ProgressBar bar = (ProgressBar) item.getData("bar");
-                            //                                bar.dispose();
-                            //                                TableEditor editor = (TableEditor) item.getData("editor");
-                            //                                bar = new ProgressBar(table, SWT.NONE);
-                            //                                bar.setMaximum(job.getTotalNumberOfTasks());
-                            //                                editor.setEditor(bar, item, i);
-                            //                                item.setData("bar", bar);
-                            //                                progressBars.add(bar);
-                            //                            }
-                            ((ProgressBar) item.getData("bar")).setSelection(job.getNumberOfFinishedTasks());
-                        } else if ((title != null) && (title.equals(COLUMN_PROGRESS_TEXT_TITLE))) {
+                        if (COLUMN_PROGRESS_TEXT_TITLE.equals(title)) {
                             item.setText(i, job.getNumberOfFinishedTasks() + "/" +
                                 job.getTotalNumberOfTasks());
                         }
@@ -374,4 +384,206 @@ public class RunningJobComposite extends AbstractJobComposite implements Running
             super.stateUpdate(jobId);
         }
     }
+}
+
+class DotTask extends TimerTask {
+
+    private static final String TOTAL_ESTIMATED_JOB_TIME_PROPERTY = "job.average.completion";
+    private static final String MAKE_AVERAGE_PROPERTY = "job.average.makeaverage";
+    protected static final int REFRESH_BAR_DELAY = 5;//in second
+    private static final int RUNNING_TASK_MAX_PERCENT = 92;
+    //default and setting value
+    private static int FINISHED_TASK_REDUCE_FACTOR = 1;
+    private static int TOTAL_ESTIMATED_JOB_TIME = 60 * 60;//1 hour (in second)
+    private static boolean MAKE_AVERAGE = false;
+    //calculated value
+    private static int RUNNING_TASK_REDUCE_FACTOR;
+
+    private TableItem item;
+    private Display display;
+    private JobState job;
+    private int colId;
+    private double[] tasks;
+    private int index;
+
+    static {
+        try {
+            if (System.getProperty(TOTAL_ESTIMATED_JOB_TIME_PROPERTY) != null) {
+                try {
+                    //set total estimated job time
+                    TOTAL_ESTIMATED_JOB_TIME = Integer.parseInt(System
+                            .getProperty(TOTAL_ESTIMATED_JOB_TIME_PROPERTY));
+                } catch (Exception e) {
+                    //no way to get the int value from property... keep default value
+                }
+            }
+            //set running reduce factor to fit estimated job time
+            RUNNING_TASK_REDUCE_FACTOR = findX();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        if (System.getProperty(MAKE_AVERAGE_PROPERTY) != null) {
+            try {
+                //set total estimated job time
+                MAKE_AVERAGE = Boolean.parseBoolean(System.getProperty(MAKE_AVERAGE_PROPERTY));
+            } catch (Exception e) {
+                //no way to get the int value from property... keep default value
+            }
+        }
+    }
+
+    private static int totalTasks = 0;
+    private static int jobNumber = 0;
+
+    public static void updateRunningFactor(JobState job) {
+        if (MAKE_AVERAGE) {
+            try {
+                totalTasks += job.getTotalNumberOfTasks();
+                jobNumber++;
+                //compute mean job duration and set total estimated job time
+                TOTAL_ESTIMATED_JOB_TIME = (TOTAL_ESTIMATED_JOB_TIME + (int) (job.getFinishedTime() - job
+                        .getStartTime()) / 1000) / 2;
+                //set running reduce factor to fit estimated job time
+                RUNNING_TASK_REDUCE_FACTOR = findX();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Create a new instance of DotTask
+     *
+     * @param items
+     * @param jobs
+     * @param colIds
+     * @param display
+     */
+    public DotTask(Display display, TableItem item, int colId, JobState job) {
+        this.item = item;
+        this.colId = colId;
+        this.display = display;
+        this.job = job;
+        this.tasks = new double[job.getTotalNumberOfTasks()];
+        this.index = job.getNumberOfFinishedTasks();
+        for (int i = 0; i < this.index; i++) {
+            this.tasks[i] = 100;
+        }
+    }
+
+    @Override
+    public void run() {
+        if (item.isDisposed()) {
+            this.cancel();
+            return;
+        }
+        if (job.getStatus() == JobStatus.RUNNING) {
+            double tmp = 0;
+            for (int i = 0; i < index; i++) {
+                if (tasks[i] < 100) {
+                    tasks[i] += ((100 - tasks[i]) / (FINISHED_TASK_REDUCE_FACTOR));
+                }
+                tmp += tasks[i];
+            }
+            try {
+                for (int i = index; i < index + job.getNumberOfRunningTasks(); i++) {
+                    if (tasks[i] < RUNNING_TASK_MAX_PERCENT) {
+                        tasks[i] += ((100 - tasks[i]) / (RUNNING_TASK_REDUCE_FACTOR));
+                    }
+                    tmp += tasks[i];
+                }
+                final int sum = (int) tmp;
+
+                display.syncExec(new Runnable() {
+                    public void run() {
+                        try {
+                            ((ProgressBar) item.getData("bar")).setSelection(sum);
+                            item.setText(colId, JobStatus.RUNNING + " (" + job.getNumberOfRunningTasks() +
+                                ")");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void finishedTask() {
+        index++;
+        run();
+    }
+
+    public void restartTask() {
+	try {
+	        //shift values
+	        for (int i = index; i < tasks.length - 1; i++) {
+	            tasks[i] = tasks[i + 1];
+	        }
+	        //set last to 0;
+	        this.tasks[tasks.length - 1] = 0;
+	        index--;
+	        run();
+	} catch (Exception e){
+		e.printStackTrace();
+	}
+    }
+
+    private static int findX() {
+        double sum;
+        int step;
+        boolean terminated = false;
+        int x1 = 1, x2 = 130000, x = (x1 + x2) / 2;//x2=divisor for 15 days
+        if (TOTAL_ESTIMATED_JOB_TIME > 15 * 24 * 3600) {//more than 15 days
+            return x2;
+        }
+        int stepToReach = (int) Math.round((double) TOTAL_ESTIMATED_JOB_TIME / (double) REFRESH_BAR_DELAY);
+        if (jobNumber > 0) {
+            stepToReach += (totalTasks / jobNumber);
+        } else if (stepToReach < 1) {
+            return x1;
+        }
+        int i = 0;
+        do {
+            sum = 0;
+            step = 0;
+            do {
+                sum += (100 - sum) / x;
+                i++;
+                step++;
+                if (step > stepToReach) {
+                    x2 = x;
+                    x = (x1 + x2) / 2;
+                    if (x2 == x) {
+                        terminated = true;
+                    }
+                    sum = 0;
+                    step = 0;
+                    break;
+                } else if (sum > RUNNING_TASK_MAX_PERCENT) {
+                    x1 = x;
+                    x = (x1 + x2) / 2;
+                    if (x1 == x) {
+                        terminated = true;
+                    }
+                    sum = 0;
+                    step = 0;
+                    break;
+                }
+            } while (!terminated);
+        } while (!terminated);
+        return x;
+    }
+
+    /**
+     * Get the job
+     *
+     * @return the job
+     */
+    public JobState getJob() {
+        return job;
+    }
+
 }
