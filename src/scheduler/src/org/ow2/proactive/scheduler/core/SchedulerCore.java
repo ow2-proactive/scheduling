8584efe40479159fe7997c4dd339fc5d3e4c870f
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-08-04 11:21:22 +0000

    PICKED FROM TRUNK 13742
    Format for SCHEDULING-383, Complete commit 13737 :
    To be cherry picked for v1.0.x
    
    
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_1.0.x@13754 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/core/SchedulerCore.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/core/SchedulerCore.java
@@ -893,8 +893,8 @@ public class SchedulerCore implements UserSchedulerInterface_, AdminMethodsInter
                     if (td.getNumberOfExecutionOnFailureLeft() > 0) {
                         td.setStatus(TaskStatus.WAITING_ON_FAILURE);
                         job.newWaitingTask();
-                        if (job.getNumberOfRunningTasks() == 0){
-				job.setStatus(JobStatus.STALLED);
+                        if (job.getNumberOfRunningTasks() == 0) {
+                            job.setStatus(JobStatus.STALLED);
                         }
                         frontend.taskStateUpdated(job.getOwner(), new NotificationData<TaskInfo>(
                             SchedulerEvent.TASK_WAITING_FOR_RESTART, td.getTaskInfo()));
