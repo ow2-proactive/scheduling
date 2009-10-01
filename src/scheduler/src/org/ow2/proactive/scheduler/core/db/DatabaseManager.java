a57101fe78ae1eda901ccc830069ac4d168a78a7
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-01 16:10:46 +0000

    Move EmptyDBImpl into databaseManager Scheduler can be started on empty db using -Dscheduler.database.nodb property
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@14261 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/core/db/DatabaseManager.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/core/db/DatabaseManager.java
@@ -31,8 +31,15 @@
  */
 package org.ow2.proactive.scheduler.core.db;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.ow2.proactive.scheduler.common.job.JobId;
+import org.ow2.proactive.scheduler.common.job.JobResult;
+import org.ow2.proactive.scheduler.common.task.TaskId;
+import org.ow2.proactive.scheduler.common.task.TaskResult;
 import org.ow2.proactive.scheduler.core.RecoverCallback;
 import org.ow2.proactive.scheduler.job.InternalJob;
 
@@ -52,8 +59,11 @@ public abstract class DatabaseManager {
 
     public static DatabaseManager getInstance() {
         if (dbManager == null) {
-            dbManager = new HibernateDatabaseManager();
-            //dbManager = new EmptyDatabaseManager();
+            if (System.getProperty("scheduler.database.nodb") != null) {
+                dbManager = new EmptyDatabaseManager();
+            } else {
+                dbManager = new HibernateDatabaseManager();
+            }
         }
         return dbManager;
     }
@@ -192,5 +202,147 @@ public abstract class DatabaseManager {
      * @param o the object to unload.
      */
     public abstract void unload(Object o);
+
+}
+
+/**
+ * This is an implementation of memory database.<br>
+ * Everything is kept in memory. Can be used for testing purpose.<br>
+ * <b>WARNING</B> this implementation can lead to "out of memory" error.
+ *
+ * @author The ProActive Team
+ * @since ProActive Scheduling 1.0
+ */
+class EmptyDatabaseManager extends DatabaseManager {
+
+    private Map<JobId, InternalJob> jobs;
+
+    /**
+     * Create a new instance of EmptyDatabaseManager
+     */
+    public EmptyDatabaseManager() {
+        jobs = new HashMap<JobId, InternalJob>();
+    }
+
+    /**
+     * Do nothing
+     */
+    public void setProperty(String propertyName, String value) {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void build() {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void close() {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void startTransaction() {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void commitTransaction() {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void rollbackTransaction() {
+    }
+
+    /**
+     * Store the given object
+     *
+     * @param o the new object to store.
+     */
+    public void register(Object o) {
+        InternalJob job = (InternalJob) o;
+        jobs.put(job.getId(), job);
+    }
+
+    /**
+     * Do nothing
+     */
+    public void delete(Object o) {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void update(Object o) {
+    }
+
+    /**
+     * Return an empty list
+     */
+    public <T> List<T> recover(Class<T> egClass) {
+        return recover(egClass, new Condition[] {});
+    }
+
+    /**
+     * If egClass is JobResultImpl, return the jobResult corresponding to the given JobId in the given condition,
+     * If egClass is TaskresultImpl, return the taskResult corresponding to the given TaskId in the given condition,
+     * Otherwise, return an empty list
+     */
+    @SuppressWarnings("unchecked")
+    public <T> List<T> recover(Class<T> egClass, Condition... conditions) {
+        if (conditions == null || conditions.length == 0) {
+            return new ArrayList<T>();
+        }
+        if (JobResult.class.isAssignableFrom(egClass)) {
+            ArrayList<T> res = new ArrayList<T>();
+            res.add((T) jobs.get(((JobId) conditions[0].getValue())).getJobResult());
+            return res;
+        } else if (TaskResult.class.isAssignableFrom(egClass)) {
+            ArrayList<T> res = new ArrayList<T>();
+            TaskId id = (TaskId) conditions[0].getValue();
+            res.add((T) jobs.get(id.getJobId()).getJobResult().getResult(id.getReadableName()));
+            return res;
+        } else {
+            return new ArrayList<T>();
+        }
+    }
+
+    /**
+     * Return an empty list of InternalJob
+     */
+    public List<InternalJob> recoverAllJobs() {
+        return recoverAllJobs(null);
+    }
+
+    /**
+     * Return an empty list of InternalJob
+     */
+    public List<InternalJob> recoverAllJobs(RecoverCallback callback) {
+        return new ArrayList<InternalJob>();
+    }
+
+    /**
+     * Do nothing
+     */
+    public void synchronize(Object o) {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void load(Object o) {
+    }
+
+    /**
+     * Do nothing
+     */
+    public void unload(Object o) {
+    }
 
 }
