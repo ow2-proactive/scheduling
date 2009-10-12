5bed1360478f318e6f8354066f22611ad47e4ad0
oleg_smirnov <oleg_smirnov@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-10-12 08:15:27 +0000

    SCHEDULING-469 Added missed class
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@14368 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/resource-manager/src/org/ow2/proactive/resourcemanager/nodesource/infrastructure/manager/DefaultInfrastructureManager.java
@@ -0,0 +1,158 @@
+/*
+ * ################################################################
+ *
+ * ProActive: The Java(TM) library for Parallel, Distributed,
+ *            Concurrent computing with Security and Mobility
+ *
+ * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Contact: proactive@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+package org.ow2.proactive.resourcemanager.nodesource.infrastructure.manager;
+
+import java.rmi.dgc.VMID;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+import org.objectweb.proactive.core.node.Node;
+import org.objectweb.proactive.core.util.log.ProActiveLogger;
+import org.ow2.proactive.resourcemanager.exception.RMException;
+import org.ow2.proactive.resourcemanager.nodesource.utils.NamesConvertor;
+import org.ow2.proactive.resourcemanager.utils.RMLoggers;
+
+
+/**
+ *
+ * A default infrastructure manager.
+ * Cannot perform the deployment but is able to register incoming nodes
+ * (existing nodes added by url) and remove them.
+ *
+ */
+public class DefaultInfrastructureManager extends InfrastructureManager {
+
+    /** logger*/
+    protected static Logger logger = ProActiveLogger.getLogger(RMLoggers.NODESOURCE);
+    /** registered nodes number */
+    protected int nodesCount = 0;
+
+    /**
+     * Proactive default constructor.
+     */
+    public DefaultInfrastructureManager() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void configure(Object... parameters) throws RMException {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void acquireNode() {
+        logger.error("acquireNode() is invalid operation");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void acquireAllNodes() {
+        logger.error("acquireAllNodes() is invalid operation");
+    }
+
+    /**
+     * Remove the node from underlying infrastructure. Terminates proactive runtime if there is no more nodes.
+     * @param node node to release
+     * @throws RMException if any problems occurred
+     */
+    public void removeNode(Node node) throws RMException {
+        try {
+            logger.info("Terminating the node " + node.getNodeInformation().getName());
+            if (!isThereNodesInSameJVM(node)) {
+                final Node n = node;
+                nodeSource.executeInParallel(new Runnable() {
+                    public void run() {
+                        try {
+                            logger.info("Terminating the runtime " + n.getProActiveRuntime().getURL());
+                            n.getProActiveRuntime().killRT(false);
+                        } catch (Exception e) {
+                            //do nothing, no exception treatment for node just killed before
+                        }
+                    }
+                });
+            }
+            nodesCount--;
+        } catch (Exception e) {
+            throw new RMException(e);
+        }
+    }
+
+    /**
+     * Node source string representation
+     */
+    public String toString() {
+        return NamesConvertor.beautifyName(this.getClass().getSimpleName());
+    }
+
+    /**
+     * Node source description
+     */
+    public String getDescription() {
+        return "Default infrastructure";
+    }
+
+    /**
+     * Check if there are any other nodes handled by the NodeSource in the same JVM of the node
+     * passed in parameter.
+     * @param node Node to check if there any other node of the NodeSource in the same JVM
+     * @return true there is another node in the node's JVM handled by the nodeSource, false otherwise.
+     */
+    public boolean isThereNodesInSameJVM(Node node) {
+        VMID nodeID = node.getVMInformation().getVMID();
+        String nodeToTestUrl = node.getNodeInformation().getURL();
+        Collection<Node> nodesList = nodeSource.getAliveNodes();
+        for (Node n : nodesList) {
+            if (!n.getNodeInformation().getURL().equals(nodeToTestUrl) &&
+                n.getVMInformation().getVMID().equals(nodeID)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void registerAcquiredNode(Node node) throws RMException {
+        nodesCount++;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void shutDown() {
+    }
+}
