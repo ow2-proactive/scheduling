4663e9146b64e5f8b215d03d6eb74ea039b679cf
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2009-12-15 10:02:58 +0000

    Fix ProActive Header
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@15111 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/common/org/ow2/proactive/authentication/NoCallbackHandler.java
+++ src/common/org/ow2/proactive/authentication/NoCallbackHandler.java
@@ -40,13 +40,14 @@ package org.ow2.proactive.authentication;
  * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
  *
- * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
+ * Copyright (C) 1997-2009 INRIA/University of 
+ * 						   Nice-Sophia Antipolis/ActiveEon
  * Contact: proactive@ow2.org
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or any later version.
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -58,9 +59,11 @@ package org.ow2.proactive.authentication;
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
  *
+ * If needed, contact us to obtain a release under GPL Version 2. 
+ *
  *  Initial developer(s):               The ProActive Team
  *                        http://proactive.inria.fr/team_members.htm
- *  Contributor(s):
+ *  Contributor(s): ActiveEon Team - http://www.activeeon.com
  *
  * ################################################################
  * $$PROACTIVE_INITIAL_DEV$$
