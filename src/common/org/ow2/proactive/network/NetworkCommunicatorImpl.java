0d0948b2f9db06170bf94f600b7016063ea8f77e
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-01-26 09:03:23 +0000

    SCHEDULING-551 & SCHEDULING-552 : * Update network classes and javadoc * Add methodCall class
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@15296 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/common/org/ow2/proactive/network/NetworkCommunicatorImpl.java
+++ src/common/org/ow2/proactive/network/NetworkCommunicatorImpl.java
@@ -4,9 +4,9 @@
  * ProActive: The Java(TM) library for Parallel, Distributed,
  *            Concurrent computing with Security and Mobility
  *
- * Copyright (C) 1997-2009 INRIA/University of 
- * 						   Nice-Sophia Antipolis/ActiveEon
- * Contact: proactive@ow2.org
+ * Copyright (C) 1997-2010 INRIA/University of 
+ * 				Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,17 +23,19 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
  *
- * If needed, contact us to obtain a release under GPL Version 2. 
+ * If needed, contact us to obtain a release under GPL Version 2 
+ * or a different license than the GPL.
  *
  *  Initial developer(s):               The ProActive Team
  *                        http://proactive.inria.fr/team_members.htm
- *  Contributor(s):
+ *  Contributor(s): ActiveEon Team - http://www.activeeon.com
  *
  * ################################################################
- * $$PROACTIVE_INITIAL_DEV$$
+ * $$ACTIVEEON_CONTRIBUTOR$$
  */
 package org.ow2.proactive.network;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -41,14 +43,29 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
+/**
+ * NetworkCommunicatorImpl is the default implementation if the network communicator.
+ * This implementation uses fixed thread pool to handle tasks.
+ *
+ * @author The ProActive Team
+ * @since ProActive Scheduling 2.0
+ */
 public class NetworkCommunicatorImpl implements NetworkCommunicator {
 
     private ExecutorService executor;
 
+    /**
+     * Create a new instance of NetworkCommunicatorImpl passing the number of threads to be used.
+     * 
+     * @param nThreads the number of thread in the thread pool
+     */
     public NetworkCommunicatorImpl(int nThreads) {
         executor = Executors.newFixedThreadPool(nThreads);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void execute(Runnable task) {
         if (task == null) {
             throw new IllegalArgumentException("Task must not be null.");
@@ -56,46 +73,51 @@ public class NetworkCommunicatorImpl implements NetworkCommunicator {
         executor.execute(task);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void execute(Collection<? extends Runnable> tasks) {
         if (tasks == null) {
             throw new IllegalArgumentException("Tasks collection must not be null.");
-        }
-        if (tasks.size() == 0) {
-            return;
         }
         for (Runnable task : tasks) {
             executor.execute(task);
         }
     }
 
-    public <T> Collection<T> execute(Collection<? extends Timed<T>> tasks, long timeout) {
+    /**
+     * {@inheritDoc}
+     */
+    public <T extends Timed<?>> Collection<T> execute(Collection<T> tasks, long timeout) {
         if (timeout <= 0) {
             throw new IllegalArgumentException("Timeout must be positive.");
         }
 
+        if (tasks.size() == 0) {
+            return new ArrayList<T>();
+        }
+
+        //put each tasks in the thread pool queue
         this.execute(tasks);
 
         Collection<T> results = new LinkedList<T>();
         long timeSpent = 0;
+        //wait for all result to be available
         do {
-
             // all results are available;
             if (tasks.size() == 0) {
                 break;
             }
 
             long start = System.currentTimeMillis();
-            Iterator<? extends Timed<T>> iter = tasks.iterator();
+            Iterator<T> iter = tasks.iterator();
 
             while (iter.hasNext()) {
-                Timed<T> task = iter.next();
+                T task = iter.next();
                 synchronized (task) {
                     if (task.isDone()) {
                         iter.remove();
-                        T result = task.getResult();
-                        if (result != null) {
-                            results.add(result);
-                        }
+                        results.add(task);
                     }
                 }
             }
@@ -104,9 +126,11 @@ public class NetworkCommunicatorImpl implements NetworkCommunicator {
             } catch (InterruptedException e) {
             }
             timeSpent += (System.currentTimeMillis() - start);
+            //time out reached
         } while (timeSpent < timeout);
 
-        for (Timed<T> task : tasks) {
+        //for every non-terminated task, call timeout action
+        for (T task : tasks) {
             synchronized (task) {
                 task.timeoutAction();
             }
