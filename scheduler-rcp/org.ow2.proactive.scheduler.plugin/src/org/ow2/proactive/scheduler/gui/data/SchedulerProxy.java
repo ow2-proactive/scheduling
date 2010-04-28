8be70887d0ef29a6006ee8c287d43720601cb408
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-04-28 12:51:56 +0000

    Fix for SCHEDULING-582
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@16026 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler-rcp/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/data/SchedulerProxy.java
+++ scheduler-rcp/org.ow2.proactive.scheduler.plugin/src/org/ow2/proactive/scheduler/gui/data/SchedulerProxy.java
@@ -85,8 +85,8 @@ import org.ow2.proactive.scheduler.gui.views.SeparatedJobView;
 public class SchedulerProxy implements AdminSchedulerInterface {
 
     private static final long SCHEDULER_SERVER_PING_FREQUENCY = 5000;
-    public static final int CONNECTED = 0;
-    public static final int LOGIN_OR_PASSWORD_WRONG = 1;
+    public static final int CONNECTED = 1;
+    public static final int LOGIN_OR_PASSWORD_WRONG = 2;
     private static SchedulerProxy instance = null;
     private SchedulerAuthenticationInterface sai;
     private UserSchedulerInterface scheduler = null;
