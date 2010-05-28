399b608a3c4e5aeeaf22226bc4aef23a5ad36dc5
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-05-28 09:43:35 +0000

    Clean RM_DB also in scheduler-start-clean script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@16254 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- bin/windows/scheduler-start-clean.bat
+++ bin/windows/scheduler-start-clean.bat
@@ -3,7 +3,7 @@ echo.
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 call init.bat scheduler-log4j-server
-%JAVA_CMD% -Dpa.scheduler.db.hibernate.dropdb=true -Dderby.stream.error.file="%PA_SCHEDULER%\.logs\derby.log" org.ow2.proactive.scheduler.util.SchedulerStarter %*
+%JAVA_CMD% -Dpa.scheduler.db.hibernate.dropdb=true -Dpa.rm.db.hibernate.dropdb=true -Dderby.stream.error.file="%PA_SCHEDULER%\.logs\derby.log" org.ow2.proactive.scheduler.util.SchedulerStarter %*
 
 ENDLOCAL
 
