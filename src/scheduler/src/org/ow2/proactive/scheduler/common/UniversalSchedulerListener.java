043079093a6ea51df3fcc3acf2ec6ecbe6247000
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-06-01 13:10:19 +0000

    Add an Universal Listener for the Scheduler (to avoid listener stub downloading form client to core)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@16283 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/scheduler/src/org/ow2/proactive/scheduler/common/UniversalSchedulerListener.java
@@ -0,0 +1,106 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2010 INRIA/University of
+ * 				Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2
+ * or a different license than the GPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+package org.ow2.proactive.scheduler.common;
+
+import org.ow2.proactive.scheduler.common.job.JobInfo;
+import org.ow2.proactive.scheduler.common.job.JobState;
+import org.ow2.proactive.scheduler.common.job.UserIdentification;
+import org.ow2.proactive.scheduler.common.task.TaskInfo;
+
+
+/**
+ * Wrapper class for Scheduler Event Listener.
+ * The goal of this class is to overcome the classloading problem (i.e. the user defined
+ * scheduler listener stub have to be loaded on Scheduler core side).
+ *
+ * @author The ProActive Team
+ * @since ProActive Scheduling 2.1
+ */
+public class UniversalSchedulerListener implements SchedulerEventListener {
+
+    // wrapped listener
+    private final SchedulerEventListener internalListener;
+
+    /**
+     * Create a wrapper for user defined listener.
+     * @param internalListener the wrapped listener.
+     */
+    public UniversalSchedulerListener(SchedulerEventListener internalListener) {
+        this.internalListener = internalListener;
+    }
+
+    // For active object creation;
+    public UniversalSchedulerListener() {
+        this.internalListener = null;
+    }
+
+    /* (non-Javadoc)
+     * @see org.ow2.proactive.scheduler.common.SchedulerEventListener#jobStateUpdatedEvent(org.ow2.proactive.scheduler.common.NotificationData)
+     */
+    public void jobStateUpdatedEvent(NotificationData<JobInfo> notification) {
+        this.internalListener.jobStateUpdatedEvent(notification);
+    }
+
+    /* (non-Javadoc)
+     * @see org.ow2.proactive.scheduler.common.SchedulerEventListener#jobSubmittedEvent(org.ow2.proactive.scheduler.common.job.JobState)
+     */
+    public void jobSubmittedEvent(JobState job) {
+        this.internalListener.jobSubmittedEvent(job);
+    }
+
+    /* (non-Javadoc)
+     * @see org.ow2.proactive.scheduler.common.SchedulerEventListener#schedulerStateUpdatedEvent(org.ow2.proactive.scheduler.common.SchedulerEvent)
+     */
+    public void schedulerStateUpdatedEvent(SchedulerEvent eventType) {
+        this.internalListener.schedulerStateUpdatedEvent(eventType);
+    }
+
+    /* (non-Javadoc)
+     * @see org.ow2.proactive.scheduler.common.SchedulerEventListener#taskStateUpdatedEvent(org.ow2.proactive.scheduler.common.NotificationData)
+     */
+    public void taskStateUpdatedEvent(NotificationData<TaskInfo> notification) {
+        this.internalListener.taskStateUpdatedEvent(notification);
+    }
+
+    /* (non-Javadoc)
+     * @see org.ow2.proactive.scheduler.common.SchedulerEventListener#usersUpdatedEvent(org.ow2.proactive.scheduler.common.NotificationData)
+     */
+    public void usersUpdatedEvent(NotificationData<UserIdentification> notification) {
+        this.internalListener.usersUpdatedEvent(notification);
+    }
+
+}
