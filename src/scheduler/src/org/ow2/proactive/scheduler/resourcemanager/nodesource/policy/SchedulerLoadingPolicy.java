c11d9054f6ee1f0cbdad826d7801aebc9eac5586
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-06-23 13:15:20 +0000

    PICKED FROM TRUNK 16476
    SCHEDULING-638 Corrected required nodes recalculation for scheduler loading policy in the resource manager.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_2.1.x@16534 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/resourcemanager/nodesource/policy/SchedulerLoadingPolicy.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/resourcemanager/nodesource/policy/SchedulerLoadingPolicy.java
@@ -190,7 +190,7 @@ public class SchedulerLoadingPolicy extends SchedulerAwarePolicy implements Init
     }
 
     protected IntWrapper getTotalNodeNumber() {
-        return new IntWrapper(nodeSource.getRMCore().getState().getFreeNodesNumber());
+        return new IntWrapper(nodeSource.getRMCore().getState().getTotalAliveNodesNumber());
     }
 
     private void refreshPolicyState() {
