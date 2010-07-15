2e3f43b483920d1e17acd43b0606d1315d31e4dc
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-07-15 09:50:37 +0000

    Reasteasy + multiple output formats per resource
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/trunk@16658 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/org/ow2/proactive_grid_cloud_portal/Test.java
@@ -0,0 +1,35 @@
+package org.ow2.proactive_grid_cloud_portal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/a")
+public class Test { 
+
+    
+    /**
+     * curl -H "Accept: text/plain" http://localhost:8080/proactive_grid_cloud_portal/a  
+     * @return
+     */
+    
+	@GET
+	@Produces("text/plain")
+	public String sayHello() {
+        return "hello";
+    }
+	
+	
+	/**
+	 * curl -H "Accept: text/html" http://localhost:8080/proactive_grid_cloud_portal/a 
+	 * @return
+	 */
+	   @GET
+	    @Produces("text/html")
+	    public String sayHelloHtml() {
+	        return "<html><body>hello</body></html>";
+	    }
+	    
+
+}
