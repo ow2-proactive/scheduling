9934f1a85f442f8a4e6418ff69469053151723df
bodnartv <bodnartv@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-07-20 10:07:09 +0000

    SCHEDULING-772: Increase VM memory in .bat starter script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@16667 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- bin/windows/scheduler-start.bat
+++ bin/windows/scheduler-start.bat
@@ -3,7 +3,7 @@ echo.
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 call init.bat scheduler-log4j-server
-%JAVA_CMD% -Dderby.stream.error.file="%PA_SCHEDULER%\.logs\derby.log" org.ow2.proactive.scheduler.util.SchedulerStarter %*
+%JAVA_CMD% -Dderby.stream.error.file="%PA_SCHEDULER%\.logs\derby.log" -Xms128m -Xmx1048m org.ow2.proactive.scheduler.util.SchedulerStarter %*
 
 ENDLOCAL
 
