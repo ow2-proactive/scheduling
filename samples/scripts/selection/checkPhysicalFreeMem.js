95c49af4739a9b931c60916304a3890b809024b3
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-08-18 11:37:59 +0000

    PICKED FROM TRUNK 16948
    Add a Physical Free Memory check in selection script samples. Works under unix* and windows but only with Sun JVMs > 1.5
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_2.1.x@16952 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ samples/scripts/selection/checkPhysicalFreeMem.js
@@ -0,0 +1,14 @@
+// Check if enough memory is available on the system
+// args[0] : expected memory size in Kb
+
+// WARNING : works only with Sun JVMs > 1.5
+
+var selected = false;
+
+var osb = java.lang.management.ManagementFactory.getOperatingSystemMXBean();
+var mem = osb.getFreePhysicalMemorySize()/1024;
+println(mem+" Kb available");
+//var swap = osb.getFreeSwapSpaceSize()/1024;
+//print(swap+" Kb");
+selected=java.lang.Integer.parseInt(mem)>java.lang.Integer.parseInt(arguments[0]);
+println("FreeMem check : "+selected);
