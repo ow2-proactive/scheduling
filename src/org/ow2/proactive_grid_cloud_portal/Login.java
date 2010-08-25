bf8545297d4c938b11531ae5db6951a6e7770391
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-08-25 16:08:22 +0000

    new version of the scheduler jars (support for tasks with graphical display) + login now returns correct http status code
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/rest_api/trunk@17038 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/Login.java
+++ src/org/ow2/proactive_grid_cloud_portal/Login.java
@@ -1,16 +1,23 @@
 package org.ow2.proactive_grid_cloud_portal;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
+import javax.security.auth.login.LoginException;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.jboss.resteasy.spi.UnauthorizedException;
+import org.objectweb.proactive.ActiveObjectCreationException;
 import org.objectweb.proactive.api.PAActiveObject;
+import org.objectweb.proactive.core.node.NodeException;
+import org.ow2.proactive.scheduler.common.exception.SchedulerException;
 import org.ow2.proactive.scheduler.ext.filessplitmerge.schedulertools.SchedulerProxyUserInterface;
 
 @Path("/login")
@@ -18,10 +25,11 @@ public class Login {
 
     @POST
    
-    public String login(@FormParam("username") String username, @FormParam("password") String password) {
+    public String login(@FormParam("username") String username, 
+            @FormParam("password") String password) throws UnauthorizedException{
         
         try {
-        
+            
             SchedulerProxyUserInterface scheduler =  PAActiveObject.newActive(
                     SchedulerProxyUserInterface.class, new Object[] {});
         
@@ -29,13 +37,17 @@ public class Login {
         
         return ""+SchedulerSessionMapper.getInstance().add(scheduler);
         
-        } catch (Throwable e) {
+        } catch (LoginException e) {
             e.printStackTrace();
-            throw new UnauthorizedException(e);
-        }
-          
-        
-       
+            throw new WebApplicationException(Response.status(HttpURLConnection.HTTP_UNAUTHORIZED).entity("invalid login/password").build());
+// throw new UnauthorizedException(e);
+        } catch (SchedulerException e) {
+            throw new WebApplicationException(Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity("no scheduler available").build());
+        } catch (ActiveObjectCreationException e) {
+            throw new WebApplicationException(Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity("no scheduler available").build());        
+        } catch (NodeException e) {
+            throw new WebApplicationException(Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity("no scheduler available").build());
+        } 
         
     }
     
