c0e45240ac5b245198a54cfde708baea4ed79a6b
ffonteno <ffonteno@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-08-26 06:58:48 +0000

    Resolved PORTAL-8: RMProxy is not java 5 compliant because of @Override annotations for methods declared in the ResourceManager interface.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/rest_api/trunk@17040 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/RMProxy.java
+++ src/org/ow2/proactive_grid_cloud_portal/RMProxy.java
@@ -42,102 +42,82 @@ public class RMProxy implements ResourceManager{
     }
     
     
-    @Override
     public BooleanWrapper addNode(String arg0) {
         return target.addNode(arg0);
     }
 
-    @Override
     public BooleanWrapper addNode(String arg0, String arg1) {
         return target.addNode(arg0, arg1);
     }
 
-    @Override
     public BooleanWrapper createNodeSource(String arg0, String arg1, Object[] arg2, String arg3, Object[] arg4) {
         return target.createNodeSource(arg0, arg1, arg2, arg3, arg4);
     }
 
-    @Override
     public BooleanWrapper disconnect() {
         return target.disconnect();
     }
 
-    @Override
     public NodeSet getAtMostNodes(int arg0, SelectionScript arg1) {
         return target.getAtMostNodes(arg0, arg1);
     }
 
-    @Override
     public NodeSet getAtMostNodes(int arg0, SelectionScript arg1, NodeSet arg2) {
         return target.getAtMostNodes(arg0, arg1, arg2);
     }
 
-    @Override
     public NodeSet getAtMostNodes(int arg0, List<SelectionScript> arg1, NodeSet arg2) {
         return target.getAtMostNodes(arg0, arg1, arg2);
     }
 
-    @Override
     public RMMonitoring getMonitoring() {
         return target.getMonitoring();
     }
 
-    @Override
     public IntWrapper getNodeSourcePingFrequency(String arg0) {
         return target.getNodeSourcePingFrequency(arg0);
     }
 
-    @Override
     public RMState getState() {
         return target.getState();
     }
 
-    @Override
     public Collection<PluginDescriptor> getSupportedNodeSourceInfrastructures() {
         return target.getSupportedNodeSourceInfrastructures();
     }
 
-    @Override
     public Collection<PluginDescriptor> getSupportedNodeSourcePolicies() {
         return target.getSupportedNodeSourcePolicies();
     }
 
-    @Override
     public BooleanWrapper isActive() {
         return target.isActive();
     }
 
-    @Override
     public BooleanWrapper nodeIsAvailable(String arg0) {
         return target.nodeIsAvailable(arg0);
     }
 
-    @Override
     public BooleanWrapper releaseNode(Node arg0) {
         return target.releaseNode(arg0);
     }
 
-    @Override
     public BooleanWrapper releaseNodes(NodeSet arg0) {
         return target.releaseNodes(arg0);
     }
 
-    @Override
     public BooleanWrapper removeNode(String arg0, boolean arg1) {
         return target.removeNode(arg0, arg1);
     }
 
-    @Override
     public BooleanWrapper removeNodeSource(String arg0, boolean arg1) {
         return target.removeNodeSource(arg0, arg1);
     }
 
-    @Override
     public BooleanWrapper setNodeSourcePingFrequency(int arg0, String arg1) {
         return target.setNodeSourcePingFrequency(arg0, arg1);
     }
 
-    @Override
     public BooleanWrapper shutdown(boolean arg0) {
         return target.shutdown(arg0);
     }
