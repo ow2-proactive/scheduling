b09cb27c44487d5b2fe6e8d42e96a5084e78a71a
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-09-03 12:21:16 +0000

    SCHEDULING-764 (WIP) : add a generic BatchJobInfrastructure that can wrap a user defined one.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@17154 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/scheduler/src/org/ow2/proactive/scheduler/resourcemanager/nodesource/policy/SchedulerLoadingPolicy.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/resourcemanager/nodesource/policy/SchedulerLoadingPolicy.java
@@ -72,6 +72,7 @@ import org.ow2.proactive.scheduler.common.task.TaskInfo;
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.GCMCustomisedInfrastructure",
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.PBSInfrastructure",
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.LSFInfrastructure",
+        "org.ow2.proactive.resourcemanager.nodesource.infrastructure.GenericBatchJobInfrastructure",
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.SSHInfrastructure",
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.SSHInfrastructure2",
         "org.ow2.proactive.resourcemanager.nodesource.infrastructure.EC2Infrastructure",
