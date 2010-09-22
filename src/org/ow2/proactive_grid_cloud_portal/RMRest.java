78c55f8b67e424aacfd4f01217a1047d384cf6b6
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-09-22 13:50:21 +0000

    rest_api : disconnect + isactive method on rm
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/rest_api/trunk@17389 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/RMRest.java
+++ src/org/ow2/proactive_grid_cloud_portal/RMRest.java
@@ -1,23 +1,38 @@
 package org.ow2.proactive_grid_cloud_portal;
 
+import java.net.HttpURLConnection;
+
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import org.jboss.resteasy.spi.UnauthorizedException;
 import org.objectweb.proactive.api.PAActiveObject;
 import org.objectweb.proactive.api.PAFuture;
 import org.ow2.proactive.resourcemanager.common.RMState;
 import org.ow2.proactive.resourcemanager.common.event.RMInitialState;
-import org.ow2.proactive.resourcemanager.frontend.RMMonitoring;
 import org.ow2.proactive.resourcemanager.frontend.ResourceManager;
 
 
 @Path("/rm")
 public class RMRest {
 
+    public ResourceManager checkAccess(String sessionId) throws WebApplicationException {
+        ResourceManager s = RMSessionMapper.getInstance().getSessionsMap().get(sessionId);
+
+        if (s == null) {
+            throw new WebApplicationException(Response.status(HttpURLConnection.HTTP_UNAUTHORIZED)
+                    .entity("you are not connected, try to log in first").build());
+        }
+
+        return s;
+    }
+    
+    
     @POST
     @Path("login")
     public String rmConnect(@FormParam("username") String username, @FormParam("password") String password) {
@@ -27,7 +42,6 @@ public class RMRest {
             RMProxy rm = PAActiveObject.newActive(RMProxy.class, new Object[] {});
 
             rm.init("rmi://localhost:1099/RM", username, password);
-
             return "" + RMSessionMapper.getInstance().add(rm);
 
         } catch (Throwable e) {
@@ -40,32 +54,38 @@ public class RMRest {
     @GET
     @Path("state")
     public RMState getState(@HeaderParam("sessionid") String sessionId) {
-        ResourceManager rm = RMSessionMapper.getInstance().getSessionsMap().get(sessionId);
-        if (rm == null) {
-            throw new UnauthorizedException();
-        }
-
+        ResourceManager rm = checkAccess(sessionId);
         return PAFuture.getFutureValue(rm.getState());
     }
 
     @GET
     @Path("monitoring")
     public RMInitialState getInitialState(@HeaderParam("sessionid") String sessionId) {
-        ResourceManager rm = RMSessionMapper.getInstance().getSessionsMap().get(sessionId);
-        if (rm == null) {
-            throw new UnauthorizedException();
-        }
+        ResourceManager rm = checkAccess(sessionId);
         return PAFuture.getFutureValue(rm.getMonitoring().getState());
     }
 
+    @GET
+    @Path("isactive")
+    public boolean isActive(@HeaderParam("sessionid") String sessionId) {
+        ResourceManager rm = checkAccess(sessionId);
+        return rm.isActive().booleanValue();
+    }
+    
     @POST
     @Path("node")
     public boolean addNode(@HeaderParam("sessionid") String sessionId, @FormParam("nodeurl") String nodeUrl) {
-        ResourceManager rm = RMSessionMapper.getInstance().getSessionsMap().get(sessionId);
-        if (rm == null) {
-            throw new UnauthorizedException();
-        }
+        ResourceManager rm = checkAccess(sessionId);
         return rm.addNode(nodeUrl).booleanValue();
     }
 
+    
+    @POST
+    @Path("disconnect")
+    public boolean disconnect(@HeaderParam("sessionid") String sessionId) {
+        ResourceManager rm = checkAccess(sessionId);
+        RMSessionMapper.getInstance().getSessionsMap().remove(rm);
+        return rm.disconnect().booleanValue();
+    }
+    
 }
