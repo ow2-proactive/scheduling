051dbbc43c3dbd0843ddadeaffd24ec4c6e5f665
acontes <acontes@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2010-11-09 09:47:24 +0000

    ++
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/rest_api/trunk@17865 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/LoginForm.java
+++ src/org/ow2/proactive_grid_cloud_portal/LoginForm.java
@@ -3,12 +3,32 @@ package org.ow2.proactive_grid_cloud_portal;
 import java.io.InputStream;
 
 import javax.ws.rs.FormParam;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.jboss.resteasy.annotations.providers.multipart.PartType;
 
+
+/**
+ * a class that represent a mean to submit credential through the rest api.
+ * A credential can be either 
+ *   - a username/password couple
+ *   - a file submitted through an HTTP form and whose name is 'credential'
+ */
+@XmlRootElement
 public class LoginForm {
+    /**
+     * a representation of a serialized Credentials class
+     */
     private InputStream credential;
+    
+    /**
+     * username
+     */
     private String username;
+    
+    /**
+     * password
+     */
     private String password;
 
     public LoginForm() {}
--- src/org/ow2/proactive_grid_cloud_portal/UserJobInfo.java
+++ src/org/ow2/proactive_grid_cloud_portal/UserJobInfo.java
@@ -1,12 +1,32 @@
 package org.ow2.proactive_grid_cloud_portal;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import org.ow2.proactive.scheduler.common.job.JobInfo;
 
-
+/**
+ * A class that contains a subset of the information available
+ * in a scheduler state.
+ * It is mostly used to provide a fast access to meaningful
+ * data within the scheduler state without having to manage the 
+ * complete state
+ *
+ */
+@XmlRootElement
 public class UserJobInfo {
+    /*
+     * the id of the job
+     */
     private String jobId;
+    
+    /*
+     * the job's owner
+     */
     private String jobOwner;
 
+    /*
+     * Jobinfo of the job
+     */
     private JobInfo jobInfo;
 
     public UserJobInfo() {
