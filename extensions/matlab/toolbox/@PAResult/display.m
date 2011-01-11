07ea1af8322ce5a33f8e443020afdf8a77947925
fviale <fviale@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-01-11 16:15:53 +0000

    fix for SCHEDULING-1038
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@18502 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- extensions/matlab/toolbox/@PAResult/display.m
+++ extensions/matlab/toolbox/@PAResult/display.m
@@ -1,35 +1,42 @@
 % PAresult/DISPLAY Command window display of a PAresult
 %
-%/*
-% * ################################################################
-% *
-% * ProActive: The Java(TM) library for Parallel, Distributed,
-% *            Concurrent computing with Security and Mobility
-% *
-% * Copyright (C) 1997-2009 INRIA/University of Nice-Sophia Antipolis
-% * Contact: proactive@ow2.org
-% *
-% * This library is free software; you can redistribute it and/or
-% * modify it under the terms of the GNU General Public License
-% * as published by the Free Software Foundation; either version
-% * 2 of the License, or any later version.
-% *
-% * This library is distributed in the hope that it will be useful,
-% * but WITHOUT ANY WARRANTY; without even the implied warranty of
-% * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-% * General Public License for more details.
-% *
-% * You should have received a copy of the GNU General Public License
-% * along with this library; if not, write to the Free Software
-% * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-% * USA
-% *
-% *  Initial developer(s):               The ProActive Team
-% *                        http://proactive.inria.fr/team_members.htm
-% *  Contributor(s):
-% *
-% * ################################################################
-% */
+%
+% /*
+%   * ################################################################
+%   *
+%   * ProActive Parallel Suite(TM): The Java(TM) library for
+%   *    Parallel, Distributed, Multi-Core Computing for
+%   *    Enterprise Grids & Clouds
+%   *
+%   * Copyright (C) 1997-2011 INRIA/University of
+%   *                 Nice-Sophia Antipolis/ActiveEon
+%   * Contact: proactive@ow2.org or contact@activeeon.com
+%   *
+%   * This library is free software; you can redistribute it and/or
+%   * modify it under the terms of the GNU Affero General Public License
+%   * as published by the Free Software Foundation; version 3 of
+%   * the License.
+%   *
+%   * This library is distributed in the hope that it will be useful,
+%   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   * Affero General Public License for more details.
+%   *
+%   * You should have received a copy of the GNU Affero General Public License
+%   * along with this library; if not, write to the Free Software
+%   * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+%   * USA
+%   *
+%   * If needed, contact us to obtain a release under GPL Version 2 or 3
+%   * or a different license than the AGPL.
+%   *
+%   *  Initial developer(s):               The ProActive Team
+%   *                        http://proactive.inria.fr/team_members.htm
+%   *  Contributor(s):
+%   *
+%   * ################################################################
+%   * $$PROACTIVE_INITIAL_DEV$$
+%   */
 function display(R)
 s=size(R);
 for i=1:s(1)
