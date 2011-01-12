19446146c49b00295fce81785eb1f9da85a8280d
mschnoor <mschnoor@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-01-12 10:21:55 +0000

    SCHEDULING-1034
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@18530 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scripts/ec2/runNode.sh
+++ scripts/ec2/runNode.sh
@@ -6,24 +6,4 @@
 # register it on a remote Resource Manager
 #
 
-PROACTIVE_HOME="/usr/share/ProActive/"
-PA_SCHEDULER="/usr/share/ProActive"
-JARS="$PROACTIVE_HOME/dist/lib/ProActive.jar"
-JAVA_HOME="/root/JDK"
-
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/script-js.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/jruby-engine.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/jython-engine.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/commons-logging-1.0.4.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive_SRM-common.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive_ResourceManager.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive_Scheduler-core.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive_Scheduler-client.jar"
-JARS=$JARS:"$PA_SCHEDULER/dist/lib/ProActive_Scheduler-worker.jar"
-
-$JAVA_HOME/bin/java -cp $JARS \
-    -Dproactive.home=$PROACTIVE_HOME \
-    -Dpa.scheduler.home=$PA_SCHEDULER \
-    -Dpa.rm.home=$PA_SCHEDULER \
-    $(python params.py)
+$(python params.py)
