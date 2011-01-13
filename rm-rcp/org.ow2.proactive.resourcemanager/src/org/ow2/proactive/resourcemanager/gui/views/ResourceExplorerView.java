c2bb61570ef03f1c91c33e2e9b7edea6203a2330
oleg_smirnov <oleg_smirnov@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-01-13 15:17:24 +0000

    SCHEDULING-944 Dynamic width of the tree view column in RM RCP.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@18609 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- rm-rcp/org.ow2.proactive.resourcemanager/src/org/ow2/proactive/resourcemanager/gui/views/ResourceExplorerView.java
+++ rm-rcp/org.ow2.proactive.resourcemanager/src/org/ow2/proactive/resourcemanager/gui/views/ResourceExplorerView.java
@@ -42,6 +42,8 @@ import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.ITreeViewerListener;
+import org.eclipse.jface.viewers.TreeExpansionEvent;
 import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -49,7 +51,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.part.DrillDownAdapter;
 import org.eclipse.ui.part.ViewPart;
 import org.ow2.proactive.resourcemanager.gui.actions.CollapseAllAction;
 import org.ow2.proactive.resourcemanager.gui.actions.ExpandAllAction;
@@ -68,6 +69,7 @@ public class ResourceExplorerView extends ViewPart {
     /** the view part id */
     public static final String ID = "org.ow2.proactive.resourcemanager.gui.views.ResourceExplorerView";
     private static RMTreeViewer treeViewer = null;
+    private static TreeViewerColumn column = null;
     private static Action expandAllAction = null;
     private static Action collapseAllAction = null;
 
@@ -77,6 +79,7 @@ public class ResourceExplorerView extends ViewPart {
 
     public static void init() {
         treeViewer.init();
+        column.getColumn().pack();
         expandAllAction.setEnabled(true);
         collapseAllAction.setEnabled(true);
     }
@@ -98,14 +101,14 @@ public class ResourceExplorerView extends ViewPart {
         parent = theParent;
         treeViewer = new RMTreeViewer(this, parent);
 
-        TreeViewerColumn column = new TreeViewerColumn(treeViewer, SWT.LEFT, 0);
+        column = new TreeViewerColumn(treeViewer, SWT.LEFT, 0);
         column.setLabelProvider(new TreeLabelProvider());
-        column.getColumn().setWidth(200);
 
         makeActions();
         hookContextMenu();
         contributeToActionBars();
         treeViewer.addSelectionChangedListener(new TreeSelectionListener());
+
         if (RMStore.isConnected()) {
             init();
             treeViewer.expandAll();
