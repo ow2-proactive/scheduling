014bef21a15e82e7c1fd904ec5b25e8d820644b8
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-06-23 14:43:00 +0000

    Update to Programming trunk r19893 ; bin scripts also are updated
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@19904 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ bin/windows/pa-dataserver-gui.bat
@@ -0,0 +1,33 @@
+@echo off
+echo.
+
+rem I did not use init.bat because it does not work, and I have no idea why
+
+set PROACTIVE=%~dp0\..
+
+if NOT DEFINED JAVA_HOME goto javahome
+if "%JAVA_HOME%" == "" goto javahome
+
+IF DEFINED CLASSPATHEXT (
+	SET CLASSPATH=%CLASSPATHEXT%
+) ELSE (
+	SET CLASSPATH=.
+)
+
+SET CLASSPATH=%CLASSPATH%;%PROACTIVE%\dist\lib\ProActive.jar;%PROACTIVE%\dist\lib\ProActive_examples.jar;%PROACTIVE%\dist\lib\ProActive_utils.jar
+set JAVA_CMD="%JAVA_HOME%\bin\java.exe" -Dproactive.home="%PROACTIVE%"  -Djava.security.manager -Djava.security.policy="%PROACTIVE%\examples\proactive.java.policy"
+%JAVA_CMD% org.objectweb.proactive.extensions.vfsprovider.gui.ServerBrowser
+
+goto end
+
+:javahome
+echo.
+echo The enviroment variable JAVA_HOME must be set the current jdk distribution
+echo installed on your computer.
+echo Use
+echo    set JAVA_HOME=<the directory where is the JDK>
+goto end
+
+echo.
+
+:end
