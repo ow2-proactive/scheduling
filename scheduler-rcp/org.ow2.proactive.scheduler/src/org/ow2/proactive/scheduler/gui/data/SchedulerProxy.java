740faff611ed7a1410137c2bc85402fc8045ae7b
jlscheef <jlscheef@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-18 13:12:56 +0000

    SCHEDULING-1175 : format
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@20184 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- scheduler-rcp/org.ow2.proactive.scheduler/src/org/ow2/proactive/scheduler/gui/data/SchedulerProxy.java
+++ scheduler-rcp/org.ow2.proactive.scheduler/src/org/ow2/proactive/scheduler/gui/data/SchedulerProxy.java
@@ -799,7 +799,7 @@ public class SchedulerProxy implements Scheduler {
      * @see org.ow2.proactive.scheduler.common.Scheduler#renewSession()
      */
     public void renewSession() throws NotConnectedException {
-    	scheduler.renewSession();
+        scheduler.renewSession();
     }
 
 }
