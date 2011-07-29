f32fe58b551a8b176c995349b07d2d2f1e773561
mschnoor <mschnoor@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-07-29 15:03:52 +0000

    clean import
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/proactive_grid_cloud_portal/rest_api/trunk@20493 28e8926c-6b08-0410-baaa-805c5e19b8d6

--- src/org/ow2/proactive_grid_cloud_portal/rm/RMRest.java
+++ src/org/ow2/proactive_grid_cloud_portal/rm/RMRest.java
@@ -48,7 +48,6 @@ import javax.management.ObjectName;
 import javax.management.ReflectionException;
 import javax.security.auth.login.LoginException;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
