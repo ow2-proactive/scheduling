9e42a7c10728d464047a50c24afd08e1be8d9078
abuis <abuis@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-08-04 13:36:43 +0000

    SCHEDULING-1299: Fix bad URI API usage and Batch scripts have been had
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/trunk@20573 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/scheduler/tests/functionaltests/executables/PTK_launcher.bat
@@ -0,0 +1,9 @@
+SET file=%0
+SET DIR=%file%\..
+
+echo Process tree killer test : detached commands launcher
+
+start /B cmd /C %DIR%\PTK_process.bat
+start /B cmd /C %DIR%\PTK_process.bat
+start /B cmd /C %DIR%\PTK_process.bat
+start cmd /C %DIR%\PTK_process.bat
