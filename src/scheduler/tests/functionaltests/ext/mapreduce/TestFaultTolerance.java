07d69cca5c6d35612b62929a2db70babd74ef802
cdelbe <cdelbe@28e8926c-6b08-0410-baaa-805c5e19b8d6>
2011-11-08 14:12:21 +0000

    PICKED FROM TRUNK 21200
    SCHEDULING-1325 : Add a few MapReduce functional tests
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/proactive/scheduling/branches/Scheduling_3.1.x@21215 28e8926c-6b08-0410-baaa-805c5e19b8d6

---
+++ src/scheduler/tests/functionaltests/ext/mapreduce/TestFaultTolerance.java
@@ -0,0 +1,169 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2011 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $PROACTIVE_INITIAL_DEV$
+ */
+
+package functionaltests.ext.mapreduce;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Assert;
+import org.ow2.proactive.scheduler.ext.mapreduce.PAMapReduceFramework;
+import org.ow2.proactive.scheduler.ext.mapreduce.PAMapReduceJobConfiguration;
+import org.ow2.tests.FunctionalTest;
+
+/**
+ * Test that the sort MapReduce job produces correct result even if first
+ * iterations of mapper and reducer fail, given that maxNumberOfExecutions is
+ * greater than 1.
+ */
+public class TestFaultTolerance extends FunctionalTest {
+    
+    /**
+     * This Mapper will fail the first time it is run, but will work on
+     * consecutive runs. Relies on a file created in tmpdir and thus requires
+     * that reruns happen on the same host as the first run.
+     */
+    public static class FailingMapper extends Mapper<Object, Text, Text, Text> {
+
+        private Text word = new Text();
+
+        public static final File MAPPER_MARKER = new File(System.getProperty("java.io.tmpdir")
+                + File.separator + TestFaultTolerance.class.getName() + ".mapper.marker");
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException,
+                InterruptedException {
+            if (MAPPER_MARKER.createNewFile()) {
+                throw new RuntimeException("First iteration of the mapper fails");
+            }
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, word);
+            }
+        }
+    }
+
+    /**
+     * This Reducer will fail the first time it is run, but will work on
+     * consecutive runs. Relies on a file created in tmpdir and thus requires
+     * that reruns happen on the same host as the first run.
+     */
+    public static class FailingReducer extends Reducer<Text, Text, Text, NullWritable> {
+
+        public static final File REDUCER_MARKER = new File(System.getProperty("java.io.tmpdir")
+                + File.separator + TestFaultTolerance.class.getName() + ".reducer.marker");
+        
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            if (REDUCER_MARKER.createNewFile()) {
+                throw new RuntimeException("First iteration of the reducer fails");
+            }
+            for (Text val : values) {
+                context.write(val, null);
+            }
+        }
+    }
+
+    private static final String INPUT = "a\nb\nsome\nwords\nin\nb\nno\nparticular\nwords\norder\n";
+    private static final String EXPECTED_OUTPUT = "a\nb\nb\nin\nno\norder\nparticular\nsome\nwords\nwords\n";
+
+    private static final MapReduceTHelper helper = new MapReduceTHelper(TestFaultTolerance.class.getName());
+
+    private static void cleanup() throws IOException {
+
+        helper.cleanup();
+        FailingMapper.MAPPER_MARKER.delete();
+        FailingReducer.REDUCER_MARKER.delete();
+
+    }
+    
+    @org.junit.Test
+    public void run() throws Throwable {
+
+        cleanup();
+        
+        // generate input
+        helper.writeFile("in/input", INPUT);
+
+        // create and configure Hadoop job
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "fault tolerance");
+        job.setMapperClass(FailingMapper.class);
+        job.setReducerClass(FailingReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        FileInputFormat.addInputPath(job, new Path("input"));
+        FileOutputFormat.setOutputPath(job, new Path("output"));
+
+        PAMapReduceJobConfiguration pamrjc = MapReduceTHelper.getConfiguration();
+     
+        pamrjc.setInputSpace("file://" + helper.getRootDir() + "/in");
+        pamrjc.setOutputSpace("file://" + helper.getRootDir() + "/out");
+        pamrjc.setInputSplitSize(20);
+        //pamrjc.setMaxNumberOfExecutions(2);
+        pamrjc.setMaxNumberOfExecutions(PAMapReduceFramework.MAPPER_PA_TASK, 2);
+        pamrjc.setMaxNumberOfExecutions(PAMapReduceFramework.REDUCER_PA_TASK, 2);
+        
+        // submit PAMapReduceJob and wait for completion
+        MapReduceTHelper.submit(job, pamrjc);
+        
+        // check output
+        String out = helper.readFiles("out/output", false);
+        System.out.println(out);
+        Assert.assertEquals("Output of the sort job", EXPECTED_OUTPUT, out);
+        Assert.assertTrue("File should be created by 1st iteration of mapper", FailingMapper.MAPPER_MARKER.exists());
+        Assert.assertTrue("File should be created by 1st iteration of reducer", FailingReducer.REDUCER_MARKER.exists());
+        
+        cleanup();        
+        
+    }
+    
+}
---
+++ src/scheduler/tests/functionaltests/ext/mapreduce/TestMapReduce.java
@@ -0,0 +1,208 @@
+package functionaltests.ext.mapreduce;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Based on Hadoop test org.apache.hadoop.mapreduce.TestMapReduceLocal.
+ * 
+ * Modified for ProActive MapReduce.
+ */
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Assert;
+import org.ow2.proactive.scheduler.common.job.JobInfo;
+import org.ow2.proactive.scheduler.common.job.JobResult;
+import org.ow2.proactive.scheduler.ext.mapreduce.PAMapReduceFramework;
+import org.ow2.proactive.scheduler.ext.mapreduce.PAMapReduceJobConfiguration;
+import org.ow2.proactive.scheduler.ext.mapreduce.ReadMode;
+import org.ow2.proactive.scheduler.ext.mapreduce.WriteMode;
+import org.ow2.tests.FunctionalTest;
+
+/**
+ * MapReduce test using word count example. Test basic functionality, various
+ * read modes, setting the number of reducers and input split size, using
+ * combiner class.
+ */
+public class TestMapReduce extends FunctionalTest {
+
+    /** 
+     * Together with {@link IntSumReducer} implements word count algorithm.
+     */
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException,
+                InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    /** 
+     * Together with {@link TokenizerMapper} implements word count algorithm.
+     */
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    private static final MapReduceTHelper helper = new MapReduceTHelper(TestMapReduce.class.getName());
+
+    private static final String INPUT1 = "this is a test\nof a word count test\nword\nword\n";
+    private static final String INPUT2 = "more test";
+    private static final String EXPECTED_OUTPUT = "a\t2\ncount\t1\nis\t1\nmore\t1\nof\t1\ntest\t3\nthis\t1\nword\t3\n";
+    
+    @org.junit.Test
+    public void run() throws Throwable {
+
+        // default configuration
+        Job job = prepareHadoopJob(true);
+        PAMapReduceJobConfiguration pamrjc = MapReduceTHelper.getConfiguration();
+        JobResult result = submitAndCheck(job, pamrjc);
+        JobInfo jobInfo = result.getJobInfo();
+        // Splitter, 2*Mapper (1 per input file), MapperJoin, Reducer,
+        // ReducerJoin -- 6 tasks overall
+        Assert.assertEquals(6, jobInfo.getTotalNumberOfTasks());
+        
+        // fullLocalRead + remoteWrite
+        job = prepareHadoopJob(false);
+        pamrjc = MapReduceTHelper.getConfiguration();
+        pamrjc.setReadMode(PAMapReduceFramework.SPLITTER_PA_TASK, ReadMode.fullLocalRead);
+        pamrjc.setReadMode(PAMapReduceFramework.MAPPER_PA_TASK, ReadMode.fullLocalRead);
+        pamrjc.setReadMode(PAMapReduceFramework.REDUCER_PA_TASK, ReadMode.fullLocalRead);
+        pamrjc.setWriteMode(PAMapReduceFramework.MAPPER_PA_TASK, WriteMode.remoteWrite);
+        pamrjc.setWriteMode(PAMapReduceFramework.REDUCER_PA_TASK, WriteMode.remoteWrite);
+        submitAndCheck(job, pamrjc);
+
+        // partialLocalRead + remoteRead
+        job = prepareHadoopJob(true);
+        pamrjc = MapReduceTHelper.getConfiguration();
+        pamrjc.setReadMode(PAMapReduceFramework.SPLITTER_PA_TASK, ReadMode.remoteRead);
+        pamrjc.setReadMode(PAMapReduceFramework.MAPPER_PA_TASK, ReadMode.partialLocalRead);
+        pamrjc.setReadMode(PAMapReduceFramework.REDUCER_PA_TASK, ReadMode.remoteRead);
+        submitAndCheck(job, pamrjc);
+
+        // 3 reducers
+        job = prepareHadoopJob(true);
+        job.setNumReduceTasks(3);
+        pamrjc = MapReduceTHelper.getConfiguration();
+        // the output of multiple reducers is not sorted globally, sort it
+        // before comparing
+        result = submitAndCheck(job, pamrjc, true);
+        jobInfo = result.getJobInfo();
+        // Splitter, 2*Mapper (1 per input file), MapperJoin, 3*Reducer,
+        // ReducerJoin -- 8 tasks overall
+        Assert.assertEquals(8, jobInfo.getTotalNumberOfTasks());
+    
+        // inputSplitSize = 10
+        job = prepareHadoopJob(false);
+        pamrjc = MapReduceTHelper.getConfiguration();
+        pamrjc.setInputSplitSize(10);
+        result = submitAndCheck(job, pamrjc);
+        jobInfo = result.getJobInfo();
+        // Splitter, 6*Mapper, MapperJoin, Reducer, ReducerJoin -- 10 tasks
+        // overall
+        Assert.assertEquals(10, jobInfo.getTotalNumberOfTasks());
+
+        helper.cleanup();
+        
+    }
+
+    private Job prepareHadoopJob(boolean combiner) throws Throwable {
+
+        helper.cleanup();
+
+        // generate input
+        helper.writeFile("in/part1", INPUT1);
+        helper.writeFile("in/part2", INPUT2);
+
+        // create and configure Hadoop job
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "word count");
+        job.setMapperClass(TokenizerMapper.class);
+        if (combiner) {
+            job.setCombinerClass(IntSumReducer.class);
+        }
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        FileInputFormat.addInputPath(job, new Path("part1"));
+        FileInputFormat.addInputPath(job, new Path("part2"));
+        FileOutputFormat.setOutputPath(job, new Path("output"));
+        
+        return job;
+    }
+    
+    /**
+     * Submit the job, wait for completion, check that the result is correct. Do
+     * not sort the output before checking.
+     */
+    private JobResult submitAndCheck(Job job, PAMapReduceJobConfiguration pamrjc) throws Throwable {
+        return submitAndCheck(job, pamrjc, false);
+    }
+
+    /**
+     * Submit the job, wait for completion, check that the result is correct.
+     * Optionally sort the output before checking.
+     */
+    private JobResult submitAndCheck(Job job, PAMapReduceJobConfiguration pamrjc, boolean sort) throws Throwable {
+
+        pamrjc.setInputSpace("file://" + helper.getRootDir() + "/in");
+        pamrjc.setOutputSpace("file://" + helper.getRootDir() + "/out");
+        
+        // submit PAMapReduceJob and wait for completion
+        JobResult result = MapReduceTHelper.submit(job, pamrjc);
+
+        // check output
+        String out = helper.readFiles("out/output", sort);
+        System.out.println(out);
+        Assert.assertEquals("Output of the job", EXPECTED_OUTPUT, out);
+        return result;
+    }
+    
+}
