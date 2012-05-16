8ce4ac46f817014df95e05156f001349ee2ac0af
esalagea <Emil.Salageanu@activeeon.com>
2012-05-16 18:25:46 +0200

    https://bugs.activeeon.com/browse/SCHEDULING-1609
    ClientJobState is incomplete
    
    The problem was that the class
    org.ow2.proactive.scheduler.job.ClientJobState was meant to be a lighter view of the JiobState object (internal view of the Scheduler)
    
    The ClientJobState overrides the JobState but, when constructed, the copy constructor did not copy all the fields into the new object.
    I have copied part of the fields in the ClientJobState and override the getter methods for fields that are not available and throwed Runtime exceptions.
    
    The same for the ClientTaskState.

--- src/scheduler/src/org/ow2/proactive/scheduler/job/ClientJobState.java
+++ src/scheduler/src/org/ow2/proactive/scheduler/job/ClientJobState.java
@@ -1,13 +1,20 @@
 package org.ow2.proactive.scheduler.job;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.ow2.proactive.db.types.BigString;
+import org.ow2.proactive.scheduler.common.Scheduler;
+import org.ow2.proactive.scheduler.common.job.JobEnvironment;
 import org.ow2.proactive.scheduler.common.job.JobInfo;
 import org.ow2.proactive.scheduler.common.job.JobState;
 import org.ow2.proactive.scheduler.common.job.JobType;
+import org.ow2.proactive.scheduler.common.task.RestartMode;
 import org.ow2.proactive.scheduler.common.task.TaskId;
 import org.ow2.proactive.scheduler.common.task.TaskInfo;
 import org.ow2.proactive.scheduler.common.task.TaskState;
@@ -16,6 +23,27 @@ import org.ow2.proactive.scheduler.task.ClientTaskState;
 import org.ow2.proactive.scheduler.task.TaskInfoImpl;
 
 
+/**
+ * 
+ * This class is a client view of a {@link JobState}. 
+ * A client will receive an instance of this class when connecting to the scheduler front-end and ask for a JobState (for instance by using {@link Scheduler#getJobState(String)}).
+ *    
+ * The value of some attributes will not be available in this view of the JobState. Therefore, calling the respective getters will throw a RuntimeException:
+ * 
+ * <ul>
+ *  <li>
+ *  {@link #getEnvironment()}
+ *  </li>
+ *  <li>
+ *  {@link #getEnvironment()}
+ *  </li>
+ * </ul> 
+ *  
+ *   
+ * 
+ * @author esalagea
+ *
+ */
 public class ClientJobState extends JobState {
 
     private JobInfoImpl jobInfo;
@@ -23,17 +51,52 @@ public class ClientJobState extends JobState {
     private JobType type;
     protected Map<TaskId, TaskState> tasks = new HashMap<TaskId, TaskState>();
 
+    private boolean cancelJobOnError;
+    private int maxNumberOfExecution;
+
+    //replacing the super's genericInformations attribute
+    private HashMap<String, String> genericInformations_client;
+
     public ClientJobState(JobState jobState) {
         // converting internal job into a light job descriptor
         jobInfo = (JobInfoImpl) jobState.getJobInfo();
         owner = jobState.getOwner();
         type = jobState.getType();
 
+        this.name = jobState.getName();
+        this.description = jobState.getDescription();
+        this.projectName = jobState.getProjectName();
+        this.priority = jobState.getPriority();
+        this.inputSpace = jobState.getInputSpace();
+        this.outputSpace = jobState.getOutputSpace();
+
+        this.cancelJobOnError = jobState.isCancelJobOnError();
+        this.maxNumberOfExecution = jobState.getMaxNumberOfExecution();
+
+        this.genericInformations_client = new HashMap<String, String>(jobState.getGenericInformations());
+
         List<ClientTaskState> taskStates = new ArrayList<ClientTaskState>();
         for (TaskState ts : jobState.getTasks()) {
             taskStates.add(new ClientTaskState(ts));
         }
         addTasks(taskStates);
+
+    }
+
+    @Override
+    public int getMaxNumberOfExecution() {
+        return this.maxNumberOfExecution;
+
+    }
+
+    @Override
+    public boolean isCancelJobOnError() {
+        return cancelJobOnError;
+    }
+
+    @Override
+    public Map<String, String> getGenericInformations() {
+        return this.genericInformations_client;
     }
 
     @Override
@@ -126,6 +189,23 @@ public class ClientJobState extends JobState {
         for (ClientTaskState ts : newTasks) {
             ts.restoreDependences(tasks);
         }
+    }
+
+    /**
+     * This property is not available for this implementation. Calling this method will throw a RuntimeException
+     */
+    @Override
+    public JobEnvironment getEnvironment() {
+        throw new RuntimeException("Not implemented: the job environment is not available on client side.");
+    }
+
+    /**
+     * This property is not available for this implementation. Calling this method will throw a RuntimeException
+     */
+    @Override
+    public RestartMode getRestartTaskOnError() {
+        throw new RuntimeException(
+            "Not implemented: the restart task on error property is not available on client side.");
     }
 
 }
