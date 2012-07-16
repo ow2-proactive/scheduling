3d21aa3495c42b8bef479d442abae59430b35277
esalagea <Emil.Salageanu@activeeon.com>
2012-07-16 12:08:36 +0200

    SCHEDULING-1667 Rename TesDataJavaExecutable 	renamed to SimpleJavaExecutable

--- src/scheduler/tests/functionaltests/TestSchedulerProxyUIWithDSSupport.java
+++ src/scheduler/tests/functionaltests/TestSchedulerProxyUIWithDSSupport.java
@@ -148,7 +148,7 @@ public class TestSchedulerProxyUIWithDSSupport extends FunctionalTest {
         TaskFlowJob job = new TaskFlowJob();
         JavaTask testTask = new JavaTask();
         testTask.setName("TestJavaTask");
-        testTask.setExecutableClassName(TestDataJavaExecutable.class.getName());
+        testTask.setExecutableClassName(SimpleJavaExecutable.class.getName());
         job.setInputSpace(dataServerURI);
         job.setOutputSpace(dataServerURI);
 
