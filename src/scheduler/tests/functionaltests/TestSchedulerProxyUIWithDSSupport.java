f28b4f3a356c83590f651e1a5abd05f8395c69cc
esalagea <Emil.Salageanu@activeeon.com>
2012-07-16 12:03:46 +0200

    SCHEDULING-1666 	increase timeout of TestSchedulerProxyUIWithDSSupport

--- src/scheduler/tests/functionaltests/TestSchedulerProxyUIWithDSSupport.java
+++ src/scheduler/tests/functionaltests/TestSchedulerProxyUIWithDSSupport.java
@@ -54,7 +54,7 @@ public class TestSchedulerProxyUIWithDSSupport extends FunctionalTest {
     public static final String dataServerFolderPath = System.getProperty("java.io.tmpdir") + File.separator +
         "testDS_remoteFolder";
 
-    public static long TIMEOUT = 10000;
+    public static long TIMEOUT = 40000;
 
     private File inputLocalFolder;
     private File outputLocalFolder;
