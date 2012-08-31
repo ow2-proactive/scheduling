1fe5bd71a3645c6d220d652762516f567cd58b2f
Oleg Smirnov <oleg.smirnov@activeeon.com>
2012-08-31 09:46:48 +0200

    SCHEDULING-1716 Changes in scheduler starting process for functional tests.
    Waiting till all forked nodes are added to the RM.
    
    Change-Id: Ieb1f263d23d7f340af753efc28ec68fc5186503c

--- src/scheduler/tests/functionaltests/SchedulerTStarter.java
+++ src/scheduler/tests/functionaltests/SchedulerTStarter.java
@@ -128,7 +128,10 @@ public class SchedulerTStarter implements Serializable {
                 nodes[i] = node;
             }
             for (int i = 0; i < RM_NODE_NUMBER; i++) {
-                rmAdmin.addNode(nodes[i].getNodeInformation().getURL());
+                // adding nodes to the RM and waiting while they will be registered
+                // without waiting test can finish earlier than nodes will be added to the RM
+                // which on windows leads to test execution hang up due to running processes
+                rmAdmin.addNode(nodes[i].getNodeInformation().getURL()).getBooleanValue();
             }
         }
     }
