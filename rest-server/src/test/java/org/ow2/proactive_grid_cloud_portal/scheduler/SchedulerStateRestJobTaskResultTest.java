fa1954ecff7b996101dd4b1b0f7590c6f5de752e
youribonnaffe <youri.bonnaffe@activeeon.com>
2013-08-29 16:00:08 +0200

    Gradle migration: doc build and unit tests
    Change-Id: I70bd5ef8d24ec95a4f4c9ec9ffb8c2f4b1901667

--- rest-server/src/test/java/org/ow2/proactive_grid_cloud_portal/scheduler/SchedulerStateRestJobTaskResultTest.java
+++ rest-server/src/test/java/org/ow2/proactive_grid_cloud_portal/scheduler/SchedulerStateRestJobTaskResultTest.java
@@ -62,8 +62,8 @@ public class SchedulerStateRestJobTaskResultTest extends RestTestServer {
         String sessionId = SchedulerSessionMapper.getInstance().add(mockOfScheduler, "bob");
 
         TaskResultImpl taskResultWithException = new TaskResultImpl(
-                TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1, false), new Exception(), null,
-                0);
+                TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1, false), null, new byte[0],
+                null,null);
         when(mockOfScheduler.getTaskResult("42", "mytask")).thenReturn(taskResultWithException);
 
         String exceptionStackTrace = (String) restInterface.valueOftaskresult(sessionId, "42", "mytask");
@@ -79,8 +79,8 @@ public class SchedulerStateRestJobTaskResultTest extends RestTestServer {
         String sessionId = SchedulerSessionMapper.getInstance().add(mockOfScheduler, "bob");
 
         TaskResultImpl taskResultWithException = new TaskResultImpl(
-                TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1, false), new Exception(), null,
-                0);
+                TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1, false), null, new byte[0],
+                null,null);
         JobResultImpl jobResultWithException = new JobResultImpl();
         jobResultWithException.addTaskResult("mytask", taskResultWithException, false);
         when(mockOfScheduler.getJobResult("42")).thenReturn(jobResultWithException);
