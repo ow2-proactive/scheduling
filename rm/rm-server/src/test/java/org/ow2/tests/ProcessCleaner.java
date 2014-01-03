8b64fac3de40f879cfd1f1a03ff3852f56067401
youribonnaffe <youri.bonnaffe@activeeon.com>
2014-01-03 17:02:12 +0100

    Tests: reduce output for process cleaning
    Change-Id: Iadd88bb9a7961aa7161ebdaa64688411784ee2ca

--- rm/rm-server/src/test/java/org/ow2/tests/ProcessCleaner.java
+++ rm/rm-server/src/test/java/org/ow2/tests/ProcessCleaner.java
@@ -103,12 +103,12 @@ public class ProcessCleaner {
         Reader r = new InputStreamReader(p.getInputStream());
         BufferedReader br = new BufferedReader(r);
 
-        if (printProcesses) System.out.println("Running java processes matching regex:");
         String line;
         while ((line = br.readLine()) != null) {
             Matcher m = this.pattern.matcher(line);
             if (m.matches()) {
-                if (printProcesses) System.out.println(line);
+                if (printProcesses)
+                    System.out.println("Running java processes matching regex:" + line);
                 String pid = line.substring(0, line.indexOf(" "));
                 pids.add(pid);
             }
