2bcd680e06af7a0d14c403d182f097afc13487ec
Oleg Smirnov <oleg.smirnov@activeeon.com>
2014-03-31 15:07:14 +0200

    Fix the race condition in TestProcessTreeKiller.
    Test expects to have 3 jobs running at the same time.
    The first task sleeps for 10 seconds and looks like
    it's not enough sometimes to launch other jobs.
    
    Change-Id: I409d6a7e24db60b9f6b285d1b1bb0d66ec5ba5ee

--- scheduler/scheduler-server/src/test/resources/functionaltests/executables/PTK_launcher2.bat
+++ scheduler/scheduler-server/src/test/resources/functionaltests/executables/PTK_launcher2.bat
@@ -5,5 +5,5 @@ start /B cmd.exe /C PTK_process2.bat
 start /B cmd.exe /C PTK_process2.bat
 start /B cmd.exe /C PTK_process2.bat
 
-echo waiting for 10 sec
-ping -n 10 127.0.0.1
+echo waiting for 30 sec
+ping -n 30 127.0.0.1
