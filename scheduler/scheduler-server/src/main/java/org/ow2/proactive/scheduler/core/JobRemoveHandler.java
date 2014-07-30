ow2-proactive/scheduling

commit 70c0f3af67b0ccf4aa93bf147b4faadfda994564

SCHEDULING-2057 Task server log files are not removed upon job removal
Make sure that we load the job with tasks from the DB.

Extend the existing test to check that task and job logs are removed
upon job removal.

Change-Id: Ic4cf8b36610f50a9f111f4134789ad5fb6852f0f

scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/JobRemoveHandler.java
@@ -1,5 +1,6 @@
 package org.ow2.proactive.scheduler.core;
 
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.log4j.Logger;
@@ -36,11 +37,14 @@ class JobRemoveHandler implements Callable<Boolean> {
 
         TerminationData terminationData = service.jobs.killJob(jobId);
         service.submitTerminationDataHandler(terminationData);
-        InternalJob job = dbManager.loadJobWithoutTasks(jobId);
+        List<InternalJob> internalJobs = dbManager.loadJobs(false, jobId);
+        if (internalJobs == null || internalJobs.isEmpty()) {
+            return false;
+        }
+        InternalJob job = internalJobs.get(0);
         if (job == null) {
             return false;
         }
-
         job.setRemovedTime(System.currentTimeMillis());
         boolean removeFromDB = PASchedulerProperties.JOB_REMOVE_FROM_DB.getValueAsBoolean();
         dbManager.removeJob(jobId, job.getRemovedTime(), removeFromDB);

scheduler/scheduler-server/src/test/java/functionaltests/TestJobServerLogs.java
@@ -38,26 +38,35 @@ package functionaltests;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 
+import org.ow2.proactive.scheduler.common.Scheduler;
+import org.ow2.proactive.scheduler.common.exception.UnknownJobException;
 import org.ow2.proactive.scheduler.common.job.Job;
 import org.ow2.proactive.scheduler.common.job.JobId;
+import org.ow2.proactive.scheduler.common.job.JobState;
 import org.ow2.proactive.scheduler.common.job.TaskFlowJob;
 import org.ow2.proactive.scheduler.common.task.JavaTask;
 import org.ow2.proactive.scheduler.common.task.TaskInfo;
+import org.ow2.proactive.scheduler.common.task.TaskState;
 import org.ow2.proactive.scheduler.examples.WaitAndPrint;
+import org.ow2.proactive.scheduler.util.ServerJobAndTaskLogs;
 import org.ow2.proactive.scripting.SelectionScript;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 
 /**
- * 
  * Test that we can retrieve job/task server logs and the semantic
  * of logs.
- * 
+ * <p/>
  * - for finished job we have logs for all the tasks
  * - for pending jobs we have correct RM output & script output
- * 
  */
 public class TestJobServerLogs extends SchedulerConsecutive {
 
@@ -66,6 +75,7 @@ public class TestJobServerLogs extends SchedulerConsecutive {
             .getResource("/functionaltests/descriptors/Job_simple.xml");
 
     private final String SCRIPT_OUTPUT = "SCRIPT_OUTPUT_" + Math.random();
+    private String logsLocation = ServerJobAndTaskLogs.getLogsLocation();
 
     private Job createPendingJob() throws Exception {
         final TaskFlowJob job = new TaskFlowJob();
@@ -75,7 +85,7 @@ public class TestJobServerLogs extends SchedulerConsecutive {
         task.addArgument("sleepTime", "1");
 
         task.addSelectionScript(new SelectionScript("print('" + SCRIPT_OUTPUT + "'); selected = false;",
-            "javascript"));
+                "javascript"));
         job.addTask(task);
 
         return job;
@@ -113,6 +123,9 @@ public class TestJobServerLogs extends SchedulerConsecutive {
             }
         }
 
+        checkRemoval(simpleJobId);
+
+
         JobId pendingJobId = SchedulerTHelper.submitJob(createPendingJob());
         Thread.sleep(5000);
         jobLogs = SchedulerTHelper.getSchedulerInterface().getJobServerLogs(pendingJobId.toString());
@@ -127,7 +140,49 @@ public class TestJobServerLogs extends SchedulerConsecutive {
             System.out.println(jobLogs);
             Assert.fail("No script output");
         }
-        SchedulerTHelper.removeJob(pendingJobId);
-        SchedulerTHelper.waitForEventJobRemoved(pendingJobId);
+
+        checkRemoval(pendingJobId);
     }
+
+    public void checkRemoval(JobId jobId) throws Exception {
+        JobState jobState = SchedulerTHelper.getSchedulerInterface().getJobState(jobId);
+        List<TaskState> tasks = jobState.getTasks();
+
+        checkJobAndTaskLogFiles(jobId, tasks, true);
+
+        SchedulerTHelper.removeJob(jobId);
+        SchedulerTHelper.waitForEventJobRemoved(jobId);
+
+        checkNoLogsFromAPI(jobId, tasks);
+        checkJobAndTaskLogFiles(jobId, tasks, false);
+    }
+
+    private void checkNoLogsFromAPI(JobId jobId, List<TaskState> tasks) throws Exception {
+        Scheduler scheduler = SchedulerTHelper.getSchedulerInterface();
+        try {
+            scheduler.getJobServerLogs(jobId.toString());
+            fail("getJobServerLogs should throw an exception for a removed job");
+        } catch (UnknownJobException expected) {
+        }
+        for (TaskState taskState : tasks) {
+            try {
+                scheduler.getTaskServerLogs(jobId.toString(), taskState.getName());
+                fail("getTaskServerLogs should throw an exception for a removed job");
+            } catch (UnknownJobException expected) {
+            }
+        }
+    }
+
+    private void checkJobAndTaskLogFiles(JobId jobId, List<TaskState> tasks, boolean shouldExist) throws Exception {
+        checkFile(shouldExist, new File(logsLocation, jobId.toString()));
+        for (TaskState taskState : tasks) {
+            checkFile(shouldExist, new File(logsLocation, taskState.getId().toString()));
+        }
+    }
+
+    private void checkFile(boolean shouldExist, File jobLogFile) {
+        String message = String.format("Log file %s should %s", jobLogFile, shouldExist ? "exist" : "not exist");
+        assertEquals(message, shouldExist, jobLogFile.exists());
+    }
+
 }

commit f7775ed54c381ff0fbe88430ff7032e2bdbfb630

SCHEDULING-2057 JobRemoveHandler.call should return false for an already removed job
 - Revert to the pre-70c0f3af67b0ccf4aa93bf147b4faadfda994564 code in
   JobRemoveHandler.

 - Rename the method SchedulerDBManager.loadJobWithoutTasks to
   loadJobWithTasksIfNotRemoved and change it accordingly.

 - Adjust the test for the method.

 - Fixes SchedulingServiceTest6.testJobRemove; breakage was caused by
   70c0f3af67b0ccf4aa93bf147b4faadfda994564.

Change-Id: I72f5528cb8ad26dea42fc021c0960f8d5e74f8db

scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/JobRemoveHandler.java
@@ -1,6 +1,5 @@
 package org.ow2.proactive.scheduler.core;
 
-import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.apache.log4j.Logger;
@@ -8,7 +7,6 @@ import org.ow2.proactive.scheduler.common.NotificationData;
 import org.ow2.proactive.scheduler.common.SchedulerEvent;
 import org.ow2.proactive.scheduler.common.job.JobId;
 import org.ow2.proactive.scheduler.common.job.JobInfo;
-import org.ow2.proactive.scheduler.common.task.TaskId;
 import org.ow2.proactive.scheduler.core.db.SchedulerDBManager;
 import org.ow2.proactive.scheduler.core.properties.PASchedulerProperties;
 import org.ow2.proactive.scheduler.job.InternalJob;
@@ -37,11 +35,7 @@ class JobRemoveHandler implements Callable<Boolean> {
 
         TerminationData terminationData = service.jobs.killJob(jobId);
         service.submitTerminationDataHandler(terminationData);
-        List<InternalJob> internalJobs = dbManager.loadJobs(false, jobId);
-        if (internalJobs == null || internalJobs.isEmpty()) {
-            return false;
-        }
-        InternalJob job = internalJobs.get(0);
+        InternalJob job = dbManager.loadJobWithTasksIfNotRemoved(jobId);
         if (job == null) {
             return false;
         }

scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/db/SchedulerDBManager.java
@@ -744,17 +744,17 @@ public class SchedulerDBManager {
         });
     }
 
-    public InternalJob loadJobWithoutTasks(final JobId id) {
+    public InternalJob loadJobWithTasksIfNotRemoved(final JobId id) {
         return runWithoutTransaction(new SessionWork<InternalJob>() {
             @Override
             public InternalJob executeWork(Session session) {
-                Query jobQuery = session.createQuery("from JobData where id = :id and removedTime = -1")
-                        .setParameter("id", jobId(id));
-                JobData jobData = (JobData) jobQuery.uniqueResult();
-                if (jobData == null) {
+                Query jobQuery = session.createQuery("from JobData as job where job.id in (:ids) and job.removedTime = -1");
+                List<InternalJob> result = new ArrayList<InternalJob>(1);
+                batchLoadJobs(session, false, jobQuery, Collections.singletonList(jobId(id)), result);
+                if (result.isEmpty()) {
                     return null;
                 } else {
-                    return jobData.toInternalJob();
+                    return result.get(0);
                 }
             }
 

scheduler/scheduler-server/src/test/java/functionaltests/schedulerdb/TestJobOperations.java
@@ -17,7 +17,7 @@ import org.ow2.proactive.scheduler.job.JobIdImpl;
 public class TestJobOperations extends BaseSchedulerDBTest {
 
     @Test
-    public void testLoadJobWithoutTasks() throws Exception {
+    public void testLoadJobWithTasksIfNotRemoved() throws Exception {
         TaskFlowJob jobDef = new TaskFlowJob();
         jobDef.addTask(createDefaultTask("task1"));
         jobDef.addTask(createDefaultTask("task2"));
@@ -26,8 +26,8 @@ public class TestJobOperations extends BaseSchedulerDBTest {
 
         InternalJob job = defaultSubmitJob(jobDef, "user1");
 
-        job = dbManager.loadJobWithoutTasks(job.getId());
-        Assert.assertEquals(0, job.getTasks().size());
+        job = dbManager.loadJobWithTasksIfNotRemoved(job.getId());
+        Assert.assertEquals(3, job.getTasks().size());
         Assert.assertEquals("user1", job.getOwner());
         Assert.assertEquals(3, job.getJobInfo().getTotalNumberOfTasks());
         Assert.assertEquals(0, job.getJobInfo().getNumberOfFinishedTasks());
@@ -37,9 +37,9 @@ public class TestJobOperations extends BaseSchedulerDBTest {
         Assert.assertEquals(JobPriority.HIGHEST, job.getJobInfo().getPriority());
 
         dbManager.removeJob(job.getId(), System.currentTimeMillis(), false);
-        Assert.assertNull(dbManager.loadJobWithoutTasks(job.getId()));
+        Assert.assertNull(dbManager.loadJobWithTasksIfNotRemoved(job.getId()));
 
-        Assert.assertNull(dbManager.loadJobWithoutTasks(JobIdImpl.makeJobId("123456789")));
+        Assert.assertNull(dbManager.loadJobWithTasksIfNotRemoved(JobIdImpl.makeJobId("123456789")));
     }
 
     @Test