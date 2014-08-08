ow2-proactive/scheduling

commit 33d9239571655d3c65417b2b01aeb63926210b27

SCHEDULING-2071 DataTransferTest fails in jenkins
- Fix for  functionaltests.DataTransferTest.test_upload_single_file failure on Windows/SunJDK.
  If the user uploads a file via REST Dataspace API and if there already exists a file/folder
in the target location, the server first tries to delete it before saving the file.
  However, FileObject.delete() seems to cause an error on Windows unless a FileSelector instance
is not specified. FileObject.delete(Selector.SELECT_ALL) seems to fix the issue.

Change-Id: Iecc6b9795bce2f6c660746052799bd63d63c3518

rest/rest-server/src/main/java/org/ow2/proactive_grid_cloud_portal/dataspace/RestDataspaceImpl.java
@@ -37,6 +37,7 @@
 package org.ow2.proactive_grid_cloud_portal.dataspace;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.vfs2.Selectors.SELECT_ALL;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -216,7 +217,7 @@ public class RestDataspaceImpl {
      * 'excludes' query parameters, in order to select which files to be deleted
      * inside the specified directory (path-name).</li>
      * </ul>
-     * 
+     *
      */
     @DELETE
     @Path("/{dataspace}/{path-name:.*}")
@@ -250,7 +251,7 @@ public class RestDataspaceImpl {
      * <p>
      * Example:
      * {@code http://localhost:8080/rest/rest/data/user/my-files/my-text-file.txt}
-     * 
+     *
      */
     @HEAD
     @Path("/{dataspace}/{path-name:.*}")
@@ -383,7 +384,7 @@ public class RestDataspaceImpl {
             throws FileSystemException, IOException {
         try {
             if (outputFile.exists()) {
-                outputFile.delete();
+                outputFile.delete(SELECT_ALL);
             }
             if (Strings.isNullOrEmpty(encoding)) {
                 outputFile.createFile();
@@ -400,11 +401,10 @@ public class RestDataspaceImpl {
             if (outputFile != null) {
                 try {
                     if (outputFile.exists()) {
-                        outputFile.delete();
+                        outputFile.delete(SELECT_ALL);
                     }
                 } catch (FileSystemException e1) {
                     logger.error("Error occurred while deleting partially created file.", e1);
-                    throw e1;
                 }
             }
             Throwables.propagateIfInstanceOf(error, FileSystemException.class);

commit 409f3e7f04e8ad751ee73acc1cd8c6fdbdef1cf7

Fixed, commit 33d9239571655d3c65417b2b01aeb63926210b27 has caused following test failures. test_automatic_transfer(functionaltests.RestSmartProxyTest) test test_no_automatic_transfer(functionaltests.RestSmartProxyTest)
Change-Id: I65efbc926dfcb6edeff86bfc6cb5ddee6bd1f919

rest/rest-server/src/main/java/org/ow2/proactive_grid_cloud_portal/dataspace/RestDataspaceImpl.java
@@ -37,7 +37,7 @@
 package org.ow2.proactive_grid_cloud_portal.dataspace;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static org.apache.commons.vfs2.Selectors.SELECT_ALL;
+import static org.apache.commons.vfs2.Selectors.SELECT_SELF;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -384,7 +384,7 @@ public class RestDataspaceImpl {
             throws FileSystemException, IOException {
         try {
             if (outputFile.exists()) {
-                outputFile.delete(SELECT_ALL);
+                outputFile.delete(SELECT_SELF);
             }
             if (Strings.isNullOrEmpty(encoding)) {
                 outputFile.createFile();
@@ -401,7 +401,7 @@ public class RestDataspaceImpl {
             if (outputFile != null) {
                 try {
                     if (outputFile.exists()) {
-                        outputFile.delete(SELECT_ALL);
+                        outputFile.delete(SELECT_SELF);
                     }
                 } catch (FileSystemException e1) {
                     logger.error("Error occurred while deleting partially created file.", e1);

rest/rest-client/src/test/java/functionaltests/DataTransferTest.java
@@ -36,11 +36,20 @@
  */
 package functionaltests;
 
+import static functionaltests.RestFuncTHelper.getRestServerUrl;
+import static org.junit.Assert.assertFalse;
+import static org.ow2.proactive.scheduler.rest.ds.IDataSpaceClient.Dataspace.USER;
+
 import java.io.File;
 import java.net.URI;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.ow2.proactive.scheduler.rest.ds.DataSpaceClient;
 import org.ow2.proactive.scheduler.rest.ds.IDataSpaceClient;
 import org.ow2.proactive.scheduler.rest.ds.LocalDestination;
@@ -49,16 +58,8 @@ import org.ow2.proactive.scheduler.rest.ds.LocalFileSource;
 import org.ow2.proactive.scheduler.rest.ds.RemoteDestination;
 import org.ow2.proactive.scheduler.rest.ds.RemoteSource;
 import org.ow2.proactive_grid_cloud_portal.dataspace.dto.ListFile;
-import com.google.common.io.Files;
-import org.apache.commons.io.FileUtils;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
-import static functionaltests.RestFuncTHelper.getRestServerUrl;
-import static org.junit.Assert.*;
-import static org.ow2.proactive.scheduler.rest.ds.IDataSpaceClient.Dataspace.USER;
+import com.google.common.io.Files;
 
 public class DataTransferTest extends AbstractRestFuncTestCase {
 
@@ -78,10 +79,10 @@ public class DataTransferTest extends AbstractRestFuncTestCase {
         File tmpFile = tmpDir.newFile("tmpfile.tmp");
         Files.write(randomFileContents(), tmpFile);
         LocalFileSource source = new LocalFileSource(tmpFile);
-        RemoteDestination dest = new RemoteDestination(USER, "tmpfile.tmp");
+        RemoteDestination dest = new RemoteDestination(USER, "test_upload_single_file/tmpfile.tmp");
         assertTrue(client.upload(source, dest));
         String destDirPath = URI.create(getScheduler().getUserSpaceURIs().get(0)).getPath();
-        File destFile = new File(destDirPath, "tmpfile.tmp");
+        File destFile = new File(destDirPath, "test_upload_single_file/tmpfile.tmp");
         assertTrue(Files.equal(tmpFile, destFile));
     }
 