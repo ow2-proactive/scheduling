4084bfa145a44e8d015ce910cbc2595f8718aabe
youribonnaffe <youri.bonnaffe@activeeon.com>
2015-07-31 17:48:33 +0200

    Relax condition in selection script for UnauthorizedSelectionScriptTest
    This test could fail on overloaded machines.

--- samples/scripts/selection/checkPhysicalFreeMem.js
+++ samples/scripts/selection/checkPhysicalFreeMem.js
@@ -1,10 +1,10 @@
 // Check if enough memory is available on the system
 
 // expected memory size in Kb
-var expectedMemorySize = 1048576;
+var expectedMemorySize = 0;
 
 var osb = java.lang.management.ManagementFactory.getOperatingSystemMXBean();
 var mem = java.lang.Math.round(osb.getFreePhysicalMemorySize() / 1024);
 
-selected = java.lang.Integer.parseInt(mem) > java.lang.Integer.parseInt(expectedMemorySize);
+selected = java.lang.Integer.parseInt(mem) >= java.lang.Integer.parseInt(expectedMemorySize);
 
