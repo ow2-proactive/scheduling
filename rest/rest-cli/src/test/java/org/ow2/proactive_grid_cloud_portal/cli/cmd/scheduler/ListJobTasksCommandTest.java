8b437933a0711913402e76f42e2bc20e02ef3c40
Paraita Wohler <paraita.wohler@activeeon.com>
2015-10-14 14:25:36 +0200

    Fix commands declaration in CommandSet
    Some commands were not properly declared so tests would fail during verification.

--- rest/rest-cli/src/test/java/org/ow2/proactive_grid_cloud_portal/cli/cmd/scheduler/ListJobTasksCommandTest.java
+++ rest/rest-cli/src/test/java/org/ow2/proactive_grid_cloud_portal/cli/cmd/scheduler/ListJobTasksCommandTest.java
@@ -37,43 +37,38 @@
 
 package org.ow2.proactive_grid_cloud_portal.cli.cmd.scheduler;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.ow2.proactive_grid_cloud_portal.cli.ApplicationContextImpl;
-import org.ow2.proactive_grid_cloud_portal.cli.cmd.ImodeCommand;
-import org.ow2.proactive_grid_cloud_portal.cli.cmd.sched.ListJobTasksCommand;
-import org.ow2.proactive_grid_cloud_portal.scheduler.exception.UnknownJobRestException;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ow2.proactive_grid_cloud_portal.cli.ApplicationContextImpl;
+import org.ow2.proactive_grid_cloud_portal.cli.cmd.sched.ListJobTasksCommand;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ApplicationContextImpl.class)
-@PowerMockIgnore({"javax.script.*", "com.sun.script.*", "org.fusesource.jansi.internal.Kernel32"})
+@PowerMockIgnore({ "javax.script.*", "com.sun.script.*", "org.fusesource.jansi.internal.Kernel32" })
 public class ListJobTasksCommandTest extends AbstractJobTagCommandTest {
-
 
     protected ArrayList<String> taskNames;
 
     protected ArrayList<String> filteredTaskNames;
 
     @Before
-    public void setUp() throws Exception{
+    public void setUp() throws Exception {
         super.setUp();
         this.filteredTaskNames = new ArrayList<>(3);
         this.filteredTaskNames.add("task1");
@@ -84,16 +79,15 @@ public class ListJobTasksCommandTest extends AbstractJobTagCommandTest {
         this.taskNames.add("task4");
     }
 
-
     @After
-    public void tearDown(){
+    public void tearDown() {
         reset(this.restApi);
     }
 
     @Test
-    public void testCommandJobIdOnly() throws Exception{
+    public void testCommandJobIdOnly() throws Exception {
         when(restApi.getJobTasksIds(anyString(), eq(jobId))).thenReturn(this.taskNames);
-        
+
         executeTest(jobId);
 
         String out = capturedOutput.toString();
@@ -103,7 +97,7 @@ public class ListJobTasksCommandTest extends AbstractJobTagCommandTest {
     }
 
     @Test
-    public void testCommandJobIdTag() throws Exception{
+    public void testCommandJobIdTag() throws Exception {
         when(restApi.getJobTasksIdsByTag(anyString(), eq(jobId), eq(tag))).thenReturn(this.filteredTaskNames);
         executeTest(jobId, tag);
 
@@ -113,10 +107,10 @@ public class ListJobTasksCommandTest extends AbstractJobTagCommandTest {
         assertThat(out, equalTo("[task1, task2]" + System.lineSeparator()));
     }
 
-
     @Test
-    public void testCommandJobIdUnknownTag() throws Exception{
-        when(restApi.getJobTasksIdsByTag(anyString(), eq(jobId), eq(unknownTag))).thenReturn(new ArrayList<String>());
+    public void testCommandJobIdUnknownTag() throws Exception {
+        when(restApi.getJobTasksIdsByTag(anyString(), eq(jobId), eq(unknownTag)))
+                .thenReturn(new ArrayList<String>());
 
         executeTest(jobId, unknownTag);
 
@@ -126,55 +120,57 @@ public class ListJobTasksCommandTest extends AbstractJobTagCommandTest {
         assertThat(out, equalTo("[]" + System.lineSeparator()));
     }
 
-
     @Test
-    public void testCommandUnknownJob() throws Exception{
+    public void testCommandUnknownJob() throws Exception {
         when(restApi.getJobTasksIds(anyString(), eq(unknownJobId))).thenThrow(exceptionUnknownJob);
         executeTest(unknownJobId);
 
         String out = capturedOutput.toString();
         System.out.println(out);
 
-        assertThat(out, equalTo("An error occurred while retrieving job('2') tasks:" + System.lineSeparator() +
-                "Error Message: Job 2 does not exists" + System.lineSeparator()));
+        assertThat(out,
+                equalTo("An error occurred while retrieving job('2') tasks:" + System.lineSeparator() +
+                    "Error message: Job 2 does not exists" + System.lineSeparator() + System.lineSeparator() +
+                    "You can enable debug mode for getting more information using -X or --debug option." +
+                    System.lineSeparator()));
     }
 
-
     @Test
-    public void testCommandUnknownJobUnknownTag() throws Exception{
-        when(restApi.getJobTasksIdsByTag(anyString(), eq(unknownJobId), anyString())).thenThrow(exceptionUnknownJob);
+    public void testCommandUnknownJobUnknownTag() throws Exception {
+        when(restApi.getJobTasksIdsByTag(anyString(), eq(unknownJobId), anyString()))
+                .thenThrow(exceptionUnknownJob);
         executeTest(unknownJobId, unknownTag);
 
         String out = capturedOutput.toString();
         System.out.println(out);
 
-        assertThat(out, equalTo("An error occurred while retrieving job('2') tasks filtered by tag unknownTag:" + System.lineSeparator() +
-                "Error Message: Job 2 does not exists" + System.lineSeparator()));
+        assertThat(out,
+                equalTo("An error occurred while retrieving job('2') tasks filtered by tag unknownTag:" +
+                    System.lineSeparator() + "Error message: Job 2 does not exists" + System.lineSeparator() +
+                    System.lineSeparator() +
+                    "You can enable debug mode for getting more information using -X or --debug option." +
+                    System.lineSeparator()));
     }
 
-
     @Test
-    public void testJobIdOnlyFromInteractive() throws Exception{
+    public void testJobIdOnlyFromInteractive() throws Exception {
         typeLine("listtasks(1)");
         executeTestInteractive();
         verify(restApi).getJobTasksIds(anyString(), eq("1"));
     }
 
-
     @Test
-    public void testJobIdTagFromInteractive() throws Exception{
+    public void testJobIdTagFromInteractive() throws Exception {
         typeLine("listtasks(1, 'LOOP-T2-1')");
         executeTestInteractive();
         verify(restApi).getJobTasksIdsByTag(anyString(), eq("1"), eq("LOOP-T2-1"));
     }
 
-
     @Override
     protected void executeCommandWithArgs(Object... args) {
-        if(args.length == 1){
+        if (args.length == 1) {
             new ListJobTasksCommand((String) args[0]).execute(this.context);
-        }
-        else{
+        } else {
             new ListJobTasksCommand((String) args[0], (String) args[1]).execute(this.context);
         }
     }
