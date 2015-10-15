a1d703afdacb263b26c42ada0ae7eb6ed64ca172
Paraita Wohler <paraita.wohler@activeeon.com>
2015-10-15 12:25:52 +0200

    Unit-test the rest getJobTasksIdsPaginated method

---
+++ rest/rest-server/src/test/java/org/ow2/proactive_grid_cloud_portal/scheduler/SchedulerStateRestPaginationTest.java
@@ -0,0 +1,172 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2015 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+package org.ow2.proactive_grid_cloud_portal.scheduler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.ow2.proactive.scheduler.common.job.JobId;
+import org.ow2.proactive.scheduler.common.job.JobState;
+import org.ow2.proactive.scheduler.common.task.TaskId;
+import org.ow2.proactive.scheduler.common.task.TaskState;
+import org.ow2.proactive.scheduler.common.util.SchedulerProxyUserInterface;
+import org.ow2.proactive.scheduler.job.JobIdImpl;
+import org.ow2.proactive.scheduler.job.JobResultImpl;
+import org.ow2.proactive.scheduler.task.TaskIdImpl;
+import org.ow2.proactive.scheduler.task.TaskResultImpl;
+import org.ow2.proactive_grid_cloud_portal.RestTestServer;
+import org.ow2.proactive_grid_cloud_portal.common.SchedulerRestInterface;
+import org.ow2.proactive_grid_cloud_portal.common.SharedSessionStoreTestUtils;
+
+
+/**
+ * The [offset, limit[ boundaries and tag filtering are out of the scope of these tests
+ * because their control is done in the <code>JobState</code> class
+ * @author paraita
+ *
+ */
+public class SchedulerStateRestPaginationTest extends RestTestServer {
+
+    private String generateReadableName(final String jobIdStr, final int i, final int nbTasks) {
+        return "JOB-" + jobIdStr + "-TASK-" + (i + 1) + "/" + nbTasks;
+    }
+
+    private JobState newMockedJob(final String jobIdStr, final int nbTasks) {
+        JobState mockedJob = mock(JobState.class);
+        JobId mockedJobId = mock(JobId.class);
+
+        List<TaskState> dumbList = new ArrayList<TaskState>(nbTasks);
+        for (int i = 0; i < nbTasks; i++) {
+            TaskState mockedTask = mock(TaskState.class);
+            TaskId mockedTaskId = mock(TaskId.class);
+            when(mockedTaskId.getReadableName()).thenReturn(generateReadableName(jobIdStr, i, nbTasks));
+            when(mockedTask.getId()).thenReturn(mockedTaskId);
+            dumbList.add(mockedTask);
+        }
+
+        when(mockedJobId.value()).thenReturn(jobIdStr);
+        when(mockedJob.getId()).thenReturn(mockedJobId);
+        when(mockedJob.getTasksPaginated(0, 50)).thenReturn(dumbList);
+        return mockedJob;
+    }
+
+    /**
+     * The [offset, limit[ boundaries and tag filtering are out of the scope of these tests
+     * because their control is done in the <code>JobState</code> class
+     * @throws Throwable
+     */
+    @Test
+    public void testGetJobTasksIdsPaginated() throws Throwable {
+        SchedulerRestInterface restInterface = new SchedulerStateRest();
+
+        SchedulerProxyUserInterface mockOfScheduler = mock(SchedulerProxyUserInterface.class);
+        String sessionId = SharedSessionStoreTestUtils.createValidSession(mockOfScheduler);
+
+        int nbTasks = 50;
+        String jobIdStr = "1";
+        JobState job = newMockedJob(jobIdStr, nbTasks);
+        when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
+
+        List<String> res = restInterface.getJobTasksIdsPaginated(sessionId, jobIdStr, 0, nbTasks);
+
+        assertEquals("Number of tasks is incorrect", nbTasks, res.size());
+
+        for (int i = 0; i < nbTasks; i++) {
+            assertEquals("Task readable name is incorrect", generateReadableName(jobIdStr, i, nbTasks),
+                    res.get(i));
+        }
+
+    }
+
+    @Test
+    public void testGetJobTasksIdsByTagPaginated() throws Throwable {
+
+    }
+
+    @Test
+    public void testGetJobTaskStatesPaginated() throws Throwable {
+
+    }
+
+    @Test
+    public void testGetJobTaskStatesByTagPaginated() throws Throwable {
+
+    }
+
+    @Test
+    public void testValueOfTaskResult_ExceptionNoMessage() throws Throwable {
+        SchedulerRestInterface restInterface = new SchedulerStateRest();
+
+        SchedulerProxyUserInterface mockOfScheduler = mock(SchedulerProxyUserInterface.class);
+        String sessionId = SharedSessionStoreTestUtils.createValidSession(mockOfScheduler);
+
+        TaskResultImpl taskResultWithException = new TaskResultImpl(
+            TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1), null, new byte[0], null);
+        when(mockOfScheduler.getTaskResult("42", "mytask")).thenReturn(taskResultWithException);
+
+        String exceptionStackTrace = (String) restInterface.valueOftaskresult(sessionId, "42", "mytask");
+
+        assertNotNull(exceptionStackTrace);
+    }
+
+    @Test
+    public void testValueOfJobResult_ExceptionNoMessage() throws Throwable {
+        SchedulerRestInterface restInterface = new SchedulerStateRest();
+
+        SchedulerProxyUserInterface mockOfScheduler = mock(SchedulerProxyUserInterface.class);
+        String sessionId = SharedSessionStoreTestUtils.createValidSession(mockOfScheduler);
+
+        TaskResultImpl taskResultWithException = new TaskResultImpl(
+            TaskIdImpl.createTaskId(JobIdImpl.makeJobId("42"), "mytask", 1), null, new byte[0], null);
+        JobResultImpl jobResultWithException = new JobResultImpl();
+        jobResultWithException.addTaskResult("mytask", taskResultWithException, false);
+        when(mockOfScheduler.getJobResult("42")).thenReturn(jobResultWithException);
+
+        Map<String, String> jobResult = restInterface.jobResultValue(sessionId, "42");
+        String exceptionStackTrace = jobResult.get("mytask");
+
+        assertNotNull(exceptionStackTrace);
+    }
+
+}
