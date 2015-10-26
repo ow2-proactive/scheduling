82a970c7a589707e530361cab576a309e25e5bb0
Paraita Wohler <paraita.wohler@activeeon.com>
2015-10-26 16:00:17 +0100

    Fix the debug mode usage message that prevents some tests from passing.
    The failing tests are: GetJobOutputCommandTest, getJobResultCommandTest, ListJobTasksCommandTest and ListTaskStatesCommandTest. The tests are failing because of the hardcoded \n value.

--- rest/rest-cli/src/main/java/org/ow2/proactive_grid_cloud_portal/cli/cmd/AbstractCommand.java
+++ rest/rest-cli/src/main/java/org/ow2/proactive_grid_cloud_portal/cli/cmd/AbstractCommand.java
@@ -37,6 +37,11 @@
 
 package org.ow2.proactive_grid_cloud_portal.cli.cmd;
 
+import static org.ow2.proactive_grid_cloud_portal.cli.CLIException.REASON_IO_ERROR;
+import static org.ow2.proactive_grid_cloud_portal.cli.CLIException.REASON_UNAUTHORIZED_ACCESS;
+import static org.ow2.proactive_grid_cloud_portal.cli.HttpResponseStatus.FORBIDDEN;
+import static org.ow2.proactive_grid_cloud_portal.cli.utils.ExceptionUtility.stackTraceAsString;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
@@ -44,6 +49,11 @@ import java.util.Stack;
 
 import javax.net.ssl.SSLPeerUnverifiedException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.codehaus.jackson.type.TypeReference;
 import org.ow2.proactive_grid_cloud_portal.cli.ApplicationContext;
 import org.ow2.proactive_grid_cloud_portal.cli.CLIException;
 import org.ow2.proactive_grid_cloud_portal.cli.CommandSet;
@@ -53,22 +63,12 @@ import org.ow2.proactive_grid_cloud_portal.cli.utils.HttpResponseWrapper;
 import org.ow2.proactive_grid_cloud_portal.cli.utils.HttpUtility;
 import org.ow2.proactive_grid_cloud_portal.cli.utils.StringUtility;
 import org.ow2.proactive_grid_cloud_portal.scheduler.exception.NotConnectedRestException;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.codehaus.jackson.type.TypeReference;
-
-import static org.ow2.proactive_grid_cloud_portal.cli.CLIException.REASON_IO_ERROR;
-import static org.ow2.proactive_grid_cloud_portal.cli.CLIException.REASON_UNAUTHORIZED_ACCESS;
-import static org.ow2.proactive_grid_cloud_portal.cli.HttpResponseStatus.FORBIDDEN;
-import static org.ow2.proactive_grid_cloud_portal.cli.utils.ExceptionUtility.stackTraceAsString;
 
 
 public abstract class AbstractCommand implements Command {
 
     private static final String DEBUG_MODE_USAGE_MESSAGE_PREFIX =
-            "\nYou can enable debug mode for getting more information using ";
+            System.lineSeparator() + "You can enable debug mode for getting more information using ";
 
     private static final String DEBUG_MODE_USAGE_INTERACTIVE_SUFFIX =
             "command '" + CommandSet.DEBUG.jsCommand() + "'.";
