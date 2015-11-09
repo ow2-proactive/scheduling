c2b42c7c576e72ef04ff5ffecb51308824dd24f8
Paraita Wohler <paraita.wohler@activeeon.com>
2015-11-09 11:25:38 +0100

    Unit-test the new REST resources
    The following REST resources have been unit-tested: tasks, tasks/tag/{tasktag}, taskstates and taskstates/tag/{tasktag}.

--- rest/rest-server/src/test/java/org/ow2/proactive_grid_cloud_portal/scheduler/SchedulerStateRestTaskCentricTest.java
+++ rest/rest-server/src/test/java/org/ow2/proactive_grid_cloud_portal/scheduler/SchedulerStateRestTaskCentricTest.java
@@ -44,8 +44,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.ow2.proactive.scheduler.common.Page;
 import org.ow2.proactive.scheduler.common.job.JobId;
 import org.ow2.proactive.scheduler.common.job.JobState;
 import org.ow2.proactive.scheduler.common.task.TaskId;
@@ -56,6 +56,8 @@ import org.ow2.proactive_grid_cloud_portal.RestTestServer;
 import org.ow2.proactive_grid_cloud_portal.common.SchedulerRestInterface;
 import org.ow2.proactive_grid_cloud_portal.common.SharedSessionStoreTestUtils;
 import org.ow2.proactive_grid_cloud_portal.scheduler.dto.RestPage;
+import org.ow2.proactive_grid_cloud_portal.scheduler.dto.TaskIdData;
+import org.ow2.proactive_grid_cloud_portal.scheduler.dto.TaskInfoData;
 import org.ow2.proactive_grid_cloud_portal.scheduler.dto.TaskStateData;
 
 
@@ -63,7 +65,6 @@ import org.ow2.proactive_grid_cloud_portal.scheduler.dto.TaskStateData;
  * The [offset, limit[ boundaries and tag filtering are out of the scope of these tests
  * because their control is done in the <code>SchedulerDBManager</code> class.
  * We are only testing the REST interface methods providing the task-centric feature.
- * @author paraita
  *
  */
 public class SchedulerStateRestTaskCentricTest extends RestTestServer {
@@ -79,33 +80,66 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
         sessionId = SharedSessionStoreTestUtils.createValidSession(mockOfScheduler);
     }
     
- // TODO PARAITA
-    @Ignore
     @Test
     public void testGetTaskIds() throws Throwable {
-        int nbTasks = 50;
+        int nbTasks = 100;
         String jobIdStr = "1";
-        JobState job = newMockedJob(jobIdStr, nbTasks);
-        when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
+        Page<TaskId> expectedPage = newMockedTaskIdPage(jobIdStr, nbTasks, null);
+        
+        when(mockOfScheduler.getTaskIds(null, 0, 0, false, true, true, true, 0, 50))
+        .thenReturn(expectedPage);
 
-        RestPage<String> page = restInterface.getTaskIds(sessionId, 0, 0, true, true, true, true, 0, 50);
+        RestPage<String> page = restInterface.getTaskIds(sessionId, 0, 0, false, true, true, true, 0, 50);
         
         assertTasks(nbTasks, jobIdStr, page);
     }
     
     @Test
     public void testGetTaskIdsByTag() throws Throwable {
-      //TODO PARAITA
-    }
+        int nbTasks = 100;
+        String jobIdStr = "500";
+        String tag = "TAG-TEST";
+        Page<TaskId> expectedPage = newMockedTaskIdPage(jobIdStr, nbTasks, tag);
+        
+        when(mockOfScheduler.getTaskIds(tag, 0, 0, false, true, true, true, 0, nbTasks))
+        .thenReturn(expectedPage);
+
+        RestPage<String> page = restInterface.getTaskIdsByTag(sessionId, tag, 0, 0, false, true, true, true, 0, nbTasks);
+        
+        assertTasks(nbTasks, jobIdStr, page);
+    } 
     
     @Test
     public void testGetTaskStates() throws Throwable {
-      //TODO PARAITA
+        int nbTasksInPage = 50;
+        int nbTotalTasks = 100;
+        String jobIdStr = "1";
+        String tag = null;
+        Page<TaskState> expectedPage = newMockedTaskStatePage(jobIdStr, tag, nbTasksInPage, nbTotalTasks);
+        
+        when(mockOfScheduler.getTaskStates(null, 0, 0, false, true, true, true, 0, nbTasksInPage))
+        .thenReturn(expectedPage);
+
+        RestPage<TaskStateData> page = restInterface.getTaskStates(sessionId, 0, 0, false, true, true, true, 0, nbTasksInPage);
+        
+        assertTaskStates(expectedPage, page);
     }
-    
+
     @Test
     public void testGetTaskStatesByTag() throws Throwable {
-      //TODO PARAITA
+        int nbTasksInPage = 50;
+        int nbTotalTasks = 100;
+        String jobIdStr = "1";
+        String tag = "TAG-TEST";
+        Page<TaskState> expectedPage = newMockedTaskStatePage(jobIdStr, tag, nbTasksInPage, nbTotalTasks);
+
+        when(mockOfScheduler.getTaskStates(tag, 0, 0, false, true, true, true, 0, nbTasksInPage))
+                .thenReturn(expectedPage);
+
+        RestPage<TaskStateData> page = restInterface.getTaskStatesByTag(sessionId, tag, 0, 0, false, true,
+                true, true, 0, nbTasksInPage);
+
+        assertTaskStates(expectedPage, page);
     }
 
     @Test
@@ -113,7 +147,7 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
 
         int nbTasks = 50;
         String jobIdStr = "1";
-        JobState job = newMockedJob(jobIdStr, nbTasks);
+        JobState job = newMockedJob(jobIdStr, "", nbTasks);
         when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
 
         RestPage<String> page = restInterface.getTasksNamesPaginated(sessionId, jobIdStr, 0, nbTasks);
@@ -126,7 +160,7 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
     public void testGetJobTasksIdsByTagPaginated() throws Throwable {
         int nbTasks = 50;
         String jobIdStr = "1";
-        JobState job = newMockedJob(jobIdStr, nbTasks);
+        JobState job = newMockedJob(jobIdStr, "", nbTasks);
         when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
 
         RestPage<String> page = restInterface.getTasksNamesPaginated(sessionId, jobIdStr, 0, nbTasks);
@@ -138,7 +172,7 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
     public void testGetJobTaskStatesPaginated() throws Throwable {
         int nbTasks = 50;
         String jobIdStr = "1";
-        JobState job = newMockedJob(jobIdStr, nbTasks);
+        JobState job = newMockedJob(jobIdStr, null, nbTasks);
         when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
 
         List<TaskStateData> res = restInterface.getJobTaskStatesPaginated(sessionId, jobIdStr, 0, nbTasks)
@@ -152,7 +186,7 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
     public void testGetJobTaskStatesByTagPaginated() throws Throwable {
         int nbTasks = 50;
         String jobIdStr = "1";
-        JobState job = newMockedJob(jobIdStr, nbTasks);
+        JobState job = newMockedJob(jobIdStr, "", nbTasks);
         when(mockOfScheduler.getJobState(jobIdStr)).thenReturn(job);
 
         List<TaskStateData> res = restInterface
@@ -162,17 +196,13 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
 
     }
 
-    private JobState newMockedJob(final String jobIdStr, final int nbTasks) {
+    private JobState newMockedJob(final String jobIdStr, final String tag, final int nbTasks) {
         JobState mockedJob = mock(JobState.class);
         JobId mockedJobId = mock(JobId.class);
 
         List<TaskState> dumbList = new ArrayList<TaskState>(nbTasks);
         for (int i = 0; i < nbTasks; i++) {
-            TaskState mockedTask = mock(TaskState.class);
-            TaskId mockedTaskId = mock(TaskId.class);
-            when(mockedTaskId.getReadableName()).thenReturn(generateReadableName(jobIdStr, i, nbTasks));
-            when(mockedTask.getId()).thenReturn(mockedTaskId);
-            dumbList.add(mockedTask);
+            dumbList.add(newTaskState(jobIdStr, null, i, nbTasks));
         }
 
         when(mockedJobId.value()).thenReturn(jobIdStr);
@@ -181,6 +211,47 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
         when(mockedJob.getTaskByTagPaginated("", 0, 50)).thenReturn(new TaskStatesPage(dumbList, nbTasks));
         return mockedJob;
     }
+    
+    private Page<TaskId> newMockedTaskIdPage(String jobIdStr, int nbTasks, String tag) {
+        List<TaskId> lTaskId = new ArrayList<TaskId>(nbTasks);
+        for (int i = 0 ; i < nbTasks ; i++) {
+            TaskId mockedTaskId = mock(TaskId.class);
+            when(mockedTaskId.getReadableName()).thenReturn(generateReadableName(jobIdStr, i, nbTasks));
+            when(mockedTaskId.getTag()).thenReturn(tag);
+            lTaskId.add(mockedTaskId);
+        }
+        return new Page<TaskId>(lTaskId, nbTasks);
+    }
+    
+    private Page<TaskState> newMockedTaskStatePage(String jobIdStr, String tag, int nbTasks, int totalNumberOfTasks) {
+        List<TaskState> lTasks = new ArrayList<TaskState>(nbTasks);
+        for (int i = 0; i < nbTasks ; i++) {
+            lTasks.add(newTaskState(jobIdStr, tag, i, nbTasks));
+        }
+        return new Page<TaskState>(lTasks, totalNumberOfTasks);
+    }
+    
+    private TaskState newTaskState(String jobIdStr, String tag, int cnt, int nbTasks) {
+        TaskId mockedTaskId = mock(TaskId.class);
+        TaskState mockedState = mock(TaskState.class);
+        when(mockedTaskId.getReadableName()).thenReturn(generateReadableName(jobIdStr, cnt, nbTasks));
+        when(mockedState.getTag()).thenReturn(tag);
+        when(mockedState.getId()).thenReturn(mockedTaskId);
+        when(mockedState.getName()).thenReturn(generateReadableName(jobIdStr, cnt, nbTasks));
+        return mockedState;
+    }
+    
+    private TaskStateData newMockedTaskStateData(String jobIdStr, String tag, int cnt, int nbTasks) {
+        TaskStateData mockedTaskStateData = mock(TaskStateData.class);
+        TaskInfoData mockedTaskInfoData = mock(TaskInfoData.class);
+        TaskIdData mockedTaskIdData = mock(TaskIdData.class);
+        when(mockedTaskIdData.getReadableName()).thenReturn(generateReadableName(jobIdStr, cnt, nbTasks));
+        when(mockedTaskInfoData.getTaskId()).thenReturn(mockedTaskIdData);
+        when(mockedTaskStateData.getTaskInfo()).thenReturn(mockedTaskInfoData);
+        when(mockedTaskStateData.getTag()).thenReturn(tag);
+        return mockedTaskStateData;
+    }
+
 
     private String generateReadableName(final String jobIdStr, final int i, final int nbTasks) {
         return "JOB-" + jobIdStr + "-TASK-" + (i + 1) + "/" + nbTasks;
@@ -194,5 +265,20 @@ public class SchedulerStateRestTaskCentricTest extends RestTestServer {
                     page.getList().get(i));
         }
     }
+    
+    private void assertTaskStates(Page<TaskState> expected, RestPage<TaskStateData> actual) {
 
+        assertEquals("Number of tasks in page is incorrect", expected.getList().size(),
+                actual.getList().size());
+        assertEquals("Total number of tasks is incorrect", expected.getSize(), actual.getSize());
+        
+        for (int i = 0; i < expected.getList().size(); i++) {
+            TaskState tExpected = expected.getList().get(i);
+            TaskStateData tActual = actual.getList().get(i);
+            assertEquals("readableName incorrect", tExpected.getId().getReadableName(), tActual.getName());
+            assertEquals("tag incorrect", tExpected.getTag(), tActual.getTag());
+        }
+        
+    }
+    
 }
