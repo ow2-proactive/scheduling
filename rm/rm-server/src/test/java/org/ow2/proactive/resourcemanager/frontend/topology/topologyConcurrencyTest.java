67ed45d095bb4d362146ad12ea4cabd134468380
fviale <fabien.viale@activeeon.com>
2015-11-25 16:47:52 +0100

    - implemented review suggestions

--- rm/rm-server/src/test/java/org/ow2/proactive/resourcemanager/frontend/topology/topologyConcurrencyTest.java
+++ rm/rm-server/src/test/java/org/ow2/proactive/resourcemanager/frontend/topology/topologyConcurrencyTest.java
@@ -1,43 +1,20 @@
 package org.ow2.proactive.resourcemanager.frontend.topology;
 
-import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
-import org.objectweb.proactive.core.ProActiveException;
 import org.objectweb.proactive.core.UniqueID;
-import org.objectweb.proactive.core.body.UniversalBody;
-import org.objectweb.proactive.core.descriptor.data.ProActiveDescriptorInternal;
-import org.objectweb.proactive.core.descriptor.data.VirtualNodeInternal;
-import org.objectweb.proactive.core.descriptor.services.TechnicalService;
-import org.objectweb.proactive.core.filetransfer.FileTransferEngine;
-import org.objectweb.proactive.core.jmx.mbean.ProActiveRuntimeWrapperMBean;
-import org.objectweb.proactive.core.jmx.notification.GCMRuntimeRegistrationNotificationData;
-import org.objectweb.proactive.core.jmx.server.ServerConnector;
-import org.objectweb.proactive.core.mop.ConstructorCall;
-import org.objectweb.proactive.core.mop.ConstructorCallExecutionFailedException;
-import org.objectweb.proactive.core.node.Node;
-import org.objectweb.proactive.core.node.NodeException;
 import org.objectweb.proactive.core.node.NodeImpl;
-import org.objectweb.proactive.core.process.UniversalProcess;
-import org.objectweb.proactive.core.runtime.ProActiveRuntime;
 import org.objectweb.proactive.core.runtime.ProActiveRuntimeImpl;
 import org.objectweb.proactive.core.runtime.VMInformation;
 import org.objectweb.proactive.core.util.ProActiveInet;
 import org.ow2.proactive.resourcemanager.core.properties.PAResourceManagerProperties;
 import org.ow2.proactive.resourcemanager.frontend.topology.pinging.HostsPinger;
-import org.ow2.proactive.resourcemanager.rmnode.RMNode;
-import org.ow2.proactive.resourcemanager.selection.SelectionManager;
 import org.ow2.proactive.resourcemanager.selection.topology.TopologyManager;
-import org.ow2.proactive.scripting.ScriptResult;
-import org.ow2.proactive.scripting.SelectionScript;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
-import java.rmi.AlreadyBoundException;
 import java.rmi.dgc.VMID;
 import java.util.ArrayList;
 import java.util.List;
@@ -55,24 +32,24 @@ import static org.mockito.Mockito.when;
  * At the end, remove all previously added nodes and verify that
  * the topology manager structure is empty
  */
-public class TestTopologyConcurrency {
+public class topologyConcurrencyTest {
 
     // number of thread used in this test
-    final int nb_threads = 20;
+    final int nbThreads = 20;
 
     // multiplier for the number of nodes to create
-    final int node_factor = 4;
+    final int nodeFactor = 4;
 
     // size of collisions, as many operations will be done on the same node
     // more removal than add
-    final int collision_size = 10;
+    final int collisionSize = 10;
 
     // total number of tasks
-    final int total = nb_threads * node_factor * collision_size;
+    final int total = nbThreads * nodeFactor * collisionSize;
 
     String baseUrl = "pnp://localhost:1234/LocalNode-";
 
-    ExecutorService s = Executors.newFixedThreadPool(nb_threads);
+    ExecutorService s = Executors.newFixedThreadPool(nbThreads);
 
     @Test
     public void action() throws Exception {
@@ -95,12 +72,13 @@ public class TestTopologyConcurrency {
             calls.add(new Callable<Boolean>() {
                 @Override
                 public Boolean call() throws Exception {
-                    int index = j - (j % collision_size);
-                    if ((j % (collision_size / 2)) == 0) {
-                        manager.addNode(new NodeImpl(runtime, baseUrl + index));
+                    int index = j - (j % collisionSize);
+                    NodeImpl node = new NodeImpl(runtime, baseUrl + index);
+                    if ((j % (collisionSize / 2)) == 0) {
+                        manager.addNode(node);
                     } else {
                         // more remove than add, to try to reproduce empty list problems
-                        manager.removeNode(new NodeImpl(runtime, baseUrl + index));
+                        manager.removeNode(node);
                     }
                     return true;
                 }
@@ -112,9 +90,9 @@ public class TestTopologyConcurrency {
             Assert.assertTrue(fut.get());
         }
 
-        calls = new ArrayList<>(nb_threads * node_factor);
+        calls = new ArrayList<>(nbThreads * nodeFactor);
         // second set of task remove all nodes created
-        for (int i = 0; i < total; i += collision_size) {
+        for (int i = 0; i < total; i += collisionSize) {
             final int j = i;
             calls.add(new Callable<Boolean>() {
                 @Override
