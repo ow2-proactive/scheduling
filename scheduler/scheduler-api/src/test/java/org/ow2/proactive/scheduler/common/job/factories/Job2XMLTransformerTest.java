34b939a774907c84a27bafd16e59863a60507091
fviale <fabien.viale@activeeon.com>
2016-01-22 19:11:13 +0100

    fix for #2418 : - added the  Job2XMLTransformerTest with walltime unit test only

---
+++ scheduler/scheduler-api/src/test/java/org/ow2/proactive/scheduler/common/job/factories/Job2XMLTransformerTest.java
@@ -0,0 +1,41 @@
+package org.ow2.proactive.scheduler.common.job.factories;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.ow2.proactive.scheduler.common.job.TaskFlowJob;
+import org.ow2.proactive.scheduler.common.task.JavaTask;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class Job2XMLTransformerTest {
+
+    @Rule
+    public TemporaryFolder tmpFolder = new TemporaryFolder();
+
+    @Test
+    public void walltimeIsPreserved() throws Exception {
+        File xmlFile = tmpFolder.newFile();
+        String taskName = "walltimeTask";
+
+        // tests for various values including one second, one minute, one minute and one second, big value, etc. miliseconds are discarded
+        long[] walltimesToTest = {0, 1000, 60000, 61000, 3600000, 3601000, 3660000, 3661000, 999999000};
+
+        for (int i = 1; i < walltimesToTest.length; i++) {
+            TaskFlowJob job = new TaskFlowJob();
+            JavaTask task = new JavaTask();
+            task.setName(taskName);
+            task.setExecutableClassName("oo.Bar");
+            task.setWallTime(walltimesToTest[i]);
+            job.addTask(task);
+
+            new Job2XMLTransformer().job2xmlFile(job, xmlFile);
+            TaskFlowJob recreatedJob = (TaskFlowJob) (JobFactory.getFactory().createJob(xmlFile.getAbsolutePath()));
+
+            assertEquals("Walltimes between original and recreated job must be equal", walltimesToTest[i], recreatedJob.getTask(taskName).getWallTime());
+        }
+    }
+}
