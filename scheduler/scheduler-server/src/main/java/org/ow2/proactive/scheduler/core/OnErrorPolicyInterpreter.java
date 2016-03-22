a3a00e8cf7605ad170d91791c3b8ac10daff12ed
Marco Castigliego <marcocast@gmail.com>
2016-03-22 14:04:39 +0100

    Removing job inheritance logic when checking the task OnTaskErrorProperty

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/OnErrorPolicyInterpreter.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/OnErrorPolicyInterpreter.java
@@ -2,54 +2,25 @@ package org.ow2.proactive.scheduler.core;
 
 import org.ow2.proactive.scheduler.common.task.OnTaskError;
 import org.ow2.proactive.scheduler.common.task.UpdatableProperties;
-import org.ow2.proactive.scheduler.job.InternalJob;
 import org.ow2.proactive.scheduler.task.internal.InternalTask;
 
 
 public class OnErrorPolicyInterpreter {
 
-    public boolean requiresPauseTaskOnError(InternalJob job, InternalTask task) {
-
-        return propertySetOnTaskOrJobLevel(job, task, OnTaskError.PAUSE_TASK);
-
+    public boolean requiresPauseTaskOnError(InternalTask task) {
+        return propertyIs(task.getOnTaskErrorProperty(), OnTaskError.PAUSE_TASK);
     }
 
-    public boolean requiresPauseJobOnError(InternalJob job, InternalTask task) {
-
-        return propertySetOnTaskOrJobLevel(job, task, OnTaskError.PAUSE_JOB);
-
+    public boolean requiresPauseJobOnError(InternalTask task) {
+        return propertyIs(task.getOnTaskErrorProperty(), OnTaskError.PAUSE_JOB);
     }
 
-    public boolean requiresCancelJobOnError(InternalJob job, InternalTask task) {
-
-        return propertySetOnTaskOrJobLevel(job, task, OnTaskError.CANCEL_JOB);
-
-    }
-
-    private boolean propertySetOnTaskOrJobLevel(InternalJob job, InternalTask task, OnTaskError onTaskError) {
-        if (propertyIs(task.getOnTaskErrorProperty(), onTaskError)) {
-            return true;
-        } else if (notSetOrNone(task.getOnTaskErrorProperty()) &&
-            propertyIs(job.getOnTaskErrorProperty(), onTaskError)) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean notSetOrNone(UpdatableProperties<OnTaskError> onTaskErrorProperty) {
-        if (!onTaskErrorProperty.isSet()) {
-            return true;
-        } else if (propertyIs(onTaskErrorProperty, OnTaskError.NONE)) {
-            return true;
-        }
-
-        return false;
-
+    public boolean requiresCancelJobOnError(InternalTask task) {
+        return propertyIs(task.getOnTaskErrorProperty(), OnTaskError.CANCEL_JOB);
     }
 
     private boolean propertyIs(UpdatableProperties<OnTaskError> onTaskErrorProperty,
             OnTaskError onTaskError) {
-
         return onTaskErrorProperty.isSet() &&
             onTaskErrorProperty.getValue().toString().equals(onTaskError.toString());
     }
--- scheduler/scheduler-server/src/test/java/org/ow2/proactive/scheduler/core/OnErrorPolicyInterpreterTest.java
+++ scheduler/scheduler-server/src/test/java/org/ow2/proactive/scheduler/core/OnErrorPolicyInterpreterTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.objectweb.proactive.ActiveObjectCreationException;
 import org.objectweb.proactive.core.node.Node;
 import org.objectweb.proactive.core.node.NodeException;
-import org.ow2.proactive.scheduler.common.job.JobType;
 import org.ow2.proactive.scheduler.common.task.OnTaskError;
 import org.ow2.proactive.scheduler.job.InternalJob;
 import org.ow2.proactive.scheduler.task.TaskLauncher;
@@ -19,16 +18,193 @@ public class OnErrorPolicyInterpreterTest {
     @Test
     public void testRequiresPauseTaskOnError() {
 
-        InternalJob job = new InternalJob() {
+        InternalTask task = createTask();
 
-            @Override
-            public JobType getType() {
-                // TODO Auto-generated method stub
-                return null;
-            }
-        };
+        task.setOnTaskError(OnTaskError.PAUSE_TASK);
 
-        job.setOnTaskError(OnTaskError.PAUSE_TASK);
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(task);
+
+        assertThat(actual, is(true));
+
+    }
+
+    @Test
+    public void testRequiresPauseTaskOnErrorNull() {
+
+        InternalTask task = createTask();
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseTaskOnErrorNotset() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.NONE);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseTaskOnErrorCancelJob() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.CANCEL_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseTaskOnErrorPauseJob() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.PAUSE_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseJobOnError() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.PAUSE_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseJobOnError(task);
+
+        assertThat(actual, is(true));
+
+    }
+
+    @Test
+    public void testRequiresPauseJobOnErrorNull() {
+
+        InternalTask task = createTask();
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseJobOnErrorNotset() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.NONE);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseJobOnErrorCancelJob() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.CANCEL_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresPauseJobOnErrorPauseTask() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.PAUSE_TASK);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresPauseJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresCancelJobOnError() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.CANCEL_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresCancelJobOnError(task);
+
+        assertThat(actual, is(true));
+
+    }
+
+    @Test
+    public void testRequiresCancelJobOnErrorNull() {
+
+        InternalTask task = createTask();
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresCancelJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresCancelJobOnErrorNotset() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.NONE);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresCancelJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresCancelJobOnErrorPauseJob() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.PAUSE_JOB);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresCancelJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    @Test
+    public void testRequiresCancelJobOnErrorPauseTask() {
+
+        InternalTask task = createTask();
+
+        task.setOnTaskError(OnTaskError.PAUSE_TASK);
+
+        boolean actual = new OnErrorPolicyInterpreter().requiresCancelJobOnError(task);
+
+        assertThat(actual, is(false));
+
+    }
+
+    private InternalTask createTask() {
         InternalTask task = new InternalTask() {
 
             @Override
@@ -44,10 +220,7 @@ public class OnErrorPolicyInterpreterTest {
                 return null;
             }
         };
-        boolean actual = new OnErrorPolicyInterpreter().requiresPauseTaskOnError(job, task);
-
-        assertThat(actual, is(true));
-
+        return task;
     }
 
 }
