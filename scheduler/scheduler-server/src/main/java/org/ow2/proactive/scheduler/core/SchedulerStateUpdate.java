59ca746711e9d9ff56284613984c7b8c3c1f42db
Marco Castigliego <marcocast@gmail.com>
2016-07-12 16:58:43 +0200

    Fixed javadoc documentation

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/SchedulerStateUpdate.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/SchedulerStateUpdate.java
@@ -78,7 +78,7 @@ public interface SchedulerStateUpdate {
     /**
      * Invoked each time the state of a job has changed and each time a scheduler event occurs.<br>
      *
-     * @param updated job.
+     * @param job the updated job.
      */
     void jobUpdatedFullData(JobState job);
 
