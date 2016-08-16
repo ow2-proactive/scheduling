f1c478009526b6b9ed47929f318c3c2a0c7f1af6
Yinan Liu <liu.damian@gmail.com>
2016-08-16 17:16:16 +0200

    fix the test

--- scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/task/RestartMode.java
+++ scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/task/RestartMode.java
@@ -70,6 +70,8 @@ public class RestartMode implements java.io.Serializable {
 
     @XmlAttribute
     private String description;
+    
+    public RestartMode(){}
 
     /**
      * Implicit constructor of a restart mode.
