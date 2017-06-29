d18bcd291c2eaa55cf673986b7a08f60a97dd77c
fviale <fabien.viale@activeeon.com>
2017-06-29 14:55:37 +0200

    Improve SelectionManager script execution
    - ProbabilisticSelectionManager : when an error occurs during a selection script execution on a node, decrease this node priority
    - for single_host or single_host_eclusive criteria, execute scripts not on all nodes at once.
    - Added Probability and ProbabilisticSelectionManager unit tests
    - In Probability, handle properly the case where the dynamicity is disabled

---
+++ rm/rm-server/src/test/java/org/ow2/proactive/resourcemanager/selection/statistics/ProbabilisticSelectionManagerTest.java
@@ -0,0 +1,264 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive.resourcemanager.selection.statistics;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.ow2.proactive.resourcemanager.core.RMCore;
+import org.ow2.proactive.resourcemanager.core.properties.PAResourceManagerProperties;
+import org.ow2.proactive.resourcemanager.rmnode.RMNode;
+import org.ow2.proactive.resourcemanager.selection.SelectionManager;
+import org.ow2.proactive.resourcemanager.selection.SelectionManagerTest;
+import org.ow2.proactive.scripting.InvalidScriptException;
+import org.ow2.proactive.scripting.ScriptResult;
+import org.ow2.proactive.scripting.SelectionScript;
+import org.ow2.proactive.topology.descriptor.TopologyDescriptor;
+import org.ow2.proactive.utils.Criteria;
+import org.ow2.proactive.utils.NodeSet;
+
+
+/**
+ * @author ActiveEon Team
+ * @since 29/06/2017
+ */
+public class ProbabilisticSelectionManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+        BasicConfigurator.resetConfiguration();
+        BasicConfigurator.configure();
+        Logger.getLogger(ProbablisticSelectionManager.class).setLevel(Level.DEBUG);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        RMCore.topologyManager = null;
+        System.setSecurityManager(null);
+    }
+
+    @Test
+    public void testSelectNodesWithNoNodes() {
+        RMCore rmCore = SelectionManagerTest.newMockedRMCore();
+        SelectionManager selectionManager = new ProbablisticSelectionManager(rmCore);
+        Criteria crit = new Criteria(1);
+        crit.setTopology(TopologyDescriptor.ARBITRARY);
+        crit.setScripts(null);
+        crit.setBlackList(null);
+        crit.setBestEffort(false);
+        NodeSet nodeSet = selectionManager.selectNodes(crit, null);
+        assertEquals(0, nodeSet.size());
+    }
+
+    @Test
+    public void testIncreasingProbabilityDynamic() throws Exception {
+        int nbNodes = 10;
+        SelectionScript script = new SelectionScript("test", "groovy", true);
+        ManagerObjects managerObjects = new ManagerObjects(nbNodes).invoke();
+        SelectionManager selectionManager = managerObjects.getSelectionManager();
+        ArrayList<RMNode> freeNodes = managerObjects.getFreeNodes();
+
+        for (int i = 0; i < nbNodes; i++) {
+            // we increase the probability for each node, lowest node has the min number of true results
+            for (int j = 0; j < i + 1; j++) {
+                selectionManager.processScriptResult(script, new ScriptResult<>(true), freeNodes.get(i));
+            }
+        }
+
+        List<RMNode> arrangedNodes = selectionManager.arrangeNodesForScriptExecution(freeNodes,
+                                                                                     Collections.singletonList(script));
+
+        // nodes are expected to be sorted in reverse order
+        for (int i = 0; i < nbNodes; i++) {
+            Assert.assertEquals("mocked-node-" + (nbNodes - i), arrangedNodes.get(i).getNodeName());
+            Assert.assertFalse(selectionManager.isPassed(script, arrangedNodes.get(i)));
+        }
+    }
+
+    @Test
+    public void testDecreasingProbabilityDynamicScriptWithDynamicityStorage() throws Exception {
+        int nbNodes = 10;
+        SelectionScript script = new SelectionScript("test", "groovy", true);
+        ManagerObjects managerObjects = new ManagerObjects(nbNodes).invoke();
+        SelectionManager selectionManager = managerObjects.getSelectionManager();
+        ArrayList<RMNode> freeNodes = managerObjects.getFreeNodes();
+
+        for (int i = 0; i < nbNodes; i++) {
+            // we decrease the probability for each node, lowest node has the max number of false results
+            for (int j = i; j < nbNodes; j++) {
+                selectionManager.processScriptResult(script, new ScriptResult<>(false), freeNodes.get(i));
+            }
+        }
+
+        List<RMNode> arrangedNodes = selectionManager.arrangeNodesForScriptExecution(freeNodes,
+                                                                                     Collections.singletonList(script));
+
+        // list is supposed to be empty because of dynamicity != 0
+        Assert.assertEquals(0, arrangedNodes.size());
+    }
+
+    @Test
+    public void testDecreasingProbabilityDynamicScriptWithoutDynamicityStorage() throws Exception {
+        int nbNodes = 10;
+        PAResourceManagerProperties.RM_SELECT_SCRIPT_NODE_DYNAMICITY.updateProperty("0");
+        try {
+
+            SelectionScript script = new SelectionScript("test", "groovy", true);
+            ManagerObjects managerObjects = new ManagerObjects(nbNodes).invoke();
+            SelectionManager selectionManager = managerObjects.getSelectionManager();
+            ArrayList<RMNode> freeNodes = managerObjects.getFreeNodes();
+
+            for (int i = 0; i < nbNodes; i++) {
+                // we decrease the probability for each node, lowest node has the max number of false results
+                for (int j = i; j < nbNodes; j++) {
+                    selectionManager.processScriptResult(script, new ScriptResult<>(false), freeNodes.get(i));
+                }
+            }
+
+            List<RMNode> arrangedNodes = selectionManager.arrangeNodesForScriptExecution(freeNodes,
+                                                                                         Collections.singletonList(script));
+
+            // list is supposed to contain all nodes because of dynamicity == 0
+            Assert.assertEquals(freeNodes.size(), arrangedNodes.size());
+
+            // nodes are expected to be sorted in reverse order
+            for (int i = 0; i < nbNodes; i++) {
+                Assert.assertEquals("mocked-node-" + (nbNodes - i), arrangedNodes.get(i).getNodeName());
+                Assert.assertFalse(selectionManager.isPassed(script, arrangedNodes.get(i)));
+            }
+        } finally {
+            PAResourceManagerProperties.RM_SELECT_SCRIPT_NODE_DYNAMICITY.updateProperty("300000");
+        }
+    }
+
+    @Test
+    public void testDecreasingProbabilityErrorsDynamicScriptWithoutDynamicityStorage() throws Exception {
+        int nbNodes = 10;
+        PAResourceManagerProperties.RM_SELECT_SCRIPT_NODE_DYNAMICITY.updateProperty("0");
+        try {
+
+            SelectionScript script = new SelectionScript("test", "groovy", true);
+            ManagerObjects managerObjects = new ManagerObjects(nbNodes).invoke();
+            SelectionManager selectionManager = managerObjects.getSelectionManager();
+            ArrayList<RMNode> freeNodes = managerObjects.getFreeNodes();
+
+            for (int i = 0; i < nbNodes; i++) {
+                // we decrease the probability for each node, lowest node has the max number of script exceptions
+                for (int j = i; j < nbNodes; j++) {
+                    selectionManager.processScriptResult(script,
+                                                         new ScriptResult<Boolean>(new IllegalArgumentException("amistake")),
+                                                         freeNodes.get(i));
+                }
+            }
+
+            List<RMNode> arrangedNodes = selectionManager.arrangeNodesForScriptExecution(freeNodes,
+                                                                                         Collections.singletonList(script));
+
+            // list is supposed to contain all nodes because of dynamicity == 0
+            Assert.assertEquals(freeNodes.size(), arrangedNodes.size());
+
+            // nodes are expected to be sorted in reverse order
+            for (int i = 0; i < nbNodes; i++) {
+                Assert.assertEquals("mocked-node-" + (nbNodes - i), arrangedNodes.get(i).getNodeName());
+                Assert.assertFalse(selectionManager.isPassed(script, arrangedNodes.get(i)));
+            }
+        } finally {
+            PAResourceManagerProperties.RM_SELECT_SCRIPT_NODE_DYNAMICITY.updateProperty("300000");
+        }
+    }
+
+    @Test
+    public void testIncreasingProbabilityStatic() throws Exception {
+        int nbNodes = 10;
+        SelectionScript script = new SelectionScript("test", "groovy", false);
+        ManagerObjects managerObjects = new ManagerObjects(nbNodes).invoke();
+        SelectionManager selectionManager = managerObjects.getSelectionManager();
+        ArrayList<RMNode> freeNodes = managerObjects.getFreeNodes();
+
+        for (int i = 0; i < nbNodes; i++) {
+            // we increase the probability for each node, but it should not change the result for static scripts
+            for (int j = 0; j < i + 1; j++) {
+                selectionManager.processScriptResult(script, new ScriptResult<>(true), freeNodes.get(i));
+            }
+        }
+
+        List<RMNode> arrangedNodes = selectionManager.arrangeNodesForScriptExecution(freeNodes,
+                                                                                     Collections.singletonList(script));
+
+        // nodes are expected to be sorted in initial order
+        for (int i = 0; i < nbNodes; i++) {
+            Assert.assertEquals("mocked-node-" + (i + 1), arrangedNodes.get(i).getNodeName());
+            Assert.assertTrue(selectionManager.isPassed(script, arrangedNodes.get(i)));
+        }
+    }
+
+    private class ManagerObjects {
+        private int nbNodes;
+
+        private ArrayList<RMNode> freeNodes;
+
+        private SelectionManager selectionManager;
+
+        public ManagerObjects(int nbNodes) {
+            this.nbNodes = nbNodes;
+        }
+
+        public ArrayList<RMNode> getFreeNodes() {
+            return freeNodes;
+        }
+
+        public SelectionManager getSelectionManager() {
+            return selectionManager;
+        }
+
+        public ManagerObjects invoke() throws InvalidScriptException {
+            RMCore rmCore = SelectionManagerTest.newMockedRMCore();
+
+            freeNodes = new ArrayList<>(nbNodes);
+            for (int i = 0; i < nbNodes; i++) {
+                freeNodes.add(SelectionManagerTest.createMockeNode("user",
+                                                                   "mocked-node-" + (i + 1),
+                                                                   "mocked-node-" + (i + 1)));
+            }
+            when(rmCore.getFreeNodes()).thenReturn(freeNodes);
+
+            selectionManager = new ProbablisticSelectionManager(rmCore);
+
+            return this;
+        }
+    }
+}
