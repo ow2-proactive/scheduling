7f099e880d3d076f7d3664ff67cd116ad95e5ab0
Luis Pineda <pineda@activeeon.local>
2017-11-20 09:15:44 +0100

    Minor fix NullPointerException

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/TerminationData.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/TerminationData.java
@@ -80,34 +80,38 @@ final class TerminationData {
                 TaskResultImpl taskResult) {
             this.taskData = taskData;
             this.terminationStatus = terminationStatus;
-            taskResult.setPropagatedVariables(restoreNonInheritedTaskVariables(taskResult));
+            if (taskResult != null) {
+                taskResult.setPropagatedVariables(restoreNonInheritedTaskVariables(taskResult));
+            }
             this.taskResult = taskResult;
             this.internalJob = internalJob;
         }
 
         private Map<String, byte[]> restoreNonInheritedTaskVariables(TaskResultImpl taskResult) {
             Map<String, Serializable> newPropagatedVariables = new HashMap<>();
-            try {
-                Map<String, Serializable> propagatedVariables = SerializationUtil.deserializeVariableMap(taskResult.getPropagatedVariables());
-                newPropagatedVariables = new HashMap<>(propagatedVariables);
-                for (Map.Entry<String, Serializable> variable : propagatedVariables.entrySet()) {
-                    if (variable.getKey().startsWith(SchedulerVars.PA_FLAG_PROPAGATED_VAR_TO_RESTORE.toString())) {
-                        newPropagatedVariables.remove(variable.getKey());
-                        newPropagatedVariables.put(variable.getKey()
-                                                           .substring(SchedulerVars.PA_FLAG_PROPAGATED_VAR_TO_RESTORE.toString()
-                                                                                                                     .length()),
-                                                   variable.getValue());
-                    } else if (variable.getKey().startsWith(SchedulerVars.PA_FLAG_TASK_VAR_TO_CLEAR.toString())) {
-                        newPropagatedVariables.remove(variable.getKey());
-                        newPropagatedVariables.remove(variable.getKey()
-                                                              .substring(SchedulerVars.PA_FLAG_TASK_VAR_TO_CLEAR.toString()
-                                                                                                                .length()));
+            if (taskResult.getPropagatedVariables() != null) {
+                try {
+                    Map<String, Serializable> propagatedVariables = SerializationUtil.deserializeVariableMap(taskResult.getPropagatedVariables());
+                    newPropagatedVariables = new HashMap<>(propagatedVariables);
+                    for (Map.Entry<String, Serializable> variable : propagatedVariables.entrySet()) {
+                        if (variable.getKey().startsWith(SchedulerVars.PA_FLAG_PROPAGATED_VAR_TO_RESTORE.toString())) {
+                            newPropagatedVariables.remove(variable.getKey());
+                            newPropagatedVariables.put(variable.getKey()
+                                                               .substring(SchedulerVars.PA_FLAG_PROPAGATED_VAR_TO_RESTORE.toString()
+                                                                                                                         .length()),
+                                                       variable.getValue());
+                        } else if (variable.getKey().startsWith(SchedulerVars.PA_FLAG_TASK_VAR_TO_CLEAR.toString())) {
+                            newPropagatedVariables.remove(variable.getKey());
+                            newPropagatedVariables.remove(variable.getKey()
+                                                                  .substring(SchedulerVars.PA_FLAG_TASK_VAR_TO_CLEAR.toString()
+                                                                                                                    .length()));
+                        }
                     }
+                } catch (IOException e) {
+                    logger.error(e.getMessage());
+                } catch (ClassNotFoundException e) {
+                    logger.error(e.getMessage());
                 }
-            } catch (IOException e) {
-                logger.error(e.getMessage());
-            } catch (ClassNotFoundException e) {
-                logger.error(e.getMessage());
             }
             return SerializationUtil.serializeVariableMap(newPropagatedVariables);
         }
