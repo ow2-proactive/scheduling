054168f200f884d8525787935548eb77c62567f4
Pedro <pedro.velho@activeeon.com>
2017-11-28 12:56:45 +0100

    Make available schedulerapi, globalapi, and userspaceapi for Clean Script.

--- rest/rest-client/src/main/java/org/ow2/proactive/scheduler/rest/ds/DataSpaceClient.java
+++ rest/rest-client/src/main/java/org/ow2/proactive/scheduler/rest/ds/DataSpaceClient.java
@@ -65,7 +65,7 @@ import com.google.common.base.Throwables;
 import com.google.common.collect.Maps;
 
 
-public class DataSpaceClient implements IDataSpaceClient {
+public class DataSpaceClient implements IDataSpaceClient, Serializable {
 
     private static final Logger log = Logger.getLogger(DataSpaceClient.class);
 
