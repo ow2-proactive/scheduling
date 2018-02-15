331c6c421e0316ef90bb5eb1dea9dd747400a0ec
marcocast <marcocast@gmail.com>
2018-02-15 08:59:41 +0100

    Addeed review comment

--- rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/selection/SelectionManager.java
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/selection/SelectionManager.java
@@ -252,6 +252,8 @@ public abstract class SelectionManager {
         // can throw Exception if topology is disabled
         TopologyHandler handler = RMCore.topologyManager.getHandler(criteria.getTopology());
 
+        int totalNumberOfAliveNodesRightNow = rmcore.getTotalAliveNodesNumber();
+
         List<RMNode> freeNodes = rmcore.getFreeNodes();
         // filtering out the "free node list"
         // removing exclusion and checking permissions
@@ -382,11 +384,12 @@ public abstract class SelectionManager {
                                                                                                                     " extra nodes"
                                                                                                                   : "";
             logger.info(client + " requested " + criteria.getSize() + " nodes with " + criteria.getTopology() +
-                        " and will get " + selectedNodes.size() + " nodes " + extraNodes + " [totalNodes:" +
-                        rmcore.getTotalAliveNodesNumber() + ";freeNodes:" + freeNodes.size() + ";filteredNodes:" +
-                        filteredNodes.size() + ";reordered after policy:" + afterPolicyNodes.size() +
-                        ";selection script present:" + hasScripts + ";nodes filtered by selection script:" +
-                        matchedNodes.size() + ";selectedNodes:" + selectedNodes.size() + "]");
+                        " and will get " + selectedNodes.size() + " nodes " + extraNodes +
+                        " [totalNumberOfAliveNodesRightNow:" + totalNumberOfAliveNodesRightNow + ";freeNodes:" +
+                        freeNodes.size() + ";filteredNodes:" + filteredNodes.size() + ";reordered after policy:" +
+                        afterPolicyNodes.size() + ";selection script present:" + hasScripts +
+                        ";nodes filtered by selection script:" + matchedNodes.size() + ";selectedNodes:" +
+                        selectedNodes.size() + "]");
         }
 
         if (loggerIsDebugEnabled) {
