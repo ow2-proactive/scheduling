0f5383be45f5f1fe1997ef8aff4e556bb8c680cb
Mykhailenko <hlib.mykhailenko@gmail.com>
2018-02-28 09:36:36 +0100

    Add diagnostic sout to TestJobServerLogs

--- scheduler/scheduler-server/src/test/java/functionaltests/job/log/TestJobServerLogs.java
+++ scheduler/scheduler-server/src/test/java/functionaltests/job/log/TestJobServerLogs.java
@@ -29,11 +29,20 @@ import static functionaltests.utils.SchedulerTHelper.log;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.junit.Test;
 import org.ow2.proactive.scheduler.common.Scheduler;
 import org.ow2.proactive.scheduler.common.exception.UnknownJobException;
@@ -148,6 +157,8 @@ public class TestJobServerLogs extends SchedulerFunctionalTestNoRestart {
         schedulerHelper.removeJob(jobId);
         schedulerHelper.waitForEventJobRemoved(jobId);
 
+        System.out.println("Suppose to remove all logs at " + new SimpleDateFormat("kk:mm:ss:SSS").format(new Date()));
+
         checkNoLogsFromAPI(jobId, tasks);
         checkJobAndTaskLogFiles(jobId, tasks, false);
     }
@@ -177,7 +188,33 @@ public class TestJobServerLogs extends SchedulerFunctionalTestNoRestart {
 
     private void checkFile(boolean shouldExist, File jobLogFile) {
         String message = String.format("Log file %s should %s", jobLogFile, shouldExist ? "exist" : "not exist");
-        assertEquals(message, shouldExist, jobLogFile.exists());
+        final boolean actualExistings = jobLogFile.exists();
+        if (actualExistings != shouldExist) {
+            System.out.println("This test is going to fail, but before we print diagnostic message." +
+                               new SimpleDateFormat("kk:mm:ss:SSS").format(new Date()));
+            // iterate over all files in the 'logsLocation'
+            for (File file : FileUtils.listFiles(new File(logsLocation),
+                                                 TrueFileFilter.INSTANCE,
+                                                 TrueFileFilter.INSTANCE)) {
+                try {
+                    BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+                    System.out.println(String.format("Name: %s, Size: %d, Created: %s, Modified: %s",
+                                                     file.getAbsolutePath(),
+                                                     attr.size(),
+                                                     attr.creationTime(),
+                                                     attr.lastModifiedTime()));
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+                    for (int i = 0; i < 5 && (line = br.readLine()) != null; ++i) {
+                        System.out.println(line);
+                    }
+                    System.out.println("............");
+                } catch (IOException e) {
+                    System.out.println("Exception ocurred during accessing file attributes " + e);
+                }
+            }
+        }
+        assertEquals(message, shouldExist, actualExistings);
     }
 
     public static boolean matchLine(String text, String regex) {
