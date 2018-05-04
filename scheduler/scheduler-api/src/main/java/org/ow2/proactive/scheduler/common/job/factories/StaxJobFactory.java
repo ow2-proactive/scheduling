70e2ab7425a1b04c43c5aaaa32031622f648dc82
Paraita Wohler <paraita.wohler@activeeon.com>
2018-05-04 11:18:00 +0200

    Apply sonarqube recommendations

--- scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/job/factories/StaxJobFactory.java
+++ scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/job/factories/StaxJobFactory.java
@@ -41,8 +41,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.xml.stream.XMLInputFactory;
