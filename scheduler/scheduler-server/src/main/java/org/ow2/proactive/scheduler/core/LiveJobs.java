685485a26323599586d4cd75ce44491fdfba48d3
sophiesongge <sophiesongge@gmail.com>
2018-09-03 16:21:58 +0200

    fix issue #3308

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/LiveJobs.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/LiveJobs.java
@@ -692,7 +692,7 @@ class LiveJobs {
 
     private void setJobStatusToInErrorIfNotPaused(InternalJob job) {
         if (!job.getStatus().equals(JobStatus.PAUSED)) {
-            if(job.getNumberOfInErrorTasks() > 0) {
+            if (job.getNumberOfInErrorTasks() > 0) {
                 job.setStatus(JobStatus.IN_ERROR);
             } else {
                 job.setStatus(JobStatus.PENDING);
