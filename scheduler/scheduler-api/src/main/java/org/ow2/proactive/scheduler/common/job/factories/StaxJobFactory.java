4b53a65fde1a09960b608eb44c67ba7eecf3318f
Justine Rochas <justine.rochas@activeeon.com>
2018-10-15 14:13:34 +0200

    Remove unused relative path attribute

--- scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/job/factories/StaxJobFactory.java
+++ scheduler/scheduler-api/src/main/java/org/ow2/proactive/scheduler/common/job/factories/StaxJobFactory.java
@@ -118,11 +118,6 @@ public class StaxJobFactory extends JobFactory {
     private XMLInputFactory xmlInputFactory = null;
 
     /**
-     * file relative path (relative file path (js) given in XML will be relative to this path)
-     */
-    private String relativePathRoot = "./";
-
-    /**
      * Create a new instance of StaxJobFactory.
      */
     StaxJobFactory() {
@@ -198,8 +193,6 @@ public class StaxJobFactory extends JobFactory {
             }
             //validate content using the proper XML schema
             File updatedFile = validate(file);
-            //set relative path
-            relativePathRoot = updatedFile.getParentFile().getAbsolutePath();
             //create and get XML STAX reader
             XMLStreamReader xmlsr;
             Map<String, ArrayList<String>> dependencies = new HashMap<>();
@@ -1395,8 +1388,6 @@ public class StaxJobFactory extends JobFactory {
                                     url = replace(cursorScript.getAttributeValue(i), variables);
                                 } else if (XMLAttributes.LANGUAGE.matches(attrtmp)) {
                                     language = replace(cursorScript.getAttributeValue(i), variables);
-                                } else if (XMLAttributes.PATH.matches(attrtmp)) {
-                                    path = checkPath(cursorScript.getAttributeValue(i), variables);
                                 } else {
                                     throw new JobCreationException("Unrecognized attribute : " + attrtmp);
                                 }
@@ -1820,25 +1811,6 @@ public class StaxJobFactory extends JobFactory {
             replacements.putAll(variables);
         }
         return filterAndUpdate(str, replacements);
-    }
-
-    /**
-     * Replace the given file path by prepending relative root path if needed.<br/>
-     * This method prepends the relative root path to the given path if it is not considered as an absolute path.
-     *
-     * @param path the path to be evaluated.
-     * @return the same path with ${...} variables replaced and the relative path directory if this path was not absolute.
-     * @throws JobCreationException if a Variable has not been found
-     */
-    private String checkPath(String path, Map<String, String> variables) throws JobCreationException {
-        if (path == null || "".equals(path)) {
-            return path;
-        }
-        //make variables replacement
-        path = replace(path, variables);
-        //prepend if file is relative
-        File f = new File(path);
-        return f.isAbsolute() ? path : relativePathRoot + File.separator + path;
     }
 
     private void displayJobInfo(Job job) {
