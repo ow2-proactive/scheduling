122a3123f36d2d6b8427cac358007f332afd7d45
Justine Rochas <justine.rochas@activeeon.com>
2018-10-19 14:10:31 +0200

    Add functional test with an SSHInfrastructreV2 and RestartDownNodesPolicy which shows the thread leak

--- rm/rm-server/src/test/java/functionaltests/nodesource/TestNodeSourceThreadPool.java
+++ rm/rm-server/src/test/java/functionaltests/nodesource/TestNodeSourceThreadPool.java
@@ -28,10 +28,12 @@ package functionaltests.nodesource;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.model.TestTimedOutException;
 import org.objectweb.proactive.utils.OperatingSystem;
 import org.ow2.proactive.resourcemanager.common.NodeState;
 import org.ow2.proactive.resourcemanager.common.event.RMEventType;
@@ -39,6 +41,8 @@ import org.ow2.proactive.resourcemanager.common.event.RMNodeEvent;
 import org.ow2.proactive.resourcemanager.core.properties.PAResourceManagerProperties;
 import org.ow2.proactive.resourcemanager.frontend.ResourceManager;
 import org.ow2.proactive.resourcemanager.nodesource.infrastructure.SSHInfrastructureV2;
+import org.ow2.proactive.resourcemanager.nodesource.policy.AccessType;
+import org.ow2.proactive.resourcemanager.nodesource.policy.RestartDownNodesPolicy;
 import org.ow2.proactive.resourcemanager.nodesource.policy.StaticPolicy;
 
 import functionaltests.monitor.RMMonitorsHandler;
@@ -48,13 +52,17 @@ import functionaltests.utils.RMTHelper;
 
 public class TestNodeSourceThreadPool extends RMFunctionalTest {
 
-    private static final String CONFIG_FILE_PATH = "/functionaltests/config/functionalTRMProperties-RM-small-node-source-thread-pool.ini";
+    private static final String TWO_THREADS_CONFIG_FILE_PATH = "/functionaltests/config/functionalTRMProperties-RM-node-source-thread-pool-2-threads.ini";
+
+    private static final String SIX_THREADS_CONFIG_FILE_PATH = "/functionaltests/config/functionalTRMProperties-RM-node-source-thread-pool-6-threads.ini";
 
     private static final String NODE_SOURCE_NAME = TestNodeSourceThreadPool.class.getSimpleName();
 
     private static final String FAKE_HOST_FOR_DEPLOYMENT_FAILURE = "10.2.3.4";
 
-    private static final int DOWN_NODE_DETECTION_TIMEOUT = 30000; // 30 seconds
+    private static final int NODE_STATE_CHANGED_TIMEOUT = 30000; // 30 seconds
+
+    private static final int ACTIVE_WAITING_PERIOD = 500; // 500 milliseconds
 
     private static final int ONE_NODE_PER_HOST = 1;
 
@@ -65,27 +73,24 @@ public class TestNodeSourceThreadPool extends RMFunctionalTest {
     @Before
     public void setup() throws Exception {
         TestSSHInfrastructureV2.startSSHServer();
-        String rmconf = new File(RMTHelper.class.getResource(CONFIG_FILE_PATH).toURI()).getAbsolutePath();
-        this.rmHelper.startRM(rmconf);
-        this.resourceManager = this.rmHelper.getResourceManager();
     }
 
     @Test
     public void testDownNodesCanStillBeDetectedWhenDeploymentThreadsAreExhausted() throws Exception {
-
+        String rmConfig = new File(RMTHelper.class.getResource(TWO_THREADS_CONFIG_FILE_PATH).toURI()).getAbsolutePath();
+        this.rmHelper.startRM(rmConfig);
         this.resourceManager = this.rmHelper.getResourceManager();
-
         this.resourceManager.defineNodeSource(NODE_SOURCE_NAME,
                                               SSHInfrastructureV2.class.getName(),
                                               getInfiniteRetryInfrastructureParameters(),
                                               StaticPolicy.class.getName(),
-                                              TestSSHInfrastructureV2.policyParameters,
+                                              getStaticPolicyParameters(),
                                               NODES_NOT_RECOVERABLE);
         this.resourceManager.deployNodeSource(NODE_SOURCE_NAME);
 
         RMTHelper.log("Waiting for the RM to have one free node");
         while (this.resourceManager.getState().getFreeNodesNumber() != ONE_NODE_PER_HOST) {
-            Thread.sleep(500);
+            Thread.sleep(ACTIVE_WAITING_PERIOD);
         }
         RMTHelper.log("The RM has one free node");
 
@@ -98,10 +103,67 @@ public class TestNodeSourceThreadPool extends RMFunctionalTest {
         RMTHelper.log("Waiting for the RM to detect the node down");
         RMNodeEvent nodeEvent = RMTHelper.waitForNodeEvent(RMEventType.NODE_STATE_CHANGED,
                                                            freeNodeUrl,
-                                                           DOWN_NODE_DETECTION_TIMEOUT,
+                                                           NODE_STATE_CHANGED_TIMEOUT,
                                                            monitor);
 
         assertEquals(NodeState.DOWN, nodeEvent.getNodeState());
+    }
+
+    @Test
+    /*
+     * This setup makes LOST nodes on one host and FREE nodes on another host.
+     * We use a configuration of two threads for the nodes deployment, and an
+     * infrastructure with infinite retry number for deploying. After cycling
+     * two redeployment phases thanks to the policy, we expect the situation
+     * to be:
+     * - one thread being always taken by the infinite retry on the fake host
+     * - one thread available to deploy with success/redeploy on the localhost
+     * A timeout is thrown if a thread leak prevents the redeployment to happen
+     */
+    public void testNodeSourceDeploymentThreadsDoNotLeakWithInfiniteRetry() throws Exception {
+        String rmConfig = new File(RMTHelper.class.getResource(SIX_THREADS_CONFIG_FILE_PATH).toURI()).getAbsolutePath();
+        this.rmHelper.startRM(rmConfig);
+        this.resourceManager = this.rmHelper.getResourceManager();
+        this.resourceManager.defineNodeSource(NODE_SOURCE_NAME,
+                                              SSHInfrastructureV2.class.getName(),
+                                              getInfiniteRetryInfrastructureParameters(),
+                                              RestartDownNodesPolicy.class.getName(),
+                                              getRestartDownNodesPolicyParameters(),
+                                              NODES_NOT_RECOVERABLE);
+        this.resourceManager.deployNodeSource(NODE_SOURCE_NAME);
+
+        RMTHelper.log("Waiting for the RM to have one free node");
+        while (this.resourceManager.getState().getFreeNodesNumber() != ONE_NODE_PER_HOST) {
+            Thread.sleep(ACTIVE_WAITING_PERIOD);
+        }
+
+        assertEquals(1, this.resourceManager.getState().getAliveNodes().size());
+
+        for (int i = 0; i < 2; i++) {
+            String freeNodeUrl = this.resourceManager.getState().getFreeNodes().iterator().next();
+            RMTHelper.killRuntime(freeNodeUrl);
+            RMMonitorsHandler monitor = this.rmHelper.getMonitorsHandler();
+            monitor.flushEvents();
+            RMTHelper.log("Waiting for the RM to detect the down node");
+            RMNodeEvent nodeEvent = RMTHelper.waitForNodeEvent(RMEventType.NODE_STATE_CHANGED,
+                                                               freeNodeUrl,
+                                                               NODE_STATE_CHANGED_TIMEOUT,
+                                                               monitor);
+
+            assertEquals(NodeState.DOWN, nodeEvent.getNodeState());
+
+            RMTHelper.log("Waiting for the RM to redeploy the down node");
+            int totalWaitingTime = 0;
+            while (this.resourceManager.getState().getFreeNodesNumber() != ONE_NODE_PER_HOST) {
+                Thread.sleep(ACTIVE_WAITING_PERIOD);
+                totalWaitingTime += ACTIVE_WAITING_PERIOD;
+                if (totalWaitingTime > NODE_STATE_CHANGED_TIMEOUT) {
+                    throw new TestTimedOutException(NODE_STATE_CHANGED_TIMEOUT, TimeUnit.MILLISECONDS);
+                }
+            }
+        }
+
+        assertEquals(1, this.resourceManager.getState().getAliveNodes().size());
     }
 
     private Object[] getInfiniteRetryInfrastructureParameters() {
@@ -120,8 +182,16 @@ public class TestNodeSourceThreadPool extends RMFunctionalTest {
     }
 
     private byte[] getHostsFileContent() {
-        return ("localhost " + ONE_NODE_PER_HOST + "\n" + FAKE_HOST_FOR_DEPLOYMENT_FAILURE + " " + ONE_NODE_PER_HOST +
+        return (FAKE_HOST_FOR_DEPLOYMENT_FAILURE + " " + ONE_NODE_PER_HOST + "\n" + "localhost " + ONE_NODE_PER_HOST +
                 "\n").getBytes();
+    }
+
+    private Object[] getStaticPolicyParameters() {
+        return new Object[] { AccessType.ALL.toString(), AccessType.ALL.toString() };
+    }
+
+    private Object[] getRestartDownNodesPolicyParameters() {
+        return new Object[] { AccessType.ALL.toString(), AccessType.ALL.toString(), "2000" };
     }
 
     @After
@@ -134,4 +204,5 @@ public class TestNodeSourceThreadPool extends RMFunctionalTest {
         }
         TestSSHInfrastructureV2.stopSSHServer();
     }
+
 }
