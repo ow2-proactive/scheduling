252eb7c4cb7db38e9b310b896cf379ef0444153d
Mykhailenko <hlib.mykhailenko@gmail.com>
2018-11-23 16:58:14 +0100

    Short index names

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/db/SelectorData.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/db/SelectorData.java
@@ -43,9 +43,9 @@ import org.ow2.proactive.scheduler.core.db.types.PatternType;
 @NamedQueries({ @NamedQuery(name = "deleteSelectorData", query = "delete from SelectorData where taskData.id.jobId = :jobId"),
                 @NamedQuery(name = "deleteSelectorDataInBulk", query = "delete from SelectorData where taskData.id.jobId in :jobIdList"),
                 @NamedQuery(name = "countSelectorData", query = "select count (*) from SelectorData") })
-@Table(name = "DS_SELECTOR_DATA", indexes = { @Index(name = "DS_SELECTOR_DATA_JOB_ID", columnList = "JOB_ID"),
-                                              @Index(name = "DS_SELECTOR_DATA_TASK_ID_JOB_ID", columnList = "TASK_ID,JOB_ID"),
-                                              @Index(name = "DS_SELECTOR_DATA_TASK_ID", columnList = "TASK_ID") })
+@Table(name = "DS_SELECTOR_DATA", indexes = { @Index(name = "DS_JOB_ID", columnList = "JOB_ID"),
+                                              @Index(name = "DS_TASK_ID_JOB_ID", columnList = "TASK_ID,JOB_ID"),
+                                              @Index(name = "DS_TASK_ID", columnList = "TASK_ID") })
 public class SelectorData {
 
     private static final String INPUT_TYPE = "input";
