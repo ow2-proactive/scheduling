a475af990699c019a11ec95701bd116d403c633d
mykhailenko <hlib.mykhailenko@gmail.com>
2019-09-19 10:26:17 +0200

    Fix test

--- rest/rest-client/src/test/java/functionaltests/RMClientTest.java
+++ rest/rest-client/src/test/java/functionaltests/RMClientTest.java
@@ -25,18 +25,28 @@
  */
 package functionaltests;
 
+import static functionaltests.RestFuncTHelper.getRestServerUrl;
+
+import java.net.URL;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.ow2.proactive.authentication.ConnectionInfo;
 import org.ow2.proactive.resourcemanager.common.event.RMNodeEvent;
 import org.ow2.proactive.scheduler.common.job.*;
 import org.ow2.proactive.scheduler.common.task.*;
 import org.ow2.proactive.scheduler.rest.ISchedulerClient;
+import org.ow2.proactive.scheduler.rest.SchedulerClient;
+import org.ow2.proactive.scripting.SimpleScript;
+import org.ow2.proactive.scripting.TaskScript;
 
 
-public class RMClientTest extends SchedulerClientTest {
+public class RMClientTest extends AbstractRestFuncTestCase {
+
+    protected static final long MAX_WAIT_TIME = 5 * 60 * 1000;
 
     @Test(timeout = MAX_WAIT_TIME)
     public void testSchedulerNodeClient() throws Throwable {
@@ -49,4 +59,39 @@ public class RMClientTest extends SchedulerClientTest {
         Assert.assertTrue(((ArrayList) tRes.value()).get(0) instanceof RMNodeEvent);
     }
 
+    protected ISchedulerClient clientInstance() throws Exception {
+        ISchedulerClient client = SchedulerClient.createInstance();
+        client.init(new ConnectionInfo(getRestServerUrl(), getLogin(), getPassword(), null, true));
+        return client;
+    }
+
+    protected JobId submitJob(Job job, ISchedulerClient client) throws Exception {
+        return client.submit(job);
+    }
+
+    protected Job nodeClientJob(String groovyScript, String forkScript, String cleaningScript) throws Exception {
+
+        URL scriptURL = SchedulerClientTest.class.getResource(groovyScript);
+
+        TaskFlowJob job = new TaskFlowJob();
+        job.setName("NodeClientJob");
+        ScriptTask task = new ScriptTask();
+        task.setName("NodeClientTask");
+        if (forkScript != null) {
+            ForkEnvironment forkEnvironment = new ForkEnvironment();
+            forkEnvironment.setEnvScript(new SimpleScript(IOUtils.toString(SchedulerClientTest.class.getResource(forkScript)
+                                                                                                    .toURI()),
+                                                          "groovy"));
+            task.setForkEnvironment(forkEnvironment);
+        }
+        task.setScript(new TaskScript(new SimpleScript(IOUtils.toString(scriptURL.toURI()), "groovy")));
+        //add CleanScript to test external APIs
+        if (cleaningScript != null) {
+            task.setCleaningScript(new SimpleScript(IOUtils.toString(SchedulerClientTest.class.getResource(cleaningScript)
+                                                                                              .toURI()),
+                                                    "groovy"));
+        }
+        job.addTask(task);
+        return job;
+    }
 }
