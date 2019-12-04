dc494a3d22df02e6e84eca3121deb21a2bcb3340
tao-xinxiu <isabelle.tao@activeeon.com>
2019-12-04 11:18:41 +0100

    feat: use child-first class loader for addons infrastructure managers.

---
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/utils/ChildFirstClassLoader.java
@@ -0,0 +1,122 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive.resourcemanager.utils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ *
+ * ChildFirstClassLoader first trys to load the class from the specified jars then the parent class loader.
+ *
+ * @author ActiveEon Team
+ * @since 28/11/19
+ */
+public class ChildFirstClassLoader extends URLClassLoader {
+
+    private static final Logger logger = Logger.getLogger(ChildFirstClassLoader.class);
+
+    public ChildFirstClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    @Override
+    protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+        // check whether the class already loaded
+        Class<?> loadedClass = findLoadedClass(name);
+        if (loadedClass == null) {
+            logger.debug("loading ..." + name);
+            try {
+                // find the class from given jar urls as in first constructor parameter.
+                loadedClass = findClass(name);
+                logger.debug(name + " loaded from jar " + Arrays.toString(getURLs()));
+            } catch (ClassNotFoundException e) {
+                logger.debug(name + " not found in jar " + Arrays.toString(getURLs()));
+                // class is not found in the given urls, try it in parent classloader.
+                // If class is still not found, then this method will throw class not found ex.
+                logger.debug(name + " loading from parent " + getParent());
+                loadedClass = super.loadClass(name, resolve);
+            }
+        }
+        if (resolve) {
+            resolveClass(loadedClass);
+        }
+        return loadedClass;
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        List<URL> allRes = new LinkedList<>();
+        // load resource from this classloader
+        Enumeration<URL> thisRes = findResources(name);
+        if (thisRes != null) {
+            while (thisRes.hasMoreElements()) {
+                allRes.add(thisRes.nextElement());
+            }
+        }
+        // then try finding resources from parent classloaders
+        Enumeration<URL> parentRes = super.findResources(name);
+        if (parentRes != null) {
+            while (parentRes.hasMoreElements()) {
+                allRes.add(parentRes.nextElement());
+            }
+        }
+        return new Enumeration<URL>() {
+            Iterator<URL> it = allRes.iterator();
+
+            @Override
+            public boolean hasMoreElements() {
+                return it.hasNext();
+            }
+
+            @Override
+            public URL nextElement() {
+                return it.next();
+            }
+        };
+    }
+
+    @Override
+    public URL getResource(String name) {
+        URL res = null;
+        if (res == null) {
+            res = findResource(name);
+        }
+        if (res == null) {
+            res = super.getResource(name);
+        }
+        return res;
+    }
+}
---
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/utils/FilePathHelper.java
@@ -0,0 +1,74 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive.resourcemanager.utils;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+
+import org.ow2.proactive.resourcemanager.core.properties.PAResourceManagerProperties;
+
+
+/**
+ * @author ActiveEon Team
+ * @since 03/12/19
+ */
+public class FilePathHelper {
+    public static final String ADDONS_DIRNAME = "addons";
+
+    public static String getAddonsPath() {
+        return getRmHome() + File.separator + ADDONS_DIRNAME;
+    }
+
+    public static String getRmHome() {
+        return PAResourceManagerProperties.RM_HOME.getValueAsString();
+    }
+
+    /**
+     * Find all the jar files in the path
+     * @param path
+     * @return
+     */
+    public static URL[] findJarsInPath(String path) {
+        File[] jarArray = new File(path).listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".jar");
+            }
+        });
+        HashSet<URL> jarNames = new HashSet<>();
+        for (File jarFile : jarArray) {
+            try {
+                jarNames.add(jarFile.toURI().toURL());
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException("Malformed URL: " + jarFile);
+            }
+        }
+        return jarNames.toArray(new URL[0]);
+    }
+}
