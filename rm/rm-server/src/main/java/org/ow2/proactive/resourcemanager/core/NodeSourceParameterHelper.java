4e9d8fe8afd9cd6dce731fcaed544758c53655c8
Mykhailenko <hlib.mykhailenko@gmail.com>
2020-01-21 10:10:49 +0100

    Revert "Dirty fix to preserve checkbox values (#3701)"
    This reverts commit f1b2aedd0d27b7b71e4ae87fdac1609d60036064.

--- rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/core/NodeSourceParameterHelper.java
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/core/NodeSourceParameterHelper.java
@@ -112,16 +112,8 @@ public class NodeSourceParameterHelper {
         String cleanNewValue = newValue.trim();
         String cleanOldValue = oldValue.trim();
 
-        // if new value is empty string and configurable field is checkbox
-        // then we keep old value.
-        // we do it, because I cannot figure out why GWT DynamicForm
-        // sends empty value for non-changed checkbox
-        if (cleanNewValue.isEmpty() && configurableField.getMeta().checkbox()) {
-            mergedParameters.set(valueIndex, cleanOldValue);
-        } else {
-            if (configurableField.getMeta().dynamic() && !cleanNewValue.equals(cleanOldValue)) {
-                mergedParameters.set(valueIndex, cleanNewValue);
-            }
+        if (configurableField.getMeta().dynamic() && !cleanNewValue.equals(cleanOldValue)) {
+            mergedParameters.set(valueIndex, cleanNewValue);
         }
     }
 
