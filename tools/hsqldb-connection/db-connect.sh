c343577959186f39f4c54197829250312a8944fc
Mohamed Boussaa <mohamedboussaa3@gmail.com>
2020-07-06 14:42:19 +0200

    Add tools to connect to HSQLDB CLI

---
+++ tools/hsqldb-connection/db-connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A Simple Shell Command To access HSQLDB CLI and connect by default to scheduler DB
+# If you would like to connect to another ProActive DB, please configure the sqltool.rc file before running the script
+# If java is not set on your system, please use java under [Schedulering_Path]/jre/bin/java
+
+java -jar sqltool-2.4.1.jar --rcFile=sqltool.rc scheduler
