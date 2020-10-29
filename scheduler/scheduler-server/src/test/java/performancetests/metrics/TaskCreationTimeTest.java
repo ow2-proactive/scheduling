a8feac3384b9a2cc11597a319bbab6023d60cb97
Fabien Viale <fabien.viale@activeeon.com>
2020-10-29 17:40:40 +0100

    Increase TaskCreationTimerTest maximum time
    (cherry picked from commit 91875d2256a35f7fc386ee7075576b62adb051d5)

--- scheduler/scheduler-server/src/test/java/performancetests/metrics/TaskCreationTimeTest.java
+++ scheduler/scheduler-server/src/test/java/performancetests/metrics/TaskCreationTimeTest.java
@@ -57,7 +57,7 @@ public class TaskCreationTimeTest extends PerformanceTestBase {
      */
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] { { 20000, 55000 } });
+        return Arrays.asList(new Object[][] { { 20000, 100000 } });
     }
 
     private final int taskNumber;
