1751a77f1d525bbeefb7ebfff2ba9531b169438a
nebil ben mabrouk <nebil.mabrouk@activeeon.com>
2021-02-22 09:39:48 +0100

    rest signal endpoint return the set of signals and throws an error if the ready signal does not exist

--- scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/SchedulerFrontend.java
+++ scheduler/scheduler-server/src/main/java/org/ow2/proactive/scheduler/core/SchedulerFrontend.java
@@ -1931,7 +1931,7 @@ public class SchedulerFrontend implements InitActive, Scheduler, RunActive, EndA
 
             // Throw an error if: (i) the corresponding ready signal does not exist and (ii) the signal is not a ready signal
             String readyPrefix = SignalApiImpl.READY_PREFIX;
-            if (!signal.startsWith(readyPrefix) && !signals.contains(readyPrefix + signal)) {
+            if (!signals.contains(readyPrefix + signal) && !signal.startsWith(readyPrefix)) {
                 throw new SignalApiException("Job " + jobId + " is not ready to receive the signal " + signal);
             }
 
