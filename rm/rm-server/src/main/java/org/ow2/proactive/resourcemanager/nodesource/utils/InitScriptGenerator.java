817ac064afa2565d01345a06fec13d24e9be6811
ali_fahs <ali.fahs@activeeon.com>
2022-01-21 04:55:39 +0100

    adding the InitScriptGenerator and NSproperties classes

---
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/nodesource/utils/InitScriptGenerator.java
@@ -0,0 +1,89 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive.resourcemanager.nodesource.utils;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.log4j.Logger;
+
+import lombok.Getter;
+
+
+public class InitScriptGenerator {
+
+    public static final String JAVA_PATH_PROPERTY = "%javaPath%";
+
+    public static final String SCHEDULING_PATH_PROPERTY = "%schedulingPath%";
+
+    public static final String FILE_ENCODING = "%fileEncoding%";
+
+    public static final String JAVA_OPTIONS_PROPERTY = "%javaOptions%";
+
+    public static final String RM_URL_PROPERTY = "%rmUrl%";
+
+    public static final String NODE_NAME_PROPERTY = "%nodeName%";
+
+    public static final String NODE_SOURCE_NAME_PROPERTY = "%nodeSourceName%";
+
+    public static final String CREDENTIALS_PROPERTY = "%credentials%";
+
+    public static final String NUMBER_OF_NODES_PER_INSTANCE_PROPERTY = "%numberOfNodesPerInstance%";
+
+    private static final Logger logger = Logger.getLogger(InitScriptGenerator.class);
+
+    protected static Configuration nsConfig;
+
+    static {
+        try {
+            //load configuration manager with the NodeSource properties file
+            nsConfig = NSProperties.loadConfig();
+        } catch (ConfigurationException e) {
+            logger.error("Exception when loading NodeSource properties", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Getter
+    protected String defaultLinuxStartupScript = nsConfig.getString(NSProperties.LINUX_AGENT_STARTUP_SCRIPT);
+
+    public String fillInAgentScriptProperties(String startupScriptTemplate, String rmUrl, String javaPath,
+            String schedulingPath, String fileEncoding, String javaOptions, String nodeSourceName, String nodeName,
+            int numberOfNodesPerInstance, String credentials) {
+        String startupScript = startupScriptTemplate;
+        startupScript = startupScript.replace(JAVA_PATH_PROPERTY, javaPath);
+        startupScript = startupScript.replace(SCHEDULING_PATH_PROPERTY, schedulingPath);
+        startupScript = startupScript.replace(FILE_ENCODING, fileEncoding);
+        startupScript = startupScript.replace(JAVA_OPTIONS_PROPERTY, javaOptions);
+        startupScript = startupScript.replace(NODE_NAME_PROPERTY, nodeName);
+        startupScript = startupScript.replace(RM_URL_PROPERTY, rmUrl);
+        startupScript = startupScript.replace(NODE_SOURCE_NAME_PROPERTY, nodeSourceName);
+        startupScript = startupScript.replace(CREDENTIALS_PROPERTY, credentials);
+        startupScript = startupScript.replace(NUMBER_OF_NODES_PER_INSTANCE_PROPERTY,
+                                              String.valueOf(numberOfNodesPerInstance));
+
+        return startupScript;
+    }
+}
---
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/nodesource/utils/NSProperties.java
@@ -0,0 +1,77 @@
+/*
+ * ProActive Parallel Suite(TM):
+ * The Open Source library for parallel and distributed
+ * Workflows & Scheduling, Orchestration, Cloud Automation
+ * and Big Data Analysis on Enterprise Grids & Clouds.
+ *
+ * Copyright (c) 2007 - 2017 ActiveEon
+ * Contact: contact@activeeon.com
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation: version 3 of
+ * the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ */
+package org.ow2.proactive.resourcemanager.nodesource.utils;
+
+import java.io.File;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.builder.fluent.PropertiesBuilderParameters;
+import org.apache.commons.configuration2.convert.DisabledListDelimiterHandler;
+import org.apache.commons.configuration2.convert.ListDelimiterHandler;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.log4j.Logger;
+
+
+public class NSProperties {
+
+    private NSProperties() {
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(NSProperties.class);
+
+    public static final String PROPERTIES_FILE = "NodeSource.properties";
+
+    private static final ListDelimiterHandler DELIMITER = new DisabledListDelimiterHandler();
+
+    public static final String LINUX_JAR_STARTUP_SCRIPT = "ns.script.linux.jar.startup.scripts";
+
+    public static final String LINUX_AGENT_STARTUP_SCRIPT = "ns.script.linux.agent.startup.scripts";
+
+    /**
+     * loads NodeSource configuration.
+     *
+     * @return NodeSource configuration
+     */
+    public static Configuration loadConfig() throws ConfigurationException {
+
+        Configuration config;
+        File propertiesFile = new File(NSProperties.class.getClassLoader().getResource(PROPERTIES_FILE).getFile());
+        PropertiesBuilderParameters propertyParameters = new Parameters().properties();
+        propertyParameters.setFile(propertiesFile);
+        propertyParameters.setThrowExceptionOnMissing(true);
+        propertyParameters.setListDelimiterHandler(DELIMITER);
+
+        FileBasedConfigurationBuilder<PropertiesConfiguration> builder = new FileBasedConfigurationBuilder<>(PropertiesConfiguration.class);
+        builder.configure(propertyParameters);
+        config = builder.getConfiguration();
+        LOGGER.debug("NodeSources configuration loaded");
+
+        return config;
+    }
+}
