1d7f6423afaf876c5cfc8f69814ff5c6c0ff1236
tao-xinxiu <isabelle.tao@activeeon.com>
2022-07-25 22:39:41 +0200

    Add tmp dir to unset sticky bit for allowing access to runAsMe tasks.

--- dist/scripts/processbuilder/linux/launch.sh
+++ dist/scripts/processbuilder/linux/launch.sh
@@ -43,7 +43,9 @@ usr=$4
 crs=$5
 
 # create temp file for return value passing
-tmp=`mktemp  2>/dev/null || mktemp  -t 'mytmpfile'`
+tmpdir=`mktemp -d`
+chmod 0777 $tmpdir
+tmp=`mktemp -p $tmpdir 2>/dev/null || mktemp -p $tmpdir -t 'mytmpfile'`
 
 if [ "$?" != "0" ]; then
   # if I can not create a tempfile
@@ -52,7 +54,7 @@ if [ "$?" != "0" ]; then
 fi;
 
 # create temp file for environment variables
-tmpenv=`mktemp  2>/dev/null || mktemp  -t 'myenvfile'`
+tmpenv=`mktemp -p $tmpdir 2>/dev/null || mktemp -p $tmpdir -t 'myenvfile'`
 
 if [ "$?" != "0" ]; then
   # if I can not create a tempfile
@@ -71,8 +73,7 @@ chmod 666 $tmpenv
 if [ "$crs" != "" ]; then
   error="$OSPL_E_PREFIX ${OSLP_PACKAGE}CoreBindingException $OSPL_E_CAUSE Core binding is not supported!"
   echo $error 1>&2
-  rm $tmp;
-  rm $tmpenv 2> /dev/null
+  rm -rf $tmpdir 2> /dev/null
   exit 1;
 else
   # If the username is not empty we proceed with the user_step
@@ -84,8 +85,7 @@ else
     # return value of the user_step is not 0 only in case the sudo failed, otherwise it is 0
     error="$OSPL_E_PREFIX ${OSLP_PACKAGE}FatalProcessBuilderException $OSPL_E_CAUSE User change script could not execute!"
     echo $error 1>&2
-    rm $tmp 2> /dev/null
-    rm $tmpenv 2> /dev/null
+    rm -rf $tmpdir 2> /dev/null
     exit 1;
     fi;
 
@@ -97,8 +97,7 @@ else
     # return value of the user_step is not 0 only in case the sudo failed, otherwise it is 0
     error="$OSPL_E_PREFIX ${OSLP_PACKAGE}FatalProcessBuilderException $OSPL_E_CAUSE Command executor script could not execute!"
     echo $error 1>&2
-    rm $tmp 2> /dev/null
-    rm $tmpenv 2> /dev/null
+    rm -rf $tmpdir 2> /dev/null
     exit 1;
     fi;
   fi;
@@ -107,7 +106,6 @@ else
 
   exitv=`cat < $tmp` 2> /dev/null
   # maybe check if it exists etc. - may come in handy if we don't break on the first error coming from the scripts
-  rm $tmp 2> /dev/null
-  rm $tmpenv 2> /dev/null
+  rm -rf $tmpdir 2> /dev/null
   exit $exitv;
 fi;
