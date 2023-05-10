688a30ca32a5df77221a1ff44424d903f7bb98f6
fviale <fabien.viale@activeeon.com>
2023-05-10 13:10:39 +0200

    Fix setBusyNodes possible ArrayIndexOutOfBoundException  - wrong list length was used in a for loop, which could trigger an ArrayIndexOutOfBoundException
    (cherry picked from commit 6981369520b5e45aa56889456cfe078b44d71d09)

--- rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/core/RMCore.java
+++ rm/rm-server/src/main/java/org/ow2/proactive/resourcemanager/core/RMCore.java
@@ -2341,7 +2341,7 @@ public class RMCore implements ResourceManager, InitActive, RunActive {
                                        owner,
                                        usageInfoCriteriaSize);
         }
-        for (int i = 0; i < nodeUrlList.size(); i++) {
+        for (int i = 0; i < rmNodeList.size(); i++) {
             RMNode rmNode = rmNodeList.get(i);
             NodeState previousNodeState = previousNodeStateList.get(i);
             this.registerAndEmitNodeEvent(rmNode.createNodeEvent(RMEventType.NODE_STATE_CHANGED,
